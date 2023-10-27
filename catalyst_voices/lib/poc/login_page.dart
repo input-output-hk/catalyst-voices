9b35f05935913a31012dbf97788684835248f225
minikin <djminikin@gmail.com>
2023-10-27 15:52:00 +0200

    feat: add constants , update login screen

--- catalyst_voices/lib/poc/login_page.dart
+++ catalyst_voices/lib/poc/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:catalyst_voices/poc/poc.dart';
 import 'package:flutter/material.dart';
 
 final class LoginPage extends StatefulWidget {
@@ -7,64 +8,98 @@ final class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  final usernameController = TextEditingController();
+abstract class _Constants {
+  static const usernameLabelText = 'Username';
+  static const passwordLabelText = 'Password';
+  static const username = 'robot';
+  static const password = '1234';
+  static const errorMessage = 'Wrong credentials';
+}
 
-  final passwordController = TextEditingController();
+final class _LoginPageState extends State<LoginPage> {
+  late TextEditingController usernameTextController;
+  late TextEditingController passwordTextController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: WidgetKeys.loginScreen,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              key: WidgetKeys.loginUsernameController,
-              controller: usernameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
+      body: Center(
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                key: WidgetKeys.usernameTextController,
+                controller: usernameTextController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: _Constants.usernameLabelText,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              key: WidgetKeys.loginPasswordController,
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              key: WidgetKeys.loginButton,
-              onPressed: () {
-                final username = usernameController.text;
-                final password = passwordController.text;
-                if (username == '1234' && password == '1234') {
-                  Navigator.of(context).pushNamed('/home-page');
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      key: WidgetKeys.loginErrorSnackbar,
-                      content: Text('Wrong credentials'),
-                    ),
-                  );
-                }
-              },
-              child: const Text('Login'),
-            ),
-          ],
+              TextFormField(
+                key: WidgetKeys.passwordTextController,
+                controller: passwordTextController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: _Constants.passwordLabelText,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                key: WidgetKeys.loginButton,
+                onPressed: () {
+                  if (_validateCredentials()) {
+                    _navigateToHomeScreen(context);
+                  } else {
+                    _showError(context);
+                  }
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    usernameTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    usernameTextController = TextEditingController();
+    passwordTextController = TextEditingController();
+  }
+
+  void _navigateToHomeScreen(BuildContext context) {
+    Navigator.of(context).pushNamed('/home-page');
+  }
+
+  void _showError(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        key: WidgetKeys.loginErrorSnackbar,
+        content: Text(_Constants.errorMessage),
+      ),
+    );
+  }
+
+  bool _validateCredentials() {
+    final username = usernameTextController.text;
+    final password = passwordTextController.text;
+    return username == _Constants.username && password == _Constants.password;
   }
 }
