48b80b67cefb525b3d70f90c42b4c2a002b2631a
Joaquín Rosales <joaquin.rosales@iohk.io>
2023-10-29 13:25:39 -0600

    feat: add FragmentsProcessingSummary object, docs & example

--- catalyst-gateway/bin/src/service/api/message/mod.rs
+++ catalyst-gateway/bin/src/service/api/message/mod.rs
@@ -11,6 +11,7 @@ pub(crate) struct MessageApi;
 #[OpenApi(prefix_path = "/message", tag = "ApiTags::Message")]
 impl MessageApi {
     #[oai(path = "/", method = "post", operation_id = "Message")]
+    /// Posts a signed transaction.
     async fn message_post(&self, message: Binary<Vec<u8>>) -> message_post::AllResponses {
         message_post::endpoint(message).await
     }
