b9ae4da529cc1688921a74bef8f45f2de67bb892
minikin <djminikin@gmail.com>
2023-11-02 16:39:20 +0100

    Update dummy.dart

--- catalyst_voices/lib/dummy/dummy.dart
+++ catalyst_voices/lib/dummy/dummy.dart
@@ -1,7 +1,7 @@
 // These files used as dummy implementations
 // for integration tests. As soon as we have
 // a real implementation, we can remove them.
-//TODO(minikin): remove dummy files, https://github.com/input-output-hk/catalyst-voices/issues/103.
+// TODO(minikin): remove dummy files, https://github.com/input-output-hk/catalyst-voices/issues/103.
 
 export 'constants.dart';
 export 'home_screen.dart';
