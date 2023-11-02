441a9dce7826a57ebdf184d8b15a406c0f8b9384
minikin <djminikin@gmail.com>
2023-11-02 15:36:03 +0100

    Update flutter_web_integration_test.sh

--- scripts/flutter_web_integration_test.sh
+++ scripts/flutter_web_integration_test.sh
@@ -14,7 +14,7 @@ set -e
 # Wait for 5 seconds to allow ChromeDriver to start
 sleep 5
 
-cd ../../../catalyst_voices
+cd ./catalyst_voices
 
 flutter drive --driver=test_driver/integration_test.dart \
 --target=integration_test/main.dart \
