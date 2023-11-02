e328e554f9815b46b4a1455432c0e89a0e145a17
minikin <djminikin@gmail.com>
2023-11-02 15:27:10 +0100

    wip

--- scripts/flutter_web_integration_test.sh
+++ scripts/flutter_web_integration_test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/flutter/web_installers.git
-cd web_installers/packages/web_drivers/
-dart pub get
+# git clone https://github.com/flutter/web_installers.git
+# cd web_installers/packages/web_drivers/
+# dart pub get
 
-# Run the web driver installer in the background
-dart lib/web_driver_installer.dart chromedriver --always-install --driver-version="114.0.5735.90" &
-# Wait for 20 seconds to allow the driver installer to complete
-sleep 20
+# # Run the web driver installer in the background
+# dart lib/web_driver_installer.dart chromedriver --always-install --driver-version="114.0.5735.90" &
+# # Wait for 20 seconds to allow the driver installer to complete
+# sleep 20
 
-chromedriver/chromedriver --port=4444 &
+/usr/local/share/chromedriver-linux64 --port=4444 &
 # Wait for 5 seconds to allow ChromeDriver to start
 sleep 5
 
