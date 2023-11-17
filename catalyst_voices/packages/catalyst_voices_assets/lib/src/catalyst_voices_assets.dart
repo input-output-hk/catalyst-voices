048ee8241cb87cdce4d6d8ea0a33dab7553041b3
minikin <djminikin@gmail.com>
2023-11-17 12:05:02 +0100

    add example for catalyst_voices_assets

--- catalyst_voices/packages/catalyst_voices_assets/lib/src/catalyst_voices_assets.dart
+++ catalyst_voices/packages/catalyst_voices_assets/lib/src/catalyst_voices_assets.dart
@@ -1,4 +1,4 @@
-export 'package:catalyst_voices_assets/src/assets.gen.dart';
+export 'package:catalyst_voices_assets/generated/assets.gen.dart';
+export 'package:catalyst_voices_assets/generated/colors.gen.dart';
+export 'package:catalyst_voices_assets/generated/fonts.gen.dart';
 export 'package:catalyst_voices_assets/src/catalyst_image.dart';
-export 'package:catalyst_voices_assets/src/colors.gen.dart';
-export 'package:catalyst_voices_assets/src/fonts.gen.dart';
