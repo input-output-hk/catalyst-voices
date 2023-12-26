9c6a67c1f475ae23b569919d04bb1c0077eadf3b
Oleksandr Prokhorenko <djminikin@gmail.com>
2023-12-26 15:32:42 +0100

    feat: update flavors to align with backend (#192)
    * chore: upgrade Flutter and Dart
    
    * feat: update dart configs
    
    * feat:  update iOS project
    
    * feat: update Android project, README
    
    * feat: update iOS assets
    
    * feat: update android assets
    
    * Update launch.recommended.json

---
+++ catalyst_voices/lib/configs/bootstrap.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:catalyst_voices/configs/app_bloc_observer.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterError.onError = (details) {
+    log(
+      details.exceptionAsString(),
+      stackTrace: details.stack,
+    );
+  };
+
+  Bloc.observer = AppBlocObserver();
+
+  runApp(await builder());
+}
