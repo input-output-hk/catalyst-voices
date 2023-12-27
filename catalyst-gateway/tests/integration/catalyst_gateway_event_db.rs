bac6de96ee86073ef8ad5a2879c48c2892d646a6
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2023-12-27 12:56:27 +0100

    refactor

--- catalyst-gateway/tests/integration/catalyst_gateway_event_db.rs
+++ catalyst-gateway/tests/integration/catalyst_gateway_event_db.rs
@@ -1,4 +1,24 @@
-#[test]
-fn it_works() {
-    assert_eq!(2 + 2, 4);
+//
+// Integration test example
+//
+#[tokio::test]
+async fn health_live() {
+    let cat_gateway_address = "http://127.0.0.1:3030";
+    let client = reqwest::Client::new();
+    let url = format!("{cat_gateway_address}/api/health/live");
+
+    let response = unwrap!(
+        client.get(&url).send().await,
+        "Failed to execute request to {url}."
+    );
+    assert_eq!(
+        response.status().as_u16(),
+        204,
+        "request failed for {url} response: {response:#?}"
+    );
+    assert_eq!(
+        Some(0),
+        response.content_length(),
+        "request failed for {url} response: {response:#?}"
+    );
 }
--- catalyst-gateway/tests/integration/main.rs
+++ catalyst-gateway/tests/integration/main.rs
@@ -1,1 +1,3 @@
+#[macro_use]
+extern crate unwrap;
 pub mod catalyst_gateway_event_db;
