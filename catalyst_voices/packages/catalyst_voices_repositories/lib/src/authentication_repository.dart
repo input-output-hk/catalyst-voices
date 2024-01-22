2752bebf913eb12436f1ba4c6b27a506d2efd8ed
minikin <djminikin@gmail.com>
2024-01-22 13:43:20 +0100

    feat(frontend): add dependencies injection configurations

--- catalyst_voices/packages/catalyst_voices_repositories/lib/src/authentication_repository.dart
+++ catalyst_voices/packages/catalyst_voices_repositories/lib/src/authentication_repository.dart
@@ -5,7 +5,7 @@ import 'package:catalyst_voices_repositories/catalyst_voices_repositories.dart';
 
 final class AuthenticationRepository {
   final CredentialsStorageRepository credentialsStorageRepository;
-  final _controller = StreamController<AuthenticationStatus>();
+  final _streamController = StreamController<AuthenticationStatus>();
 
   AuthenticationRepository({required this.credentialsStorageRepository});
 
@@ -22,10 +22,10 @@ final class AuthenticationRepository {
       yield AuthenticationStatus.unknown;
     }
 
-    yield* _controller.stream;
+    yield* _streamController.stream;
   }
 
-  Future<void> dispose() async => _controller.close();
+  Future<void> dispose() async => _streamController.close();
 
   Future<SessionData?> getSessionData() async {
     try {
@@ -43,7 +43,7 @@ final class AuthenticationRepository {
 
   void logOut() {
     credentialsStorageRepository.clearSessionData;
-    _controller.add(AuthenticationStatus.unauthenticated);
+    _streamController.add(AuthenticationStatus.unauthenticated);
   }
 
   Future<void> signIn({
@@ -60,7 +60,7 @@ final class AuthenticationRepository {
     // TODO(minikin): remove this delay after implementing real auth flow.
     await Future.delayed(
       const Duration(milliseconds: 300),
-      () => _controller.add(AuthenticationStatus.authenticated),
+      () => _streamController.add(AuthenticationStatus.authenticated),
     );
   }
 }
