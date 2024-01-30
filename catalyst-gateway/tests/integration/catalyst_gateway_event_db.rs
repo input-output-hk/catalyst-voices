5adbfd7c313a7cf6e595ce529eda1c77aa2a7f70
bkioshn <bkioshn@gmail.com>
2024-01-30 17:02:14 +0700

    fix: health live response

--- catalyst-gateway/tests/integration/catalyst_gateway_event_db.rs
+++ catalyst-gateway/tests/integration/catalyst_gateway_event_db.rs
@@ -5,24 +5,20 @@ async fn health_live() {
     let cat_gateway_address = "http://127.0.0.1:3030";
     let client = reqwest::Client::new();
     let url = format!("{cat_gateway_address}/api/health/live");
+    let response = client
+        .get(&url)
+        .send()
+        .await
+        .expect("Failed to execute request to {url}.");
 
-    let response = client.get(&url).send().await;
-
-    match response {
-        Ok(res) => {
-            assert_eq!(
-                res.status().as_u16(),
-                204,
-                "request failed for {url} response: {res:#?}"
-            );
-            assert_eq!(
-                Some(0),
-                res.content_length(),
-                "request failed for {url} response: {res:#?}"
-            );
-        },
-        Err(err) => {
-            eprint!("request failed for {url} error: {err:#?}");
-        },
-    }
+    assert_eq!(
+        response.status().as_u16(),
+        204,
+        "request failed for {url} response: {response:#?}"
+    );
+    assert_eq!(
+        Some(0),
+        response.content_length(),
+        "request failed for {url} response: {response:#?}"
+    );
 }
