350aaa301a0093daac1feb9babad0c14b5de14ab
minikin <djminikin@gmail.com>
2024-02-26 14:33:47 +0100

    wip

--- catalyst_voices/packages/catalyst_voices_remote_widgets/lib/src/catalyst_voices_remote_widgets.dart
+++ catalyst_voices/packages/catalyst_voices_remote_widgets/lib/src/catalyst_voices_remote_widgets.dart
@@ -1,1 +1,1 @@
-
+export 'proposal_builder_m2/proposal_builder_body_m2.dart';
---
+++ catalyst_voices/packages/catalyst_voices_remote_widgets/lib/src/proposal_builder_m2/proposal_builder_body_m2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProposalBuilderBodyM2 extends StatelessWidget {
+  const ProposalBuilderBodyM2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Proposal Builder'),
+      ),
+      body: SafeArea(
+        child: Center(),
+      ),
+    );
+  }
+}
