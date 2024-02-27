b1d0819672f022404012a5c52f0f1992b5eca49c
cong-or <conchubhar.gannon@gmail.com>
2024-02-27 00:22:38 +0000

    refactor(housekeeping): clean

--- catalyst-gateway/bin/src/follower.rs
+++ catalyst-gateway/bin/src/follower.rs
@@ -2,7 +2,7 @@
 use std::{error::Error, path::PathBuf, str::FromStr, sync::Arc};
 
 /// Handler for follower tasks, allows for control over spawned follower threads
-pub type ManageFollower = JoinHandle<()>;
+pub type ManageTasks = JoinHandle<()>;
 
 use async_recursion::async_recursion;
 use cardano_chain_follower::{
@@ -84,7 +84,7 @@ pub(crate) async fn start_followers(
 async fn spawn_followers(
     configs: (Vec<NetworkMeta>, FollowerMeta), db: Arc<EventDB>, data_refresh_tick: u64,
     machine_id: String,
-) -> Result<Vec<ManageFollower>, Box<dyn Error>> {
+) -> Result<Vec<ManageTasks>, Box<dyn Error>> {
     let snapshot_path = configs.1.mithril_snapshot_path;
 
     let mut follower_tasks = Vec::new();
@@ -173,7 +173,7 @@ async fn find_last_update_point(
 async fn init_follower(
     network: Network, relay: &str, start_from: (Option<SlotNumber>, Option<BlockHash>),
     db: Arc<EventDB>, machine_id: MachineId, snapshot: &str,
-) -> Result<ManageFollower, Box<dyn Error>> {
+) -> Result<ManageTasks, Box<dyn Error>> {
     let mut follower = follower_connection(start_from, snapshot, network, relay).await?;
 
     let genesis_values =
