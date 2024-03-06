922ddef2e4be7c5a1d8da40840fa11a7aa061f12
cong-or <conchubhar.gannon@gmail.com>
2024-03-06 20:00:18 +0000

    feat(policy assets): index assets

--- catalyst-gateway/bin/src/util.rs
+++ catalyst-gateway/bin/src/util.rs
@@ -6,40 +6,38 @@ use serde::Serialize;
 #[derive(Default, Debug, Serialize)]
 /// Assets
 pub struct Asset {
+    /// Policy id
     pub policy_id: String,
+    /// Asset name
     pub asset_name: String,
+    /// Amount in lovelace
     pub amount: u64,
 }
 
 #[derive(Default, Debug, Serialize)]
 /// Parsed Assets
 pub struct PolicyAsset {
+    /// Policy identifier
     pub policy_hash: String,
+    /// All policy assets
     pub assets: Vec<Asset>,
 }
 
 /// Eras before staking should be ignored
 pub fn valid_era(era: Era) -> bool {
-    match era {
-        Era::Shelley => true,
-        Era::Allegra => true,
-        Era::Mary => true,
-        Era::Alonzo => true,
-        Era::Babbage => true,
-        Era::Conway => true,
-        _ => false,
-    }
+    matches!(
+        era,
+        Era::Shelley | Era::Allegra | Era::Mary | Era::Alonzo | Era::Babbage | Era::Conway
+    )
 }
 
 /// Extract assets
 pub fn parse_policy_assets(assets: &Vec<MultiEraPolicyAssets<'_>>) -> Vec<PolicyAsset> {
     assets
         .iter()
-        .map(|asset| {
-            PolicyAsset {
-                policy_hash: asset.policy().to_string(),
-                assets: parse_child_assets(asset.assets()),
-            }
+        .map(|asset| PolicyAsset {
+            policy_hash: asset.policy().to_string(),
+            assets: parse_child_assets(asset.assets()),
         })
         .collect()
 }
@@ -48,24 +46,18 @@ pub fn parse_policy_assets(assets: &Vec<MultiEraPolicyAssets<'_>>) -> Vec<Policy
 pub fn parse_child_assets(assets: Vec<MultiEraAsset>) -> Vec<Asset> {
     assets
         .iter()
-        .map(|asset| {
-            match asset {
-                MultiEraAsset::AlonzoCompatibleOutput(id, name, amount) => {
-                    Asset {
-                        policy_id: id.to_string(),
-                        asset_name: name.to_string(),
-                        amount: *amount,
-                    }
-                },
-                MultiEraAsset::AlonzoCompatibleMint(id, name, amount) => {
-                    Asset {
-                        policy_id: id.to_string(),
-                        asset_name: name.to_string(),
-                        amount: *amount as u64,
-                    }
-                },
-                _ => Asset::default(),
-            }
+        .map(|asset| match asset {
+            MultiEraAsset::AlonzoCompatibleOutput(id, name, amount) => Asset {
+                policy_id: id.to_string(),
+                asset_name: name.to_string(),
+                amount: *amount,
+            },
+            MultiEraAsset::AlonzoCompatibleMint(id, name, amount) => Asset {
+                policy_id: id.to_string(),
+                asset_name: name.to_string(),
+                amount: *amount as u64,
+            },
+            _ => Asset::default(),
         })
         .collect()
 }
