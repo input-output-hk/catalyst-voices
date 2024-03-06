4249e3c12988d31b16160dfc9bfb24e4ca81ad65
Mr-Leshiy <leshiy12345678@gmail.com>
2024-03-06 21:00:24 +0200

    wip

--- catalyst-gateway/bin/src/event_db/voter_registration/mod.rs
+++ catalyst-gateway/bin/src/event_db/voter_registration/mod.rs
@@ -5,11 +5,11 @@ use super::{Error, EventDB};
 /// Transaction id
 pub(crate) type TxId = String;
 /// Stake credential
-pub(crate) type StateCredential = String;
+pub(crate) type StateCredential = Box<[u8]>;
 /// Public voting key
-pub(crate) type PublicVotingKey = String;
+pub(crate) type PublicVotingKey = Box<[u8]>;
 /// Payment address
-pub(crate) type PaymentAddress = String;
+pub(crate) type PaymentAddress = Box<[u8]>;
 /// Nonce
 pub(crate) type Nonce = i64;
 /// Metadata 61284
@@ -32,9 +32,9 @@ impl EventDB {
         let _rows = conn
             .query(include_str!("insert_voter_registration.sql"), &[
                 &hex::decode(tx_id).map_err(|e| Error::DecodeHex(e.to_string()))?,
-                &hex::decode(stake_credential).map_err(|e| Error::DecodeHex(e.to_string()))?,
-                &hex::decode(public_voting_key).map_err(|e| Error::DecodeHex(e.to_string()))?,
-                &hex::decode(payment_address).map_err(|e| Error::DecodeHex(e.to_string()))?,
+                &stake_credential.as_ref(),
+                &public_voting_key.as_ref(),
+                &payment_address.as_ref(),
                 &nonce,
                 &metadata_61284.as_ref(),
                 &metadata_61285.as_ref(),
