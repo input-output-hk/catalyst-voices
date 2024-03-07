ca50901dc3f5eb0331d0134b80ee5d5d2514f5b8
Mr-Leshiy <leshiy12345678@gmail.com>
2024-03-07 13:40:09 +0200

    wip

--- catalyst-gateway/bin/src/event_db/voter_registration.rs
+++ catalyst-gateway/bin/src/event_db/voter_registration.rs
@@ -5,7 +5,7 @@ use super::{Error, EventDB};
 /// Transaction id
 pub(crate) type TxId = String;
 /// Stake credential
-pub(crate) type StateCredential = Box<[u8]>;
+pub(crate) type StakeCredential = Box<[u8]>;
 /// Public voting key
 pub(crate) type PublicVotingKey = Box<[u8]>;
 /// Payment address
@@ -23,24 +23,29 @@ impl EventDB {
     /// Inserts voter registration data, replacing any existing data.
     #[allow(dead_code, clippy::too_many_arguments)]
     async fn insert_voter_registration(
-        &self, tx_id: TxId, stake_credential: StateCredential, public_voting_key: PublicVotingKey,
+        &self, tx_id: TxId, stake_credential: StakeCredential, public_voting_key: PublicVotingKey,
         payment_address: PaymentAddress, nonce: Nonce, metadata_61284: Metadata61284,
         metadata_61285: Metadata61285, valid: bool, stats: Stats,
     ) -> Result<(), Error> {
         let conn = self.pool.get().await?;
 
         let _rows = conn
-            .query(include_str!("insert_voter_registration.sql"), &[
-                &hex::decode(tx_id).map_err(|e| Error::DecodeHex(e.to_string()))?,
-                &stake_credential.as_ref(),
-                &public_voting_key.as_ref(),
-                &payment_address.as_ref(),
-                &nonce,
-                &metadata_61284.as_ref(),
-                &metadata_61285.as_ref(),
-                &valid,
-                &stats,
-            ])
+            .query(
+                include_str!(
+                    "../../../event-db/queries/voter_registration/insert_voter_registration.sql"
+                ),
+                &[
+                    &hex::decode(tx_id).map_err(|e| Error::DecodeHex(e.to_string()))?,
+                    &stake_credential.as_ref(),
+                    &public_voting_key.as_ref(),
+                    &payment_address.as_ref(),
+                    &nonce,
+                    &metadata_61284.as_ref(),
+                    &metadata_61285.as_ref(),
+                    &valid,
+                    &stats,
+                ],
+            )
             .await?;
 
         Ok(())
