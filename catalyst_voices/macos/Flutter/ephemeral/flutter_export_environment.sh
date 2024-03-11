07fe646453c7b2380829e626389d96ccfe538c38
minikin <djminikin@gmail.com>
2024-03-11 10:58:23 +0000

    chore: update flutter to 3.19.3

---
+++ catalyst_voices/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/minikin/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/minikin/IOG/Code/catalyst-voices/catalyst_voices"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
