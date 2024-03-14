4aef9f7f8eab792322bd4e42eee0d0740b162274
cong-or <conchubhar.gannon@gmail.com>
2024-03-14 22:46:57 +0000

    feat(find stake credential on tx): match with cert output

--- catalyst-gateway/bin/src/util.rs
+++ catalyst-gateway/bin/src/util.rs
@@ -43,9 +43,11 @@ pub struct PolicyAsset {
 pub fn parse_policy_assets(assets: &[MultiEraPolicyAssets<'_>]) -> Vec<PolicyAsset> {
     assets
         .iter()
-        .map(|asset| PolicyAsset {
-            policy_hash: asset.policy().to_string(),
-            assets: parse_child_assets(&asset.assets()),
+        .map(|asset| {
+            PolicyAsset {
+                policy_hash: asset.policy().to_string(),
+                assets: parse_child_assets(&asset.assets()),
+            }
         })
         .collect()
 }
@@ -54,21 +56,25 @@ pub fn parse_policy_assets(assets: &[MultiEraPolicyAssets<'_>]) -> Vec<PolicyAss
 pub fn parse_child_assets(assets: &[MultiEraAsset]) -> Vec<Asset> {
     assets
         .iter()
-        .filter_map(|asset| match asset {
-            MultiEraAsset::AlonzoCompatibleOutput(id, name, amount) => Some(Asset {
-                policy_id: id.to_string(),
-                name: name.to_string(),
-                amount: *amount,
-            }),
-            MultiEraAsset::AlonzoCompatibleMint(id, name, amount) => {
-                let amount = u64::try_from(*amount).ok()?;
-                Some(Asset {
-                    policy_id: id.to_string(),
-                    name: name.to_string(),
-                    amount,
-                })
-            },
-            _ => Some(Asset::default()),
+        .filter_map(|asset| {
+            match asset {
+                MultiEraAsset::AlonzoCompatibleOutput(id, name, amount) => {
+                    Some(Asset {
+                        policy_id: id.to_string(),
+                        name: name.to_string(),
+                        amount: *amount,
+                    })
+                },
+                MultiEraAsset::AlonzoCompatibleMint(id, name, amount) => {
+                    let amount = u64::try_from(*amount).ok()?;
+                    Some(Asset {
+                        policy_id: id.to_string(),
+                        name: name.to_string(),
+                        amount,
+                    })
+                },
+                _ => Some(Asset::default()),
+            }
         })
         .collect()
 }
@@ -94,11 +100,13 @@ pub fn extract_stake_credentials_from_certs(
                 pallas::ledger::primitives::alonzo::Certificate::StakeDelegation(
                     stake_credential,
                     _,
-                ) => match stake_credential {
-                    StakeCredential::AddrKeyhash(stake_credential) => {
-                        keys.push(hex::encode(stake_credential.as_slice()));
-                    },
-                    StakeCredential::Scripthash(_) => (),
+                ) => {
+                    match stake_credential {
+                        StakeCredential::AddrKeyhash(stake_credential) => {
+                            keys.push(hex::encode(stake_credential.as_slice()));
+                        },
+                        StakeCredential::Scripthash(_) => (),
+                    }
                 },
                 _ => continue,
             }
@@ -150,8 +158,11 @@ pub fn find_matching_stake_credential(
 
     match matched_pub_key {
         Some(pub_key) => Ok(pub_key.to_string()),
-        None => Err(
-            "No stake credential from the certificates matches any of the witness pub keys".into(),
-        ),
+        None => {
+            Err(
+                "No stake credential from the certificates matches any of the witness pub keys"
+                    .into(),
+            )
+        },
     }
 }
