75be97e9cf88377de9d0e53f0fd264d13cd46892
Lucio Baglione <coire1@gmail.com>
2024-03-19 11:03:06 +0100

    feat: Add PlatformWidgetBuilder.
    This widget abstracts the functionality to detect the platform
    used by the user. Related to #139.

--- catalyst_voices/packages/catalyst_voices_shared/lib/src/catalyst_voices_shared.dart
+++ catalyst_voices/packages/catalyst_voices_shared/lib/src/catalyst_voices_shared.dart
@@ -1,1 +1,2 @@
 export 'platform/catalyst_platform.dart';
+export 'platform_widget_builder/platform_widget_builder.dart';
---
+++ catalyst_voices/packages/catalyst_voices_shared/lib/src/platform_widget_builder/platform_widget_builder.dart
@@ -0,0 +1,66 @@
+import 'package:catalyst_voices_shared/src/platform/catalyst_platform.dart';
+import 'package:flutter/widgets.dart';
+
+class PlatformWidgetBuilder<T> extends StatelessWidget {
+  final Widget Function(BuildContext context, T? data) builder;
+  final T? android;
+  final T? desktop;
+  final T? fuchsia;
+  final T? iOS;
+  final T? linux;
+  final T? macOS;
+  final T? mobile;
+  final T? mobileWeb;
+  final T? web;
+  final T? webDesktop;
+  final T? windows;
+  final T? other;
+
+  const PlatformWidgetBuilder({
+    super.key,
+    required this.builder,
+    this.android,
+    this.desktop,
+    this.fuchsia,
+    this.iOS,
+    this.linux,
+    this.macOS,
+    this.mobile,
+    this.mobileWeb,
+    this.web,
+    this.webDesktop,
+    this.windows,
+    this.other,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (CatalystPlatform.isAndroid && android != null) {
+      return builder(context, android as T);
+    } else if (CatalystPlatform.isDesktop && desktop != null) {
+      return builder(context, desktop as T);
+    } else if (CatalystPlatform.isFuchsia && fuchsia != null) {
+      return builder(context, fuchsia as T);
+    } else if (CatalystPlatform.isIOS && iOS != null) {
+      return builder(context, iOS as T);
+    } else if (CatalystPlatform.isLinux && linux != null) {
+      return builder(context, linux as T);
+    } else if (CatalystPlatform.isMacOS && macOS != null) {
+      return builder(context, linux as T);
+    } else if (CatalystPlatform.isMobile && mobile != null) {
+      return builder(context, mobile as T);
+    } else if (CatalystPlatform.isMobileWeb && mobileWeb != null) {
+      return builder(context, mobileWeb as T);
+    } else if (CatalystPlatform.isWeb && web != null) {
+      return builder(context, web as T);
+    } else if (CatalystPlatform.isWebDesktop && webDesktop != null) {
+      return builder(context, webDesktop as T);
+    } else if (CatalystPlatform.isWindows && windows != null) {
+      return builder(context, windows as T);
+    } else if (other != null) {
+      return builder(context, other as T);
+    } else {
+      return builder(context, null);
+    }
+  }
+}
