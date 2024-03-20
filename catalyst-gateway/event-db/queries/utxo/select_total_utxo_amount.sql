bd56fd88f31fc2cf4ab6fc4e27f4f2f86d2eeb2b
Mr-Leshiy <leshiy12345678@gmail.com>
2024-03-20 12:33:20 +0200

    fix

--- catalyst-gateway/event-db/queries/utxo/select_total_utxo_amount.sql
+++ catalyst-gateway/event-db/queries/utxo/select_total_utxo_amount.sql
@@ -1,17 +1,20 @@
 -- Select total UTXO's corresponded to the provided stake credential from the given network that have occurred before the given time. 
-SELECT cardano_txn_index.network, SUM(cardano_utxo.value) as total_utxo_amount
+SELECT
+  cardano_txn_index.network,
+  SUM(cardano_utxo.value) AS total_utxo_amount
 
 FROM cardano_utxo
 
 INNER JOIN cardano_txn_index
-    ON cardano_txn_index.id = cardano_utxo.tx_id
+  ON cardano_utxo.tx_id = cardano_txn_index.id
 
 INNER JOIN cardano_slot_index
-    ON cardano_slot_index.slot_no = cardano_txn_index.slot_no
-    AND cardano_slot_index.network = cardano_txn_index.network
+  ON
+    cardano_txn_index.slot_no = cardano_slot_index.slot_no
+    AND cardano_txn_index.network = cardano_slot_index.network
 
 WHERE
-    cardano_utxo.stake_credential = $1
-    AND cardano_slot_index.block_time <= $2
+  cardano_utxo.stake_credential = $1
+  AND cardano_slot_index.block_time <= $2
 
 GROUP BY cardano_txn_index.network;
