5ee86be8949199e20cf9bf9cf2179a92288cd673
minikin <djminikin@gmail.com>
2024-03-21 21:38:08 +0100

    Update catalyst_core.dart

--- catalyst_voices/packages/catalyst_voices_remote_widgets/lib/src/core/catalyst_core.dart
+++ catalyst_voices/packages/catalyst_voices_remote_widgets/lib/src/core/catalyst_core.dart
@@ -5,11 +5,6 @@ Map<String, LocalWidgetBuilder> get _catalystCoreWidgetsDefinitions =>
     <String, LocalWidgetBuilder>{
       'TextField': (BuildContext context, DataSource source) {
         return TextField(
-          // controller: source.v<TextEditingController>(['controller']),
-          // focusNode: source.v<FocusNode>(['focusNode']),
-          // decoration: source.v<InputDecoration>(['decoration']),
-          // keyboardType: source.v<TextInputType>(['keyboardType']),
-          // textInputAction: source.v<TextInputAction>(['textInputAction']),
           style: ArgumentDecoders.textStyle(source, ['style']),
           onChanged: source.handler(
             <Object>['onChanged'],
