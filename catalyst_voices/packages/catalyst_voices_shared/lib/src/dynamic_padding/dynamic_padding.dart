fec49cf9cd8bda3653047dd3f31d42af048b4451
Lucio Baglione <coire1@gmail.com>
2024-03-25 09:18:27 +0100

    refactor: Use `ResponsiveBuilder` to calculate padding values.

--- catalyst_voices/packages/catalyst_voices_shared/lib/src/dynamic_padding/dynamic_padding.dart
+++ catalyst_voices/packages/catalyst_voices_shared/lib/src/dynamic_padding/dynamic_padding.dart
@@ -1,15 +1,16 @@
+import 'package:catalyst_voices_shared/src/responsive_builder/responsive_breakpoint_key.dart';
+import 'package:catalyst_voices_shared/src/responsive_builder/responsive_builder.dart';
 import 'package:flutter/widgets.dart';
 
 // A [DynamicPadding] is a StatelessWidget that applies a padding based on the
 // current screen size.
 //
-// The widget wraps its child into a Padding and it applies the specified
-// EdgeInsets for each screen size.
-//
-// The size considered is the screen width and it's extracted from MediaQuery.
+// The widget wraps its [child] in a ResponsiveBuilder that calculates the 
+// proper padding value based on the screen size and wraps it again in a Padding
+// to display the selected padding value.
 //
 // The possible arguments are [xs], [sm], [md], [lg], [other] following the 
-// Material design standards for naming and breakpoint sizes.
+// Material design standards and the ResponsiveBuilder arguments.
 // Each screen size has a default value to simplify widget usage.
 //
 // Example usage:
@@ -26,7 +27,7 @@ import 'package:flutter/widgets.dart';
 
 class DynamicPadding extends StatelessWidget {
   final Widget child;
-  final Map<String, EdgeInsets> _paddings;
+  final Map<ResponsiveBreakpointKey, EdgeInsets> _paddings;
 
   DynamicPadding({
     super.key,
@@ -37,38 +38,26 @@ class DynamicPadding extends StatelessWidget {
     EdgeInsets lg = const EdgeInsets.all(16),
     EdgeInsets other = const EdgeInsets.all(8),
   }) : _paddings = {
-    'xs': xs,
-    'sm': sm,
-    'md': md,
-    'lg': lg,
-    'other': other,
+    ResponsiveBreakpointKey.xs: xs,
+    ResponsiveBreakpointKey.sm: sm,
+    ResponsiveBreakpointKey.md: md,
+    ResponsiveBreakpointKey.lg: lg,
+    ResponsiveBreakpointKey.other: other,
   };
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: _selectPadding(context),
-      child: child,
+    return ResponsiveBuilder<EdgeInsets>(
+      builder: (context, padding) => Padding(
+        padding: padding!,
+        child: child,
+      ),
+      xs: _paddings[ResponsiveBreakpointKey.xs],
+      sm: _paddings[ResponsiveBreakpointKey.sm],
+      md: _paddings[ResponsiveBreakpointKey.md],
+      lg: _paddings[ResponsiveBreakpointKey.lg],
+      other: _paddings[ResponsiveBreakpointKey.other]!,
     );
   }
 
-  EdgeInsets _selectPadding(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final currentPaddingKey = _screenSizes.entries
-      .firstWhere(
-        (entry) => (
-          screenWidth >= entry.value.min && screenWidth <= entry.value.max
-        ),
-        orElse: () => const MapEntry('other', (min: 0, max: 0)),
-      )
-      .key;
-    return _paddings[currentPaddingKey]!;
-  }
-
-  final Map<String, ({int min, int max})> _screenSizes = {
-    'xs': (min: 0, max: 599),
-    'sm': (min: 600, max: 1239),
-    'md': (min: 1240, max: 1439),
-    'lg': (min: 1440, max: 2048),
-  };
 }
--- catalyst_voices/packages/catalyst_voices_shared/test/src/dynamic_padding_test.dart
+++ catalyst_voices/packages/catalyst_voices_shared/test/src/dynamic_padding_test.dart
@@ -10,9 +10,9 @@ void main() {
       home: Scaffold(
         body: DynamicPadding(
           xs: const EdgeInsets.all(2),
-          sm: const EdgeInsets.symmetric(vertical: 4),
-          md: const EdgeInsets.symmetric(horizontal: 8),
-          lg: const EdgeInsets.only(top: 16),
+          sm: const EdgeInsets.symmetric(vertical: 3),
+          md: const EdgeInsets.symmetric(horizontal: 4),
+          lg: const EdgeInsets.only(top: 5),
           child: const Text('Test data!'),
         ),
       ),
@@ -22,9 +22,9 @@ void main() {
 
     final sizesToTest = {
       280.0: const EdgeInsets.all(2),
-      620.0: const EdgeInsets.symmetric(vertical: 4),
-      1280.0: const EdgeInsets.symmetric(horizontal: 8),
-      1600.0: const EdgeInsets.only(top: 16),
+      620.0: const EdgeInsets.symmetric(vertical: 3),
+      1280.0: const EdgeInsets.symmetric(horizontal: 4),
+      1600.0: const EdgeInsets.only(top: 5),
     };
 
     for (final entry in sizesToTest.entries) {
