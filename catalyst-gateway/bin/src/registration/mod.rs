e31c3874a4e19ddd54d842f9652e92cedcb34a59
cong-or <conchubhar.gannon@gmail.com>
2024-04-03 16:56:54 +0100

    clippy lints

--- catalyst-gateway/bin/src/registration/mod.rs
+++ catalyst-gateway/bin/src/registration/mod.rs
@@ -180,9 +180,11 @@ pub fn raw_sig_conversion(raw_cbor: &[u8]) -> Result<Signature, Box<dyn Error>> 
 
     let sig = signature_61285.first().ok_or("no 61285 key")?.clone();
     let sig_bytes: [u8; 64] = match sig.into_bytes() {
-        Ok(s) => match s.try_into() {
-            Ok(sig) => sig,
-            Err(err) => return Err(format!("Invalid signature length {err:?}").into()),
+        Ok(s) => {
+            match s.try_into() {
+                Ok(sig) => sig,
+                Err(err) => return Err(format!("Invalid signature length {err:?}").into()),
+            }
         },
         Err(err) => return Err(format!("Invalid signature parsing {err:?}").into()),
     };
@@ -236,11 +238,15 @@ pub fn inspect_voting_key(metamap: &[(Value, Value)]) -> Result<VotingKey, Box<d
                             .ok_or("Issue parsing weight")?
                             .as_integer()
                         {
-                            Some(weight) => match weight.try_into() {
-                                Ok(weight) => weight,
-                                Err(_err) => {
-                                    return Err("Invalid weight in delegation".to_string().into())
-                                },
+                            Some(weight) => {
+                                match weight.try_into() {
+                                    Ok(weight) => weight,
+                                    Err(_err) => {
+                                        return Err("Invalid weight in delegation"
+                                            .to_string()
+                                            .into())
+                                    },
+                                }
                             },
                             None => return Err("Invalid delegation".to_string().into()),
                         };
