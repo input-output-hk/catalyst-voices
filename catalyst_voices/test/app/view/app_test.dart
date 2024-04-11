cb31fbb3eac3631b00ce71868933ae509f90b520
Dominik Toton <dominik.toton@iohk.io>
2024-04-11 14:48:46 +0200

    ci: melos script to generate test report

--- catalyst_voices/test/app/view/app_test.dart
+++ catalyst_voices/test/app/view/app_test.dart
@@ -1,5 +1,11 @@
+import 'package:catalyst_voices/pages/coming_soon/coming_soon.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../helpers/helpers.dart';
+
 void main() {
-  test('empty', () {});
+  testWidgets('placeholder_test', (tester) async {
+    await tester.pumpApp(const ComingSoonPage());
+    await tester.pumpAndSettle();
+  });
 }
