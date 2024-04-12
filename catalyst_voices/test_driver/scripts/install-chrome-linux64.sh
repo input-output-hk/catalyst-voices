46858d12e296f1a3f4a6b545984163a6981029db
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-04-12 13:33:03 +0200

    feat: adds some logging

--- catalyst_voices/test_driver/scripts/install-chrome-linux64.sh
+++ catalyst_voices/test_driver/scripts/install-chrome-linux64.sh
@@ -5,7 +5,7 @@ DISTR="Debian 12 (Bookworm)"
 
 # Installing dependencies for Chrome. Workaround for:
 # https://github.com/GoogleChromeLabs/chrome-for-testing/issues/55
-echo "Installing Google Chrome dependencies"
+echo -e "\033[1;34mInstalling Google Chrome dependencies"
 chrome_deps=$(curl -s https://raw.githubusercontent.com/chromium/chromium/main/chrome/installer/linux/debian/dist_package_versions.json)
 deps=$(echo "$chrome_deps" | jq -r ".\"$DISTR\" | keys[]")
 apt-get update
@@ -17,7 +17,7 @@ done
 json_chrome=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json)
 
 # Install chrome
-echo "Installing Google Chrome..."
+echo -e "\033[1;34mInstalling Google Chrome..."
 latest_chrome=$(echo "$json_chrome" | jq -r ".channels.Stable.downloads.chrome[].url | select(contains(\"$PLATFORM\"))")
 curl -s --create-dirs -o chrome.zip --output-dir /opt/chrome "$latest_chrome"
 unzip -d /opt/chrome -j /opt/chrome/chrome.zip && rm /opt/chrome/chrome.zip
@@ -26,7 +26,7 @@ chmod +x /opt/chrome/chrome
 google-chrome --version
 
 # Install chromedriver
-echo "Installing Chromedriver..."
+echo -e "\033[1;34mInstalling Chromedriver..."
 latest_chromedriver=$(echo "$json_chrome" | jq -r ".channels.Stable.downloads.chromedriver[].url | select(contains(\"$PLATFORM\"))")
 curl -s --create-dirs -o chromedriver.zip --output-dir /opt/chromedriver "$latest_chromedriver"
 unzip -d /opt/chromedriver -j /opt/chromedriver/chromedriver.zip && rm /opt/chromedriver/chromedriver.zip
