00318d2a51f18d2dc8e91ad88268668e34fd2b71
Mr-Leshiy <leshiy12345678@gmail.com>
2024-04-15 14:53:07 +0300

    update follower queries error handling

--- catalyst-gateway/bin/src/event_db/error.rs
+++ catalyst-gateway/bin/src/event_db/error.rs
@@ -2,6 +2,11 @@
 
 use bb8::RunError;
 
+/// DB not found error
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
+#[error("Cannot find this item")]
+pub(crate) struct NotFoundError;
+
 /// DB connection timeout error
 #[derive(thiserror::Error, Debug)]
 #[error("Connection to DB timed out")]
