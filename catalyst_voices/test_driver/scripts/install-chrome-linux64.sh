148442801aa8843b0e575462f643ddc2a76d36a6
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-04-16 10:18:09 +0200

    feat: adds browser install for arm64

--- catalyst_voices/test_driver/scripts/install-chrome-linux64.sh
+++ catalyst_voices/test_driver/scripts/install-chrome-linux64.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
-# This script installs Chrome for testing and Chromedriver
-echo "TARGET ARCHITECTURE $TARGETARCH"
-if [ "$TARGETARCH" == "amd64" ]
+# This script installs Chrome/Chromium for testing and Chromedriver
+if [ "$TARGETARCH" == "arm64"]
+  # There is no Chrome for testing for arm64, using chromium instead
+  # https://github.com/GoogleChromeLabs/chrome-for-testing/issues/1
 then
-    PLATFORM=linux64
+    echo -e "\033[1;34mInstalling Chromium..."
+    apt-get update
+    apt-get install -y chromium
+    ln -s /usr/bin/chromium /usr/local/bin/google-chrome
+    google-chrome --version
+
+    apt-get install chromium-driver
+    chromedriver --version
 else
-    PLATFORM=mac-arm64
+    DISTR="Debian 12 (Bookworm)"
+    # Installing dependencies for Chrome. Workaround for:
+    # https://github.com/GoogleChromeLabs/chrome-for-testing/issues/55
+    echo -e "\033[1;34mInstalling Google Chrome dependencies..."
+    chrome_deps=$(curl -s https://raw.githubusercontent.com/chromium/chromium/main/chrome/installer/linux/debian/dist_package_versions.json)
+    deps=$(echo "$chrome_deps" | jq -r ".\"$DISTR\" | keys[]")
+    apt-get update
+    for dep in $deps; do
+      apt-get install -y $dep
+    done
+
+    # Get latest chrome for testing version
+    json_chrome=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json)
+
+    # Install chrome
+    echo -e "\033[1;34mInstalling Google Chrome..."
+    latest_chrome=$(echo "$json_chrome" | jq -r ".channels.Stable.downloads.chrome[].url | select(contains(\"linux64\"))")
+    curl -s --create-dirs -o chrome.zip --output-dir /opt/chrome "$latest_chrome"
+    unzip -d /opt/chrome -j /opt/chrome/chrome.zip && rm /opt/chrome/chrome.zip
+    ln -s /opt/chrome/chrome /usr/local/bin/google-chrome
+    chmod +x /opt/chrome/chrome
+    google-chrome --version
+
+    # Install chromedriver
+    echo -e "\033[1;34mInstalling Chromedriver..."
+    latest_chromedriver=$(echo "$json_chrome" | jq -r ".channels.Stable.downloads.chromedriver[].url | select(contains(\"linux64\"))")
+    curl -s --create-dirs -o chromedriver.zip --output-dir /opt/chromedriver "$latest_chromedriver"
+    unzip -d /opt/chromedriver -j /opt/chromedriver/chromedriver.zip && rm /opt/chromedriver/chromedriver.zip
+    ln -s /opt/chromedriver/chromedriver /usr/local/bin/chromedriver
+    chmod +x /opt/chromedriver/chromedriver
+    chromedriver --version
 fi
-
-DISTR="Debian 12 (Bookworm)"
-
-# Installing dependencies for Chrome. Workaround for:
-# https://github.com/GoogleChromeLabs/chrome-for-testing/issues/55
-echo -e "\033[1;34mInstalling Google Chrome dependencies for $PLATFORM"
-chrome_deps=$(curl -s https://raw.githubusercontent.com/chromium/chromium/main/chrome/installer/linux/debian/dist_package_versions.json)
-deps=$(echo "$chrome_deps" | jq -r ".\"$DISTR\" | keys[]")
-apt-get update
-for dep in $deps; do
-  apt-get install -y $dep
-done
-
-# Get latest chrome for testing version
-json_chrome=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json)
-
-# Install chrome
-echo -e "\033[1;34mInstalling Google Chrome..."
-latest_chrome=$(echo "$json_chrome" | jq -r ".channels.Stable.downloads.chrome[].url | select(contains(\"$PLATFORM\"))")
-curl -s --create-dirs -o chrome.zip --output-dir /opt/chrome "$latest_chrome"
-unzip -d /opt/chrome -j /opt/chrome/chrome.zip && rm /opt/chrome/chrome.zip
-ln -s /opt/chrome/chrome /usr/local/bin/google-chrome
-chmod +x /opt/chrome/chrome
-google-chrome --version
-
-# Install chromedriver
-echo -e "\033[1;34mInstalling Chromedriver..."
-latest_chromedriver=$(echo "$json_chrome" | jq -r ".channels.Stable.downloads.chromedriver[].url | select(contains(\"$PLATFORM\"))")
-curl -s --create-dirs -o chromedriver.zip --output-dir /opt/chromedriver "$latest_chromedriver"
-unzip -d /opt/chromedriver -j /opt/chromedriver/chromedriver.zip && rm /opt/chromedriver/chromedriver.zip
-ln -s /opt/chromedriver/chromedriver /usr/local/bin/chromedriver
-chmod +x /opt/chromedriver/chromedriver
-chromedriver --version
--- catalyst_voices/test_driver/scripts/install-firefox-linux64.sh
+++ catalyst_voices/test_driver/scripts/install-firefox-linux64.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # This script installs Firefox and Geckodriver. Geckodriver supported versions:
 # https://firefox-source-docs.mozilla.org/testing/geckodriver/Support.html
-PLATFORM=linux64
-
-echo "Installing Firefox..."
+if [ "$TARGETARCH" == "arm64"]
+then
+    PLATFORM=aarch64
+else
+    PLATFORM=linux64
+fi
+echo -e "\033[1;34mInstalling Firefox..."
 install -d -m 0755 /etc/apt/keyrings
 curl -fSsL https://packages.mozilla.org/apt/repo-signing-key.gpg | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
@@ -15,7 +19,7 @@ Pin-Priority: 1000
 apt-get update && apt-get install -y firefox
 firefox --version
 
-echo "Installing Geckodriver..."
+echo -e "\033[1;34mInstalling Geckodriver..."
 json_geckodriver=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
 latest_geckodriver=$(echo "$json_geckodriver" | jq -r ".assets[].browser_download_url | select(contains(\"$PLATFORM\") and endswith(\"gz\"))")
 curl -sL --create-dirs -o geckodriver.tar.gz --output-dir /opt/geckodriver "$latest_geckodriver"
