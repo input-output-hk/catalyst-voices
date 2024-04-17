054e584029e205e869f409e551b059705bb81e34
Lucio Baglione <coire1@gmail.com>
2024-04-17 15:59:16 +0200

    chore: Use proper doc syntax.

--- catalyst_voices/packages/catalyst_voices_blocs/lib/src/brand/brand_bloc.dart
+++ catalyst_voices/packages/catalyst_voices_blocs/lib/src/brand/brand_bloc.dart
@@ -7,55 +7,54 @@ import 'package:flutter/material.dart';
 part 'brand_event.dart';
 part 'brand_state.dart';
 
-// [BrandBloc] is a Bloc responsible for managing the brand state.
-// This Bloc listens for [BrandEvent]s and updates the [BrandState]
-// accordingly.
-// The [BrandState] can be passed to the [MaterialApp] as a theme
-// allowing to change the theme at runtime.
-//
-// The [BrandChanged] accepts a [BrandKey] and uses that is used for
-// the selection of the related [ThemeData] defined in the [brands]
-// map.
-// To trigger the theme change is just necessary to dispatch the
-// [BrandChanged] with the appropriate key:
-//
-// ```dart
-// context.read<BrandBloc>().add(
-//   const BrandChanged(BrandKey.catalyst),
-// );
-// ```
-//
-// Usage example:
-//
-// ```dart
-// BlocProvider(
-//   create: (context) => BrandBloc(),
-//   child: BlocBuilder<BrandBloc, BrandState>(
-//     builder: (context, state) {
-//       return MaterialApp(
-//         builder: (context, state) => Scaffold(
-//           body: Row(
-//             children: [
-//               MaterialButton(
-//                 color: Theme.of(context).primaryColor,
-//                 onPressed: () {
-//                   context.read<BrandBloc>().add(
-//                     const BrandChanged(BrandKey.catalyst),
-//                   );
-//                 },
-//                 child: const Text('Switch to Catalyst Theme'),
-//               ),
-//             ],
-//           ),
-//         ),
-//         theme: state.themeData,
-//       );
-//     },
-//   ),
-// );
-// ```
-
-
+/// [BrandBloc] is a Bloc responsible for managing the brand state.
+/// 
+/// This Bloc listens for [BrandEvent]s and updates the [BrandState]
+/// accordingly.
+/// The [BrandState] can be passed to the [MaterialApp] as a theme
+/// allowing to change the theme at runtime.
+///
+/// The [BrandChanged] accepts a [BrandKey] and uses that is used for
+/// the selection of the related [ThemeData] defined in the [brands]
+/// map.
+/// To trigger the theme change is just necessary to dispatch the
+/// [BrandChanged] with the appropriate key:
+///
+/// ```dart
+/// context.read<BrandBloc>().add(
+///   const BrandChanged(BrandKey.catalyst),
+/// );
+/// ```
+///
+/// Usage example:
+///
+/// ```dart
+/// BlocProvider(
+///   create: (context) => BrandBloc(),
+///   child: BlocBuilder<BrandBloc, BrandState>(
+///     builder: (context, state) {
+///       return MaterialApp(
+///         builder: (context, state) => Scaffold(
+///           body: Row(
+///             children: [
+///               MaterialButton(
+///                 color: Theme.of(context).primaryColor,
+///                 onPressed: () {
+///                   context.read<BrandBloc>().add(
+///                     const BrandChanged(BrandKey.catalyst),
+///                   );
+///                 },
+///                 child: const Text('Switch to Catalyst Theme'),
+///               ),
+///             ],
+///           ),
+///         ),
+///         theme: state.themeData,
+///       );
+///     },
+///   ),
+/// );
+/// ```
 final class BrandBloc extends Bloc<BrandEvent, BrandState> {
   BrandBloc() : super(BrandState()) {
     on<BrandChanged>(_onBrandChanged);
--- catalyst_voices/packages/catalyst_voices_models/lib/src/brands.dart
+++ catalyst_voices/packages/catalyst_voices_models/lib/src/brands.dart
@@ -3,11 +3,11 @@ import 'package:catalyst_voices_assets/generated/colors.gen.dart';
 
 import 'package:flutter/material.dart';
 
-// [brands] is a Map that stores the ThemeData for all the
-// available brands in the Catalyst Voices app.
-// This map is used by the [BrandBloc] to properly
-// switch the active theme based on the BlocState.
-
+/// [brands] is a Map that stores the ThemeData for all the available brands in
+/// the Catalyst Voices app.
+/// 
+/// This map is used by the BrandBloc to properly switch the active theme based
+/// on the BlocState.
 final Map<BrandKey, ThemeData> brands = {
   BrandKey.catalyst: ThemeData(
     colorScheme: ColorScheme.fromSeed(
