1099447ae5ad6064caa8462b753529bd80edf70b
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2024-04-19 12:48:32 +0200

    feat: cardano multiplatform lib plugin structure (#441)
    * chore: melos bootstrap
    
    * feat: add federated package structure for cardano_multiplatform_lib
    
    * style: improve changelog style to fix analyzer issues
    
    * docs: document better instance initialization with assertions
    
    * chore: add cardano multiplatform lib related words to cspell dictionary
    
    * chore: run melos bootstrap before running tests to make sure melos overrides paths to local packages
    
    * fix: remove empty tests which cause code coverage failure
    
    * refactor: rename cardano_multiplatform_lib to catalyst_cardano

---
+++ catalyst_voices_packages/catalyst_cardano/catalyst_cardano_platform_interface/lib/src/catalyst_cardano_platform.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+/// The interface that implementations of catalyst_cardano must
+/// implement.
+///
+/// Platform implementations should extend this class rather than implement
+/// it as `catalyst_cardano` does not consider newly added methods to
+/// be breaking changes. Extending this class (using `extends`) ensures that the
+/// subclass will get the default implementation, while platform implementations
+/// that `implements` this interface will be broken by newly added
+/// [CatalystCardanoPlatform] methods.
+abstract class CatalystCardanoPlatform extends PlatformInterface {
+  static final Object _token = Object();
+
+  static CatalystCardanoPlatform? _instance;
+
+  /// The default instance of [CatalystCardanoPlatform] to use.
+  ///
+  /// Must be set with [instance] setter before it can be used.
+  static CatalystCardanoPlatform get instance {
+    assert(
+      _instance != null,
+      'Make sure to register the instance via '
+      'instance setter before it is used.',
+    );
+    return _instance!;
+  }
+
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [CatalystCardanoPlatform] when they register
+  /// themselves.
+  static set instance(CatalystCardanoPlatform instance) {
+    PlatformInterface.verify(instance, _token);
+    _instance = instance;
+  }
+
+  /// Constructs a CatalystCardanoPlatform.
+  CatalystCardanoPlatform() : super(token: _token);
+
+  /// Temporary method which will be replaced by actual implementation of
+  /// cardano multiplatform lib.
+  Future<void> printHello() {
+    throw UnimplementedError('printHello() has not been implemented.');
+  }
+}
