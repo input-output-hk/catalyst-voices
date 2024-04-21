5d498fa2a6a8d675e47a98b7a02be18d2608336b
Mr-Leshiy <leshiy12345678@gmail.com>
2024-04-21 15:49:33 +0300

    fix fmt

--- catalyst-gateway/bin/src/cardano/mod.rs
+++ catalyst-gateway/bin/src/cardano/mod.rs
@@ -14,7 +14,7 @@ use tracing::{debug, error, info};
 use crate::{
     cardano::util::valid_era,
     event_db::{
-        cardano::{config::FollowerConfig, chain_state::MachineId},
+        cardano::{chain_state::MachineId, config::FollowerConfig},
         error::NotFoundError,
         EventDB,
     },
