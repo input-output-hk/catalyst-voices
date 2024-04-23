2fbd9393d1e5041f96569fe4f3a7ca80941a5061
Dominik Toton <dominik.toton@iohk.io>
2024-04-23 14:32:41 +0200

    fix: make sure exception is not silenced

--- catalyst_voices/lib/app/view/app_page.dart
+++ catalyst_voices/lib/app/view/app_page.dart
@@ -47,6 +47,8 @@ final class _AppState extends State<App> {
       FlutterError.dumpErrorToConsole(
         FlutterErrorDetails(exception: error, stack: stackTrace),
       );
+
+      rethrow;
     }
   }
 
