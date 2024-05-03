ef402e66a3383d798f911dac2dea783e4b9079d9
cong-or <conchubhar.gannon@gmail.com>
2024-05-03 15:17:16 +0100

    test error handling

--- catalyst-gateway/bin/src/cardano/cip36_registration/mod.rs
+++ catalyst-gateway/bin/src/cardano/cip36_registration/mod.rs
@@ -164,7 +164,7 @@ impl Cip36Metadata {
         if let Some(raw_61284) = raw_61284 {
             let _ = validate_signature(&raw_61284, &registration, &signature).map_or_else(
                 |_err| {
-                    //errors_report.push(format!("cannot validate signature {err}"));
+                    // errors_report.push(format!("cannot validate signature {err}"));
                     None
                 },
                 Some,
