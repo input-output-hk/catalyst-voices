3981421f7538501c73ea51677cfe378b2ccda97a
Lucio Baglione <coire1@gmail.com>
2024-05-09 12:29:38 +0200

    chore: Format.

--- catalyst_voices/packages/catalyst_voices_brands/lib/src/theme_builder/theme_builder.dart
+++ catalyst_voices/packages/catalyst_voices_brands/lib/src/theme_builder/theme_builder.dart
@@ -4,14 +4,14 @@ import 'package:catalyst_voices_brands/src/themes/fallback.dart';
 import 'package:flutter/material.dart';
 
 /// A utility class to build themes dynamically based on brand keys.
-/// 
+///
 /// [buildTheme] can be used to obtain the corresponding theme data for the
 /// [BrandKey] passed to the method.
-/// 
+///
 /// [buildDarkTheme] operates in the same way but picks the dark version of the
-/// theme for a specific brand. 
-/// 
-/// For each brand there is a specific key defined in the [BrandKey] enum 
+/// theme for a specific brand.
+///
+/// For each brand there is a specific key defined in the [BrandKey] enum
 /// and a corresponding [ThemeData] in the `themes` folder.
 /// For each brand a light and a dark [ThemeData] should be defined.
 ///
