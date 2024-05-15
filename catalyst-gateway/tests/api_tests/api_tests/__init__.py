496d7d07d19f3b8a9a8f7806519ef8bbdaccf544
Mr-Leshiy <leshiy12345678@gmail.com>
2024-05-15 15:00:53 +0300

    екн

--- catalyst-gateway/tests/api_tests/api_tests/__init__.py
+++ catalyst-gateway/tests/api_tests/api_tests/__init__.py
@@ -22,8 +22,8 @@ def cat_gateway_endpoint_url(endpoint: str):
 
 def check_is_live():
     resp = requests.get(cat_gateway_endpoint_url("api/health/live"))
-    assert resp.status_code == 204
     print(f"resp: {resp}, code: {resp.status_code}")
+    assert resp.status_code == 204
     logger.info("cat-gateway service is LIVE.")
 
 
