ab47eee47de1de559a1c7cf35202c6aa7b7ec5c1
Steven Johnson <sakurainds@gmail.com>
2024-05-17 19:58:03 +0700

    feat(ci): local custer WIP

--- utilities/local-cluster/Readme.md
+++ utilities/local-cluster/Readme.md
@@ -5,6 +5,8 @@ To eliminate variability and simplify local deployment, we have standardized the
 
 * [VirtualBox](https://www.virtualbox.org/)
 * [Vagrant](https://developer.hashicorp.com/vagrant/install?product_intent=vagrant)
+* [kubectl](https://kubernetes.io/docs/tasks/tools/)
+* [helm](https://helm.sh/docs/intro/install/)
 
 These tools allow us to define VMs that are consistent and provide a uniform Kubernetes environment
 for local testing.
@@ -16,13 +18,21 @@ The Cluster is based on [K3s](https://k3s.io/), which is a lightweight version o
 We set up 1 server node which hosts the control-plane and is the master node.
 Configured with:
 
-* 2 Cores
-* 2 GB of RAM
+* 4 Cores
+* 5 GB of RAM
 
 It has 3 Internal nodes, configured as:
 
 * 4 Cores
 * 4 GB of RAM
+
+## Default Services
+
+### Container Registry
+
+### Traefik
+
+### Grafana/Prometheus/Loki
 
 ## Interacting with the cluster
 
@@ -48,4 +58,31 @@ earthly +test-cluster
 
 ```sh
 earthly +test-inside-earthly
+```
+
+## Debugging the cluster
+
+### SSH into a running VM
+
+To SSH into a VM running the cluster, use `vagrant`:
+
+```sh
+vagrant ssh server
+```
+
+```sh
+vagrant ssh agent1
+```
+
+etc.
+
+### Debug the local registry
+
+The local registry is running inside docker, on the `server` VM.
+First ssh into the VM.
+
+To display the registry service logs:
+
+```sh
+docker logs registry
 ```
---
+++ utilities/local-cluster/scripts/check-cluster-dns.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+
+
+def parse_extra_hostnames(hostnames):
+    """
+    This function takes a file of hostnames and returns a list of tuples containing the IP address and hostname for each entry.
+    The input should be a file with one hostname per line, formatted as "IP_Address Hostname".
+    The function will ignore any lines that start with "#" and any extra hostnames on the same line.
+    """
+    with open(hostnames) as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        lines = [line for line in lines if line != "" and not line.startswith("#")]
+
+    result = []
+    for line in lines:
+        split = line.split()
+        result.append((split[0], split[1]))
+    return result
+
+
+def status(hostname, resolved_ip, ok):
+    print(f"{hostname:<25} : {resolved_ip:>15} : {ok}")
+
+
+def check_hostname(hostname, ip):
+    # Check the hostame resolves to the expected IP
+    ok = "OK"
+    try:
+        resolved_ip = socket.gethostbyname(hostname)
+        if resolved_ip != ip:
+            ok = f"FAIL. Set `{ip:<15} {hostname:<25}` in /etc/hosts."
+    except socket.error as e:
+        resolved_ip = "None"
+        ok = f"FAIL. Add `{ip:<15} {hostname:<25}` to /etc/hosts"
+
+    status(hostname, resolved_ip, ok)
+
+    return ok == "OK"
+
+
+everything_ok = True
+
+extra_hosts = parse_extra_hostnames(sys.argv[1])
+
+status("HOSTNAME", "RESOLVED IP", "STATUS")
+status("-------------------------", "---------------", "------")
+
+for host in extra_hosts:
+    everything_ok = check_hostname(host[1], host[0]) and everything_ok
+
+if not everything_ok:
+    sys.exit(1)
+sys.exit(0)
