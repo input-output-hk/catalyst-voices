6f249e0d3a2be1db49d64032945e4989479b220c
Lucio Baglione <coire1@gmail.com>
2024-05-17 11:25:54 +0200

    chore: Replace static `asset` method for CatalystSvgPicture and CatalystImage.

--- catalyst_voices/packages/catalyst_voices_assets/lib/src/catalyst_svg_picture.dart
+++ catalyst_voices/packages/catalyst_voices_assets/lib/src/catalyst_svg_picture.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-/// [CatalystSvgPicture] is a wrapper around [SvgPicture.asset] that sets the
-/// package property to `catalyst_voices_assets` by default.
+/// [CatalystSvgPicture] extends [SvgPicture] to have an asset constructor
+/// tat sets the package property to `catalyst_voices_assets` by default.
 /// This allows to use the asset without having to specify the package name
 /// every time.
 /// For more information, see [SvgPicture.asset].
-final class CatalystSvgPicture {
-  static SvgPicture asset(
+final class CatalystSvgPicture extends SvgPicture {
+  CatalystSvgPicture.asset(
     String name, {
     AssetBundle? bundle,
     String? semanticsLabel,
@@ -23,23 +23,21 @@ final class CatalystSvgPicture {
     Clip clipBehavior = Clip.hardEdge,
     SvgTheme? theme,
     ColorFilter? colorFilter,
-  }) {
-    return SvgPicture.asset(
-      name,
-      bundle: bundle,
-      semanticsLabel: semanticsLabel,
-      excludeFromSemantics: excludeFromSemantics,
-      width: width,
-      height: height,
-      fit: fit,
-      alignment: alignment,
-      matchTextDirection: matchTextDirection,
-      package: package,
-      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
-      placeholderBuilder: placeholderBuilder,
-      clipBehavior: clipBehavior,
-      theme: theme,
-      colorFilter: colorFilter,
-    );
-  }
+  }) : super.asset(
+          name,
+          bundle: bundle,
+          semanticsLabel: semanticsLabel,
+          excludeFromSemantics: excludeFromSemantics,
+          width: width,
+          height: height,
+          fit: fit,
+          alignment: alignment,
+          matchTextDirection: matchTextDirection,
+          package: package,
+          allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+          placeholderBuilder: placeholderBuilder,
+          clipBehavior: clipBehavior,
+          theme: theme,
+          colorFilter: colorFilter,
+        );
 }
