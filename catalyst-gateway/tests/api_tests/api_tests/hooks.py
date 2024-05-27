3171957f0e44d80e19113c302e36bff803a16609
Dee <duy.le@iohk.io>
2024-05-27 17:02:46 +0700

    feat: add put patch delete method, refactor

--- catalyst-gateway/tests/api_tests/api_tests/hooks.py
+++ catalyst-gateway/tests/api_tests/api_tests/hooks.py
@@ -9,16 +9,28 @@ def before_load_schema(
     raw_schema: Dict[str, Any],
     ) -> None:
     paths = dict(raw_schema["paths"])
-    for key in paths.keys(): 
-        values = dict(raw_schema["paths"][key])
-        if "get" in values:
-            ref_info = raw_schema["paths"][key]["get"]
-            new_info = copy.deepcopy(ref_info)
-            new_info["responses"] = {"405": {"description": "method not allowed"}}
-            raw_schema["paths"][key] = {"get": ref_info, "post": new_info}
+    for endpoint in paths.keys(): 
+        endpoint_data = dict(raw_schema["paths"][endpoint])
 
-        else:
-            ref_info = raw_schema["paths"][key]["post"]
-            new_info = copy.deepcopy(ref_info)
-            new_info["responses"] = {"405": {"description": "method not allowed"}}
-            raw_schema["paths"][key] = {"post": ref_info, "get": new_info}
+        # Get first method data as reference
+        ref_info = next(iter(endpoint_data.values()))
+
+        # Create new method data using reference data, replacing 'responses' field
+        new_info = copy.deepcopy(ref_info)
+        new_info["responses"] = {"405": {"description": "method not allowed"}}
+
+        # Update endpoint if a method is not declared
+        if "get" not in endpoint_data:
+            endpoint_data.update([("get", new_info),])
+        if "post" not in endpoint_data:
+            endpoint_data.update([("post", new_info),])
+        if "put" not in endpoint_data:
+            endpoint_data.update([("put", new_info),])
+        if "patch" not in endpoint_data:
+            endpoint_data.update([("patch", new_info),])
+        if "delete" not in endpoint_data:
+            endpoint_data.update([("patch", new_info),])
+
+        # Update endpoint
+        raw_schema["paths"][endpoint] = endpoint_data
+        
