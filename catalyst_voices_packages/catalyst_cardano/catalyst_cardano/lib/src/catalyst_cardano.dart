0c2b118bb5b9edf7d01f7b99bc5ab7d9bdb429e0
Dominik Toton <dominik.toton@iohk.io>
2024-05-27 09:39:11 +0200

    docs: document package version

--- catalyst_voices_packages/catalyst_cardano/catalyst_cardano/lib/src/catalyst_cardano.dart
+++ catalyst_voices_packages/catalyst_cardano/catalyst_cardano/lib/src/catalyst_cardano.dart
@@ -1,26 +1,16 @@
-import 'dart:typed_data';
-
 import 'package:catalyst_cardano_platform_interface/catalyst_cardano_platform_interface.dart';
 
-/// A Flutter plugin wrapping the Cardano Multiplatform Lib.
+/// A Flutter plugin exposing the [CIP-30](https://cips.cardano.org/cip/CIP-30)
+/// and [CIP-95](https://cips.cardano.org/cip/CIP-95) APIs.
 class CatalystCardano {
   /// The default instance of [CatalystCardano] to use.
   static final CatalystCardano instance = CatalystCardano._();
 
   CatalystCardano._();
 
-  /// Encodes [bytes] as metadatum, function to be removed later by providing
-  /// actual implementations of methods related to cardano multiplatform lib.
-  Future<void> encodeArbitraryBytesAsMetadatum(Uint8List bytes) async {
-    await CatalystCardanoPlatform.instance
-        .encodeArbitraryBytesAsMetadatum(bytes);
-  }
-
-  /// Initializer method to bootstrap internals of Cardano Multiplatform Lib.
-  ///
-  /// Must be called and awaited exactly once before any
-  /// additional interaction with the lib.
-  Future<void> init() async {
-    await CatalystCardanoPlatform.instance.init();
+  /// Returns available wallet extensions exposed under
+  /// cardano.{walletName} according to CIP-30 standard.
+  Future<List<CardanoWallet>> getCardanoWallets() async {
+    return CatalystCardanoPlatform.instance.getCardanoWallets();
   }
 }
