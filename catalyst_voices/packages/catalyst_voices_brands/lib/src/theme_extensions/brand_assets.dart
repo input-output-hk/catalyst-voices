40880aced950bf221acbf2826d27ea4742c3b01b
Lucio Baglione <coire1@gmail.com>
2024-05-27 15:44:03 +0200

    docs: Add docs for `BrandAssets` and `VoicesColorScheme`.

--- catalyst_voices/packages/catalyst_voices_brands/lib/src/theme_extensions/brand_assets.dart
+++ catalyst_voices/packages/catalyst_voices_brands/lib/src/theme_extensions/brand_assets.dart
@@ -1,6 +1,18 @@
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:flutter/material.dart';
 
+/// A `ThemeExtension` that holds brand-specific assets for theming purposes.
+///
+/// `BrandAssets` is used to encapsulate theme-dependent assets such as the
+/// brand's logo and logo icon as SVG images which can be utilized throughout
+/// the application wherever is required.
+///
+/// Example usage:
+/// ```dart
+/// final logo = Theme.of(context).brandAssets.logo;
+/// final logoIcon = Theme.of(context).brandAssets.logoIcon;
+/// ```
+@immutable
 class BrandAssets extends ThemeExtension<BrandAssets> {
   final SvgGenImage logo;
   final SvgGenImage logoIcon;
