36d5ea2dfe05beb524f2aaf6070c81dd9af72b81
bkioshn <bkioshn@gmail.com>
2024-05-29 09:52:09 +0700

    fix: clean up c509 enum

--- catalyst-gateway/crates/c509-certificate/src/c509_enum.rs
+++ catalyst-gateway/crates/c509-certificate/src/c509_enum.rs
@@ -1,50 +1,51 @@
 #[allow(unused)]
+/// C509 certificate type
+/// Currently support Natively Signed c509 cert
 pub(crate) enum C509CertificateType {
     // Natively Signed C509 cert
     SignedC509Cert,
-    // Re-encoded x.509 v3 DER cert
-    C509Cert,
 }
+
 #[allow(unused)]
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 pub(crate) enum AttributesRegistry {
-    Email,
-    CommonName,             // CN
-    SurName,                // SN
-    SerialNumber,
-    Country,                // C
-    Locality,               // L
-    StateOrProvince,        // ST
-    StreetAddress,
-    Organization,           // O
-    OrganizationUnit,       // OU
-    Title,                  // T
-    Business,
-    PostalCode,             // PC
-    GivenName,
-    Initials,
-    GenerationQualifier,
-    DNQualifier,
-    Pseudonym,
-    OrganizationIdentifier,
-    IncLocality,
-    IncState,
-    IncCountry,
-    DomainComponent,        // DC
-    PostalAddress,          // postalAddress
-    Name,                   // name
-    TelephoneNumber,        // telephoneNumber
-    DirManDomainName,       // dmdName
-    UserID,                 // uid
-    UnstructuredName,       // unstructuredName
-    UnstructuredAddress,    // unstructuredAddress
+    Email = 0,
+    CommonName = 1, // CN
+    SurName = 2,    // SN
+    SerialNumber = 3,
+    Country = 4,         // C
+    Locality = 5,        // L
+    StateOrProvince = 6, // ST
+    StreetAddress = 7,
+    Organization = 8,     // O
+    OrganizationUnit = 9, // OU
+    Title = 10,           // T
+    Business = 11,
+    PostalCode = 12, // PC
+    GivenName = 13,
+    Initials = 14,
+    GenerationQualifier = 15,
+    DNQualifier = 16,
+    Pseudonym = 17,
+    OrganizationIdentifier = 18,
+    IncLocality = 19,
+    IncState = 20,
+    IncCountry = 21,
+    DomainComponent = 22,     // DC
+    PostalAddress = 23,       // postalAddress
+    Name = 24,                // name
+    TelephoneNumber = 25,     // telephoneNumber
+    DirManDomainName = 26,    // dmdName
+    UserID = 27,              // uid
+    UnstructuredName = 28,    // unstructuredName
+    UnstructuredAddress = 29, // unstructuredAddress
 }
 
 #[allow(unused)]
 #[derive(PartialEq)]
 pub(crate) enum PubKeyAlgoRegistry {
     RSA = 0,
-    Secp256r1 = 1 ,
+    Secp256r1 = 1,
     Secp384r1 = 2,
     Secp521r1 = 3,
     X25519 = 8,
@@ -59,14 +60,4 @@ pub(crate) enum PubKeyAlgoRegistry {
     Brainpool512r1 = 26,
     Frp256v1 = 27,
     SM2P256v1 = 28,
-}
-
-#[allow(unused)]
-#[derive(Debug)]
-pub(crate) enum StringType {    
-    Utf8String(String),
-    PrintableString(String),
-    Ia5String(String),
-    HexByteString(Vec<u8>),
-    Eui64ByteString(Vec<u8>),
 }
