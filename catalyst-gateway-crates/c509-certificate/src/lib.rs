c079a17f1b1eac97abd57e0b9347174009f46fc2
bkioshn <bkioshn@gmail.com>
2024-06-03 15:27:55 +0700

    fix: lib.rs

--- catalyst-gateway-crates/c509-certificate/src/lib.rs
+++ catalyst-gateway-crates/c509-certificate/src/lib.rs
@@ -1,18 +1,14 @@
 //! x509 Certificate library
 
-use wasm_bindgen::prelude::*;
+use c509_cert::TbsCertificate;
+use wasm_bindgen::prelude::wasm_bindgen;
 mod c509_cert;
 mod c509_enum;
 mod cbor_encoder;
 mod extensions;
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 /// Sample function for test only
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
+pub fn generate_c509_cert(tbs_cert: TbsCertificate) -> Vec<u8> {
+    c509_cert::generate_c509_cert(tbs_cert)
 }
