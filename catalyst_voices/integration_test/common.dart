85ee45b810af78245d22d8b3fdac9ec8e0216e06
Dee <duy.le@iohk.io>
2024-06-03 21:46:17 +0700

    feat: add patrol test

---
+++ catalyst_voices/integration_test/common.dart
@@ -0,0 +1,35 @@
+import 'package:catalyst_voices/app/app.dart';
+import 'package:catalyst_voices/app/view/app_page.dart';
+import 'package:catalyst_voices/configs/main_qa.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+export 'package:flutter_test/flutter_test.dart';
+export 'package:patrol/patrol.dart';
+
+const _nativeAutomatorConfig = NativeAutomatorConfig(
+  findTimeout: Duration(seconds: 30), // 10 seconds is too short for some CIs
+);
+const _patrolTesterConfig = PatrolTesterConfig();
+
+Future<void> createApp(PatrolIntegrationTester $) async {
+  await app.main();
+  await $.pumpWidgetAndSettle(const App());
+}
+
+void patrol(
+  String description,
+  Future<void> Function(PatrolIntegrationTester) callback, {
+  bool? skip,
+  NativeAutomatorConfig? nativeAutomatorConfig,
+  LiveTestWidgetsFlutterBindingFramePolicy framePolicy =
+      LiveTestWidgetsFlutterBindingFramePolicy.fadePointers,
+}) {
+  patrolTest(
+    description,
+    nativeAutomatorConfig: nativeAutomatorConfig ?? _nativeAutomatorConfig,
+    framePolicy: framePolicy,
+    skip: skip,
+    callback,
+  );
+}
