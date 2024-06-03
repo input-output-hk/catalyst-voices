d1c04d280951cb28a1f97aa8195da36416ebe60a
bkioshn <bkioshn@gmail.com>
2024-06-03 15:26:11 +0700

    feat: add sigalgo registry

--- catalyst-gateway-crates/c509-certificate/src/c509_enum.rs
+++ catalyst-gateway-crates/c509-certificate/src/c509_enum.rs
@@ -1,14 +1,14 @@
 #[allow(unused)]
 /// C509 certificate type
 /// Currently support Natively Signed c509 cert
-pub(crate) enum C509CertificateType {
+pub enum C509CertificateType {
     // Natively Signed C509 cert
     SignedC509Cert,
 }
 
 #[allow(unused)]
 #[derive(PartialEq, Copy, Clone)]
-pub(crate) enum AttributesRegistry {
+pub enum AttributesRegistry {
     Email = 0,
     CommonName = 1, // CN
     SurName = 2,    // SN
@@ -43,7 +43,7 @@ pub(crate) enum AttributesRegistry {
 
 #[allow(unused)]
 #[derive(PartialEq)]
-pub(crate) enum PubKeyAlgoRegistry {
+pub enum PubKeyAlgoRegistry {
     RSA = 0,
     Secp256r1 = 1,
     Secp384r1 = 2,
@@ -60,4 +60,34 @@ pub(crate) enum PubKeyAlgoRegistry {
     Brainpool512r1 = 26,
     Frp256v1 = 27,
     SM2P256v1 = 28,
+}
+
+// Section 9.10
+#[allow(unused)]
+#[derive(PartialEq)]
+pub enum SignatureAlgoRegistry {
+    RsassaPkcs1V15Sha1 = -256,
+    EcdsaSha1 = -255,
+    EcdsaSha256 = 0,
+    EcdsaSha384 = 1,
+    EcdsaSha512 = 2,
+    EcdsaShake128 = 3,
+    EcdsaShake256 = 4,
+    Ed25519 = 12,
+    Ed448 = 13,
+    Sha256WithHmacSha256 = 14,
+    Sha384WithHmacSha384 = 15,
+    Sha512WithHmacSha512 = 16,
+    RsassaPkcs1V15Sha256 = 23,
+    RsassaPkcs1V15Sha384 = 24,
+    RsassaPkcs1V15Sha512 = 25,
+    RsassaPssSha256 = 26,
+    RsassaPssSha384 = 27,
+    RsassaPssSha512 = 28,
+    RsassaPssShake128 = 29,
+    RsassaPssShake256 = 30,
+    HssLms = 42,
+    Xmss = 43,
+    XmssMt = 44,
+    Sm2WithSm3 = 45,
 }
