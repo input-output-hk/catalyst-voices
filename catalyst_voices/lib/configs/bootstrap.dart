7f46b261c8e901d12d4145d85a3fc554f7ace8d1
Dominik Toton <dominik.toton@iohk.io>
2024-06-05 11:32:31 +0200

    refactor: cleanup boostrap

--- catalyst_voices/lib/configs/bootstrap.dart
+++ catalyst_voices/lib/configs/bootstrap.dart
@@ -7,6 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_strategy/url_strategy.dart';
 
+/// Initializes and runs the application provided by the [builder].
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -23,4 +24,16 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   Bloc.observer = AppBlocObserver();
 
   runApp(await builder());
+}
+
+/// Initializer for integration tests, similar to [bootstrap] but:
+/// - Must not  call [WidgetsFlutterBinding.ensureInitialized].
+/// - Must not call [runApp].
+/// - Must not override [FlutterError.onError].
+/// - Must be synced with [bootstrap], to run exactly the same
+///   initialization logic except for the above exclusions.
+Future<void> bootstrapForTests() async {
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  setPathUrlStrategy();
+  Bloc.observer = AppBlocObserver();
 }
