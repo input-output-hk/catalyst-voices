a80f477bb37b20c1b833212afd1abbdf154a260b
bkioshn <bkioshn@gmail.com>
2024-06-17 14:36:09 +0900

    fix: naming and test

--- catalyst-gateway-crates/c509-certificate/src/c509_oid.rs
+++ catalyst-gateway-crates/c509-certificate/src/c509_oid.rs
@@ -1,6 +1,6 @@
 //! This module provides an encoding and decoding of C509 Object Identifier (OID).
 //! Please refer to [RFC9090](https://datatracker.ietf.org/doc/rfc9090/) for OID encoding
-//! Please refer to the [CDDL Wrapping](https://datatracker.ietf.org/doc/html/rfc8610#section-3.7) for unwrapped types.
+//! Please refer to [CDDL Wrapping](https://datatracker.ietf.org/doc/html/rfc8610#section-3.7) for unwrapped types.
 
 use asn1_rs::FromDer;
 use minicbor::{Decoder, Encoder};
@@ -30,7 +30,7 @@ impl<'a> C509Oid<'a> {
     ///
     /// A vector of bytes containing the CBOR encoded unwrap OID.
     /// if the encoding fails, it will return an error.
-    pub fn encode_unwrap_oid(&self) -> anyhow::Result<Vec<u8>> {
+    pub fn encode(&self) -> anyhow::Result<Vec<u8>> {
         let mut buffer = Vec::new();
         let mut encoder = Encoder::new(&mut buffer);
         encoder
@@ -45,7 +45,7 @@ impl<'a> C509Oid<'a> {
     ///
     /// A C509Oid instance containing the decoded OID.
     /// if the decoding fails, it will return an error.
-    pub fn decode_unwrap_oid(cbor_bytes: &Vec<u8>) -> anyhow::Result<C509Oid> {
+    pub fn decode(cbor_bytes: &Vec<u8>) -> anyhow::Result<C509Oid> {
         let mut decoder = Decoder::new(cbor_bytes);
         let oid_bytes = decoder
             .bytes()
@@ -67,26 +67,33 @@ mod test_c509_oid {
     // Test reference 3.1. Encoding of the SHA-256 OID
     // https://datatracker.ietf.org/doc/rfc9090/
     #[test]
-    fn test_encode_decode_unwrap_oid() {
+    fn test_encode_decode() {
         let oid = C509Oid::new(oid!(2.16.840 .1 .101 .3 .4 .2 .1));
-        let encoded_unwrap_oid = oid.encode_unwrap_oid().expect("Failed to encode OID");
+        let encoded_unwrap_oid = oid.encode().expect("Failed to encode OID");
         assert_eq!(
             hex::encode(encoded_unwrap_oid.clone()),
             "49608648016503040201"
         );
         let decoded_unwrap_oid =
-            C509Oid::decode_unwrap_oid(&encoded_unwrap_oid).expect("Failed to decode OID");
+            C509Oid::decode(&encoded_unwrap_oid).expect("Failed to decode OID");
         assert_eq!(oid, decoded_unwrap_oid);
     }
 
     #[test]
-    fn test_encode_decode_unwrap_oid_from_registry() {
+    fn test_encode_decode_from_registry() {
         // OID_HASH_SHA1 = 1.3.14.3.2.26
         let oid = C509Oid::new(oid_registry::OID_HASH_SHA1);
-        let encoded_unwrap_oid = oid.encode_unwrap_oid().expect("Failed to encode OID");
+        let encoded_unwrap_oid = oid.encode().expect("Failed to encode OID");
         assert_eq!(hex::encode(encoded_unwrap_oid.clone()), "452b0e03021a");
         let decoded_unwrap_oid =
-            C509Oid::decode_unwrap_oid(&encoded_unwrap_oid).expect("Failed to decode OID");
+            C509Oid::decode(&encoded_unwrap_oid).expect("Failed to decode OID");
         assert_eq!(oid, decoded_unwrap_oid);
+    }
+
+    #[test]
+    fn test_partial_equal() {
+        let oid1 = C509Oid::new(oid_registry::OID_HASH_SHA1);
+        let oid2 = C509Oid::new(oid!(1.3.14 .3 .2 .26));
+        assert_eq!(oid1, oid2);
     }
 }
