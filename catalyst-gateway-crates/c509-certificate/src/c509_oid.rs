ccc6700a6745ce93c7276078acfaac4c291ef20a
bkioshn <bkioshn@gmail.com>
2024-06-18 15:02:18 +0900

    feat: add OID PEN encode and decode

--- catalyst-gateway-crates/c509-certificate/src/c509_oid.rs
+++ catalyst-gateway-crates/c509-certificate/src/c509_oid.rs
@@ -4,6 +4,7 @@
 
 use asn1_rs::FromDer;
 use minicbor::{
+    data::Tag,
     decode,
     encode::{self, Write},
     Decode, Decoder, Encode, Encoder,
@@ -19,6 +20,17 @@ pub struct C509oid<'a> {
 
 /// Tag representing DER OID.
 const DER_OID_TAG: u8 = 0x06;
+/// Tag representing DER Relative OID.
+const DER_RELATIVE_OID_TAG: u8 = 0x0d;
+/// IANA Private Enterprise Number OID prefix.
+const PEN_PREFIX: &str = "1.3.6.1.4.1.";
+/// Number of bytes for the PEN_PREFIX.
+/// - five bytes shorter by leaving out the prefix h'2b06010401'
+/// from the enclosed byte string.
+const PEN_PREFIX_BYTES_LEN: usize = 5;
+/// Tag number representing IANA Private Enterprise Number (PEN) OID.
+const OID_PEN_TAG: u64 = 112;
+/// Array of CBOR bytes representing the PEN tag.
 
 #[allow(dead_code)]
 impl<'a> C509oid<'a> {
@@ -31,7 +43,7 @@ impl<'a> C509oid<'a> {
 impl<C> Encode<C> for C509oid<'_> {
     /// Encode an unwrapped OID (as bytes string without tag).
     /// ~oid indicates a unwrapped OID.
-    ///
+    /// Encode PEN (Private Enterprise Number) given
     /// # Returns
     ///
     /// A vector of bytes containing the CBOR encoded unwrapped OID.
@@ -39,8 +51,14 @@ impl<C> Encode<C> for C509oid<'_> {
     fn encode<W: Write>(
         &self, e: &mut Encoder<W>, _ctx: &mut C,
     ) -> Result<(), encode::Error<W::Error>> {
-        let oid_bytes: &[u8] = self.oid.as_bytes();
-        e.bytes(oid_bytes)?.ok()
+        let oid_bytes = self.oid.as_bytes();
+
+        if self.oid.to_string().starts_with(PEN_PREFIX) {
+            e.tag(Tag::new(OID_PEN_TAG))?;
+            e.bytes(&oid_bytes[PEN_PREFIX_BYTES_LEN..])?.ok()
+        } else {
+            e.bytes(oid_bytes)?.ok()
+        }
     }
 }
 
@@ -52,12 +70,30 @@ impl<'b, C> Decode<'b, C> for C509oid<'_> {
     /// A C509oid instance containing the decoded OID.
     /// If the decoding fails, it will return an error.
     fn decode(d: &mut Decoder<'b>, _ctx: &mut C) -> Result<Self, decode::Error> {
-        let oid_bytes = d.bytes()?;
-        // Time Length Value (TLV) format.
-        let mut der_bytes = vec![DER_OID_TAG, oid_bytes.len() as u8];
-        der_bytes.extend_from_slice(oid_bytes);
-        let oid = Oid::from_der(&der_bytes).map_err(|e| decode::Error::message(e.to_string()))?;
-        Ok(C509oid::new(oid.1.to_owned()))
+        let dt = d.datatype()?;
+        if dt == minicbor::data::Type::Tag {
+            let tag = d.tag()?;
+            if tag == Tag::new(112) {
+                let oid_bytes = d.bytes()?;
+                let mut der_bytes = vec![DER_RELATIVE_OID_TAG, 6 + oid_bytes.len() as u8, 1, 3, 6, 1, 4, 1];
+                der_bytes.extend_from_slice(oid_bytes);
+                println!("OID bytes: {:?}", der_bytes);
+                let oid =
+                    Oid::from_der(&der_bytes).map_err(|e| decode::Error::message(e.to_string()))?;
+                return Ok(C509oid::new(oid.1.to_owned()));
+            } else {
+                todo!()
+            }
+        } else {
+            let oid_bytes = d.bytes()?;
+            // Time Length Value (TLV) format.
+            let mut der_bytes = vec![DER_OID_TAG, oid_bytes.len() as u8];
+            der_bytes.extend_from_slice(oid_bytes);
+            println!("OID bytes: {:?}", der_bytes);
+            let oid =
+                Oid::from_der(&der_bytes).map_err(|e| decode::Error::message(e.to_string()))?;
+            Ok(C509oid::new(oid.1.to_owned()))
+        }
     }
 }
 
@@ -82,7 +118,7 @@ mod test_c509_oid {
 
         let mut decoder = Decoder::new(&buffer);
         let decoded_oid = C509oid::decode(&mut decoder, &mut ()).expect("Failed to decode OID");
-        assert_eq!(oid, decoded_oid);
+        assert_eq!(decoded_oid, oid);
     }
 
     #[test]
@@ -97,7 +133,21 @@ mod test_c509_oid {
 
         let mut decoder = Decoder::new(&buffer);
         let decoded_oid = C509oid::decode(&mut decoder, &mut ()).expect("Failed to decode OID");
-        assert_eq!(oid, decoded_oid);
+        assert_eq!(decoded_oid, oid);
+    }
+
+    #[test]
+    fn test_encode_decode_pen() {
+        let mut buffer = Vec::new();
+        let mut encoder = Encoder::new(&mut buffer);
+        let oid = C509oid::new(oid!(1.3.6 .1 .4 .1 .1 .1 .29));
+        oid.encode(&mut encoder, &mut ())
+            .expect("Failed to encode OID");
+        assert_eq!(hex::encode(buffer.clone()), "d8704301011d");
+
+        let mut decoder = Decoder::new(&buffer);
+        let decoded_oid = C509oid::decode(&mut decoder, &mut ()).expect("Failed to decode OID");
+        assert_eq!(decoded_oid, oid);
     }
 
     #[test]
