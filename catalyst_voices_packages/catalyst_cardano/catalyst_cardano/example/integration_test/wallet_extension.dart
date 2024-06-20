6b12dcccd58e57883df4947d354f8340fa7a21f2
Dee <duy.le@iohk.io>
2024-06-20 10:39:53 +0700

    feat: add install extension program

--- catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/integration_test/wallet_extension.dart
+++ catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/integration_test/wallet_extension.dart
@@ -1,33 +1,76 @@
 import 'dart:io';
+
+import 'package:catalyst_cardano/catalyst_cardano.dart';
+import 'package:path/path.dart' as path;
 import 'package:webdriver/async_io.dart';
 
 void main() async {
-  const extensionPath = '../integration_test/extension-wallet';
+  const extensionPath = './integration_test/extension-wallet';
 
-  final chromedriver = await Process.start('chromedriver', []);
+  final chromedriver =
+      await Process.start('chromedriver', ['--port=4444', '--url-base=wd/hub']);
 
   await Future.delayed(const Duration(seconds: 2));
 
   final options = {
-    'args': ['--load-extension=$extensionPath']
+    'args': [
+      '--load-extension=$extensionPath',
+      '--disable-dev-shm-usage',
+      '--no-sandbox',
+    ],
   };
 
   final driver = await createDriver(
-    uri: Uri.parse('http://localhost:9515'),
+    uri: Uri.parse('http://localhost:4444/wd/hub/'),
     desired: {
       'browserName': 'chrome',
       'goog:chromeOptions': options,
     },
   );
-  await driver.get('https://www.google.co.uk/');
-  // await driver.get('http://localhost:8000');
+  await driver.timeouts.setImplicitTimeout(const Duration(seconds: 60));
+  await driver
+      .get('chrome-extension://hmbohmbpddljjdejnpdomihefihbpifa/index.html#/');
+  const addWalletBy = By.xpath('//span[text()="Add Wallet"]');
+  final addWalletButton = await driver.findElement(addWalletBy);
+  await addWalletButton.click();
+
+  const importWalletBy = By.xpath('//span[text()="Import an old wallet"]');
+  final importWalletButton = await driver.findElement(importWalletBy);
+  await importWalletButton.click();
+
+  final baseDir = path.dirname(Platform.script.toFilePath());
+  var filePath = path.join(baseDir, './eternl-test-wallet.json');
+  filePath = path.normalize(filePath);
+
+  const inputFileBy = By.xpath(
+    '//input[@id="files"]',
+  );
+  final inputFile = await driver.findElement(inputFileBy);
+  await inputFile.sendKeys(filePath);
+
+  // Connect to dapp
+  const addedWalletBy = By.xpath(
+    '//span[text()="My Wallets"]/parent::div/parent::div/following-sibling::div',
+  );
+  final addedWalletButton = await driver.findElement(addedWalletBy);
+  await addedWalletButton.click();
+
+  const connectBy = By.xpath(
+    '//span[text()="Buy"]/parent::div/parent::div/preceding-sibling::div[2]',
+  );
+  final connectButton = await driver.findElement(connectBy);
+  await connectButton.click();
+
   await Future.delayed(const Duration(seconds: 10));
-
-  // final element = await driver.findElement(const By.xpath('//*[text()="Enable wallet"]'));
-  // await element.click();
-
-  await Future.delayed(const Duration(seconds: 30));
+  // Go to web app
+  await driver.get('http://localhost:8000/');
 
   // await driver.quit();
   // await chromedriver.kill();
+
+  // final wallets = await CatalystCardanoPlatform.instance.getWallets();
+  // print('wallets are');
+  // print(wallets);
+  // final api = await wallets.first.enable();
+  // final balance = await api.getBalance();
 }
--- catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/integration_test/wallet_test.dart
+++ catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/integration_test/wallet_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:catalyst_cardano/catalyst_cardano.dart';
 import 'package:catalyst_cardano_example/main.dart';
 // import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -8,44 +9,17 @@ import 'package:integration_test/integration_test.dart';
 import 'package:webdriver/async_io.dart';
 
 void main() {
-
   group('Connect wallet', () {
-    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    // IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
     testWidgets('Enable wallet', (tester) async {
-      await tester.pumpWidget(const MyApp());
-      await tester.pumpAndSettle();
-      late WebDriver driver;
-      // late Process chromeDriverProcess;
+      final wallets = await CatalystCardanoPlatform.instance.getWallets();
+      print('wallets are');
+      print(wallets);
+      final api = await wallets.first.enable();
+      final balance = await api.getBalance();
 
-      // chromeDriverProcess = await Process.start('chromedriver', ['--port=4444', '--url-base=wd/hub']);
-
-      driver = await createDriver(
-        uri: Uri.parse('http://localhost:4444/wd/hub/'),
-        desired: Capabilities.chrome,
-      );
-
-      await _installChromeExtension(driver, '/home/sotatek/IOHK/catalyst-voices/catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/test/Eternl.crx');
-      await driver.get('https://chromewebstore.google.com/detail/eternl/kmhcihpebfmpgmihbkipmjlmmioameka');
-      await driver.get('http://localhost:8000/');
-
-      final element = await driver.findElement(const By.xpath('//*[text()="Enable wallet"]'));
-      await element.click();
-
-      // await tester.tap(find.widgetWithText(ElevatedButton, 'Enable wallet'));
-      // await tester.pumpAndSettle();
-
-      // await driver.quit();
+      expect(balance, 0);
     });
   });
-}
-
-Future<void> _installChromeExtension(WebDriver driver, String extensionPath) async {
-  var base64Extension = base64.encode(File(extensionPath).readAsBytesSync());
-  try {
-    await driver.execute(
-      'await chrome.runtime.sendMessage("kmhcihpebfmpgmihbkipmjlmmioameka", {type: "LOAD_EXTENSION", id: "$base64Extension"});', []);
-  } catch (e) {
-    print(e);
-  }
 }
