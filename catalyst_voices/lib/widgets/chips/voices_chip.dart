6569f89802e6be7cdd459280a6ea2c4f21696702
Dominik Toton <dominik.toton@iohk.io>
2024-06-24 15:10:14 +0200

    docs: missing documentation

--- catalyst_voices/lib/widgets/chips/voices_chip.dart
+++ catalyst_voices/lib/widgets/chips/voices_chip.dart
@@ -1,15 +1,31 @@
 import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:flutter/material.dart';
 
+/// A contextual chip acting as a label with some extra
+/// small piece of information.
 class VoicesChip extends StatelessWidget {
+  /// The main content of the chip, usually a [Text] widget.
   final Widget content;
+
+  /// The widget in front of the [content].
   final Widget? leading;
+
+  /// The widget in back of the [content].
   final Widget? trailing;
+
+  /// The color of the background, transparent by default.
   final Color? backgroundColor;
+
+  /// The radius of rounded corners of the chip.
   final BorderRadius borderRadius;
+
+  /// The internal padding from the widget borders to the internal content.
   final EdgeInsets padding;
+
+  /// The callback called when the widget is tapped.
   final VoidCallback? onTap;
 
+  /// The default constructor for the [VoicesChip].
   const VoicesChip({
     super.key,
     required this.content,
@@ -21,6 +37,7 @@ class VoicesChip extends StatelessWidget {
     this.onTap,
   });
 
+  /// A constructor that creates a [VoicesChip] with fully rounded corners.
   const VoicesChip.round({
     super.key,
     required this.content,
@@ -31,6 +48,8 @@ class VoicesChip extends StatelessWidget {
     this.onTap,
   }) : borderRadius = const BorderRadius.all(Radius.circular(32));
 
+  /// A constructor that creates a [VoicesChip] with slightly rounded,
+  /// rectangular corners.
   const VoicesChip.rectangular({
     super.key,
     required this.content,
