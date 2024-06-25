15bea2a81adfffade21cccff3a996de7188d7cee
Oleksandr Prokhorenko <djminikin@gmail.com>
2024-06-25 13:06:25 +0200

    feat: add sentry (#564)
    * feat: add sentry
    
    * Update README.md
    
    * Update app_router.dart
    
    * feat: add build-flutter-web ci job
    
    * chore: typo fixes
    
    * Update build-flutter-web.yml
    
    * Update build-flutter-web.yml
    
    * Update build-flutter-web.yml
    
    * Update build-flutter-web.yml
    
    * Update build-flutter-web.yml
    
    * Update build_environment.dart
    
    * chore: minor updates
    
    * Update build_config.dart
    
    * Update build-flutter-web.yml
    
    * ci: add build-web-on-pr, clean up build-flutter-web
    
    * chore: clean up
    
    * Update Earthfile
    
    * Update Earthfile
    
    * Update Earthfile

--- catalyst_voices/lib/configs/bootstrap.dart
+++ catalyst_voices/lib/configs/bootstrap.dart
@@ -2,13 +2,20 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:catalyst_voices/configs/app_bloc_observer.dart';
+import 'package:catalyst_voices/configs/sentry_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // There's no need to call WidgetsFlutterBinding.ensureInitialized()
+  // since this is already done internally by SentryFlutter.init()
+  // More info here: https://github.com/getsentry/sentry-dart/issues/2063
+  if (!kReleaseMode) {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
 
   GoRouter.optionURLReflectsImperativeAPIs = true;
   setPathUrlStrategy();
@@ -22,5 +29,13 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
 
   Bloc.observer = AppBlocObserver();
 
-  runApp(await builder());
+  await _runApp(await builder());
+}
+
+Future<void> _runApp(Widget app) async {
+  if (kReleaseMode) {
+    await SentryService.init(app);
+  } else {
+    runApp(app);
+  }
 }
