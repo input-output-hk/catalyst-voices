eb053d9f370a334a0d416feb58f51cc297695279
bkioshn <bkioshn@gmail.com>
2024-06-30 14:42:51 +0700

    feat: add alternative name encode and decode

--- catalyst-gateway-crates/c509-certificate/src/c509_extensions/alt_name.rs
+++ catalyst-gateway-crates/c509-certificate/src/c509_extensions/alt_name.rs
@@ -1,23 +1,33 @@
-//! Subject Alternative Name
-//!
+//! C509 Alternative Name uses for Subject Alternative Name extension and
+//! Issuer Alternative Name extension.
 
-use minicbor::{encode::Write, Encode, Encoder};
+use minicbor::{encode::Write, Decode, Decoder, Encode, Encoder};
 
 use crate::c509_general_name::GeneralNames;
 
-/// SubjectAltName = GeneralNames / text
+// FIXME - revisit visibility
+
+/// Alternative can be a `GeneralNames` or a text.
 #[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
 pub(crate) enum GeneralNamesOrText {
     GeneralNames(GeneralNames),
     Text(String),
 }
 
+/// Alternative Name extension.
+///
+/// # Fields
+/// * value - A value of alternative name that can be either a `GeneralNames` or a text.
 #[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
 pub(crate) struct AltName {
     value: GeneralNamesOrText,
 }
 
+#[allow(dead_code)]
 impl AltName {
+    /// Create a new instance of AltName given value.
     fn new(value: GeneralNamesOrText) -> Self {
         Self { value }
     }
@@ -25,16 +35,16 @@ impl AltName {
 
 impl<C> Encode<C> for AltName {
     fn encode<W: Write>(
-        &self, e: &mut Encoder<W>, _ctx: &mut C,
+        &self, e: &mut Encoder<W>, ctx: &mut C,
     ) -> Result<(), minicbor::encode::Error<W::Error>> {
         match &self.value {
             GeneralNamesOrText::GeneralNames(gns) => {
                 // FIXME - ugly
                 // Check whether there is only 1 item in the array which is a DNSName
                 if gns.get_gns().len() == 1 && gns.get_gns()[0].get_gn().get_int() == 2 {
-                    Ok(gns.get_gns()[0].get_gn().encode(e, &mut ()))?
+                    Ok(gns.get_gns()[0].get_gn().encode(e, ctx))?
                 } else {
-                    Ok(gns.encode(e, &mut ()))?
+                    Ok(gns.encode(e, ctx))?
                 }
             },
             GeneralNamesOrText::Text(text) => {
@@ -45,6 +55,22 @@ impl<C> Encode<C> for AltName {
     }
 }
 
+impl<'a, C> Decode<'a, C> for AltName {
+    fn decode(d: &mut Decoder<'a>, ctx: &mut C) -> Result<Self, minicbor::decode::Error> {
+        // FIXME - can't distinguish between GeneralNames(only DNSName) and Text
+        match d.datatype()? {
+            minicbor::data::Type::String => {
+                let text = d.str()?;
+                Ok(AltName::new(GeneralNamesOrText::Text(text.to_string())))
+            },
+            minicbor::data::Type::Array => {
+                let gns = GeneralNames::decode(d, ctx)?;
+                Ok(AltName::new(GeneralNamesOrText::GeneralNames(gns)))
+            },
+            _ => Err(minicbor::decode::Error::message("Invalid type for AltName")),
+        }
+    }
+}
 #[cfg(test)]
 mod test_alt_name {
     use crate::c509_general_name::{GeneralName, GeneralNamesRegistry};
@@ -52,7 +78,7 @@ mod test_alt_name {
     use super::*;
 
     #[test]
-    fn test_encode_decode_alt_name_only_dns() {
+    fn test_encode_alt_name_only_dns() {
         let mut buffer = Vec::new();
         let mut encoder = Encoder::new(&mut buffer);
         let mut gns = GeneralNames::new();
@@ -71,12 +97,17 @@ mod test_alt_name {
     fn test_encode_decode_alt_name_text() {
         let mut buffer = Vec::new();
         let mut encoder = Encoder::new(&mut buffer);
-      
+
         let alt_name = AltName::new(GeneralNamesOrText::Text("example.com".to_string()));
         alt_name
             .encode(&mut encoder, &mut ())
             .expect("Failed to encode AltName");
         // "example.com": 0x6b6578616d706c652e636f6d
         assert_eq!(hex::encode(buffer.clone()), "6b6578616d706c652e636f6d");
+
+        let mut decoder = Decoder::new(&buffer);
+        let decoded_alt_name =
+            AltName::decode(&mut decoder, &mut ()).expect("Failed to decode Alternative Name");
+        assert_eq!(decoded_alt_name, alt_name);
     }
 }
