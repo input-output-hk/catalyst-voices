110758821c11ad1540843f07db39692c36344ae9
bkioshn <bkioshn@gmail.com>
2024-07-10 10:22:40 +0700

    chore: fix test comment

--- catalyst-gateway-crates/c509-certificate/src/c509_general_names/mod.rs
+++ catalyst-gateway-crates/c509-certificate/src/c509_general_names/mod.rs
@@ -113,6 +113,7 @@ mod test_general_names {
         ));
         gns.encode(&mut encoder, &mut ())
             .expect("Failed to encode GeneralNames");
+        // Array of 4 GeneralName: 0x84
         assert_eq!(hex::encode(buffer.clone()), "84026b6578616d706c652e636f6d20824960864801650304020144010203040744c0a801010849608648016503040201");
 
         let mut decoder = Decoder::new(&buffer);
@@ -141,6 +142,8 @@ mod test_general_names {
         ));
         gns.encode(&mut encoder, &mut ())
             .expect("Failed to encode GeneralNames");
+        // Array of 3 GeneralName: 0x83
+        // DNSName with "example.com": 0x026b6578616d706c652e636f6d
         assert_eq!(
             hex::encode(buffer.clone()),
             "83026b6578616d706c652e636f6d026b6578616d706c652e636f6d026b6578616d706c652e636f6d"
