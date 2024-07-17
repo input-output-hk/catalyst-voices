f41f8b2e3e32813be6cfa1c09d57375d45644890
bkioshn <bkioshn@gmail.com>
2024-07-17 20:11:22 +0700

    fix: lib generate and verify functionality and docs

--- catalyst-gateway-crates/c509-certificate/src/lib.rs
+++ catalyst-gateway-crates/c509-certificate/src/lib.rs
@@ -35,9 +35,11 @@
 //!
 //! Please refer to the [C509 Certificate](https://datatracker.ietf.org/doc/draft-ietf-cose-cbor-encoded-cert/09/) for more information.
 
-use anyhow::Error;
+use anyhow::anyhow;
 use c509::C509;
+use minicbor::{Decode, Encode};
 use signing::{PrivateKey, PublicKey};
+use tbs_cert::TbsCert;
 pub mod c509;
 pub mod c509_algo_identifier;
 pub mod c509_attributes;
@@ -53,46 +55,66 @@ pub mod signing;
 mod tables;
 pub mod tbs_cert;
 
-/// Generate a signed C509 certificate.
+/// Generate a signed or unsigned C509 certificate.
 ///
 /// # Arguments
-/// - `tbs_cert` - A cbor encoded TBS certificate.
-/// - `private_key` - The private key used to sign the certificate.
+/// - `tbs_cert` - A TBS certificate.
+/// - `private_key` - An optional private key, if provided certificate is signed.
 ///
 /// # Returns
-/// Returns a signed C509 certificate.
+/// Returns a signed or unsigned C509 certificate.
 ///
 /// # Errors
 ///
-/// Returns an error if tne sign data cannot be converted to CBOR bytes.
-pub fn generate_signed_c509_cert(
-    tbs_cert: Vec<u8>, private_key: &PrivateKey,
-) -> Result<C509, Error> {
-    let mut buffer = Vec::new();
-    let mut encoder = minicbor::Encoder::new(&mut buffer);
-    let sign_data = private_key.sign(&tbs_cert);
-    encoder.bytes(&sign_data)?;
-    Ok(C509::new(tbs_cert, buffer))
+/// Returns an error if tne data cannot be converted to CBOR bytes.
+
+pub fn generate(tbs_cert: &TbsCert, private_key: Option<&PrivateKey>) -> anyhow::Result<Vec<u8>> {
+    // Encode the TbsCert
+    let encoded_tbs = {
+        let mut buffer = Vec::new();
+        let mut encoder = minicbor::Encoder::new(&mut buffer);
+        tbs_cert.encode(&mut encoder, &mut ())?;
+        buffer
+    };
+    let sign_data = private_key.map(|pk| pk.sign(&encoded_tbs));
+
+    // Encode the whole C509 certificate including `TbSCert` and `issuerSignatureValue`
+    let encoded_c509 = {
+        let mut buffer = Vec::new();
+        let mut encoder = minicbor::Encoder::new(&mut buffer);
+        let c509 = C509::new(tbs_cert.clone(), sign_data);
+        c509.encode(&mut encoder, &mut ())?;
+        buffer
+    };
+    Ok(encoded_c509)
 }
 
 /// Verify the signature of a C509 certificate.
 ///
 /// # Arguments
-/// - `c509` - The C509 certificate to verify.
+/// - `c509` - The cbor encoded C509 certificate to verify.
 /// - `public_key` - The public key used to verify the certificate.
 ///
 /// # Errors
 /// Returns an error if the `issuer_signature_value` is invalid or the signature cannot be
 /// verified.
-pub fn verify_c509_signature(c509: &C509, public_key: &PublicKey) -> Result<(), Error> {
-    let mut decoder = minicbor::Decoder::new(c509.get_issuer_signature_value());
-    let signature = decoder.bytes()?;
-    public_key.verify(c509.get_tbs_cert(), signature)
+pub fn verify(c509: &[u8], public_key: &PublicKey) -> anyhow::Result<()> {
+    let mut d = minicbor::Decoder::new(c509);
+    let c509 = C509::decode(&mut d, &mut ())?;
+    let mut encoded_tbs = Vec::new();
+    let mut encoder = minicbor::Encoder::new(&mut encoded_tbs);
+    c509.get_tbs_cert().encode(&mut encoder, &mut ())?;
+    let issuer_sig = c509
+        .get_issuer_signature_value()
+        .clone()
+        .ok_or(anyhow!("No issuer signature"))?;
+    public_key.verify(&encoded_tbs, &issuer_sig)
 }
 
 #[cfg(test)]
 mod test {
-    use minicbor::Encode;
+    use std::str::FromStr;
+
     use signing::tests::private_key_str;
     use tbs_cert::test_tbs_cert::tbs;
 
@@ -101,19 +123,12 @@ mod test {
     #[test]
     fn test_generate_and_verify_signed_c509_cert() {
         let tbs_cert = tbs();
-        let mut buffer = Vec::new();
-        let mut encoder = minicbor::Encoder::new(&mut buffer);
 
-        tbs_cert
-            .encode(&mut encoder, &mut ())
-            .expect("Failed to encode TbsCertificate");
+        let private_key = FromStr::from_str(&private_key_str()).expect("Cannot create private key");
 
-        let private_key =
-            PrivateKey::from_str(&private_key_str()).expect("Cannot create private key");
-
-        let c509 = generate_signed_c509_cert(buffer, &private_key)
+        let signed_c509 = generate(&tbs_cert, Some(&private_key))
             .expect("Failed to generate signed C509 certificate");
 
-        assert!(verify_c509_signature(&c509, &private_key.public_key()).is_ok());
+        assert!(verify(&signed_c509, &private_key.public_key()).is_ok());
     }
 }
