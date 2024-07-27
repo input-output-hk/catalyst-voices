82d92a0adcaa8d952ec6d68e84a36fb9575f14f6
bkioshn <bkioshn@gmail.com>
2024-07-27 14:37:03 +0700

    fix: update index.js

--- catalyst-gateway-crates/c509-certificate/examples/web/index.js
+++ catalyst-gateway-crates/c509-certificate/examples/web/index.js
@@ -24,16 +24,16 @@ const tbs = {
 	c509_certificate_type: 0,
 	certificate_serial_number: 1000000n,
 	issuer: {
-		RelativeDistinguishedName: [
+		relative_distinguished_name: [
 			{
 				oid: "2.5.4.3",
-				value: [{ Text: "RFC test CA" }],
+				value: [{ text: "RFC test CA" }],
 			},
 		],
 	},
 	validity_not_before: 1_672_531_200n,
 	validity_not_after: 1_767_225_600n,
-	subject: { Text: "01-23-45-ff-fe-67-89-AB" },
+	subject: { text: "01-23-45-ff-fe-67-89-AB" },
 	subject_public_key_algorithm: {
 		oid: "1.3.101.112",
 	},
@@ -41,7 +41,7 @@ const tbs = {
 	extensions: [
 		{
 			oid: "2.5.29.19",
-			value: { Int: -2n },
+			value: { int: -2n },
 			critical: false,
 		},
 	],
@@ -58,6 +58,7 @@ async function run() {
 
 	// Call the generate with private key to create a signed version
 	let c509 = generate(tbs, sk);
+	console.log(c509);
 	// Verify the generated C509 with the public key
 	console.log(verify(c509, pk));
 	// Decode the generated C509 back to readable format
