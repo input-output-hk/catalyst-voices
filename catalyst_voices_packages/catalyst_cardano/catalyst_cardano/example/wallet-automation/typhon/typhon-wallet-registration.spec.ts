3f469e1da8cd148f9f81f0634db95f2e965b4944
SotaTek-DuyLe <duy.le@iohk.com>
2024-07-29 18:25:59 +0700

    fix: get wallet balance

--- catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/wallet-automation/typhon/typhon-wallet-registration.spec.ts
+++ catalyst_voices_packages/catalyst_cardano/catalyst_cardano/example/wallet-automation/typhon/typhon-wallet-registration.spec.ts
@@ -1,4 +1,4 @@
-import { test, chromium, expect } from '@playwright/test';
+import { test, chromium, expect, Page } from '@playwright/test';
 import { getWalletCredentials, getRegistrationPin } from './credentials';
 import { getSeedPhrase } from './seed-phrase';
 import { waitForDebugger } from 'inspector';
@@ -7,7 +7,9 @@ import fs from 'fs-extra';
 
 // extension ID for Typhon: kfdniefadaanbjodldohaedphafoffoh
 
-test('import wallet', async ({ }) => {
+let newTab: Page
+
+test.beforeAll(async () => {
     const extensionPath: string = path.resolve(__dirname, 'extensions/KFDNIEFADAANBJODLDOHAEDPHAFOFFOH_unzipped');
     const browser = await chromium.launchPersistentContext('', {
         headless: false, // extensions only work in headful mode
@@ -21,7 +23,7 @@ test('import wallet', async ({ }) => {
         return browser.pages().length;
     }, { timeout: 5000 }).toBe(2);
     const pages = browser.pages();
-    const newTab = pages[pages.length - 1];
+    newTab = pages[pages.length - 1];
     await newTab.bringToFront();
 
     // interact with elements on the background page
@@ -54,82 +56,67 @@ test('import wallet', async ({ }) => {
 
     await clickBlankSpace(newTab);
     await newTab.getByRole('button', { name: 'Unlock Wallet' }).click();
+    
+    try {
+        await newTab.waitForSelector('//*[@id="lc"]/div[2]/div[2]/div[2]/div[1]/div[2]', { timeout: 5000 });
+        const textContent = await newTab.$eval('//*[@id="lc"]/div[2]/div[2]/div[2]/div[1]/div[2]', el => el.textContent);
 
-    const elementHandle = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/div[1]/div/span[1]');
+        if (textContent) {
+            console.log("registered for voting successfully!");
+        } else {
+            console.log('text content not found');
+        }
+    } catch (error) {
+        console.error('an error occurred:', error.toString());
+        console.log('an error occurred');
+    }
+
+    await newTab.waitForTimeout(5000);
+    await newTab.goto('http://localhost:8000/');
+    await newTab.locator('//*[text()="Enable wallet"]').click();
+
+    await expect.poll(async () => {
+        return browser.pages().length;
+    }, { timeout: 5000 }).toBe(3);
+
+    const updatedPages = browser.pages();
+    const allowTab = updatedPages[updatedPages.length - 1];
+    await allowTab.bringToFront();
+
+    await allowTab.getByRole('button', { name: 'Allow' }).click();
+    await newTab.waitForTimeout(10000);
+});
+
+
+test('get wallet balance', async ({ }) => {
+    const elementHandle = await newTab.waitForSelector('#flt-semantic-node-13', { timeout: 5000 });
+
     if (elementHandle) {
-        // retrieve the text content of the element
+        // Retrieve the text content of the element
         const textContent = await elementHandle.textContent();
+
         if (textContent !== null) {
-            // remove any formatting that might interfere with parseFloat
-            const cleanedText = textContent.replace(/,/g, '').trim();
-            const floatValue = parseFloat(cleanedText);
-            console.log('ADA:', floatValue)
-            if (!isNaN(floatValue)) {
-                if (floatValue < 500) {
+            // Extract the balance from the text content
+            const match = textContent.match(/Balance: Ada \(lovelaces\): (\d+)/);
+            if (match && match[1]) {
+                const balance = ((parseInt(match[1], 10))/1_000_000).toFixed(2);
+                console.log('ADA (lovelaces):', balance);
+
+                if (parseInt(balance) < 500) {
                     console.log('not eligible for voting ☹️');
                 } else {
                     console.log('eligible for voting ☺');
                 }
             } else {
-                console.log('text content is not a valid float:', textContent);
+                console.log('balance not found in text content:', textContent);
             }
         } else {
             console.log('no text content found for the specified selector:', elementHandle);
         }
     } else {
-        console.log('element not found for the specified XPath:', elementHandle);
+        console.log('element not found for the specified selector');
     }
-
-    // Voting Registration
-    // await newTab.getByText('Voting').click();
-    // await newTab.getByRole('button', { name: 'Register for voting' }).click();
-    // await newTab.getByRole('button', { name: 'Continue' }).click();
-
-    // const RegistrationPin = getRegistrationPin('WALLET1');
-    // const pinReg1 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[1]');
-    // await pinReg1.fill(RegistrationPin.one);
-
-    // const pinReg2 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[2]');
-    // await pinReg2.fill(RegistrationPin.two);
-
-    // const pinReg3 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[3]');
-    // await pinReg3.fill(RegistrationPin.one);
-
-    // const pinReg4 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[4]');
-    // await pinReg4.fill(RegistrationPin.one);
-    // await newTab.getByRole('button', { name: 'Continue' }).click();
-
-    // Confirm password change
-    // const pinConfirm1 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[1]');
-    // await pinConfirm1.fill(RegistrationPin.one);
-
-    // const pinConfirm2 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[2]');
-    // await pinConfirm2.fill(RegistrationPin.two);
-
-    // const pinConfirm3 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[3]');
-    // await pinConfirm3.fill(RegistrationPin.one);
-
-    // const pinConfirm4 = newTab.locator('xpath=//*[@id="lc"]/div[2]/div[2]/div[1]/div[2]/div[1]/div/div[1]/input[4]');
-    // await pinConfirm4.fill(RegistrationPin.one);
-    // await newTab.getByRole('button', { name: 'Continue' }).click();
-
-    // await newTab.getByRole('button', { name: 'confirm' }).click();
-    // await newTab.getByPlaceholder('Password', { exact: true }).fill(WalletCredentials.password);
-    // await newTab.getByRole('button', { name: 'confirm' }).click();
-
-    // try {
-    //     await newTab.waitForSelector('//*[@id="lc"]/div[2]/div[2]/div[2]/div[1]/div[2]', { timeout: 5000 });
-    //     const textContent = await newTab.$eval('//*[@id="lc"]/div[2]/div[2]/div[2]/div[1]/div[2]', el => el.textContent);
-
-    //     if (textContent) {
-    //         console.log("registered for voting successfully!");
-    //     } else {
-    //         console.log('text content not found');
-    //     }
-    // } catch (error) {
-    //     console.error('an error occurred:', error.toString());
-    //     console.log('an error occurred');
-    // }
+});
 
     // Logout 
     // const logOut = '//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[2]/div[11]/div[2]';
@@ -144,19 +131,3 @@ test('import wallet', async ({ }) => {
     // await newTab.locator('button.btn.bg-primary').click();
 
     // const addNew = '//*[@id="app"]/div/div/div[3]/div/div[2]/div/div/div[4]';
-    // await newTab.locator(addNew).click();
-
-    await newTab.goto('http://localhost:8000/');
-
-    await newTab.locator('//*[text()="Enable wallet"]').click();
-
-    await expect.poll(async () => {
-        return browser.pages().length;
-    }, { timeout: 5000 }).toBe(3);
-
-    const updatedPages = browser.pages();
-    const allowTab = updatedPages[updatedPages.length - 1];
-    await allowTab.bringToFront();
-
-    await allowTab.getByRole('button', { name: 'Allow' }).click();
-});
