2686fc177f926a245df6be4cd3f7f95a4128be3b
Damian Molinski <damian.molinski@iohk.io>
2024-07-31 09:52:23 +0200

    refactor: change widgets folders from voices_[name] to just [name] to be consistent

--- catalyst_voices/lib/widgets/app_bar/voices_app_bar.dart
+++ catalyst_voices/lib/widgets/app_bar/voices_app_bar.dart
@@ -1,4 +1,4 @@
-import 'package:catalyst_voices/widgets/voices_app_bar/actions/voices_app_bar_actions.dart';
+import 'package:catalyst_voices/widgets/app_bar/actions/voices_app_bar_actions.dart';
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
