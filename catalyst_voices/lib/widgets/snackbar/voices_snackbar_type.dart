7082fd302f615218ff320b32fb17097519ce76eb
minikin <djminikin@gmail.com>
2024-08-01 12:07:14 +0200

    Update voices_snackbar_type.dart

--- catalyst_voices/lib/widgets/snackbar/voices_snackbar_type.dart
+++ catalyst_voices/lib/widgets/snackbar/voices_snackbar_type.dart
@@ -3,97 +3,76 @@ import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:flutter/material.dart';
 
-enum VoicesSnackBarType {
-  info,
-  success,
-  warning,
-  error;
+enum VoicesSnackBarType { info, success, warning, error }
 
-  Color? backgroundColor(BuildContext context) {
-    final colors = Theme.of(context).colors;
+class _SnackBarData {
+  final IconData icon;
+  final Color? Function(VoicesColorScheme) iconColor;
+  final Color? Function(VoicesColorScheme) titleColor;
+  final Color? Function(VoicesColorScheme) backgroundColor;
+  final String Function(VoicesLocalizations) message;
+  final String Function(VoicesLocalizations) title;
 
-    switch (this) {
-      case VoicesSnackBarType.info:
-        return colors.primaryContainer;
-      case VoicesSnackBarType.success:
-        return colors.successContainer;
-      case VoicesSnackBarType.warning:
-        return colors.warningContainer;
-      case VoicesSnackBarType.error:
-        return colors.errorContainer;
-    }
-  }
+  const _SnackBarData({
+    required this.icon,
+    required this.iconColor,
+    required this.titleColor,
+    required this.backgroundColor,
+    required this.message,
+    required this.title,
+  });
+}
 
-  IconData icon(BuildContext context) {
-    switch (this) {
-      case VoicesSnackBarType.info:
-        return CatalystVoicesIcons.information_circle;
-      case VoicesSnackBarType.success:
-        return CatalystVoicesIcons.check_circle;
-      case VoicesSnackBarType.warning:
-        return CatalystVoicesIcons.exclamation;
-      case VoicesSnackBarType.error:
-        return CatalystVoicesIcons.exclamation_circle;
-    }
-  }
+extension VoicesSnackBarTypeExtension on VoicesSnackBarType {
+  static final Map<VoicesSnackBarType, _SnackBarData> _data = {
+    VoicesSnackBarType.info: _SnackBarData(
+      icon: CatalystVoicesIcons.information_circle,
+      iconColor: (colors) => colors.iconsPrimary,
+      titleColor: (colors) => colors.onPrimaryContainer,
+      backgroundColor: (colors) => colors.primaryContainer,
+      message: (l10n) => l10n.snackbarInfoMessageText,
+      title: (l10n) => l10n.snackbarInfoLabelText,
+    ),
+    VoicesSnackBarType.success: _SnackBarData(
+      icon: CatalystVoicesIcons.check_circle,
+      iconColor: (colors) => colors.iconsSuccess,
+      titleColor: (colors) => colors.onSuccessContainer,
+      backgroundColor: (colors) => colors.successContainer,
+      message: (l10n) => l10n.snackbarSuccessMessageText,
+      title: (l10n) => l10n.snackbarSuccessLabelText,
+    ),
+    VoicesSnackBarType.warning: _SnackBarData(
+      icon: CatalystVoicesIcons.exclamation,
+      iconColor: (colors) => colors.iconsWarning,
+      titleColor: (colors) => colors.onWarningContainer,
+      backgroundColor: (colors) => colors.warningContainer,
+      message: (l10n) => l10n.snackbarWarningMessageText,
+      title: (l10n) => l10n.snackbarWarningLabelText,
+    ),
+    VoicesSnackBarType.error: _SnackBarData(
+      icon: CatalystVoicesIcons.exclamation_circle,
+      iconColor: (colors) => colors.iconsError,
+      titleColor: (colors) => colors.onErrorContainer,
+      backgroundColor: (colors) => colors.errorContainer,
+      message: (l10n) => l10n.snackbarErrorMessageText,
+      title: (l10n) => l10n.snackbarErrorLabelText,
+    ),
+  };
 
-  Color? iconColor(BuildContext context) {
-    final colors = Theme.of(context).colors;
+  _SnackBarData get _snackBarData => _data[this]!;
 
-    switch (this) {
-      case VoicesSnackBarType.info:
-        return colors.iconsPrimary;
-      case VoicesSnackBarType.success:
-        return colors.iconsSuccess;
-      case VoicesSnackBarType.warning:
-        return colors.iconsWarning;
-      case VoicesSnackBarType.error:
-        return colors.iconsError;
-    }
-  }
+  Color? backgroundColor(BuildContext context) =>
+      _snackBarData.backgroundColor(Theme.of(context).colors);
 
-  String message(BuildContext context) {
-    final l10n = context.l10n;
+  IconData icon(BuildContext context) => _snackBarData.icon;
 
-    switch (this) {
-      case VoicesSnackBarType.info:
-        return l10n.snackbarInfoMessageText;
-      case VoicesSnackBarType.success:
-        return l10n.snackbarSuccessMessageText;
-      case VoicesSnackBarType.warning:
-        return l10n.snackbarWarningMessageText;
-      case VoicesSnackBarType.error:
-        return l10n.snackbarErrorMessageText;
-    }
-  }
+  Color? iconColor(BuildContext context) =>
+      _snackBarData.iconColor(Theme.of(context).colors);
 
-  String title(BuildContext context) {
-    final l10n = context.l10n;
+  String message(BuildContext context) => _snackBarData.message(context.l10n);
 
-    switch (this) {
-      case VoicesSnackBarType.info:
-        return l10n.snackbarInfoLabelText;
-      case VoicesSnackBarType.success:
-        return l10n.snackbarSuccessLabelText;
-      case VoicesSnackBarType.warning:
-        return l10n.snackbarWarningLabelText;
-      case VoicesSnackBarType.error:
-        return l10n.snackbarErrorLabelText;
-    }
-  }
+  String title(BuildContext context) => _snackBarData.title(context.l10n);
 
-  Color? titleColor(BuildContext context) {
-    final colors = Theme.of(context).colors;
-
-    switch (this) {
-      case VoicesSnackBarType.info:
-        return colors.onPrimaryContainer;
-      case VoicesSnackBarType.success:
-        return colors.onSuccessContainer;
-      case VoicesSnackBarType.warning:
-        return colors.onWarningContainer;
-      case VoicesSnackBarType.error:
-        return colors.onErrorContainer;
-    }
-  }
+  Color? titleColor(BuildContext context) =>
+      _snackBarData.titleColor(Theme.of(context).colors);
 }
