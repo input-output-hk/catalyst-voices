73af28e384d9d4401e1e89993e8d1485b7be9ac3
Dominik Toton <dominik.toton@iohk.io>
2024-08-07 22:21:19 +0200

    chore!: remove federated plugin structure from catalyst_cose

--- catalyst_voices_packages/catalyst_cose/catalyst_cose_web/lib/src/interop/catalyst_cose_interop.dart
+++
@@ -1,8 +0,0 @@
-@JS('catalyst_cose')
-library catalyst_cose_interop;
-
-import 'dart:js_interop';
-
-/// Signs the message [bytes] and returns a resulting COSE signature.
-@JS()
-external JSPromise<JSString> signMessage(JSString bytes);
