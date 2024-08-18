ee4f8eadd9483e9e54be353ee0cf7fe27cc092e6
cong-or <conchubhar.gannon@gmail.com>
2024-08-18 21:34:31 +0100

    feat(auth token encode and decode): permissionless auth

---
+++ catalyst-gateway/bin/src/service/api/auth/mod.rs
@@ -0,0 +1,127 @@
+use anyhow::Ok;
+use base64::{prelude::BASE64_STANDARD, Engine};
+use ed25519_dalek::{Signer, SigningKey, SECRET_KEY_LENGTH, SIGNATURE_LENGTH};
+use pallas::codec::minicbor;
+
+/// Key ID - Blake2b-128 hash of the Role 0 Certificate defining the Session public key.
+/// BLAKE2b-128 produces digest side of 16 bytes.
+#[derive(Debug)]
+pub struct Kid([u8; 16]);
+
+/// Identifier for this token, encodes both the time the token was issued and a random
+/// nonce.
+#[derive(Debug)]
+pub struct UlidBytes([u8; 16]);
+
+// Ed25519 signatures are (64 bytes)
+#[allow(dead_code)]
+pub struct SignatureEd25519([u8; 64]);
+
+/// The Encoded Binary Auth Token is a [CBOR sequence] that consists of 3 fields.
+/// A CBOR Sequence consists of any number of encoded CBOR data items, simply concatenated
+/// in sequence. `kid` : The key identifier - 16 bytes .
+/// `ulid` : A ULID which defines when the token was issued, and a random nonce - 16
+/// bytes. `signature` : The signature over the `kid` and `ulid` fields - 64 bytes.
+/// kid + ulid + sig = 96 bytes
+#[allow(dead_code)]
+pub fn encode_auth_token_ed25519(
+    kid: Kid, ulid: UlidBytes, secret_key_bytes: [u8; SECRET_KEY_LENGTH],
+) -> anyhow::Result<String> {
+    const AUTH_TOKEN_PREFIX: &str = "catv1";
+
+    let sk: SigningKey = SigningKey::from_bytes(&secret_key_bytes);
+
+    let signature: [u8; SIGNATURE_LENGTH] = sk.sign(&[kid.0, ulid.0].concat()).to_bytes();
+
+    let out: Vec<u8> = Vec::new();
+    let mut encoder = minicbor::Encoder::new(out);
+
+    encoder.bytes(&kid.0)?;
+    encoder.bytes(&ulid.0)?;
+    encoder.bytes(&signature)?;
+
+    Ok(format!(
+        "{}.{}",
+        AUTH_TOKEN_PREFIX,
+        BASE64_STANDARD.encode(&encoder.writer())
+    ))
+}
+
+/// Decode base64 auth token into constituent parts of (kid, ulid, signature)
+/// e.g catv1.UAARIjNEVWZ3iJmqu8zd7v9QAZEs7HHPLEwUpV1VhdlNe1hAAAAAAAAAAAAA...
+#[allow(dead_code)]
+pub fn decode_auth_token_ed25519(
+    auth_token: String,
+) -> anyhow::Result<(Kid, UlidBytes, SignatureEd25519)> {
+    const AUTH_TOKEN_PREFIX: &str = "catv1";
+
+    let token = auth_token.split(".").collect::<Vec<&str>>();
+
+    let prefix = token.get(0).ok_or(anyhow::anyhow!("No valid prefix"))?;
+    if *prefix != AUTH_TOKEN_PREFIX {
+        return Err(anyhow::anyhow!("Corrupt token, invalid prefix"));
+    } else {
+        let token_base64 = token.get(1).ok_or(anyhow::anyhow!("No valid token"))?;
+        let decoded_token = BASE64_STANDARD.decode(token_base64)?;
+
+        let mut cbor_decoder = minicbor::Decoder::new(&decoded_token);
+
+        let kid = Kid(cbor_decoder
+            .bytes()
+            .map_err(|e| anyhow::anyhow!(format!("invalid cbor for kid : {e}")))?
+            .try_into()?);
+
+        let ulid = UlidBytes(
+            cbor_decoder
+                .bytes()
+                .map_err(|e| anyhow::anyhow!(format!("invalid cbor for ulid : {e}")))?
+                .try_into()?,
+        );
+
+        let signature = SignatureEd25519(
+            cbor_decoder
+                .bytes()
+                .map_err(|e| anyhow::anyhow!(format!("invalid cbor for sig : {e}")))?
+                .try_into()?,
+        );
+
+        Ok((kid, ulid, signature))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use ed25519_dalek::{Signature, SECRET_KEY_LENGTH};
+
+    use super::{encode_auth_token_ed25519, Kid, UlidBytes};
+    use crate::service::api::auth::decode_auth_token_ed25519;
+
+    #[test]
+    fn test_token_generation() {
+        let kid: [u8; 16] = hex::decode("00112233445566778899aabbccddeeff".to_string())
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let ulid: [u8; 16] = hex::decode("01912cec71cf2c4c14a55d5585d94d7b".to_string())
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+        let secret_key_bytes: [u8; SECRET_KEY_LENGTH] = [0; 32];
+
+        let auth_token =
+            encode_auth_token_ed25519(Kid(kid), UlidBytes(ulid), secret_key_bytes).unwrap();
+
+        assert_eq!(auth_token,"catv1.UAARIjNEVWZ3iJmqu8zd7v9QAZEs7HHPLEwUpV1VhdlNe1hAm8DrLsNgd7vEwuCx1g6kxmBsaZVG6l+I5FSP6ie/fF/wp4+NBn/mDtdq6a9xQLczyLkyixXR7mxZIx3+5ahzBQ==");
+
+        let decoded_token = decode_auth_token_ed25519(auth_token).unwrap();
+
+        assert_eq!(decoded_token.0 .0, kid);
+        assert_eq!(decoded_token.1 .0, ulid);
+
+        let sig = Signature::from_bytes(&decoded_token.2 .0);
+
+        println!("signature {:?}", hex::encode(sig.to_vec()));
+    }
+}
