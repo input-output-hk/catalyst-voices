a0022ed473fe9c0310499c5220d4ae3870de045a
Jakub Szwiec <kuba.szwiec@gmail.com>
2024-08-22 12:43:16 +0200

    feat: menu - more scalable syntax

--- catalyst_voices/uikit_example/lib/examples/voices_menu_example.dart
+++ catalyst_voices/uikit_example/lib/examples/voices_menu_example.dart
@@ -12,32 +12,78 @@ class VoicesMenuExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Voices Menu')),
-      body: Center(
-        child: VoicesMenu(
-          onSelected: (label) => debugPrint('Selected label: $label'),
-          items: [
-            MenuItem(
-              label: 'Rename',
-              icon: const Icon(CatalystVoicesIcons.pencil, size: 20),
-            ),
-            SubMenuItem(
-              label: 'Move Private Team',
-              icon: const Icon(CatalystVoicesIcons.cube, size: 20),
-              children: [
-                MenuItem(label: 'Team 1: The Vikings'),
-                MenuItem(label: 'Team 2: Pure Hearts'),
-              ],
-            ),
-            MenuItem(
-              label: 'Move to public',
-              icon: const Icon(CatalystVoicesIcons.cube, size: 20),
-            ),
-            MenuItem(
-              label: 'Delete',
-              icon: const Icon(CatalystVoicesIcons.trash, size: 20),
-            ),
-          ],
-        ),
+      body: Row(
+        children: [
+          Column(
+            children: [
+              VoicesMenu(
+                  onSelected: (label) => debugPrint('Selected label: $label'),
+                  menuItems: [
+                    MenuItem(
+                      label: 'Rename',
+                      icon: const Icon(CatalystVoicesIcons.pencil, size: 20),
+                    ),
+                    SubMenuItem(
+                      label: 'Move Private Team',
+                      icon: const Icon(CatalystVoicesIcons.cube, size: 20),
+                      children: [
+                        MenuItem(label: 'Team 1: The Vikings'),
+                        MenuItem(label: 'Team 2: Pure Hearts'),
+                      ],
+                    ),
+                    MenuItem(
+                      label: 'Move to public',
+                      icon: const Icon(CatalystVoicesIcons.cube, size: 20),
+                    ),
+                    MenuItem(
+                      label: 'Delete',
+                      icon: const Icon(CatalystVoicesIcons.trash, size: 20),
+                    ),
+                  ],
+                  child: const SizedBox(
+                    height: 56,
+                    width: 200,
+                    child: ListTile(
+                      title: Text('My first proposal'),
+                    ),
+                  )),
+              VoicesMenu(
+                  onSelected: (label) => debugPrint('Selected label: $label'),
+                  menuItems: [
+                    MenuItem(
+                      label: 'Rename',
+                      icon: const Icon(CatalystVoicesIcons.pencil, size: 20),
+                    ),
+                    SubMenuItem(
+                      label: 'Move Private Team',
+                      icon: const Icon(CatalystVoicesIcons.cube, size: 20),
+                      children: [
+                        MenuItem(label: 'Team 1: The Vikings'),
+                        MenuItem(label: 'Team 2: Pure Hearts'),
+                      ],
+                    ),
+                    MenuItem(
+                      label: 'Move to public',
+                      icon: const Icon(CatalystVoicesIcons.cube, size: 20),
+                    ),
+                    MenuItem(
+                      label: 'Delete',
+                      icon: const Icon(CatalystVoicesIcons.trash, size: 20),
+                    ),
+                  ],
+                  child: const SizedBox(
+                    height: 56,
+                    width: 200,
+                    child: ListTile(
+                      title: Text('My second proposal'),
+                    ),
+                  )),
+            ],
+          ),
+          Expanded(
+            child: Container(),
+          )
+        ],
       ),
     );
   }
