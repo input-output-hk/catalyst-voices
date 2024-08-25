47063231d7bbdcb6fc001f360e7389ae503052b8
Pal Dorogi <pal.dorogi@gmail.com>
2024-08-25 21:06:20 +0200

    fix(catalyst_cardano_serialization): Add `Redeemer` class and allow malformed UTF-8 in `AssetName`

---
+++ catalyst_voices_packages/catalyst_cardano_serialization/lib/src/redeemer.dart
@@ -0,0 +1,27 @@
+import 'package:catalyst_cardano_serialization/catalyst_cardano_serialization.dart';
+import 'package:cbor/cbor.dart';
+import 'package:equatable/equatable.dart';
+
+// TODO(ilap): implement proper redeemer class, this is just a placeholder.
+/// Class representing a redeemer in Cardano.
+/// A Redeemer is a piece of data that is used to validate a plutus script in a
+/// transaction.
+///
+final class Redeemer extends Equatable implements CborEncodable {
+  /// redeemer placeholder,
+  final CborValue redeemer;
+
+  /// The default constructor for [Redeemer].
+  const Redeemer({required this.redeemer});
+
+  /// Factory constructor to create a [Redeemer] from a CBOR-encoded value.
+  /// The CBOR value is expected to be a list or a map.
+  factory Redeemer.fromCbor(CborValue value) => Redeemer(redeemer: value);
+
+  /// Converts the [Redeemer] to a CBOR-encoded value.
+  @override
+  CborValue toCbor() => redeemer;
+
+  @override
+  List<Object?> get props => [redeemer];
+}
