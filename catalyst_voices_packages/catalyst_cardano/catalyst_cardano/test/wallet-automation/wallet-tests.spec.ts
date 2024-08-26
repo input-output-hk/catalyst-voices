4391b976bdbde1a282806afc11311fb6551e983a
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-08-26 10:29:25 +0200

    wip

--- catalyst_voices_packages/catalyst_cardano/catalyst_cardano/test/wallet-automation/wallet-tests.spec.ts
+++ catalyst_voices_packages/catalyst_cardano/catalyst_cardano/test/wallet-automation/wallet-tests.spec.ts
@@ -1,20 +1,21 @@
 import { test, chromium, expect, BrowserContext, Page } from '@playwright/test';
-import { allowExtension, downloadExtension, importWallet} from './utils';
+import { allowExtension, downloadExtension, getWalletCredentials, importWallet} from './utils';
 
 let browser: BrowserContext;
 let extensionPath: string;
+let extTab: Page;
 
 [
  { name: 'Typhon', id: 'kfdniefadaanbjodldohaedphafoffoh' },
- { name: 'Lace', id: 'gafhhkghbfjjkeiendhlofajokpaflmk' },
+ //{ name: 'Lace', id: 'gafhhkghbfjjkeiendhlofajokpaflmk' },
   ].forEach(({ name, id }) => {
     test.describe(`Testing with ${name}`,() => {
 
         test.beforeAll(async () => {
             extensionPath = await downloadExtension(id);
-        });
+        //});
 
-        test.beforeEach(async () => {
+        //test.beforeEach(async () => {
             browser = await chromium.launchPersistentContext('', {
                 headless: false, // extensions only work in headful mode
                 args: [
@@ -26,7 +27,7 @@ let extensionPath: string;
             await expect.poll(async () => {
                 return browser.pages().length;
             }, { timeout: 5000 }).toBe(2);
-            const extTab = browser.pages()[1];
+            extTab = browser.pages()[1];
             await extTab.bringToFront();
             await importWallet(extTab,name);
             //TODO switch wait for wait on load page
@@ -49,10 +50,19 @@ let extensionPath: string;
 
         });
 
+        // Get and match text content
+        const matchTextContent = async (selector: string, regex: RegExp) => {
+            const textContent = await extTab.locator(selector).textContent({ timeout: 5000 });
+            expect(textContent).not.toBeNull();
+            const match = textContent!.match(regex);
+            expect(match).not.toBeNull();
+            return match![1].trim();
+        };
 
-        test('get wallet details' + name , async () => {
-
-            /* // Get wallet balance
+        test('Get wallet details for ' + name , async () => {
+            //TODO wait for load page
+            await extTab.waitForTimeout(5000);
+            // Get wallet balance
              const balanceTextContent = await matchTextContent('#flt-semantic-node-13', /Balance: Ada \(lovelaces\): (\d+)/);
              const balanceAda = (parseInt(balanceTextContent, 10) / 1_000_000).toFixed(2);
              expect(parseInt(balanceAda)).toBeGreaterThan(500);
@@ -71,7 +81,7 @@ let extensionPath: string;
              expect(matchTextContent('#flt-semantic-node-19', /Used addresses:\s*(\S+)/)).not.toBeNaN();
 
              // Get UTXOs info
-             const utxoTextContent = await newTab.locator('#flt-semantic-node-20').textContent({ timeout: 5000 });
+             const utxoTextContent = await extTab.locator('#flt-semantic-node-20').textContent({ timeout: 5000 });
              expect(utxoTextContent).not.toBeNull();
 
              const utxoLines = utxoTextContent!.split('\n').map(line => line.trim());
@@ -88,17 +98,16 @@ let extensionPath: string;
              expect(amount).not.toBeNaN();
 
              expect(parseInt(amountAda)).toBeGreaterThan(500);
-             await newTab.waitForTimeout(5000);
-         */});
+         });
 
-         /*async function openSignTab(buttonName: string) {
-             await newTab.getByRole('button', { name: buttonName }).click();
+        async function openSignTab(buttonName: string) {
+             await extTab.getByRole('button', { name: buttonName }).click();
              await expect.poll(async () => browser.pages().length, { timeout: 5000 }).toBe(3);
              const signPage = browser.pages();
              const signTab = signPage[signPage.length - 1];
              await signTab.bringToFront();
              return signTab;
-         }*/
+         }
 
          async function signData(signTab: Page, password: string) {
              await signTab.getByRole('button', { name: 'Sign' }).click();
@@ -106,27 +115,27 @@ let extensionPath: string;
              await signTab.getByRole('button', { name: 'confirm' }).click();
          }
 
-         test('Sign data' + name, async () => {/*
+         test('Sign data ' + name, async () => {
              const signTab = await openSignTab('Sign data');
              const WalletCredentials = await getWalletCredentials('WALLET1');
              await signData(signTab, WalletCredentials.password);
-             await expect(newTab.getByText('DataSignature')).toBeVisible();
-         */});
-
-
-         test('Sign and submit tx' + name, async () => {
-             /*const signTab = await openSignTab('Sign & submit tx')
-             const WalletCredentials = await getWalletCredentials('WALLET1');
-             await signData(signTab, WalletCredentials.password);
-             await expect(newTab.getByText('Tx hash')).toBeVisible();*/
+             await expect(extTab.getByText('DataSignature')).toBeVisible();
          });
 
-         /*
+
+         test('Sign and submit tx ' + name, async () => {
+             const signTab = await openSignTab('Sign & submit tx')
+             const WalletCredentials = await getWalletCredentials('WALLET1');
+             await signData(signTab, WalletCredentials.password);
+             await expect(extTab.getByText('Tx hash')).toBeVisible();
+         });
+
+
          test('Sign and submit RBAC tx', async () => {
              const signTab = await openSignTab('Sign & submit RBAC tx');
              const WalletCredentials = await getWalletCredentials('WALLET1');
              await signData(signTab, WalletCredentials.password);
-             await expect(newTab.getByText('Tx hash')).toBeVisible();
+             await expect(extTab.getByText('Tx hash')).toBeVisible();
          });
 
          test('Fail to Sign data with incorrect password', async () => {
@@ -149,12 +158,6 @@ let extensionPath: string;
              await signData(signTab, wrongPassword);
              await expect(signTab.getByText('Wrong password')).toBeVisible();
          });
-
-         test('Empty wallet', async ({ page }) => {
-             await page.goto('http://localhost:8000/', { waitUntil: 'load' });
-             await expect(page.getByText('There are no active wallet extensions')).toBeVisible();
-         });*/
-
 
     });
   });
