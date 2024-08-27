7fbd7345d32bae5626fdf57207bc79689e2ab429
cong-or <conchubhar.gannon@gmail.com>
2024-08-27 21:15:36 +0100

    feat(auth token): cache TTL

--- catalyst-gateway/bin/src/service/api/auth/endpoint.rs
+++ catalyst-gateway/bin/src/service/api/auth/endpoint.rs
@@ -1,4 +1,4 @@
-use std::sync::LazyLock;
+use std::{sync::LazyLock, time::Duration};
 
 use dashmap::DashMap;
 use ed25519_dalek::{Signature, VerifyingKey, PUBLIC_KEY_LENGTH};
@@ -17,10 +17,16 @@ pub type DecodedAuthToken = (Kid, UlidBytes, SignatureEd25519);
 pub type EncodedAuthToken = String;
 
 /// Cached auth tokens
-#[allow(dead_code)]
 static CACHE: LazyLock<Cache<EncodedAuthToken, DecodedAuthToken>> = LazyLock::new(|| {
-    const CACHE_LIMIT: u64 = 10_000;
-    Cache::new(CACHE_LIMIT)
+    let cache = Cache::builder()
+        // Time to live (TTL): 30 minutes
+        .time_to_live(Duration::from_secs(30 * 60))
+        // Time to idle (TTI):  5 minutes
+        .time_to_idle(Duration::from_secs(5 * 60))
+        // Create the cache.
+        .build();
+
+    cache
 });
 
 /// Mocked Valid certificates
@@ -47,6 +53,8 @@ static CERTS: LazyLock<DashMap<String, [u8; PUBLIC_KEY_LENGTH]>> = LazyLock::new
     checker = "checker_api_catalyst_auth"
 )]
 #[allow(dead_code)]
+/// Auth token security scheme
+/// Add to endpoint params e.g async fn endpoint(&self, auth: CatalystSecurityScheme)
 pub struct CatalystSecurityScheme(pub DecodedAuthToken);
 
 #[derive(Debug, thiserror::Error)]
@@ -59,48 +67,67 @@ impl ResponseError for AuthTokenError {
     }
 }
 
+/// When added to an endpoint, this hook is called per request to verify the bearer token is valid.
 async fn checker_api_catalyst_auth(
     _req: &Request, bearer: Bearer,
 ) -> poem::Result<DecodedAuthToken> {
-    // Decode bearer token
-    let (kid, ulid, sig, msg) = match decode_auth_token_ed25519(bearer.token) {
-        Ok((kid, ulid, sig, msg)) => (kid, ulid, sig, msg),
-        Err(err) => {
-            error!("Corrupt auth token: {:?}", err);
-            Err(AuthTokenError)?
-        },
-    };
+    if !CACHE.contains_key(&bearer.token) {
+        // Decode bearer token
+        let (kid, ulid, sig, msg) = match decode_auth_token_ed25519(bearer.token.clone()) {
+            Ok((kid, ulid, sig, msg)) => (kid, ulid, sig, msg),
+            Err(err) => {
+                error!("Corrupt auth token: {:?}", err);
+                Err(AuthTokenError)?
+            },
+        };
 
-    // Get pub key from CERTS state given decoded KID from decoded bearer token
-    let pub_key_bytes = match CERTS.get(&hex::encode(kid.0)) {
-        Some(cert) => cert.clone(),
-        None => {
-            error!("Invalid KID {:?}", kid);
-            Err(AuthTokenError)?
-        },
-    };
+        // Get pub key from CERTS state given decoded KID from decoded bearer token
+        let pub_key_bytes = match CERTS.get(&hex::encode(kid.0)) {
+            Some(cert) => cert.clone(),
+            None => {
+                error!("Invalid KID {:?}", kid);
+                Err(AuthTokenError)?
+            },
+        };
 
-    let public_key = match VerifyingKey::from_bytes(&pub_key_bytes) {
-        Ok(pub_key) => pub_key,
-        Err(err) => {
-            error!("Invalid public key: {:?}", err);
-            Err(AuthTokenError)?
-        },
-    };
+        let public_key = match VerifyingKey::from_bytes(&pub_key_bytes) {
+            Ok(pub_key) => pub_key,
+            Err(err) => {
+                error!("Invalid public key: {:?}", err);
+                Err(AuthTokenError)?
+            },
+        };
 
-    // Strictly verify a signature on a message with this keypair's public key.
-    match public_key.verify_strict(&msg, &Signature::from_bytes(&sig.0)) {
-        Ok(_) => (),
-        Err(err) => {
-            error!(
-                "Message {:?} was not signed by this pub key {:?} {:?}",
-                hex::encode(msg),
-                public_key,
-                err
-            );
-            Err(AuthTokenError)?
-        },
-    };
+        // Strictly verify a signature on a message with this keypair's public key.
+        match public_key.verify_strict(&msg, &Signature::from_bytes(&sig.0)) {
+            Ok(_) => (),
+            Err(err) => {
+                error!(
+                    "Message {:?} was not signed by this pub key {:?} {:?}",
+                    hex::encode(msg),
+                    public_key,
+                    err
+                );
+                Err(AuthTokenError)?
+            },
+        };
 
-    Ok((kid, ulid, sig))
+        // This entry will expire after 5 minutes (TTI) if there is no get().
+        CACHE
+            .insert(bearer.token, (kid.clone(), ulid.clone(), sig.clone()))
+            .await;
+
+        Ok((kid, ulid, sig))
+    } else {
+        // This get() will extend the entry life for another 5 minutes.
+        // Even though we keep calling get(), the entry will expire
+        // after 30 minutes (TTL) from the origin insert().
+        match CACHE.get(&bearer.token).await {
+            Some((kid, ulid, sig)) => Ok((kid, ulid, sig)),
+            None => {
+                error!("Auth token is not in the cache: {:?}", bearer.token);
+                Err(AuthTokenError)?
+            },
+        }
+    }
 }
--- catalyst-gateway/bin/src/service/api/health/mod.rs
+++ catalyst-gateway/bin/src/service/api/health/mod.rs
@@ -9,8 +9,6 @@ mod ready_get;
 mod started_get;
 pub(crate) use started_get::started;
 
-use super::auth::endpoint::CatalystSecurityScheme;
-
 /// Health API Endpoints
 pub(crate) struct HealthApi;
 
@@ -53,8 +51,7 @@ impl HealthApi {
     ///
     /// *This endpoint is for internal use of the service deployment infrastructure.
     /// It may not be exposed publicly. Refer to []*
-    async fn live_get(&self, auth: CatalystSecurityScheme) -> live_get::AllResponses {
-        let (_kid, _ulid, _sig) = auth.0;
+    async fn live_get(&self) -> live_get::AllResponses {
         live_get::endpoint().await
     }
 
