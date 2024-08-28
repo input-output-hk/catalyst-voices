87583a070ae732b05339e87cea231a775a697981
digitalheartxs <jakub.szwiec@scalac.io>
2024-08-28 12:44:26 +0200

    feat: VoicesRichText - add tests

---
+++ catalyst_voices/test/widgets/rich_text/voices_rich_text_test.dart
@@ -0,0 +1,20 @@
+import 'package:catalyst_voices/widgets/rich_text/voices_rich_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(VoicesRichText, () {
+    testWidgets('renders correctly', (tester) async {
+      // Given
+      final widget = MaterialApp(home: VoicesRichText());
+
+      // When
+      await tester.pumpWidget(widget);
+
+      // Then
+      expect(find.byType(QuillEditor), findsOneWidget);
+      expect(find.byType(QuillToolbar), findsOneWidget);
+    });
+  });
+}
