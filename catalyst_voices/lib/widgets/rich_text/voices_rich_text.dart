24429fd4085962f0353754205b9e34a34f1f76a1
digitalheartxs <jakub.szwiec@scalac.io>
2024-08-30 13:02:59 +0200

    feat: VoicesRichText - add limited set of actions to toolbar

--- catalyst_voices/lib/widgets/rich_text/voices_rich_text.dart
+++ catalyst_voices/lib/widgets/rich_text/voices_rich_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
 
 /// A component for rich text writing
 /// using Quill under the hood
@@ -24,14 +25,50 @@ class _VoicesRichTextState extends State<VoicesRichText> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        QuillToolbar.simple(
-          configurations:
-              QuillSimpleToolbarConfigurations(controller: _controller),
+        QuillToolbar(
+          configurations: const QuillToolbarConfigurations(),
+          child: Row(
+            children: [
+              QuillToolbarSelectHeaderStyleDropdownButton(
+                controller: _controller,
+              ),
+              QuillToolbarToggleStyleButton(
+                options: const QuillToolbarToggleStyleButtonOptions(),
+                controller: _controller,
+                attribute: Attribute.bold,
+              ),
+              QuillToolbarToggleStyleButton(
+                options: const QuillToolbarToggleStyleButtonOptions(),
+                controller: _controller,
+                attribute: Attribute.italic,
+              ),
+              QuillToolbarToggleStyleButton(
+                controller: _controller,
+                attribute: Attribute.ol,
+              ),
+              QuillToolbarToggleStyleButton(
+                controller: _controller,
+                attribute: Attribute.ul,
+                //options: QuillToolbarToggleStyleButtonOptions(iconData: Icons.abc),
+              ),
+              QuillToolbarIndentButton(
+                controller: _controller,
+                isIncrease: true,
+              ),
+              QuillToolbarIndentButton(
+                controller: _controller,
+                isIncrease: false,
+              ),
+              QuillToolbarImageButton(
+                controller: _controller,
+              ),
+            ],
+          ),
         ),
         Expanded(
           child: QuillEditor.basic(
+            controller: _controller,
             configurations: QuillEditorConfigurations(
-              controller: _controller,
               padding: const EdgeInsets.all(16),
               placeholder: 'Start writing your text...',
             ),
