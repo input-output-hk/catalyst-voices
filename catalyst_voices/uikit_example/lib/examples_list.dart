186fb71cc44d13c3b94aff7aafde65e8151dd579
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2024-08-30 14:04:06 +0200

    feat: TabBarStackView and SectionHeader (#730)
    * feat: TabBarStackView and SectionHeader
    
    * fix: add vsync to known words
    
    * chore: missing comma
    
    * chore: doc typo
    
    * refactor: more descriptive variable names in TabBarStackView state

--- catalyst_voices/uikit_example/lib/examples_list.dart
+++ catalyst_voices/uikit_example/lib/examples_list.dart
@@ -10,6 +10,7 @@ import 'package:uikit_example/examples/voices_buttons_example.dart';
 import 'package:uikit_example/examples/voices_checkbox_example.dart';
 import 'package:uikit_example/examples/voices_chip_example.dart';
 import 'package:uikit_example/examples/voices_fab_example.dart';
+import 'package:uikit_example/examples/voices_headers_examples.dart';
 import 'package:uikit_example/examples/voices_indicators_example.dart';
 import 'package:uikit_example/examples/voices_menu_example.dart';
 import 'package:uikit_example/examples/voices_navigation_example.dart';
@@ -20,6 +21,7 @@ import 'package:uikit_example/examples/voices_separators_example.dart';
 import 'package:uikit_example/examples/voices_snackbar_example.dart';
 import 'package:uikit_example/examples/voices_spaces_example.dart';
 import 'package:uikit_example/examples/voices_switch_example.dart';
+import 'package:uikit_example/examples/voices_tabs_example.dart';
 import 'package:uikit_example/examples/voices_text_field_example.dart';
 import 'package:uikit_example/examples/voices_tooltips_example.dart';
 
@@ -127,6 +129,16 @@ class ExamplesListPage extends StatelessWidget {
         title: 'Voices Menu',
         route: VoicesMenuExample.route,
         page: VoicesMenuExample(),
+      ),
+      ExampleTile(
+        title: 'Voices Tabs',
+        route: VoicesTabsExample.route,
+        page: VoicesTabsExample(),
+      ),
+      ExampleTile(
+        title: 'Voices Headers',
+        route: VoicesHeadersExamples.route,
+        page: VoicesHeadersExamples(),
       ),
     ];
   }
