120841c8960d345fec0dd8856ba81bc9b0602b5b
Damian Molinski <damian.molinski@iohk.io>
2024-09-05 14:42:11 +0200

    fix: Change initial page to coming_soon

--- catalyst_voices/lib/routes/routing/routes.dart
+++ catalyst_voices/lib/routes/routing/routes.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 /// Semantic anchor for generated routes so only this class
 /// knows how to work with them.
 abstract final class Routes {
-  static String get initialLocation => const TreasuryRoute().location;
+  static String get initialLocation => const ComingSoonRoute().location;
 
   static List<RouteBase> get routes => $appRoutes;
 }
