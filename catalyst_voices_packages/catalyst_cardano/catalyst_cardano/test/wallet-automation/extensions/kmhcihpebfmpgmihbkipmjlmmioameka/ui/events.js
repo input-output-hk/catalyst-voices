1f4533b6a36a8c35b2a58b1dbb048781483ca6b2
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2024-09-05 16:50:21 +0200

    Revert "feat: update cardano catalyst testplan"
    This reverts commit e19740fd5dde65107d54488ce2a4cd15bd8660fc.

--- catalyst_voices_packages/catalyst_cardano/catalyst_cardano/test/wallet-automation/extensions/kmhcihpebfmpgmihbkipmjlmmioameka/ui/events.js
+++
@@ -1,7 +0,0 @@
-import { j6 as requireEvents, dp as getDefaultExportFromCjs } from "./index.js";
-var eventsExports = requireEvents();
-const Vt$1 = /* @__PURE__ */ getDefaultExportFromCjs(eventsExports);
-export {
-  Vt$1 as V,
-  eventsExports as e
-};
