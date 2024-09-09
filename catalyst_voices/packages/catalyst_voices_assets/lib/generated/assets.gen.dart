2068c09e9b667de6358d1e51420bc8526618d531
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2024-09-09 08:36:19 +0200

    feat: funded proposal card (#772)
    * feat: add funded proposal card
    
    * chore: don't check spelling in GeneratedPluginRegistrant.swift
    
    * docs: fix
    
    * refactor: review feedback

--- catalyst_voices/packages/catalyst_voices_assets/lib/generated/assets.gen.dart
+++ catalyst_voices/packages/catalyst_voices_assets/lib/generated/assets.gen.dart
@@ -7,10 +7,10 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:vector_graphics/vector_graphics.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
@@ -71,6 +71,14 @@ class $AssetsImagesGen {
   /// File path: assets/images/node_open.svg
   SvgGenImage get nodeOpen => const SvgGenImage('assets/images/node_open.svg');
 
+  /// File path: assets/images/proposal_background_1.webp
+  AssetGenImage get proposalBackground1 =>
+      const AssetGenImage('assets/images/proposal_background_1.webp');
+
+  /// File path: assets/images/proposal_background_2.webp
+  AssetGenImage get proposalBackground2 =>
+      const AssetGenImage('assets/images/proposal_background_2.webp');
+
   /// File path: assets/images/view_grid.svg
   SvgGenImage get viewGrid => const SvgGenImage('assets/images/view_grid.svg');
 
@@ -97,6 +105,8 @@ class $AssetsImagesGen {
         linkedinMono,
         nodeClosed,
         nodeOpen,
+        proposalBackground1,
+        proposalBackground2,
         viewGrid,
         x,
         xMono
@@ -208,7 +218,7 @@ class SvgGenImage {
   final Set<String> flavors;
   final bool _isVecFormat;
 
-  SvgPicture svg({
+  _svg.SvgPicture svg({
     Key? key,
     bool matchTextDirection = false,
     AssetBundle? bundle,
@@ -221,29 +231,29 @@ class SvgGenImage {
     WidgetBuilder? placeholderBuilder,
     String? semanticsLabel,
     bool excludeFromSemantics = false,
-    SvgTheme? theme,
+    _svg.SvgTheme? theme,
     ColorFilter? colorFilter,
     Clip clipBehavior = Clip.hardEdge,
     @deprecated Color? color,
     @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
     @deprecated bool cacheColorFilter = false,
   }) {
-    final BytesLoader loader;
+    final _svg.BytesLoader loader;
     if (_isVecFormat) {
-      loader = AssetBytesLoader(
+      loader = _vg.AssetBytesLoader(
         _assetName,
         assetBundle: bundle,
         packageName: package,
       );
     } else {
-      loader = SvgAssetLoader(
+      loader = _svg.SvgAssetLoader(
         _assetName,
         assetBundle: bundle,
         packageName: package,
         theme: theme,
       );
     }
-    return SvgPicture(
+    return _svg.SvgPicture(
       loader,
       key: key,
       matchTextDirection: matchTextDirection,
