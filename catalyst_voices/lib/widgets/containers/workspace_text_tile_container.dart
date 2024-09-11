4dac324c0e5d0b256732d69741fc9757668dc67f
digitalheartxs <jakub.szwiec@scalac.io>
2024-09-11 21:47:24 +0200

    feat: Workspace - add shadow

--- catalyst_voices/lib/widgets/containers/workspace_text_tile_container.dart
+++ catalyst_voices/lib/widgets/containers/workspace_text_tile_container.dart
@@ -23,33 +23,43 @@ class WorkspaceTextTileContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return AnimatedContainer(
-      duration: kThemeChangeDuration,
-      decoration: BoxDecoration(
-        color: theme.colors.elevationsOnSurfaceNeutralLv1White,
-        borderRadius: BorderRadius.horizontal(
-          left: isSelected ? Radius.zero : Radius.circular(28),
-          right: Radius.circular(28),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: AnimatedContainer(
+        duration: kThemeChangeDuration,
+        decoration: BoxDecoration(
+          color: theme.colors.elevationsOnSurfaceNeutralLv1White,
+          borderRadius: BorderRadius.horizontal(
+            left: isSelected ? Radius.zero : Radius.circular(28),
+            right: Radius.circular(28),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colors.onSurfacePrimary08!,
+              offset: Offset(0, 1),
+              blurRadius: 4,
+            ),
+          ],
         ),
-      ),
-      foregroundDecoration: BoxDecoration(
-        border: Border(
-          left: isSelected
-              ? BorderSide(
-                  color: theme.colorScheme.primary,
-                  width: 5,
-                )
-              : BorderSide.none,
+        foregroundDecoration: BoxDecoration(
+          border: Border(
+            left: isSelected
+                ? BorderSide(
+                    color: theme.colorScheme.primary,
+                    width: 5,
+                  )
+                : BorderSide.none,
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _Header(name, headerActions),
-          _Content(child: content),
-          _Footer(child: footer),
-        ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _Header(name, headerActions),
+            _Content(child: content),
+            _Footer(child: footer),
+          ],
+        ),
       ),
     );
   }
--- catalyst_voices/lib/widgets/containers/workspace_tile_container.dart
+++ catalyst_voices/lib/widgets/containers/workspace_tile_container.dart
@@ -16,26 +16,42 @@ class WorkspaceTileContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return AnimatedContainer(
-      duration: kThemeChangeDuration,
-      decoration: BoxDecoration(
-        color: theme.colors.elevationsOnSurfaceNeutralLv1White,
-        borderRadius: BorderRadius.horizontal(
-          left: isSelected ? Radius.zero : Radius.circular(28),
-          right: Radius.circular(28),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: AnimatedContainer(
+        duration: kThemeChangeDuration,
+        decoration: BoxDecoration(
+          color: theme.colors.elevationsOnSurfaceNeutralLv1White,
+          borderRadius: BorderRadius.horizontal(
+            left: isSelected ? Radius.zero : Radius.circular(28),
+            right: Radius.circular(28),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colors.onSurfacePrimary08!,
+              offset: Offset(0, 1),
+              blurRadius: 4,
+            ),
+          ],
+        ),
+        foregroundDecoration: BoxDecoration(
+          border: Border(
+            left: isSelected
+                ? BorderSide(
+                    color: theme.colorScheme.primary,
+                    width: 5,
+                  )
+                : BorderSide.none,
+          ),
+        ),
+        child: ClipRRect(
+          child: content,
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(28),
+            bottomLeft: Radius.circular(isSelected ? 0 : 28),
+          ),
         ),
       ),
-      foregroundDecoration: BoxDecoration(
-        border: Border(
-          left: isSelected
-              ? BorderSide(
-                  color: theme.colorScheme.primary,
-                  width: 5,
-                )
-              : BorderSide.none,
-        ),
-      ),
-      child: content,
     );
   }
 }
