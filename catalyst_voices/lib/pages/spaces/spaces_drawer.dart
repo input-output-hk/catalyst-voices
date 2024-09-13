b7392984172d49eed2412bdac8bf7a07afe352e7
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2024-09-13 14:02:35 +0200

    feat: do not close drawer when space changes (#806)

--- catalyst_voices/lib/pages/spaces/spaces_drawer.dart
+++ catalyst_voices/lib/pages/spaces/spaces_drawer.dart
@@ -24,7 +24,6 @@ class SpacesDrawer extends StatelessWidget {
       bottom: VoicesDrawerSpaceChooser(
         currentSpace: space,
         onChanged: (space) {
-          Scaffold.of(context).closeDrawer();
           _goTo(context, space: space);
         },
         onOverallTap: () {
