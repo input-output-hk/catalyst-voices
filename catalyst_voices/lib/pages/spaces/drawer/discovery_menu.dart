f11d237699ac71ca49f6aa11d46e537c43b03d9e
Damian Molinski <damian.molinski@iohk.io>
2024-09-17 12:04:57 +0200

    feat: drawer discovery menu

--- catalyst_voices/lib/pages/spaces/drawer/discovery_menu.dart
+++ catalyst_voices/lib/pages/spaces/drawer/discovery_menu.dart
@@ -1,4 +1,7 @@
+import 'package:catalyst_voices/common/ext/ext.dart';
 import 'package:catalyst_voices/pages/spaces/drawer/space_header.dart';
+import 'package:catalyst_voices/widgets/widgets.dart';
+import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +13,26 @@ class DiscoveryDrawerMenu extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        SpaceHeader(Space.discovery),
+        const SpaceHeader(Space.discovery),
+        VoicesNavTile(
+          leading: VoicesAssets.icons.home.buildIcon(),
+          name: 'Discovery Dashboard',
+          backgroundColor: Space.discovery.backgroundColor(context),
+        ),
+        const VoicesDivider(),
+        VoicesNavTile(
+          leading: VoicesAssets.icons.user.buildIcon(),
+          name: 'Catalyst Roles',
+        ),
+        VoicesNavTile(
+          leading: VoicesAssets.icons.annotation.buildIcon(),
+          name: 'Feedback',
+        ),
+        const VoicesDivider(),
+        VoicesNavTile(
+          leading: VoicesAssets.icons.arrowRight.buildIcon(),
+          name: 'Catalyst Gitbook documentation',
+        ),
       ],
     );
   }
