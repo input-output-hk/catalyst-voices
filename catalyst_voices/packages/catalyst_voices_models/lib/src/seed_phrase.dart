fb080fbc863e70bd4ed56c00da8661fa25df0cdc
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-09-25 21:57:37 +0700

    feat: offset param

--- catalyst_voices/packages/catalyst_voices_models/lib/src/seed_phrase.dart
+++ catalyst_voices/packages/catalyst_voices_models/lib/src/seed_phrase.dart
@@ -67,9 +67,11 @@ class SeedPhrase {
   /// The mnemonic phrase as a list of individual words.
   List<String> get mnemonicWords => mnemonic.split(' ');
 
-  Future<Ed25519KeyPair> generateKeyPair() async {
+  Future<Ed25519KeyPair> generateKeyPair({int offset = 0}) async {
+    final modifiedSeed = Uint8List.fromList(uint8ListSeed.sublist(offset));
+
     // Derive the private key using the Ed25519 HD key derivation
-    final masterKey = await ED25519_HD_KEY.getMasterKeyFromSeed(uint8ListSeed);
+    final masterKey = await ED25519_HD_KEY.getMasterKeyFromSeed(modifiedSeed);
     final privateKey = masterKey.key;
 
     // Generate the matching public key
