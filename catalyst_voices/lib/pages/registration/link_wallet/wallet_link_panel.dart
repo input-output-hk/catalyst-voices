ce63639008503dbfdbf7fba67fe5f24b4bda01ee
Dominik Toton <dominik.toton@iohk.io>
2024-09-26 14:06:14 +0200

    feat: add missing panels

--- catalyst_voices/lib/pages/registration/link_wallet/wallet_link_panel.dart
+++ catalyst_voices/lib/pages/registration/link_wallet/wallet_link_panel.dart
@@ -1,5 +1,9 @@
 import 'package:catalyst_voices/pages/registration/link_wallet/intro/intro_panel.dart';
+import 'package:catalyst_voices/pages/registration/link_wallet/rbac_transaction/rbac_transaction_panel.dart';
+import 'package:catalyst_voices/pages/registration/link_wallet/roles_chooser/roles_chooser_panel.dart';
+import 'package:catalyst_voices/pages/registration/link_wallet/roles_summary/roles_summary_panel.dart';
 import 'package:catalyst_voices/pages/registration/link_wallet/select_wallet/select_wallet_panel.dart';
+import 'package:catalyst_voices/pages/registration/link_wallet/wallet_details/wallet_details_panel.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:flutter/material.dart';
 
@@ -16,11 +20,10 @@ class WalletLinkPanel extends StatelessWidget {
     return switch (stage) {
       WalletLinkStage.intro => const IntroPanel(),
       WalletLinkStage.selectWallet => const SelectWalletPanel(),
-      // TODO(dtscalac): define panels
-      WalletLinkStage.walletDetails => const SelectWalletPanel(),
-      WalletLinkStage.rolesChooser => const SelectWalletPanel(),
-      WalletLinkStage.rolesSummary => const SelectWalletPanel(),
-      WalletLinkStage.rbacTransaction => const SelectWalletPanel(),
+      WalletLinkStage.walletDetails => const WalletDetailsPanel(),
+      WalletLinkStage.rolesChooser => const RolesChooserPanel(),
+      WalletLinkStage.rolesSummary => const RolesSummaryPanel(),
+      WalletLinkStage.rbacTransaction => const RbacTransactionPanel(),
     };
   }
 }
