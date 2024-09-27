74702324bebe156b18009456fa203a14fa1097cd
Joaquín Rosales <joaquin.rosales@iohk.io>
2024-09-26 21:28:00 -0700

    wip(cat-gateway): add caches for chain_root

--- catalyst-gateway/bin/src/db/index/block/rbac509/mod.rs
+++ catalyst-gateway/bin/src/db/index/block/rbac509/mod.rs
@@ -1,13 +1,15 @@
 //! Index Role-Based Access Control (RBAC) Registration.
+#![allow(dead_code)]
 
 mod insert_chain_root_for_role0_key;
 mod insert_chain_root_for_stake_address;
 mod insert_chain_root_for_txn_id;
 mod insert_rbac509;
 
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 
 use cardano_chain_follower::{Metadata, MultiEraBlock};
+use moka::{policy::EvictionPolicy, sync::Cache};
 use scylla::Session;
 
 use crate::{
@@ -17,6 +19,51 @@ use crate::{
     },
     settings::cassandra_db::EnvVars,
 };
+
+/// Transaction Id Hash
+type TransactionIdHash = Vec<u8>;
+/// Chain Root Hash
+type ChainRootHash = TransactionIdHash;
+/// Slot Number
+type SlotNumber = u64;
+/// TX Index
+type TxIndex = i16;
+/// Role 0 Key
+type Role0Key = Vec<u8>;
+/// Stake Address
+type StakeAddress = Vec<u8>;
+/// Cached Values for `Role0Key` lookups.
+type Role0KeyValue = (ChainRootHash, SlotNumber, TxIndex);
+/// Cached Values for `StakeAddress` lookups.
+type StakeAddressValue = (ChainRootHash, SlotNumber, TxIndex);
+
+/// Cached Chain Root By Transaction ID.
+static CHAIN_ROOT_BY_TXN_ID_CACHE: LazyLock<Cache<TransactionIdHash, ChainRootHash>> =
+    LazyLock::new(|| {
+        Cache::builder()
+            // Set Eviction Policy to `LRU`
+            .eviction_policy(EvictionPolicy::lru())
+            // Create the cache.
+            .build()
+    });
+/// Cached Chain Root By Role 0 Key.
+static CHAIN_ROOT_BY_ROLE0_KEY_CACHE: LazyLock<Cache<Role0Key, Role0KeyValue>> =
+    LazyLock::new(|| {
+        Cache::builder()
+            // Set Eviction Policy to `LRU`
+            .eviction_policy(EvictionPolicy::lru())
+            // Create the cache.
+            .build()
+    });
+/// Cached Chain Root By Stake Address.
+static CHAIN_ROOT_BY_STAKE_ADDRESS_CACHE: LazyLock<Cache<StakeAddress, StakeAddressValue>> =
+    LazyLock::new(|| {
+        Cache::builder()
+            // Set Eviction Policy to `LRU`
+            .eviction_policy(EvictionPolicy::lru())
+            // Create the cache.
+            .build()
+    });
 
 /// Index RBAC 509 Registration Query Parameters
 pub(crate) struct Rbac509InsertQuery {
@@ -61,22 +108,28 @@ impl Rbac509InsertQuery {
             #[allow(irrefutable_let_patterns)]
             if let Metadata::DecodedMetadataValues::Cip509(rbac) = &decoded_metadata.value {
                 let transaction_id = txn_hash.to_vec();
-                let chain_root = if let Some(_tx_id) = rbac.prv_tx_id {
-                    // WIP: fetch chain_root from cache or DB
-                    vec![]
+                let chain_root_opt = if let Some(_tx_id) = rbac.prv_tx_id {
+                    // Attempt to get Chain Root for the previous transaction ID.
+                    CHAIN_ROOT_BY_TXN_ID_CACHE.get(&transaction_id).or({
+                        // WIP: Attempt to get from DB
+                        None
+                    })
                 } else {
-                    transaction_id.clone()
+                    let chain_root = transaction_id.clone();
+                    Some(chain_root)
                 };
                 // WIP: Check for role0_key
-                let role0_key = {
+                let role0_key_opt = {
                     // WIP
-                    &[]
+                    Some(&[])
                 };
                 // WIP: Check for stake_address
-                let stake_address = {
+                let stake_address_opt = {
                     // WIP
-                    &[]
+                    Some(&[])
                 };
+                if let (Some(chain_root), Some(role0_key), Some(stake_address)) =
+                    (chain_root_opt, role0_key_opt, stake_address_opt)
                 {
                     self.registrations.push(insert_rbac509::Params::new(
                         &chain_root,
@@ -85,22 +138,23 @@ impl Rbac509InsertQuery {
                         txn_index,
                         rbac,
                     ));
+
+                    CHAIN_ROOT_BY_TXN_ID_CACHE.insert(transaction_id.clone(), chain_root.clone());
                     self.chain_root_for_txn_id
                         .push(insert_chain_root_for_txn_id::Params::new(
                             &transaction_id,
                             &chain_root,
                         ));
 
-                    if !role0_key.is_empty() {
-                        self.chain_root_for_role0_key.push(
-                            insert_chain_root_for_role0_key::Params::new(
-                                role0_key,
-                                &chain_root,
-                                slot_no,
-                                txn_index,
-                            ),
-                        );
-                    }
+                    self.chain_root_for_role0_key.push(
+                        insert_chain_root_for_role0_key::Params::new(
+                            role0_key,
+                            &chain_root,
+                            slot_no,
+                            txn_index,
+                        ),
+                    );
+
                     if !stake_address.is_empty() {
                         self.chain_root_for_stake_address.push(
                             insert_chain_root_for_stake_address::Params::new(
