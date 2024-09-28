d21f90d5aabee31e8a6026780f4f15523d9fd5d0
Steven Johnson <sakurainds@gmail.com>
2024-09-28 15:59:20 +0700

    refactor(cat-gateway): fix code format

--- catalyst-gateway/bin/src/db/index/schema/mod.rs
+++ catalyst-gateway/bin/src/db/index/schema/mod.rs
@@ -114,14 +114,16 @@ fn remove_comments_and_join_query_lines(text: &str) -> String {
 
 /// Generates a unique schema version identifier based on the content of all CQL schemas.
 ///
-/// This function processes each CQL schema, removes comments from its lines and joins them into a
-/// single string. It then sorts these processed strings to ensure consistency in schema versions
-/// regardless of their order in the list. Finally, it generates a UUID from a 127 bit hash of this
-/// sorted collection of schema contents, which serves as a unique identifier for the current
-/// version of all schemas.
+/// This function processes each CQL schema, removes comments from its lines and joins
+/// them into a single string. It then sorts these processed strings to ensure consistency
+/// in schema versions regardless of their order in the list. Finally, it generates a UUID
+/// from a 127 bit hash of this sorted collection of schema contents, which serves as a
+/// unique identifier for the current version of all schemas.
 ///
 /// # Returns
-///     A string representing the UUID derived from the concatenated and cleaned CQL schema contents.
+///
+/// A string representing the UUID derived from the concatenated and cleaned CQL
+/// schema contents.
 fn generate_cql_schema_version() -> String {
     // Where we will actually store the bytes we derive the UUID from.
     let mut clean_schemas: Vec<String> = Vec::new();
--- catalyst-gateway/bin/src/settings/mod.rs
+++ catalyst-gateway/bin/src/settings/mod.rs
@@ -344,13 +344,10 @@ impl Settings {
             ENV_VARS.github_repo_name.as_str()
         );
 
-        match Url::parse_with_params(
-            &path,
-            &[
-                ("template", ENV_VARS.github_issue_template.as_str()),
-                ("title", title),
-            ],
-        ) {
+        match Url::parse_with_params(&path, &[
+            ("template", ENV_VARS.github_issue_template.as_str()),
+            ("title", title),
+        ]) {
             Ok(url) => Some(url),
             Err(e) => {
                 error!("Failed to generate github issue url {:?}", e.to_string());
@@ -450,13 +447,10 @@ mod tests {
             &SocketAddr::from(([127, 0, 0, 1], 8080)),
             "http://api.prod.projectcatalyst.io , https://api.dev.projectcatalyst.io:1234",
         );
-        assert_eq!(
-            configured_hosts,
-            vec![
-                "http://api.prod.projectcatalyst.io",
-                "https://api.dev.projectcatalyst.io:1234"
-            ]
-        );
+        assert_eq!(configured_hosts, vec![
+            "http://api.prod.projectcatalyst.io",
+            "https://api.dev.projectcatalyst.io:1234"
+        ]);
     }
 
     #[test]
@@ -465,10 +459,9 @@ mod tests {
             &SocketAddr::from(([127, 0, 0, 1], 8080)),
             "not a hostname , https://api.dev.projectcatalyst.io:1234",
         );
-        assert_eq!(
-            configured_hosts,
-            vec!["https://api.dev.projectcatalyst.io:1234"]
-        );
+        assert_eq!(configured_hosts, vec![
+            "https://api.dev.projectcatalyst.io:1234"
+        ]);
     }
 
     #[test]
