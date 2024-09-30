8ceb88dc160b93d8d7b8734a63d082fcb6febf7f
Damian Molinski <damian.molinski@iohk.io>
2024-09-30 09:39:25 +0200

    refactor: move SeedPhraseState fields to the top of class

--- catalyst_voices/packages/catalyst_voices_blocs/lib/src/registration/seed_phrase_state.dart
+++ catalyst_voices/packages/catalyst_voices_blocs/lib/src/registration/seed_phrase_state.dart
@@ -2,13 +2,13 @@ import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:equatable/equatable.dart';
 
 final class SeedPhraseState extends Equatable {
+  final SeedPhrase? seedPhrase;
+  final bool isStoredConfirmed;
+
   const SeedPhraseState({
     this.seedPhrase,
     this.isStoredConfirmed = false,
   });
-
-  final SeedPhrase? seedPhrase;
-  final bool isStoredConfirmed;
 
   @override
   List<Object?> get props => [
