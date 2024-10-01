d7ce1410d1ed17e4db11366645e082a4683fea59
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2024-10-01 08:49:15 +0200

    feat(cat-voices): download seed phrase (#906)
    * feat: seed phrase download
    
    * chore: update seed_phrase filename
    
    * fix: downloader stub to fix tests
    
    * chore: add path to melos.yaml + logger message

--- catalyst_voices/packages/catalyst_voices_blocs/lib/src/registration/cubits/keychain_creation_cubit.dart
+++ catalyst_voices/packages/catalyst_voices_blocs/lib/src/registration/cubits/keychain_creation_cubit.dart
@@ -1,11 +1,22 @@
 // ignore_for_file: avoid_positional_boolean_parameters
+import 'dart:async';
+import 'dart:convert' show utf8;
 
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_services/catalyst_voices_services.dart';
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+final _logger = Logger('KeychainCreationCubit');
+
 final class KeychainCreationCubit extends Cubit<CreateKeychain> {
-  KeychainCreationCubit() : super(const CreateKeychain());
+  final Downloader _downloader;
+
+  KeychainCreationCubit({
+    required Downloader downloader,
+  })  : _downloader = downloader,
+        super(const CreateKeychain());
 
   set _seedPhraseState(SeedPhraseState newValue) {
     emit(state.copyWith(seedPhraseState: newValue));
@@ -35,6 +46,38 @@ final class KeychainCreationCubit extends Cubit<CreateKeychain> {
   void setSeedPhraseStoredConfirmed(bool newValue) {
     if (_seedPhraseState.isStoredConfirmed != newValue) {
       _seedPhraseState = _seedPhraseState.copyWith(isStoredConfirmed: newValue);
+    }
+  }
+
+  Future<void> downloadSeedPhrase() async {
+    final mnemonic = _seedPhraseState.seedPhrase?.mnemonic;
+    if (mnemonic == null) {
+      throw StateError('SeedPhrase is not generated. Make sure it exits first');
+    }
+
+    FutureOr<Uri> buildWebMnemonicDownloadUri() {
+      return Uri.dataFromString(
+        mnemonic,
+        encoding: utf8,
+        base64: true,
+      );
+    }
+
+    FutureOr<Uri> buildIOMnemonicDownloadUri() {
+      throw UnimplementedError();
+    }
+
+    final uri = CatalystPlatform.isWeb
+        ? await buildWebMnemonicDownloadUri()
+        : await buildIOMnemonicDownloadUri();
+
+    final path = Uri(path: 'catalyst_seed_phrase.txt');
+
+    try {
+      await _downloader.download(uri, path: path);
+    } catch (error, stackTrace) {
+      _logger.severe('Downloading keychain failed', error, stackTrace);
+      // TODO(damian-molinski): Show snack bar
     }
   }
 
