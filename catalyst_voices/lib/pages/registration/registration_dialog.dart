000f36f8bf224ba7ef5e894899dd1765138de1e3
Damian Molinski <damian.molinski@iohk.io>
2024-10-04 11:00:36 +0200

    feat: confirm exit dialog

--- catalyst_voices/lib/pages/registration/registration_dialog.dart
+++ catalyst_voices/lib/pages/registration/registration_dialog.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:catalyst_voices/dependency/dependencies.dart';
 import 'package:catalyst_voices/pages/registration/create_keychain/create_keychain_panel.dart';
 import 'package:catalyst_voices/pages/registration/finish_account/finish_account_creation_panel.dart';
 import 'package:catalyst_voices/pages/registration/get_started/get_started_panel.dart';
+import 'package:catalyst_voices/pages/registration/registration_exit_confirm_dialog.dart';
 import 'package:catalyst_voices/pages/registration/registration_info_panel.dart';
 import 'package:catalyst_voices/pages/registration/wallet_link/wallet_link_panel.dart';
 import 'package:catalyst_voices/widgets/widgets.dart';
@@ -41,28 +44,52 @@ class _RegistrationDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return VoicesTwoPaneDialog(
-      left: const RegistrationInfoPanel(),
-      right: switch (state) {
-        GetStarted() => const GetStartedPanel(),
-        FinishAccountCreation() => const FinishAccountCreationPanel(),
-        Recover() => const Placeholder(),
-        CreateKeychain(
-          :final stage,
-          :final seedPhrase,
-          :final unlockPassword,
-        ) =>
-          CreateKeychainPanel(
-            stage: stage,
-            seedPhraseState: seedPhrase,
-            unlockPasswordState: unlockPassword,
-          ),
-        WalletLink(:final stage, :final stateData) => WalletLinkPanel(
-            stage: stage,
-            stateData: stateData,
-          ),
-        AccountCompleted() => const Placeholder(),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        unawaited(_confirmedExit(context, didPop: didPop));
       },
+      child: VoicesTwoPaneDialog(
+        left: const RegistrationInfoPanel(),
+        right: switch (state) {
+          GetStarted() => const GetStartedPanel(),
+          FinishAccountCreation() => const FinishAccountCreationPanel(),
+          Recover() => const Placeholder(),
+          CreateKeychain(
+            :final stage,
+            :final seedPhrase,
+            :final unlockPassword,
+          ) =>
+            CreateKeychainPanel(
+              stage: stage,
+              seedPhraseState: seedPhrase,
+              unlockPasswordState: unlockPassword,
+            ),
+          WalletLink(:final stage, :final stateData) => WalletLinkPanel(
+              stage: stage,
+              stateData: stateData,
+            ),
+          AccountCompleted() => const Placeholder(),
+        },
+      ),
     );
+  }
+
+  Future<void> _confirmedExit(
+    BuildContext context, {
+    required bool didPop,
+  }) async {
+    if (didPop) {
+      return;
+    }
+
+    final confirmed = await VoicesQuestionDialog.show(
+      context,
+      builder: (_) => const RegistrationExitConfirmDialog(),
+    );
+
+    if (context.mounted && confirmed) {
+      Navigator.of(context).pop();
+    }
   }
 }
