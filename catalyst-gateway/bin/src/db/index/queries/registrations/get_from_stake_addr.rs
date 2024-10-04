fca1d62f87c5a5c6e93e1abec0d9b02d579668d1
cong-or <60357579+cong-or@users.noreply.github.com>
2024-10-04 15:06:17 +0100

    feat(cat-gateway): CIP36 Registration queries (#933)
    * feat(barebones): scylla queries
    
    * feat(barebones): scylla queries
    
    * feat(barebones): scylla queries
    
    * feat(barebones): scylla queries
    
    * feat(barebones): scylla queries
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * feat(latest registration endpoints): given stake addr and stake key hash
    
    * refactor(stake addr voting key association): check pair is active
    
    stake addresses need to be individually checked to make sure they are still actively associated with the voting key, and have not been registered to another voting key.
    
    * feat(invalids): add error reporting in resp
    
    * feat(error reporting): invalids
    
    include any erroneous registrations which occur AFTER the slot# of the last valid registration
    
    * feat(error reporting): invalids
    
    include any erroneous registrations which occur AFTER the slot# of the last valid registration
    
    * refactor(docs): clean
    
    * refactor(housekeeping): clean
    
    * feat(error reporting): vote key
    
    * feat(error reporting): vote key
    
    * feat(error reporting): vote key
    
    * fix(code formatting): format
    
    * style(spelling): cspell
    
    * fix(cat-gateway): spelling
    
    * fix(cat-gateway): docs
    
    * fix(cat-gateway): generated dart openapi files
    
    * ci(docs): lints
    
    * ci(docs): lints
    
    * fix(cat-gateway): bump cache version
    
    * docs(cat-gateway): fix docs
    
    * ci(oai lints): add missing endpoint annotations
    
    ---------
    
    Co-authored-by: Steven Johnson <stevenj@users.noreply.github.com>
    Co-authored-by: Steven Johnson <sakurainds@gmail.com>

---
+++ catalyst-gateway/bin/src/db/index/queries/registrations/get_from_stake_addr.rs
@@ -0,0 +1,94 @@
+//! Get stake addr registrations
+
+use std::sync::Arc;
+
+use scylla::{
+    prepared_statement::PreparedStatement, transport::iterator::TypedRowIterator, SerializeRow,
+    Session,
+};
+use tracing::error;
+
+use crate::db::index::{
+    queries::{PreparedQueries, PreparedSelectQuery},
+    session::CassandraSession,
+};
+
+/// Get registrations from stake addr query.
+const GET_REGISTRATIONS_FROM_STAKE_ADDR_QUERY: &str =
+    include_str!("../cql/get_registrations_w_stake_addr.cql");
+
+/// Get registration
+#[derive(SerializeRow)]
+pub(crate) struct GetRegistrationParams {
+    /// Stake address.
+    pub stake_address: Vec<u8>,
+}
+
+impl GetRegistrationParams {
+    /// Create a new instance of [`GetRegistrationParams`]
+    pub(crate) fn new(stake_addr: Vec<u8>) -> GetRegistrationParams {
+        Self {
+            stake_address: stake_addr,
+        }
+    }
+}
+
+/// Get registration given stake addr or vote key
+#[allow(clippy::expect_used)]
+mod result {
+    use scylla::FromRow;
+
+    /// Get registration query result.
+    #[derive(FromRow)]
+    pub(crate) struct GetRegistrationQuery {
+        /// Full Stake Address (not hashed, 32 byte ED25519 Public key).
+        pub stake_address: Vec<u8>,
+        /// Nonce value after normalization.
+        pub nonce: num_bigint::BigInt,
+        /// Slot Number the cert is in.
+        pub slot_no: num_bigint::BigInt,
+        /// Transaction Index.
+        pub txn: i16,
+        /// Voting Public Key
+        pub vote_key: Vec<u8>,
+        /// Full Payment Address (not hashed, 32 byte ED25519 Public key).
+        pub payment_address: Vec<u8>,
+        /// Is the stake address a script or not.
+        pub is_payable: bool,
+        /// Is the Registration CIP36 format, or CIP15
+        pub cip36: bool,
+    }
+}
+/// Get registration query.
+pub(crate) struct GetRegistrationQuery;
+
+impl GetRegistrationQuery {
+    /// Prepares a get registration query.
+    pub(crate) async fn prepare(session: Arc<Session>) -> anyhow::Result<PreparedStatement> {
+        let get_registrations_query = PreparedQueries::prepare(
+            session,
+            GET_REGISTRATIONS_FROM_STAKE_ADDR_QUERY,
+            scylla::statement::Consistency::All,
+            true,
+        )
+        .await;
+
+        if let Err(ref error) = get_registrations_query {
+            error!(error=%error, "Failed to prepare get registration query.");
+        };
+
+        get_registrations_query
+    }
+
+    /// Executes get registration info for given stake addr query.
+    pub(crate) async fn execute(
+        session: &CassandraSession, params: GetRegistrationParams,
+    ) -> anyhow::Result<TypedRowIterator<result::GetRegistrationQuery>> {
+        let iter = session
+            .execute_iter(PreparedSelectQuery::RegistrationFromStakeAddr, params)
+            .await?
+            .into_typed::<result::GetRegistrationQuery>();
+
+        Ok(iter)
+    }
+}
