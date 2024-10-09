ceaa2088c39f46c981087cbd5ed7e1a941365e7e
Damian Molinski <damian.molinski@iohk.io>
2024-10-09 10:44:01 +0200

    test: SeedPhrasesSequencer

--- catalyst_voices/test/widgets/seed_phrase/seed_phrases_sequencer_test.dart
+++ catalyst_voices/test/widgets/seed_phrase/seed_phrases_sequencer_test.dart
@@ -81,5 +81,52 @@ void main() {
         expect(selectedWords, expectedWords);
       },
     );
+
+    testWidgets(
+      'picking duplicated word selects only clicked one ',
+      (tester) async {
+        // Given
+        const words = [
+          SeedPhraseWord('broken', nr: 1),
+          SeedPhraseWord('member', nr: 2),
+          SeedPhraseWord('repeat', nr: 3),
+          SeedPhraseWord('liquid', nr: 4),
+          SeedPhraseWord('barely', nr: 5),
+          SeedPhraseWord('electric', nr: 6),
+          SeedPhraseWord('theory', nr: 7),
+          SeedPhraseWord('paddle', nr: 8),
+          SeedPhraseWord('coyote', nr: 9),
+          SeedPhraseWord('behind', nr: 10),
+          SeedPhraseWord('unique', nr: 11),
+          SeedPhraseWord('member', nr: 12),
+        ];
+
+        final selectedWords = <SeedPhraseWord>[];
+
+        const expectedWords = [
+          SeedPhraseWord('member', nr: 2),
+        ];
+
+        final sequencer = SeedPhrasesSequencer(
+          words: words,
+          selectedWords: selectedWords,
+          onChanged: (value) {
+            selectedWords
+              ..clear()
+              ..addAll(value);
+          },
+        );
+
+        // When
+        await tester.pumpApp(sequencer);
+        await tester.pumpAndSettle();
+
+        final key = ValueKey('PickerSeedPhrase${words[1].nr}CellKey');
+        await tester.tap(find.byKey(key));
+
+        // Then
+        expect(selectedWords, expectedWords);
+      },
+    );
   });
 }
