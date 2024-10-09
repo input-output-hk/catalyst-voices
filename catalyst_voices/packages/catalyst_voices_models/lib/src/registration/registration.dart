0029a6ac0b7ba4b9d19feeb92deabc3c765648e1
Dominik Toton <dominik.toton@iohk.io>
2024-10-09 17:36:16 +0200

    fix: remove wrong export

--- catalyst_voices/packages/catalyst_voices_models/lib/src/registration/registration.dart
+++ catalyst_voices/packages/catalyst_voices_models/lib/src/registration/registration.dart
@@ -1,7 +1,6 @@
 export 'create_account_type.dart';
 export 'create_keychain_stage.dart';
 export 'exception/registration_exception.dart';
-export 'exception/registration_insufficient_balance_exception.dart';
 export 'recover_seed_phrase_stage.dart';
 export 'registration_recover_method.dart';
 export 'registration_step.dart';
