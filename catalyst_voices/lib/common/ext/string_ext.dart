df53a61651cf9824a65f736bb142d09418e6fd92
digitalheartxs <jakub.szwiec@scalac.io>
2024-10-14 13:45:02 +0200

    feat: Registration completed screen - implement capitalize edge cases

--- catalyst_voices/lib/common/ext/string_ext.dart
+++ catalyst_voices/lib/common/ext/string_ext.dart
@@ -1,5 +1,9 @@
 extension StringExt on String {
   String capitalize() {
-    return '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
+    if (isNotEmpty) {
+      return '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
+    } else {
+      return '';
+    }
   }
 }
