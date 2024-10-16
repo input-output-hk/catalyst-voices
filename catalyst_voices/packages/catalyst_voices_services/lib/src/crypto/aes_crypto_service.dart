e5a9e84c8c9a264208a25e8c54f6691492cd701a
Damian Molinski <damian.molinski@iohk.io>
2024-10-16 12:36:24 +0200

    feat: first iteration of AesCryptoService

---
+++ catalyst_voices/packages/catalyst_voices_services/lib/src/crypto/aes_crypto_service.dart
@@ -0,0 +1,129 @@
+import 'dart:math';
+
+import 'package:catalyst_voices_services/src/crypto/crypto_service.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:flutter/foundation.dart';
+
+final class AesCryptoService implements CryptoService {
+  /// Salt length for Argon2 key derivation.
+  static const int _saltLength = 16;
+
+  /// Salt length for AesGcm data encryption.
+  static const int _viLength = 12;
+
+  /// Derived key hash length.
+  static const int _keyLength = 16;
+
+  final Random _random;
+
+  AesCryptoService({
+    Random? random,
+  }) : _random = random ?? Random.secure();
+
+  @override
+  Future<Uint8List> deriveKey(
+    Uint8List seed, {
+    Uint8List? salt,
+  }) async {
+    final algorithm = Argon2id(
+      parallelism: 2,
+      memory: 10000, // 10 000 x 1kB block = 10 MB
+      iterations: 1,
+      hashLength: _keyLength,
+    );
+
+    salt ??= _generateRandomList(length: _saltLength);
+
+    final secretKey = await algorithm.deriveKey(
+      secretKey: SecretKey(seed),
+      nonce: salt,
+    );
+
+    final keyBytes = await secretKey.extractBytes().then(Uint8List.fromList);
+
+    secretKey.destroy();
+
+    // Combine salt and hashed password for storage
+    return Uint8List.fromList([...salt, ...keyBytes]);
+  }
+
+  @override
+  Future<bool> verifyKey(
+    Uint8List seed, {
+    required Uint8List key,
+  }) async {
+    final salt = key.sublist(0, _saltLength);
+    if (salt.length < _saltLength) {
+      return false;
+    }
+
+    final derivedKey = await deriveKey(seed, salt: salt);
+    return listEquals(derivedKey, key);
+  }
+
+  @override
+  Future<Uint8List> decrypt(
+    Uint8List data, {
+    required Uint8List key,
+  }) async {
+    final algorithm = AesGcm.with256bits(nonceLength: _viLength);
+
+    final secretKey = SecretKey(key);
+
+    try {
+      final secretBox = SecretBox.fromConcatenation(
+        data,
+        nonceLength: _viLength,
+        macLength: algorithm.macAlgorithm.macLength,
+      );
+
+      await secretBox.checkMac(
+        macAlgorithm: algorithm.macAlgorithm,
+        secretKey: secretKey,
+        aad: [],
+      );
+
+      return algorithm
+          .decrypt(secretBox, secretKey: secretKey)
+          .then(Uint8List.fromList);
+    } finally {
+      secretKey.destroy();
+    }
+  }
+
+  @override
+  Future<Uint8List> encrypt(
+    Uint8List data, {
+    required Uint8List key,
+  }) async {
+    final algorithm = AesGcm.with256bits(nonceLength: _viLength);
+    final secretKey = SecretKey(key);
+
+    try {
+      final secretBox = await algorithm.encrypt(
+        data,
+        secretKey: secretKey,
+        nonce: null,
+        aad: [],
+        possibleBuffer: null,
+      );
+
+      return secretBox.concatenation();
+    } finally {
+      secretKey.destroy();
+    }
+  }
+
+  /// Builds list with [length] and random bytes in it.
+  Uint8List _generateRandomList({
+    required int length,
+  }) {
+    final list = Uint8List(length);
+
+    for (var i = 0; i < length; i++) {
+      list[i] = _random.nextInt(16);
+    }
+
+    return list;
+  }
+}
--- catalyst_voices/packages/catalyst_voices_services/lib/src/crypto/crypto_service.dart
+++ catalyst_voices/packages/catalyst_voices_services/lib/src/crypto/crypto_service.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/foundation.dart';
 
 abstract interface class CryptoService {
-  Future<Uint8List> deriveKey(Uint8List data);
+  Future<Uint8List> deriveKey(
+    Uint8List seed, {
+    Uint8List? salt,
+  });
+
+  Future<bool> verifyKey(
+    Uint8List seed, {
+    required Uint8List key,
+  });
 
   Future<Uint8List> decrypt(
     Uint8List data, {
