b683e3bd72afa352d4c12a1375cfa7291895d83b
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-10-16 21:50:23 +0700

    chore: change affacted functions to future

--- catalyst_voices_packages/catalyst_compression/catalyst_compression_web/lib/src/interop/catalyst_compression_interop.dart
+++ catalyst_voices_packages/catalyst_compression/catalyst_compression_web/lib/src/interop/catalyst_compression_interop.dart
@@ -2,6 +2,7 @@
 library catalyst_compression_interop;
 
 import 'dart:js_interop';
+import 'dart:js_util';
 
 import 'package:catalyst_compression_platform_interface/catalyst_compression_platform_interface.dart';
 import 'package:convert/convert.dart';
@@ -11,28 +12,28 @@ import 'package:convert/convert.dart';
 /// The bytes are transferred as hex string due to
 /// raw bytes not being supported by the js_interop.
 @JS()
-external JSString brotliCompress(JSString bytes);
+external Object brotliCompress(JSString bytes);
 
 /// Decompresses the [bytes] using brotli algorithm.
 ///
 /// The bytes are transferred as hex string due to
 /// raw bytes not being supported by the js_interop.
 @JS()
-external JSString brotliDecompress(JSString bytes);
+external Object brotliDecompress(JSString bytes);
 
 /// Compresses the [bytes] using zstd algorithm.
 ///
 /// The bytes are transferred as hex string due to
 /// raw bytes not being supported by the js_interop.
 @JS()
-external JSString zstdCompress(JSString bytes);
+external Object zstdCompress(JSString bytes);
 
 /// Decompresses the [bytes] using zstd algorithm.
 ///
 /// The bytes are transferred as hex string due to
 /// raw bytes not being supported by the js_interop.
 @JS()
-external JSString zstdDecompress(JSString bytes);
+external Object zstdDecompress(JSString bytes);
 
 /// The JS implementation of brotli compressor.
 class JSBrotliCompressor implements CatalystCompressor {
@@ -40,15 +41,17 @@ class JSBrotliCompressor implements CatalystCompressor {
   const JSBrotliCompressor();
 
   @override
-  List<int> compress(List<int> bytes) {
-    final data = brotliCompress(hex.encode(bytes).toJS).toDart;
-    return hex.decode(data);
+  Future<List<int>> compress(List<int> bytes) async {
+    final data =
+        await promiseToFuture<JSString>(brotliCompress(hex.encode(bytes).toJS));
+    return hex.decode(data.toDart);
   }
 
   @override
-  List<int> decompress(List<int> bytes) {
-    final data = brotliDecompress(hex.encode(bytes).toJS).toDart;
-    return hex.decode(data);
+  Future<List<int>> decompress(List<int> bytes) async {
+    final data =
+        await promiseToFuture<JSString>(brotliDecompress(hex.encode(bytes).toJS));
+    return hex.decode(data.toDart);
   }
 }
 
@@ -65,20 +68,22 @@ class JSZstdCompressor implements CatalystCompressor {
   const JSZstdCompressor();
 
   @override
-  List<int> compress(List<int> bytes) {
+  Future<List<int>> compress(List<int> bytes) async {
     if (bytes.length < _minLength) {
       throw CompressionNotSupportedException(
         'Bytes too short, actual: ${bytes.length}, required: $_minLength',
       );
     }
 
-    final data = zstdCompress(hex.encode(bytes).toJS).toDart;
-    return hex.decode(data);
+    final data =
+        await promiseToFuture<JSString>(zstdCompress(hex.encode(bytes).toJS));
+    return hex.decode(data.toDart);
   }
 
   @override
-  List<int> decompress(List<int> bytes) {
-    final data = zstdDecompress(hex.encode(bytes).toJS).toDart;
-    return hex.decode(data);
+  Future<List<int>> decompress(List<int> bytes) async {
+    final data =
+        await promiseToFuture<JSString>(zstdDecompress(hex.encode(bytes).toJS));
+    return hex.decode(data.toDart);
   }
 }
