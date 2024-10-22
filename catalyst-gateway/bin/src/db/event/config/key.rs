601332739f3708b6af6133dc533d0f563c89b485
bkioshn <bkioshn@gmail.com>
2024-10-22 17:23:56 +0700

    fix: config key
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- catalyst-gateway/bin/src/db/event/config/key.rs
+++ catalyst-gateway/bin/src/db/event/config/key.rs
@@ -6,7 +6,7 @@ use jsonschema::{BasicOutput, Validator};
 use serde_json::{json, Value};
 use tracing::error;
 
-use crate::{db::utilities::update_refs, service::api_spec};
+use crate::utils::schema::{extract_json_schema_for, SCHEMA_VERSION};
 
 /// Configuration key
 #[derive(Debug, Clone, PartialEq)]
@@ -27,17 +27,8 @@ impl Display for ConfigKey {
 }
 
 /// Frontend schema from API specification.
-pub(crate) static FRONTEND_SCHEMA: LazyLock<Value> = LazyLock::new(|| {
-    let base_json = api_spec();
-    let config_json = api_spec()
-        .get("components")
-        .and_then(|components| components.get("schemas"))
-        .and_then(|schemas| schemas.get("FrontendConfig"))
-        .cloned()
-        .unwrap_or_default();
-
-    update_refs(&config_json, &base_json)
-});
+pub(crate) static FRONTEND_SCHEMA: LazyLock<Value> =
+    LazyLock::new(|| extract_json_schema_for("FrontendConfig"));
 
 /// Frontend schema validator.
 static FRONTEND_SCHEMA_VALIDATOR: LazyLock<Validator> =
@@ -54,22 +45,25 @@ static FRONTEND_IP_DEFAULT: LazyLock<Value> =
 /// Helper function to create a JSON validator from a JSON schema.
 /// If the schema is invalid, a default JSON validator is created.
 fn schema_validator(schema: &Value) -> Validator {
-    jsonschema::validator_for(schema).unwrap_or_else(|err| {
-        error!(
-            id = "schema_validator",
-            error=?err,
-            "Error creating JSON validator"
-        );
+    Validator::options()
+        .with_draft(jsonschema::Draft::Draft202012)
+        .build(schema)
+        .unwrap_or_else(|err| {
+            error!(
+                id = "schema_validator",
+                error=?err,
+                "Error creating JSON validator"
+            );
 
-        // Create a default JSON validator as a fallback
-        // This should not fail since it is hard coded
-        #[allow(clippy::expect_used)]
-        Validator::new(&json!({
-            "$schema": "http://json-schema.org/draft-07/schema#",
-            "type": "object"
-        }))
-        .expect("Failed to create default JSON validator")
-    })
+            // Create a default JSON validator as a fallback
+            // This should not fail since it is hard coded
+            #[allow(clippy::expect_used)]
+            Validator::new(&json!({
+                "$schema": SCHEMA_VERSION,
+                "type": "object"
+            }))
+            .expect("Failed to create default JSON validator")
+        })
 }
 
 /// Helper function to convert a JSON string to a JSON value.
@@ -126,10 +120,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_valid_validate() {
-        let value = json!({
-            "test": "test"
+    fn test_schema_for_schema() {
+        // Invalid schema
+        let invalid_schema = json!({
+            "title": "Invalid Example Schema",
+            "type": "object",
+
+            "properties": {
+                "invalidProperty": {
+                    "type": "unknownType"
+                }
+            },
+
         });
+        // This should not fail
+        schema_validator(&invalid_schema);
+    }
+
+    #[test]
+    fn test_valid_validate_1() {
+        let value = json!({
+            "sentry": {
+                "dsn": "https://test.com"
+            }
+        });
+        let result = ConfigKey::Frontend.validate(&value);
+        assert!(result.is_valid());
+        println!("{:?}", serde_json::to_value(result).unwrap());
+    }
+
+    #[test]
+    fn test_valid_validate_2() {
+        let value = json!({});
         let result = ConfigKey::Frontend.validate(&value);
         assert!(result.is_valid());
         println!("{:?}", serde_json::to_value(result).unwrap());
