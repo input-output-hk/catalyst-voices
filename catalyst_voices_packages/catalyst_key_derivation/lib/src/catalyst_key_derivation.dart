49e1d171e6c28716d18cd1a40a57f4e16f001ba2
Dominik Toton <dominik.toton@iohk.io>
2024-10-23 14:43:17 +0200

    feat(flutter/catalyst_key_derivation): convert to federated plugin structure

--- catalyst_voices_packages/catalyst_key_derivation/lib/src/catalyst_key_derivation.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:typed_data';
-
-/// Implements a CIP-1852 Cardano HD Key Derivation.
-class CatalystKeyDerivation {
-  /// Derives a master key from a [mnemonic].
-  Future<Uint8List> deriveMasterKey(String mnemonic) async {
-    throw UnimplementedError();
-  }
-}
