e6e2f143e9576a658b11ab7abb64bbb33684c729
Damian Molinski <damian.molinski@iohk.io>
2024-10-23 13:38:25 +0200

    refactor: Extract Lockable interface

--- catalyst_voices/packages/catalyst_voices_services/lib/src/keychain/keychain.dart
+++ catalyst_voices/packages/catalyst_voices_services/lib/src/keychain/keychain.dart
@@ -1,23 +1,13 @@
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_services/src/lockable.dart';
 import 'package:flutter/foundation.dart';
 
-// TODO(damian-molinski): Extract lockable interface.
-abstract interface class Keychain {
+abstract interface class Keychain implements Lockable {
   String get id;
 
   Future<bool> get isEmpty;
 
-  Future<bool> get isUnlocked;
-
-  Stream<bool> get watchIsUnlocked;
-
   Future<KeychainMetadata> get metadata;
-
-  Future<void> setLock(LockFactor lockFactor);
-
-  Future<bool> unlock(LockFactor lockFactor);
-
-  Future<void> lock();
 
   Future<Uint8List?> getRootKey();
 
--- catalyst_voices/packages/catalyst_voices_services/lib/src/storage/vault/vault.dart
+++ catalyst_voices/packages/catalyst_voices_services/lib/src/storage/vault/vault.dart
@@ -1,4 +1,5 @@
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_services/src/lockable.dart';
 import 'package:catalyst_voices_services/src/storage/storage.dart';
 
 /// Secure version of [Storage] where any read/write methods can take
@@ -6,20 +7,4 @@ import 'package:catalyst_voices_services/src/storage/storage.dart';
 ///
 /// In order to unlock [Vault] sufficient [LockFactor] have to be
 /// set via [unlock] that can unlock [LockFactor] from [setLock].
-abstract interface class Vault implements Storage {
-  /// Returns true when have sufficient [LockFactor] from [unlock].
-  Future<bool> get isUnlocked;
-
-  Stream<bool> get watchIsUnlocked;
-
-  /// Deletes unlockFactor if have any.
-  Future<void> lock();
-
-  /// Changes [isUnlocked] when [unlock] can unlock [LockFactor]
-  /// from [setLock].
-  Future<bool> unlock(LockFactor unlock);
-
-  /// Sets [LockFactor] that which prevents read/write on this [Vault]
-  /// unless unlocked with matching [LockFactor] via [unlock].
-  Future<void> setLock(LockFactor lock);
-}
+abstract interface class Vault implements Storage, Lockable {}
