4bfd8d5a7919cfb92a4c601f3c12f1ec434b665d
Dominik Toton <dominik.toton@iohk.io>
2024-10-23 13:36:37 +0200

    feat(flutter/catalyst_key_derivation): add catalyst key derivation package structure

---
+++ catalyst_voices_packages/catalyst_key_derivation/lib/src/catalyst_key_derivation.dart
@@ -0,0 +1,9 @@
+import 'dart:typed_data';
+
+/// Implements a CIP-1852 Cardano HD Key Derivation.
+class CatalystKeyDerivation {
+  /// Derives a master key from a [mnemonic].
+  Future<Uint8List> deriveMasterKey(String mnemonic) async {
+    throw UnimplementedError();
+  }
+}
