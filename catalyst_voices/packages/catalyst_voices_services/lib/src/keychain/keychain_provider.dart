bbd1b8fbce622e1107d5bc21991be237bb6956d2
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2024-10-24 09:53:32 +0200

    Update catalyst_voices/packages/catalyst_voices_services/lib/src/keychain/keychain_provider.dart
    Co-authored-by: Dominik Toton <166132265+dtscalac@users.noreply.github.com>

--- catalyst_voices/packages/catalyst_voices_services/lib/src/keychain/keychain_provider.dart
+++ catalyst_voices/packages/catalyst_voices_services/lib/src/keychain/keychain_provider.dart
@@ -3,7 +3,7 @@ import 'package:catalyst_voices_services/catalyst_voices_services.dart';
 abstract interface class KeychainProvider {
   Future<Keychain> create(String id);
 
-  Future<bool> exits(String id);
+  Future<bool> exists(String id);
 
   Future<Keychain> get(String id);
 
