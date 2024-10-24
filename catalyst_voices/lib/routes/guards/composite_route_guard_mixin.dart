1508d9f5e1f01a0a5afb392c8a83ddcec8752fd5
Dominik Toton <dominik.toton@iohk.io>
2024-10-24 20:10:40 +0200

    refactor: add composite route guard mixin

---
+++ catalyst_voices/lib/routes/guards/composite_route_guard_mixin.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:catalyst_voices/routes/guards/route_guard.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+/// A default implementation for [redirect]
+/// that can handle multiple [routeGuards].
+mixin CompositeRouteGuardMixin on GoRouteData {
+  /// An abstract getter to be overriden all [RouteGuard]'s
+  /// that a given route will have.
+  ///
+  /// The guards are called in the order they are returned
+  /// from this method in a FIFO manner.
+  List<RouteGuard> get routeGuards;
+
+  /// A default implementation of route guard logic, it's allowed to override
+  /// it to add custom redirect logic however make sure to call
+  /// `super.redirect()` if your own implementation doesn't redirect anywhere.
+  @override
+  FutureOr<String?> redirect(BuildContext context, GoRouterState state) async {
+    for (final guard in routeGuards) {
+      final location = await guard.redirect(context, state);
+      if (location != null) {
+        return location;
+      }
+    }
+
+    return null;
+  }
+}
--- catalyst_voices/lib/routes/routing/account_route.dart
+++ catalyst_voices/lib/routes/routing/account_route.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:catalyst_voices/pages/account/account_page.dart';
-import 'package:catalyst_voices/routes/guards/unlocked_keychain_guard_mixin.dart';
+import 'package:catalyst_voices/routes/guards/route_guard.dart';
+import 'package:catalyst_voices/routes/guards/session_unlocked_guard.dart';
+import 'package:catalyst_voices/routes/guards/composite_route_guard_mixin.dart';
 import 'package:catalyst_voices/routes/routing/routes.dart';
 import 'package:catalyst_voices/routes/routing/transitions/fade_page_transition_mixin.dart';
 import 'package:flutter/widgets.dart';
@@ -13,8 +15,11 @@ part 'account_route.g.dart';
   path: '/${Routes.currentMilestone}/account',
 )
 final class AccountRoute extends GoRouteData
-    with FadePageTransitionMixin, UnlockedKeychainGuardMixin {
+    with FadePageTransitionMixin, CompositeRouteGuardMixin {
   const AccountRoute();
+
+  @override
+  List<RouteGuard> get routeGuards => [const SessionUnlockedGuard()];
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
