f4e392be20d25b6d2bcaa15872e98d6e9db9203b
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2024-10-25 10:57:53 +0200

    feat(cat-voices): add route guard for account page (#1047)
    * feat(cat-voices): add route guard for account page
    
    * refactor: add composite route guard mixin
    
    * style: reformat

---
+++ catalyst_voices/lib/routes/guards/session_unlocked_guard.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:catalyst_voices/routes/guards/route_guard.dart';
+import 'package:catalyst_voices/routes/routing/routing.dart';
+import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+/// A guard which redirects to the discovery page
+/// if the keychain is locked.
+final class SessionUnlockedGuard implements RouteGuard {
+  const SessionUnlockedGuard();
+
+  @override
+  FutureOr<String?> redirect(BuildContext context, GoRouterState state) {
+    final sessionCubit = context.read<SessionCubit>();
+    if (sessionCubit.state is ActiveAccountSessionState) {
+      // if already unlocked skip redirection
+      return null;
+    } else {
+      return const DiscoveryRoute().location;
+    }
+  }
+}
