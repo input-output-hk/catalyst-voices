96cb81cc3a4ec28405ab2e4f5c64757865832992
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-10-25 20:17:33 +0700

    chore: fmtfix

--- catalyst-gateway/bin/src/service/api/legacy/v0/settings_get.rs
+++ catalyst-gateway/bin/src/service/api/legacy/v0/settings_get.rs
@@ -23,12 +23,18 @@ pub(crate) mod dto {
 
     #[derive(Object, Default)]
     pub(crate) struct SettingsInfoDto {
-        #[oai(rename = "block0Hash", validator(max_length = 66, min_length = 0, pattern = "[0-9a-f]"))]
+        #[oai(
+            rename = "block0Hash",
+            validator(max_length = 66, min_length = 0, pattern = "[0-9a-f]")
+        )]
         block_0_hash: String,
         #[oai]
         fees: Fees,
-        #[oai(rename = "slotsPerEpoch", validator(min_length = 1, pattern = "[0-9]+"))]
-        slots_per_epoch: String
+        #[oai(
+            rename = "slotsPerEpoch",
+            validator(min_length = 1, pattern = "[0-9]+")
+        )]
+        slots_per_epoch: String,
     }
 
     #[derive(Object, Default)]
@@ -37,19 +43,19 @@ pub(crate) mod dto {
         per_vote_certificate_fees: PerVoteCertificateFees,
         constant: u32,
         coefficient: u32,
-        certificate: u32
+        certificate: u32,
     }
 
     #[derive(Object, Default)]
     pub(crate) struct PerCertificateFee {
         certificate_pool_registration: u32,
         certificate_stake_delegation: u32,
-        certificate_owner_stake_delegation: u32
+        certificate_owner_stake_delegation: u32,
     }
 
     #[derive(Object, Default)]
     pub(crate) struct PerVoteCertificateFees {
         certificate_vote_plan: u32,
-        certificate_vote_cast: u32
+        certificate_vote_cast: u32,
     }
 }
