63a38cfe37b88ebe375006512e642f5886afb0e4
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-10-25 20:17:08 +0700

    feat: settings dto

--- catalyst-gateway/bin/src/service/api/legacy/v0/settings_get.rs
+++ catalyst-gateway/bin/src/service/api/legacy/v0/settings_get.rs
@@ -7,7 +7,7 @@ use crate::service::common::responses::WithErrorResponses;
 #[derive(ApiResponse)]
 pub(crate) enum Responses {
     #[oai(status = 200)]
-    Ok(Json<Vec<u8>>),
+    Ok(Json<dto::SettingsInfoDto>),
 }
 
 /// All responses
@@ -15,5 +15,41 @@ pub(crate) type AllResponses = WithErrorResponses<Responses>;
 
 #[allow(clippy::unused_async)]
 pub(crate) async fn endpoint() -> AllResponses {
-    Responses::Ok(Json(Vec::new())).into()
+    Responses::Ok(Json(dto::SettingsInfoDto::default())).into()
+}
+
+pub(crate) mod dto {
+    use poem_openapi::Object;
+
+    #[derive(Object, Default)]
+    pub(crate) struct SettingsInfoDto {
+        #[oai(rename = "block0Hash", validator(max_length = 66, min_length = 0, pattern = "[0-9a-f]"))]
+        block_0_hash: String,
+        #[oai]
+        fees: Fees,
+        #[oai(rename = "slotsPerEpoch", validator(min_length = 1, pattern = "[0-9]+"))]
+        slots_per_epoch: String
+    }
+
+    #[derive(Object, Default)]
+    pub(crate) struct Fees {
+        per_certificate_fees: PerCertificateFee,
+        per_vote_certificate_fees: PerVoteCertificateFees,
+        constant: u32,
+        coefficient: u32,
+        certificate: u32
+    }
+
+    #[derive(Object, Default)]
+    pub(crate) struct PerCertificateFee {
+        certificate_pool_registration: u32,
+        certificate_stake_delegation: u32,
+        certificate_owner_stake_delegation: u32
+    }
+
+    #[derive(Object, Default)]
+    pub(crate) struct PerVoteCertificateFees {
+        certificate_vote_plan: u32,
+        certificate_vote_cast: u32
+    }
 }
