5cafefb1aabe0d4a5711a700f0292b60087f7745
Dominik Toton <dominik.toton@iohk.io>
2024-10-28 10:27:08 +0100

    chore: run heave computation in a thread pool

--- catalyst_voices_packages/catalyst_key_derivation/rust/src/api/simple.rs
+++ catalyst_voices_packages/catalyst_key_derivation/rust/src/api/simple.rs
@@ -1,11 +1,24 @@
+use flutter_rust_bridge::spawn_blocking_with;
+use crate::frb_generated::FLUTTER_RUST_BRIDGE_HANDLER;
+
+// spawn_blocking_with works similary to tokio spawn_blocking
+// basically running blocking operations on a separate thread
+// Just use FLUTTER_RUST_BRIDGE_HANDLER.thread_pool() as the second argument
+// as mention in https://github.com/fzyzcjy/flutter_rust_bridge/blob/master/frb_rust/src/rust_async/io.rs
+// https://cjycode.com/flutter_rust_bridge/guides/cross-platform/async
 pub async fn greet(name: String) -> String {
-    let iterations = 50000000;
-    let mut result = 0.0;
-    for i in 0..iterations {
-        for j in 0..iterations {
-            result += (i as f64).sqrt();
+    let iterations = 50_000_000;
+    
+    let result = spawn_blocking_with(move || {
+        let mut sum = 0.0;
+        for i in 0..iterations {
+            sum += (i as f64).sqrt();
         }
-    }
+        sum
+    }, FLUTTER_RUST_BRIDGE_HANDLER.thread_pool())
+    .await 
+    .unwrap();
+
     format!("Hello, {name} {result}!")
 }
 
