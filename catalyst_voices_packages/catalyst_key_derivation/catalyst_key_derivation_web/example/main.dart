0153c8ee23fffce16fee28a0f544705f4c0c04d2
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2024-10-28 12:18:53 +0100

    feat(flutter/catalyst_key_derivation): Setup for flutter_rust_bridge in catalyst_key_derivation package (#1054)
    * feat: key derivation in rust bridge
    
    * feat: convert to async (wip)
    
    * chore: remove unused stuff
    
    * chore: update rust sample to block main thread, generate code
    
    * docs: add example how to run the app
    
    * docs: fix how to instructions
    
    * chore: drop unused files
    
    * chore: drop unused files
    
    * chore: add script
    
    * chore: fix script
    
    * chore: fix script
    
    * chore: run heave computation in a thread pool
    
    * chore: cleanup code, extract dependencies, add docs
    
    * chore: cleanup
    
    * chore: ignore pubspec.lock
    
    * chore: ignore pubspec.lock
    
    * chore: fix spelling
    
    * chore: fix markdown
    
    * chore: temporarily disable publishing for catalyst_key_derivation package

--- catalyst_voices_packages/catalyst_key_derivation/catalyst_key_derivation_web/example/main.dart
+++
@@ -1,4 +0,0 @@
-void main() {
-  // ignore: avoid_print
-  print('Example is located in catalyst_key_derivation/example directory.');
-}
---
+++ catalyst_voices_packages/catalyst_key_derivation/example/android/app/src/main/kotlin/com/example/catalyst_key_derivation_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.catalyst_key_derivation_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
