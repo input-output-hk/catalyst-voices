79f70a58bd69c5d71a2e8bba5b16366a667276a1
Damian Molinski <damian.molinski@iohk.io>
2024-10-28 09:01:15 +0100

    fix: Keychain metadata typo fix + migration tests

--- catalyst_voices/packages/catalyst_voices_models/lib/src/crypto/keychain_metadata.dart
+++ catalyst_voices/packages/catalyst_voices_models/lib/src/crypto/keychain_metadata.dart
@@ -2,41 +2,45 @@ import 'package:equatable/equatable.dart';
 
 // TODO(damian-molinski): Migrate serialization to json_serializable.
 final class KeychainMetadata extends Equatable {
-  final DateTime createAt;
+  final DateTime createdAt;
   final DateTime updatedAt;
 
   const KeychainMetadata({
-    required this.createAt,
+    required this.createdAt,
     required this.updatedAt,
   });
 
   factory KeychainMetadata.fromJson(Map<String, dynamic> json) {
+    // Typo migration
+    if (!json.containsKey('createdAt') && json.containsKey('createAt')) {
+      json['createdAt'] = json['createAt'];
+    }
     return KeychainMetadata(
-      createAt: DateTime.parse(json['createAt'] as String),
+      createdAt: DateTime.parse(json['createdAt'] as String),
       updatedAt: DateTime.parse(json['updatedAt'] as String),
     );
   }
 
   KeychainMetadata copyWith({
-    DateTime? createAt,
+    DateTime? createdAt,
     DateTime? updatedAt,
   }) {
     return KeychainMetadata(
-      createAt: createAt ?? this.createAt,
+      createdAt: createdAt ?? this.createdAt,
       updatedAt: updatedAt ?? this.updatedAt,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'createAt': createAt.toIso8601String(),
+      'createdAt': createdAt.toIso8601String(),
       'updatedAt': updatedAt.toIso8601String(),
     };
   }
 
   @override
   List<Object?> get props => [
-        createAt,
+        createdAt,
         updatedAt,
       ];
 }
