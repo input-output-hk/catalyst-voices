28e9678c9d588ce8be2cf585f2fbdc4483373b74
Dominik Toton <dominik.toton@iohk.io>
2024-10-28 10:05:51 +0100

    chore: fix script

--- catalyst_voices_packages/catalyst_key_derivation/run.sh
+++ catalyst_voices_packages/catalyst_key_derivation/run.sh
@@ -3,4 +3,6 @@
 flutter_rust_bridge_codegen generate
 flutter_rust_bridge_codegen build-web
 cd example
+rm -fR web/pkg
+cp -R ../web/pkg web/
 flutter run --web-header=Cross-Origin-Opener-Policy=same-origin --web-header=Cross-Origin-Embedder-Policy=require-corp -d chrome
