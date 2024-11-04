62e5f43778fab323d7c1e4ebab4b5e89c1ba0cb5
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2024-11-04 14:03:36 +0100

    refactor(cat-voices): self contained frontend dir (#1082)
    * refactor: move uikit_example into utilities dir
    
    * fix: exclude example packages from melos bootstrap
    
    * refactor: move poc_local_storage into catalyst_voices/utilities
    
    * refactor: move catalyst_voices_remote_widgets package to catalyst_voices/utilities dir
    
    * refactor: move scripts into catalyst_voices dir
    
    * refactor: move catalyst_voices/packages into catalyst_voices/packages/internal
    
    * refactor: move catalyst_voices_packages into catalyst_voices/packages/external
    
    * refactor: move melos.yaml into catalyst_voices
    
    * refactor: move app into apps/voices dir
    
    * refactor: rename packages/external to packages/libs
    
    * docs: update README
    
    * fix: builder target
    
    * fix: update paths in Earthfile
    
    * fix: uikit_example pubspec + libs examples deps override
    
    * chore: test
    
    * fix: update cspell and check markdown rules
    
    * fix: README formatting
    
    * fix: build-web target path
    
    * fix: remove build-web target suffix separator
    
    * fix: wallet-automation tests target path
    
    * fix: wallet-automation builder target
    
    * fix: voices automation tests target
    
    * fix: code-generator target path
    
    * fix: vscode launch.recommended paths
    
    * feat: catalyst_voices justfile
    
    * refactor: rename catalyst_voices_remote_widgets to remote_widgets
    
    * fix: remote_widget spelling and licence checks
    
    * fix: remote_widget analyzer
    
    * refactor: move repo-catalyst-voices from root Earthfile to catalyst_voices/Earthfile
    
    * fix: file drop
    
    * fix: force downgraded version of flutter_dropzone
    
    * chore: update repository links + codeowners paths
    
    * fix: deploy uikit workflow
    
    * fix: uikit_example earthly build target
    
    * fix: uikit deploy workflow path
    
    * chore: integration tests docs readme paths
    
    * chore: remote widget readme path
    
    * chore: markdown ignore macos pods path fix
    
    * chore: integration test scripts
    
    * chore: build_runner assets
    
    * chore: document flutter_dropzone issue
    
    ---------
    
    Co-authored-by: Dominik Toton <dominik.toton@iohk.io>

---
+++ catalyst_voices/scripts/flutter_web_integration_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Path to ChromeDriver in Ubuntu image in GitHub Workspace
+/usr/local/share/chromedriver-linux64/chromedriver --port=4444 &
+# Wait for 5 seconds to allow ChromeDriver to start
+sleep 5
+
+cd ./catalyst_voices/apps/voices
+
+flutter drive --driver=test_driver/integration_tests.dart \
+--target=integration_test/main.dart \
+--flavor development \
+-d web-server \
+--profile --browser-name=chrome
---
+++ catalyst_voices/utilities/remote_widgets/README.md
@@ -0,0 +1,8 @@
+# Catalyst Voices Remote Widgets
+
+> ‼️ This proof of concept and is not yet ready for production use.
+
+This package provides a set of widgets that can be used to build remote data-driven UIs.
+
+For  more navigate to the
+[example](/catalyst_voices/utilities/remote_widgets/example/README.md)
