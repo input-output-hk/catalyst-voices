685fd60d2de84b9f1d3ce30a767cc21f58f1edaf
bkioshn <bkioshn@gmail.com>
2024-11-04 16:43:23 +0700

    fix(cat-gateway): add endpoints
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- catalyst-gateway/bin/src/service/api/legacy/v0/mod.rs
+++ catalyst-gateway/bin/src/service/api/legacy/v0/mod.rs
@@ -1,9 +1,14 @@
 //! `v0` Endpoints
-use poem_openapi::{payload::Binary, OpenApi};
+use poem_openapi::{
+    payload::{Binary, Json},
+    OpenApi,
+};
 
 use crate::service::{
-    common::tags::ApiTags, utilities::middleware::schema_validation::schema_version_validation,
+    common::{objects::legacy::{challenges::Challenge, search::SearchQuery}, tags::ApiTags},
+    utilities::middleware::schema_validation::schema_version_validation,
 };
+mod challenges;
 mod message_post;
 mod plans_get;
 mod search;
@@ -40,8 +45,8 @@ impl V0Api {
         plans_get::endpoint().await
     }
 
-    /// Search various resources with various constraints
-    /// 
+    /// Search various resources with various constraints.
+    ///
     /// Search various resources especially challenges and proposals with various constraints like contains some string etc.
     #[oai(
         path = "/search",
@@ -49,43 +54,62 @@ impl V0Api {
         operation_id = "PostSearch",
         deprecated = true
     )]
-    async fn search_post(&self) -> search::AllResponses {
-        search::endpoint().await
+    async fn search_post(&self, body: Json<SearchQuery>) -> search::SearchAllResponses {
+        search::search_post(body).await
     }
 
-    // POST /api/v0/search_count
+    /// Returns count of the result set of search operation.
+    ///
+    /// Search various resources with various constraints and returns count of the result set.
     #[oai(
         path = "/search_count",
         method = "post",
         operation_id = "PostSearchCount",
         deprecated = true
     )]
-    // Get /api/v0/challenges/
+    async fn search_count_post(&self, body: Json<i32>) -> search::SearchCountAllResponses {
+        search::search_count_post(body).await
+    }
+
+    /// Get all available challenges
+    ///
+    /// Lists all available challenges following insertion order.
     #[oai(
         path = "/challenges",
         method = "get",
-        operation_id = "GetChallenges",
+        operation_id = "GetAllChallenges",
         deprecated = true
     )]
-    // Get /api/v0/challenges/{challenge_id}
+    async fn challenges_get(&self, body: Json<Challenge>) -> challenges::SearchAllResponses {
+            challenges::challenges_get(body).await
+        }
+
+    // // Get /api/v0/challenges/{challenge_id}
+    /// Get challenge by id.
+    /// 
+    /// Retrieves information on the identified challenge,
+    /// including the proposals submitted for it.
     #[oai(
-        path = "/challenges/{challenge_id}",
+        path = "/challenges/{id}",
         method = "get",
-        operation_id = "GetChallengeId",
+        operation_id = "GetChallengeById",
         deprecated = true
     )]
-    // Get /api/v0/node/stats
-    #[oai(
-        path = "/node/stats",
-        method = "get",
-        operation_id = "NodeStats",
-        deprecated = true
-    )]
-    // Get /api/v0/account/{account_id} 
-    #[oai(
-        path = "/account/{account_id}",
-        method = "get",
-        operation_id = "GetAccountId",
-        deprecated = true
-    )]
+    async fn challenges_id_get(&self) -> challenges::SearchAllResponses {
+        challenges::challenges_id_get(0).await
+    }
+    // // Get /api/v0/node/stats
+    // #[oai(
+    //     path = "/node/stats",
+    //     method = "get",
+    //     operation_id = "NodeStats",
+    //     deprecated = true
+    // )]
+    // // Get /api/v0/account/{account_id}
+    // #[oai(
+    //     path = "/account/{account_id}",
+    //     method = "get",
+    //     operation_id = "GetAccountId",
+    //     deprecated = true
+    // )]
 }
