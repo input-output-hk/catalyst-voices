47ac6ef8f25899c09590979751028ed7db56d998
Damian Molinski <damian.molinski@iohk.io>
2024-11-05 16:39:58 +0100

    chore: remove unused class

--- catalyst_voices/apps/voices/lib/routes/routing/routing.dart
+++ catalyst_voices/apps/voices/lib/routes/routing/routing.dart
@@ -2,6 +2,5 @@ export 'account_route.dart' hide $appRoutes;
 export 'coming_soon_route.dart' hide $appRoutes;
 export 'login_route.dart' hide $appRoutes;
 export 'overall_spaces_route.dart' hide $appRoutes;
-export 'router_refresh_stream.dart';
 export 'routes.dart';
 export 'spaces_route.dart' hide $appRoutes;
