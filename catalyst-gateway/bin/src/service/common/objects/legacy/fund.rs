eee866e997981a44d1d97f0193f83926be89d1ad
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-11-08 01:29:58 +0700

    docs: partial lintfix

--- catalyst-gateway/bin/src/service/common/objects/legacy/fund.rs
+++ catalyst-gateway/bin/src/service/common/objects/legacy/fund.rs
@@ -2,8 +2,10 @@
 
 use poem_openapi::Object;
 
+/// The fund object.
 #[allow(clippy::missing_docs_in_private_items)]
 #[derive(Object, Default)]
 pub(crate) struct Fund {
+    #[oai(validator(max_length = 256, min_length = 0, pattern = "[A-Za-z0-9_-]"))]
     id: String,
 }
