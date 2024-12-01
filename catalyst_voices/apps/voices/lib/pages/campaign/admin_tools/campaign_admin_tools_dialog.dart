cad8963163ab38b19ec9d921ad0027da9fb8ed60
Dominik Toton <dominik.toton@iohk.io>
2024-12-01 20:49:06 +0100

    feat: add views section to switch between auth user states

--- catalyst_voices/apps/voices/lib/pages/campaign/admin_tools/campaign_admin_tools_dialog.dart
+++ catalyst_voices/apps/voices/lib/pages/campaign/admin_tools/campaign_admin_tools_dialog.dart
@@ -1,12 +1,16 @@
+import 'dart:async';
+
 import 'package:catalyst_voices/common/ext/space_ext.dart';
 import 'package:catalyst_voices/widgets/widgets.dart';
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
+import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// A draggable [CampaignAdminToolsDialog],
 /// should be used as a child of a [Stack].
@@ -320,7 +324,7 @@ class _EventItem extends StatelessWidget {
             Expanded(
               child: Text(
                 _text(context.l10n),
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                       color: color,
                     ),
               ),
@@ -328,7 +332,7 @@ class _EventItem extends StatelessWidget {
             if (isActive)
               Text(
                 context.l10n.active.toUpperCase(),
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                       fontWeight: FontWeight.w500,
                       fontSize: 11,
                       color: color,
@@ -396,16 +400,102 @@ class _EventTimelapseControls extends StatelessWidget {
   }
 }
 
-class _ViewsTab extends StatelessWidget {
+class _ViewsTab extends StatefulWidget {
   const _ViewsTab();
 
   @override
+  State<_ViewsTab> createState() => _ViewsTabState();
+}
+
+class _ViewsTabState extends State<_ViewsTab> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      color: Colors.blue,
+    return Material(
+      color: Theme.of(context).colors.elevationsOnSurfaceNeutralLv1Grey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              context.l10n.userAuthenticationState,
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+            const SizedBox(height: 16),
+            BlocBuilder<SessionCubit, SessionState>(
+              builder: (context, state) {
+                return VoicesSegmentedButton<_UserAuthState>(
+                  segments: [
+                    for (final userState in _UserAuthState.values)
+                      ButtonSegment(
+                        value: userState,
+                        label: Text(userState.name(context.l10n)),
+                      ),
+                  ],
+                  selected: {_UserAuthState.fromSessionState(state)},
+                  onChanged: (selected) {
+                    unawaited(_switchToState(selected.first));
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> _switchToState(_UserAuthState state) async {
+    switch (state) {
+      case _UserAuthState.actor:
+        await _switchToActor();
+      case _UserAuthState.guest:
+        await _switchToGuest();
+      case _UserAuthState.visitor:
+        await _switchToVisitor();
+    }
+  }
+
+  Future<void> _switchToActor() async {
+    final sessionBloc = context.read<SessionCubit>();
+    await sessionBloc
+        .switchToDummyAccount()
+        .then((_) => sessionBloc.unlock(SessionCubit.dummyUnlockFactor));
+  }
+
+  Future<void> _switchToGuest() async {
+    final sessionBloc = context.read<SessionCubit>();
+    if (sessionBloc.state is ActiveAccountSessionState) {
+      await sessionBloc.lock();
+    } else if (sessionBloc.state is VisitorSessionState) {
+      await sessionBloc.switchToDummyAccount().then((_) => sessionBloc.lock());
+    }
+  }
+
+  Future<void> _switchToVisitor() async {
+    await context.read<SessionCubit>().removeKeychain();
+  }
+}
+
+enum _UserAuthState {
+  actor,
+  guest,
+  visitor;
+
+  factory _UserAuthState.fromSessionState(SessionState state) {
+    return switch (state) {
+      VisitorSessionState() => _UserAuthState.visitor,
+      GuestSessionState() => _UserAuthState.guest,
+      ActiveAccountSessionState() => _UserAuthState.actor,
+    };
+  }
+
+  String name(VoicesLocalizations l10n) {
+    return switch (this) {
+      _UserAuthState.actor => l10n.actor,
+      _UserAuthState.guest => l10n.guest,
+      _UserAuthState.visitor => l10n.visitor,
+    };
   }
 }
 
