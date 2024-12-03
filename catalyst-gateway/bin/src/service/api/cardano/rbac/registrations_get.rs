46f73442ccc187b7e1e6b792c53eeab38ee9054f
Apisit Ritruengroj <38898766+apskhem@users.noreply.github.com>
2024-12-03 22:26:53 +0700

    fix(cat-gateway): bump `scylla` to v0.15.0 (#1316)
    * refactor: initial
    
    * fix: arc type
    
    * fix: query iter
    
    * fix: functions
    
    * fix: final
    
    * chore: fmtfix
    
    * chore: remove lints
    
    * chore: remove lint from database object
    
    * chore: remove result wrapper

--- catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get.rs
+++ catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get.rs
@@ -1,6 +1,6 @@
 //! Implementation of the GET `/rbac/registrations` endpoint.
 use anyhow::anyhow;
-use futures::StreamExt as _;
+use futures::StreamExt;
 use poem_openapi::{payload::Json, ApiResponse, Object};
 use tracing::error;
 
