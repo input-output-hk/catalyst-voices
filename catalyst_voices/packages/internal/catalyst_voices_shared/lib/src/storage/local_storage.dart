5c05aa77a33deff4e8de5f216d653f7baa3b3ebf
Damian Molinski <damian.molinski@iohk.io>
2024-12-06 16:34:01 +0100

    feat: local storage

---
+++ catalyst_voices/packages/internal/catalyst_voices_shared/lib/src/storage/local_storage.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
+import 'package:catalyst_voices_shared/src/storage/storage_string_mixin.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+base class LocalStorage with StorageAsStringMixin implements Storage {
+  final String key;
+  final SharedPreferencesAsync _sharedPreferences;
+
+  LocalStorage({
+    this.key = 'LocalStorage',
+    SharedPreferencesAsync? sharedPreferences,
+  }) : _sharedPreferences = sharedPreferences ?? SharedPreferencesAsync();
+
+  @override
+  Future<bool> contains({required String key}) {
+    final effectiveKey = _effectiveKey(key);
+
+    return _sharedPreferences.containsKey(effectiveKey);
+  }
+
+  @override
+  Future<String?> readString({required String key}) {
+    final effectiveKey = _effectiveKey(key);
+
+    return _sharedPreferences.getString(effectiveKey);
+  }
+
+  @override
+  Future<void> writeString(
+    String? value, {
+    required String key,
+  }) async {
+    final effectiveKey = _effectiveKey(key);
+
+    if (value != null) {
+      await _sharedPreferences.setString(effectiveKey, value);
+    } else {
+      await _sharedPreferences.remove(effectiveKey);
+    }
+  }
+
+  @override
+  Future<void> clear() async {
+    final keys = await _sharedPreferences.getKeys();
+    final storageKeys =
+        List.of(keys).where((element) => element.startsWith(key));
+
+    for (final key in storageKeys) {
+      await _sharedPreferences.remove(key);
+    }
+  }
+
+  String _effectiveKey(String value) {
+    return '$key.$value';
+  }
+}
