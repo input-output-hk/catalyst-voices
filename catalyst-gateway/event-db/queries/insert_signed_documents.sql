41162e9efd2565cac61c31e9b02b6fed0b2922d2
Mr-Leshiy <leshiy12345678@gmail.com>
2024-12-10 13:14:37 +0200

    fix sqlfluff

--- catalyst-gateway/event-db/queries/insert_signed_documents.sql
+++ catalyst-gateway/event-db/queries/insert_signed_documents.sql
@@ -12,10 +12,11 @@ VALUES
 ($1, $2, $3, $4, $5, $6, $7)
 ON CONFLICT (id, ver) DO UPDATE
 SET
-  type = signed_docs.type
-WHERE 
-  signed_docs.type = EXCLUDED.type
-  AND signed_docs.author = EXCLUDED.author
-  AND signed_docs.metadata = EXCLUDED.metadata
-  AND signed_docs.payload = EXCLUDED.payload
-  AND signed_docs.raw = EXCLUDED.raw
+type = signed_docs.type
+
+WHERE
+signed_docs.type = excluded.type
+AND signed_docs.author = excluded.author
+AND signed_docs.metadata = excluded.metadata
+AND signed_docs.payload = excluded.payload
+AND signed_docs.raw = excluded.raw
