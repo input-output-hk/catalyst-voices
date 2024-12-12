cde922d2ea3ec2182277dddeb1b3716451aa80b9
Damian Molinski <damian.molinski@iohk.io>
2024-12-12 16:02:26 +0100

    chore: typo

--- catalyst_voices/packages/internal/catalyst_voices_shared/lib/src/utils/active_aware.dart
+++ catalyst_voices/packages/internal/catalyst_voices_shared/lib/src/utils/active_aware.dart
@@ -3,7 +3,7 @@
 /// Active objects are typically performing some kind of work or are ready to
 /// do so. Inactive objects are not currently performing any work.
 abstract interface class ActiveAware {
-  /// Whether this vault is currently beaning used.
+  /// Whether this vault is currently being used.
   bool get isActive;
 
   /// Updates usage status.
