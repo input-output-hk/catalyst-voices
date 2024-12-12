bc762dc98d1fc84b373741136988ffa1fbc8d867
Mr-Leshiy <leshiy12345678@gmail.com>
2024-12-12 11:45:46 +0200

    add upsert signed docs query

---
+++ catalyst-gateway/bin/src/db/event/signed_docs/mod.rs
@@ -0,0 +1,24 @@
+//! Signed docs queries
+
+use super::EventDB;
+
+/// Upsert sql query
+const UPSERT_SIGNED_DOCS: &str = include_str!("./sql/upsert_signed_documents.sql");
+
+/// Make an upsert query into the `event-db` by adding data into the `signed_docs` table
+///
+/// * IF the record primary key (id,ver) does not exist, then add the new record. Return
+///   success.
+/// * IF the record does exist, but all values are the same as stored, return Success.
+/// * Otherwise return an error. (Can not over-write an existing record with new data).
+#[allow(dead_code)]
+pub(crate) async fn upsert_signed_docs(
+    id: &uuid::Uuid, ver: &uuid::Uuid, doc_type: &uuid::Uuid, author: &String,
+    metadata: &serde_json::Value, payload: &serde_json::Value, raw: &Vec<u8>,
+) -> anyhow::Result<()> {
+    EventDB::modify(UPSERT_SIGNED_DOCS, &[
+        id, ver, doc_type, author, metadata, payload, raw,
+    ])
+    .await?;
+    Ok(())
+}
