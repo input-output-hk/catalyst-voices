0118b2718fe0af8178f624d486f9436ea79619bd
Damian Molinski <damian.molinski@iohk.io>
2024-12-12 16:01:41 +0100

    docs: AppActiveStateListener

--- catalyst_voices/apps/voices/lib/app/view/app_active_state_listener.dart
+++ catalyst_voices/apps/voices/lib/app/view/app_active_state_listener.dart
@@ -2,6 +2,8 @@ import 'package:catalyst_voices/dependency/dependencies.dart';
 import 'package:catalyst_voices_services/catalyst_voices_services.dart';
 import 'package:flutter/material.dart';
 
+/// Observes application lifecycle and updates any dependencies that are
+/// interested in app state.
 class AppActiveStateListener extends StatefulWidget {
   final Widget child;
 
