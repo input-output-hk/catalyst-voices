e3754b29280e8b98e6c0c747cafb82c30bb6bfff
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-12-19 20:49:37 +0700

    chore: tmp

--- catalyst-gateway/bin/src/db/index/tests/scylla_session.rs
+++ catalyst-gateway/bin/src/db/index/tests/scylla_session.rs
@@ -1,10 +1,10 @@
 //! Integration tests of the `IndexDB` queries testing on its session
 
+use crate::db::index::session::CassandraSession;
+
 #[ignore = "An integration test which requires a running Scylla node instance, disabled from `testunit` CI run"]
 #[tokio::test]
 async fn test_session() -> Result<(), String> {
-    use crate::db::index::session::CassandraSession;
-
     let Some(_) = CassandraSession::get(true) else {
         return Err(String::from("Failed to acquire db session"));
     };
