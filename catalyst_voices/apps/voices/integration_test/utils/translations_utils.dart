f3f68587ee1bf45db4f674a2e9c1ef7168f65fe5
Janusz Janus <janusz.janus@iohk.io>
2024-12-24 16:01:11 +0100

    formatting fix (#1436)

--- catalyst_voices/apps/voices/integration_test/utils/translations_utils.dart
+++ catalyst_voices/apps/voices/integration_test/utils/translations_utils.dart
@@ -1,6 +1,6 @@
-//wrapper that we should adapt to read actual i18n translations we use in the app
+//wrapper that we should adapt to read actual i18n translations we use in app
 //it will also support different locales once we have it
-//right now this class is here so we can easily replace this implementation and know where
+//now this is here so we can easily replace this implementation and know where
 class T {
   static String get(String key, {String? locale}) {
     return key;
