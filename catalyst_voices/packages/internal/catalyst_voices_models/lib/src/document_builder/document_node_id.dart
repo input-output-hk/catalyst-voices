e6f41e99be4e98221b96dc05385438792e05b14a
Dominik Toton <dominik.toton@iohk.io>
2024-12-24 12:22:22 +0100

    chore: convert document node id to a class

--- catalyst_voices/packages/internal/catalyst_voices_models/lib/src/document_builder/document_node_id.dart
+++ catalyst_voices/packages/internal/catalyst_voices_models/lib/src/document_builder/document_node_id.dart
@@ -1,8 +1,19 @@
+import 'package:equatable/equatable.dart';
+
 /// The unique id of an object in a document for segments/sections/elements
 /// in a format of paths from the top-level node down to the nested node.
-extension type const DocumentNodeId._(List<String> paths) {
+final class DocumentNodeId extends Equatable {
   /// The top-level node in the document.
   static const DocumentNodeId root = DocumentNodeId._([]);
+
+  /// The nested paths in a document that lead to an object.
+  final List<String> paths;
+
+  /// The default constructor for the [DocumentNodeId].
+  ///
+  /// From outside this file new nodes must be created by interacting
+  /// with [root] node and calling either [child] or [parent] methods.
+  const DocumentNodeId._(this.paths);
 
   /// Returns a parent node.
   ///
@@ -25,4 +36,10 @@ extension type const DocumentNodeId._(List<String> paths) {
       path,
     ]);
   }
+
+  @override
+  List<Object?> get props => paths;
+
+  @override
+  String toString() => paths.join('.');
 }
