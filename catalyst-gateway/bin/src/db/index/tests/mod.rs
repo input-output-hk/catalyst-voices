dc5ad4e1be221052c79d4d3cb4fb90660264ef3b
Apisit Ritruengroj <38898766+apskhem@users.noreply.github.com>
2025-01-01 16:37:47 +0700

    test(cat-gateway): Add CRUD queries testing into Scylla integration test (#1448)
    * feat: init
    
    * feat: get assets by stake address
    
    * feat: shared session
    
    * fix: cspell
    
    * chore: spelling
    
    * feat: add test filter
    
    * feat: testing template
    
    * feat: query testing
    
    * feat: cleanup and insert
    
    * feat: row reading

--- catalyst-gateway/bin/src/db/index/tests/mod.rs
+++ catalyst-gateway/bin/src/db/index/tests/mod.rs
@@ -1,13 +1,18 @@
 //! Integration tests of the `IndexDB` queries
+//! This module contains utility functions used with different testing modules.
 
 use std::sync::Arc;
 
+use tokio::sync::OnceCell;
+
 use super::session::CassandraSession;
 
-mod scylla_schema;
+mod scylla_queries;
 mod scylla_session;
 
-pub(super) async fn setup_test_database() -> Result<(), String> {
+static SHARED_SESSION: OnceCell<Result<(), String>> = OnceCell::const_new();
+
+async fn setup_test_database() -> Result<(), String> {
     CassandraSession::init();
 
     CassandraSession::wait_is_ready(core::time::Duration::from_secs(1)).await;
@@ -19,7 +24,7 @@ pub(super) async fn setup_test_database() -> Result<(), String> {
     Ok(())
 }
 
-pub(super) fn get_session() -> Result<(Arc<CassandraSession>, Arc<CassandraSession>), String> {
+fn get_session() -> Result<(Arc<CassandraSession>, Arc<CassandraSession>), String> {
     let Some(persistent) = CassandraSession::get(true) else {
         return Err(String::from("Failed to acquire db session"));
     };
@@ -28,4 +33,10 @@ pub(super) fn get_session() -> Result<(Arc<CassandraSession>, Arc<CassandraSessi
     };
 
     Ok((persistent, volatile))
+}
+
+async fn get_shared_session() -> Result<(Arc<CassandraSession>, Arc<CassandraSession>), String> {
+    SHARED_SESSION.get_or_init(setup_test_database).await;
+
+    get_session()
 }
