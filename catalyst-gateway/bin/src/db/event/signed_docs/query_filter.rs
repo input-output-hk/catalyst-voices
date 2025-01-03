6b25bd9805455da40b904bf557136632f1205ac7
Alex Pozhylenkov <leshiy12345678@gmail.com>
2025-01-03 12:58:11 +0200

    feat(cat-gateway): Add simple `signed_docs` select query, update insert query. (#1393)
    * add upsert signed docs query
    
    * add integration tests for signed docs query
    
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * wip
    
    * remake query from upsert to insert
    
    * add simple select signed docs query
    
    * cleanup jinja, add jinit initialization
    
    * update tests
    
    * fix
    
    * fix spelling
    
    * add advanced select query
    
    * fix docs
    
    * add QueryLimits
    
    * wip
    
    * wip
    
    * update tests
    
    * fix docs
    
    * fix cardano-chain-follower dep, remove init of jinja
    
    * refactor QueryLimits struct
    
    * revert select_signed_docs query
    
    * refactor QueryLimits
    
    * wip
    
    * rename QueryLimits methods
    
    * replace `new` with TryFrom impl
    
    * wip
    
    * wip
    
    * wip
    
    * refactor
    
    * fix fmt
    
    * wip
    
    * move docs query filters into different mod
    
    * refactor `filtered_select_signed_docs` function
    
    * wip
    
    * replace query_stmt with trait Display impl
    
    * rename methods
    
    * add query_stream fn
    
    * fix clippy
    
    * fix test
    
    * get rid of usage of `pin!`
    
    ---------
    
    Co-authored-by: Steven Johnson <stevenj@users.noreply.github.com>

---
+++ catalyst-gateway/bin/src/db/event/signed_docs/query_filter.rs
@@ -0,0 +1,32 @@
+//! `DocsQueryFilter` struct implementation.
+
+use std::fmt::Display;
+
+/// A `select_signed_docs` query filtering argument.
+#[allow(dead_code)]
+pub(crate) enum DocsQueryFilter {
+    /// All entries
+    All,
+    /// Select docs with the specific `type` field
+    DocType(uuid::Uuid),
+    /// Select docs with the specific `id` field
+    DocId(uuid::Uuid),
+    /// Select docs with the specific `id` and `ver` field
+    DocVer(uuid::Uuid, uuid::Uuid),
+    /// Select docs with the specific `author` field
+    Author(String),
+}
+
+impl Display for DocsQueryFilter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::All => write!(f, "TRUE"),
+            Self::DocType(doc_type) => write!(f, "signed_docs.type = '{doc_type}'"),
+            Self::DocId(id) => write!(f, "signed_docs.id = '{id}'"),
+            Self::DocVer(id, ver) => {
+                write!(f, "signed_docs.id = '{id}' AND signed_docs.ver = '{ver}'")
+            },
+            Self::Author(author) => write!(f, "signed_docs.author = '{author}'"),
+        }
+    }
+}
--- catalyst-gateway/bin/src/db/event/signed_docs/tests/mod.rs
+++ catalyst-gateway/bin/src/db/event/signed_docs/tests/mod.rs
@@ -1,50 +1,116 @@
 //! Integration tests of the `signed docs` queries
 
+use futures::TryStreamExt;
+
 use super::*;
-use crate::db::event::establish_connection;
+use crate::db::event::{common::query_limits::QueryLimits, establish_connection};
 
 #[ignore = "An integration test which requires a running EventDB instance, disabled from `testunit` CI run"]
 #[tokio::test]
-async fn some_test() {
+async fn queries_test() {
     establish_connection();
 
-    let docs = [
-        (
-            uuid::Uuid::now_v7(),
-            uuid::Uuid::now_v7(),
-            uuid::Uuid::new_v4(),
-            "Alex".to_string(),
-            &Some(serde_json::Value::Null),
-            &Some(serde_json::Value::Null),
+    let doc_type = uuid::Uuid::new_v4();
+
+    let docs = vec![
+        FullSignedDoc::new(
+            SignedDocBody::new(
+                uuid::Uuid::now_v7(),
+                uuid::Uuid::now_v7(),
+                doc_type,
+                "Alex".to_string(),
+                Some(serde_json::Value::Null),
+            ),
+            Some(serde_json::Value::Null),
             vec![1, 2, 3, 4],
         ),
-        (
-            uuid::Uuid::now_v7(),
-            uuid::Uuid::now_v7(),
-            uuid::Uuid::new_v4(),
-            "Steven".to_string(),
-            &Some(serde_json::Value::Null),
-            &Some(serde_json::Value::Null),
+        FullSignedDoc::new(
+            SignedDocBody::new(
+                uuid::Uuid::now_v7(),
+                uuid::Uuid::now_v7(),
+                doc_type,
+                "Steven".to_string(),
+                Some(serde_json::Value::Null),
+            ),
+            Some(serde_json::Value::Null),
             vec![5, 6, 7, 8],
         ),
-        (
-            uuid::Uuid::now_v7(),
-            uuid::Uuid::now_v7(),
-            uuid::Uuid::new_v4(),
-            "Sasha".to_string(),
-            &None,
-            &None,
+        FullSignedDoc::new(
+            SignedDocBody::new(
+                uuid::Uuid::now_v7(),
+                uuid::Uuid::now_v7(),
+                doc_type,
+                "Sasha".to_string(),
+                None,
+            ),
+            None,
             vec![9, 10, 11, 12],
         ),
     ];
 
-    for (id, ver, doc_type, author, metadata, payload, raw) in &docs {
-        insert_signed_docs(id, ver, doc_type, author, metadata, payload, raw)
+    for doc in &docs {
+        doc.store().await.unwrap();
+        // try to insert the same data again
+        doc.store().await.unwrap();
+        // try another doc with the same `id` and `ver` and with different other fields
+        let another_doc = FullSignedDoc::new(
+            SignedDocBody::new(*doc.id(), *doc.ver(), doc_type, "Neil".to_string(), None),
+            None,
+            vec![],
+        );
+        assert!(another_doc.store().await.is_err());
+
+        let res_doc = FullSignedDoc::retrieve(doc.id(), Some(doc.ver()))
             .await
             .unwrap();
-        // // try to insert the same data again
-        // insert_signed_docs(id, ver, doc_type, author, metadata, payload, raw)
-        //     .await
-        //     .unwrap();
+        assert_eq!(doc, &res_doc);
+
+        let res_doc = FullSignedDoc::retrieve(doc.id(), None).await.unwrap();
+        assert_eq!(doc, &res_doc);
+
+        let mut res_docs =
+            SignedDocBody::retrieve(&DocsQueryFilter::DocId(*doc.id()), &QueryLimits::ALL)
+                .await
+                .unwrap();
+        let res_doc = res_docs.try_next().await.unwrap().unwrap();
+        assert_eq!(doc.body(), &res_doc);
+        assert!(res_docs.try_next().await.unwrap().is_none());
+
+        let mut res_docs = SignedDocBody::retrieve(
+            &DocsQueryFilter::DocVer(*doc.id(), *doc.ver()),
+            &QueryLimits::ALL,
+        )
+        .await
+        .unwrap();
+        let res_doc = res_docs.try_next().await.unwrap().unwrap();
+        assert_eq!(doc.body(), &res_doc);
+        assert!(res_docs.try_next().await.unwrap().is_none());
+
+        let mut res_docs = SignedDocBody::retrieve(
+            &DocsQueryFilter::Author(doc.author().clone()),
+            &QueryLimits::ALL,
+        )
+        .await
+        .unwrap();
+        let res_doc = res_docs.try_next().await.unwrap().unwrap();
+        assert_eq!(doc.body(), &res_doc);
+        assert!(res_docs.try_next().await.unwrap().is_none());
+    }
+
+    let mut res_docs =
+        SignedDocBody::retrieve(&DocsQueryFilter::DocType(doc_type), &QueryLimits::ALL)
+            .await
+            .unwrap();
+    for exp_doc in docs.iter().rev() {
+        let res_doc = res_docs.try_next().await.unwrap().unwrap();
+        assert_eq!(exp_doc.body(), &res_doc);
+    }
+
+    let mut res_docs = SignedDocBody::retrieve(&DocsQueryFilter::All, &QueryLimits::ALL)
+        .await
+        .unwrap();
+    for exp_doc in docs.iter().rev() {
+        let res_doc = res_docs.try_next().await.unwrap().unwrap();
+        assert_eq!(exp_doc.body(), &res_doc);
     }
 }
