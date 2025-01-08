911be19d6c3b05d8ef817aaa12b34e356c75e1dd
Damian Molinski <damian.molinski@iohk.io>
2025-01-08 15:12:13 +0100

    chore: cleanup ProposalBuilderBloc

--- catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/proposal_builder/proposal_builder_bloc.dart
+++ catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/proposal_builder/proposal_builder_bloc.dart
@@ -1,18 +1,12 @@
 import 'package:catalyst_voices_blocs/src/proposal_builder/proposal_builder_event.dart';
 import 'package:catalyst_voices_blocs/src/proposal_builder/proposal_builder_state.dart';
-import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_services/catalyst_voices_services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 final class ProposalBuilderBloc
     extends Bloc<ProposalBuilderEvent, ProposalBuilderState> {
+  // ignore: unused_field
   final CampaignService _campaignService;
-
-  final _answers = <NodeId, MarkdownData>{};
-  final _guidances = <NodeId, List<Guidance>>{};
-
-  String? proposalId;
-  NodeId? _activeStepId;
 
   ProposalBuilderBloc(
     this._campaignService,
@@ -25,73 +19,15 @@ final class ProposalBuilderBloc
   Future<void> _loadProposal(
     LoadProposalEvent event,
     Emitter<ProposalBuilderState> emit,
-  ) async {
-    /*   _answers.clear();
-    _guidances.clear();
-
-    final activeCampaign = await _campaignService.getActiveCampaign();
-    if (activeCampaign == null) {
-      emit(
-        state.copyWith(
-          sections: [],
-          guidance: const ProposalGuidance(isNoneSelected: true),
-        ),
-      );
-      return;
-    }
-
-    final template = activeCampaign.proposalTemplate;
-
-    final sections = template.sections.map(_mapProposalSection).toList();
-
-    // TODO(damian-molinski): To be reimplemented.
-    */ /*for (final section in template.sections) {
-      for (final step in section.steps) {
-        final id = (sectionId: section.id, stepId: step.id);
-        _guidances[id] = step.guidances;
-      }
-    }
-*/ /*
-    final activeStepId = _activeStepId;
-    final guidances = _guidances[activeStepId] ?? <Guidance>[];
-    final guidance = ProposalGuidance(
-      isNoneSelected: activeStepId == null,
-      guidances: guidances,
-    );
-
-    emit(
-      state.copyWith(
-        sections: sections,
-        guidance: guidance,
-      ),
-    );*/
-  }
+  ) async {}
 
   void _updateStepAnswer(
     UpdateStepAnswerEvent event,
     Emitter<ProposalBuilderState> emit,
-  ) {
-    final answer = event.data;
-    if (answer != null) {
-      _answers[event.id] = answer;
-    } else {
-      _answers.remove(event.id);
-    }
-  }
+  ) {}
 
   void _handleActiveStepEvent(
     ActiveStepChangedEvent event,
     Emitter<ProposalBuilderState> emit,
-  ) {
-    _activeStepId = event.id;
-
-    final activeStepId = _activeStepId;
-    final guidances = _guidances[activeStepId] ?? <Guidance>[];
-    final guidance = ProposalGuidance(
-      isNoneSelected: activeStepId == null,
-      guidances: guidances,
-    );
-
-    emit(state.copyWith(guidance: guidance));
-  }
+  ) {}
 }
