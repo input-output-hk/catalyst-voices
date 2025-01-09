13b3f93a4efb197c3b87c4e54c8cd39d9eafd665
januszjanus <janusz.janus@iohk.io>
2025-01-09 11:12:13 +0100

    style fix

--- catalyst_voices/apps/voices/integration_test/utils/test_context.dart
+++ catalyst_voices/apps/voices/integration_test/utils/test_context.dart
@@ -32,7 +32,7 @@ class TestContext {
       return TestContext.instance.context[key]!;
     }
     throw Exception(
-        'You tried to access $key property, but it does not exist.');
+        'You tried to access $key property, but it does not exist.',);
   }
 
   static void clearContext() {
