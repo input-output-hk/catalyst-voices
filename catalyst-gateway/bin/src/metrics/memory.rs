a606c269c56d64c9f38b2759391aff2b0b8d17dc
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-01-13 18:41:31 +0700

    feat: memory tracker

--- catalyst-gateway/bin/src/metrics/memory.rs
+++ catalyst-gateway/bin/src/metrics/memory.rs
@@ -1,2 +1,57 @@
 //! Metrics related to memory analytics.
 
+use memory_stats::memory_stats;
+use stats_alloc::{Region, Stats, StatsAlloc, INSTRUMENTED_SYSTEM};
+use std::alloc::System;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+lazy_static::lazy_static! {
+  static ref GLOBAL_METRICS: Arc<Mutex<MemoryMetrics>> = Arc::new(Mutex::new(MemoryMetrics::default()));
+}
+
+const UPDATE_INTERVAL_MILLI: u64 = 1000;
+
+/// Use the instrumented allocator for gathering allocation statistics.
+#[global_allocator]
+static GLOBAL: &StatsAlloc<System> = &INSTRUMENTED_SYSTEM;
+
+#[derive(Debug, Default)]
+struct MemoryMetrics {
+    pub(crate) allocator_stats: Stats,
+    pub(crate) physical_usage: Option<usize>,
+    pub(crate) virtual_usage: Option<usize>,
+}
+
+impl MemoryMetrics {
+    fn update(&mut self) {
+        let stats = Region::new(&GLOBAL);
+
+        self.allocator_stats = stats.change();
+
+        if let Some(mem_stats) = memory_stats() {
+            self.physical_usage = Some(mem_stats.physical_mem);
+            self.virtual_usage = Some(mem_stats.virtual_mem);
+        } else {
+            self.physical_usage = None;
+            self.virtual_usage = None;
+        }
+    }
+}
+
+pub(crate) fn start_metrics_updater() {
+    let metrics_clone = Arc::clone(&GLOBAL_METRICS);
+    thread::spawn(move || {
+        let interval = Duration::from_millis(UPDATE_INTERVAL_MILLI); // Update every 5 seconds
+        loop {
+            {
+                let mut metrics = metrics_clone.lock().unwrap();
+                metrics.update();
+            }
+            thread::sleep(interval);
+        }
+    });
+}
