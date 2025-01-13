53a4ddbe1065b327b5a0e303f31b07d479aaed1d
Emir Hodzic <emir.hodzich@gmail.com>
2025-01-13 12:47:40 +0100

    test(cat-voices): Adding account tests (#1491)

--- catalyst_voices/apps/voices/integration_test/pageobject/app_bar_page.dart
+++ catalyst_voices/apps/voices/integration_test/pageobject/app_bar_page.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppBarPage {
   static const spacesDrawerButton = Key('DrawerButton');
   static const getStartedBtn = Key('GetStartedButton');
+  static const accountPopupBtn = Key('AccountPopupButton');
 }
