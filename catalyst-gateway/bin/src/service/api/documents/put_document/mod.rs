f9eca949ca1b21e89ab5eca27ca2925687405cfa
Steven Johnson <stevenj@users.noreply.github.com>
2025-01-15 16:31:39 +0700

    feat(cat-gateway): Add document endpoints (#1470)
    * feat(cat-gateway): Add document get endpoint
    
    * feat(cat-gateway): Add generic cbor payload type
    
    * fix(docs): spelling
    
    * fix(cat-gateway): code format
    
    * feat(cat-gateway): Add document put endpoint
    
    * fix(cat-gateway): fix documentation for the error responses.
    
    * feat(general): Bump cat-ci to v3.2.31 to get documentation updates
    
    * feat(docs): Add alternative OpenAPI viewers because Elements has issues
    
    * feat(cat-gateway): Add document index endpoint (request side only)
    
    * feat(cat-gateway): Add response type for document index endpoint
    
    * fix(cat-gateway): code format
    
    * fix api lint errors
    
    * fix(cat-gateway): Response Documentation consistency for document endpoints
    
    * feat: increase time schemathesis waits for schema
    
    ---------
    
    Co-authored-by: kukkok3 <93382903+kukkok3@users.noreply.github.com>

---
+++ catalyst-gateway/bin/src/service/api/documents/put_document/mod.rs
@@ -0,0 +1,50 @@
+//! Implementation of the PUT `/document` endpoint
+
+use bad_put_request::PutDocumentBadRequest;
+use bytes::Bytes;
+use poem_openapi::{payload::Json, ApiResponse};
+
+use crate::service::common::responses::WithErrorResponses;
+
+pub(crate) mod bad_put_request;
+
+/// Maximum size of a Signed Document (1MB)
+pub(crate) const MAXIMUM_DOCUMENT_SIZE: usize = 1_048_576;
+
+/// Endpoint responses.
+#[derive(ApiResponse)]
+#[allow(dead_code)]
+pub(crate) enum Responses {
+    /// ## Created
+    ///
+    /// The Document was stored OK for the first time.
+    #[oai(status = 201)]
+    Created,
+    /// ## No Content
+    ///
+    /// The Document was already stored, and has not changed.
+    #[oai(status = 204)]
+    NoContent,
+    /// ## Bad Request
+    ///
+    /// Error Response. The document submitted is invalid.
+    #[oai(status = 400)]
+    BadRequest(Json<PutDocumentBadRequest>),
+    /// ## Content Too Large
+    ///
+    /// Payload Too Large. The document exceeds the maximum size of a legitimate single
+    /// document.
+    #[oai(status = 413)]
+    PayloadTooLarge,
+}
+
+/// All responses.
+pub(crate) type AllResponses = WithErrorResponses<Responses>;
+
+/// # PUT `/document`
+#[allow(clippy::unused_async, clippy::no_effect_underscore_binding)]
+pub(crate) async fn endpoint(document: Bytes) -> AllResponses {
+    let _doc = document;
+
+    Responses::BadRequest(Json(PutDocumentBadRequest::new("unimplemented"))).into()
+}
