e7bf4c9b551e1ea720f04c405a612ff108551843
Damian Molinski <damian.molinski@iohk.io>
2025-01-17 11:55:37 +0100

    feat: create base profile stage enum

---
+++ catalyst_voices/packages/internal/catalyst_voices_models/lib/src/registration/steps/create_base_profile_stage.dart
@@ -0,0 +1,31 @@
+/// Describes the base profile creation during registration.
+enum CreateBaseProfileStage {
+  /// Givens context to user about what is seed phrase.
+  instructions,
+
+  /// User inputs base profile data here. (email and display name).
+  setup,
+
+  /// Checkboxes with mandatory acknowledgements.
+  acknowledgements;
+
+  CreateBaseProfileStage? get next {
+    final index = CreateBaseProfileStage.values.indexOf(this);
+    final isLast = index == CreateBaseProfileStage.values.length - 1;
+    if (isLast) {
+      return null;
+    }
+
+    return CreateBaseProfileStage.values[index + 1];
+  }
+
+  CreateBaseProfileStage? get previous {
+    final index = CreateBaseProfileStage.values.indexOf(this);
+    final isFirst = index == 0;
+    if (isFirst) {
+      return null;
+    }
+
+    return CreateBaseProfileStage.values[index - 1];
+  }
+}
