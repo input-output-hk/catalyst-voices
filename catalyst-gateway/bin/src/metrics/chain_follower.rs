7e9a43c7ccb775a1715661ba78b6fd34b218d691
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-01-20 16:59:22 +0700

    feat: metrics by namespace

--- catalyst-gateway/bin/src/metrics/chain_follower.rs
+++ catalyst-gateway/bin/src/metrics/chain_follower.rs
@@ -5,7 +5,7 @@ use std::{
     thread,
 };
 
-use cardano_chain_follower::Statistics;
+use cardano_chain_follower::{Network, Statistics};
 
 use crate::settings::Settings;
 
@@ -25,36 +25,30 @@ pub(crate) fn init_metrics_reporter() {
     let service_id = Settings::service_id();
     let chain = Settings::cardano_network();
 
+    let idx = match chain {
+        Network::Mainnet => 1,
+        Network::Preprod => 2,
+        Network::Preview => 3,
+    };
+
     thread::spawn(move || {
         loop {
             {
                 let follower_stats = Statistics::new(chain);
 
                 // mithril part
-                let is_downloading =
-                    follower_stats.mithril.dl_end < follower_stats.mithril.dl_start;
-                let is_extracting =
-                    follower_stats.mithril.extract_end < follower_stats.mithril.extract_start;
-                let is_validating =
-                    follower_stats.mithril.validate_end < follower_stats.mithril.validate_start;
-
-                if is_downloading {
-                    reporter::FOLLOWER_DL_FAILURES
-                        .with_label_values(&[&api_host_names, service_id])
-                        .set(i64::try_from(follower_stats.mithril.dl_failures).unwrap_or(-1));
-                    reporter::FOLLOWER_DL_SIZE
-                        .with_label_values(&[&api_host_names, service_id])
-                        .set(i64::try_from(follower_stats.mithril.dl_size).unwrap_or(-1));
-                }
-                if is_extracting {
-                    reporter::FOLLOWER_EXTRACT_FAILURES
-                        .with_label_values(&[&api_host_names, service_id])
-                        .set(i64::try_from(follower_stats.mithril.extract_failures).unwrap_or(-1));
-                    reporter::FOLLOWER_EXTRACT_SIZE
-                        .with_label_values(&[&api_host_names, service_id])
-                        .set(i64::try_from(follower_stats.mithril.extract_size).unwrap_or(-1));
-                }
-                if is_validating {}
+                reporter::FOLLOWER_DL_FAILURES[idx]
+                    .with_label_values(&[&api_host_names, service_id])
+                    .set(i64::try_from(follower_stats.mithril.dl_failures).unwrap_or(-1));
+                reporter::FOLLOWER_DL_SIZE[idx]
+                    .with_label_values(&[&api_host_names, service_id])
+                    .set(i64::try_from(follower_stats.mithril.dl_size).unwrap_or(-1));
+                reporter::FOLLOWER_EXTRACT_FAILURES[idx]
+                    .with_label_values(&[&api_host_names, service_id])
+                    .set(i64::try_from(follower_stats.mithril.extract_failures).unwrap_or(-1));
+                reporter::FOLLOWER_EXTRACT_SIZE[idx]
+                    .with_label_values(&[&api_host_names, service_id])
+                    .set(i64::try_from(follower_stats.mithril.extract_size).unwrap_or(-1));
 
                 // live part
             }
@@ -71,46 +65,57 @@ mod reporter {
 
     use prometheus::{register_int_gauge_vec, IntGaugeVec};
 
+    /// Chain networks use as the metrics namespace.
+    const FOLLOWER_METRIC_NETWORKS: [&str; 3] = ["mainnet", "preprod", "preview"];
+
     /// Labels for the chain follower metrics
     const FOLLOWER_METRIC_LABELS: [&str; 2] = ["api_host_names", "service_id"];
 
     /// The number of times download failed.
-    pub(super) static FOLLOWER_DL_FAILURES: LazyLock<IntGaugeVec> = LazyLock::new(|| {
-        register_int_gauge_vec!(
-            "follower_dl_failures",
-            "Number of times download failed",
-            &FOLLOWER_METRIC_LABELS
-        )
-        .unwrap()
+    pub(super) static FOLLOWER_DL_FAILURES: LazyLock<[IntGaugeVec; 3]> = LazyLock::new(|| {
+        FOLLOWER_METRIC_NETWORKS.map(|network| {
+            register_int_gauge_vec!(
+                format!("{network}_follower_dl_failures"),
+                "Number of times download failed",
+                &FOLLOWER_METRIC_LABELS
+            )
+            .unwrap()
+        })
     });
 
     /// The size of the download archive, in bytes.
-    pub(super) static FOLLOWER_DL_SIZE: LazyLock<IntGaugeVec> = LazyLock::new(|| {
-        register_int_gauge_vec!(
-            "follower_dl_size",
-            "Size of the download archive",
-            &FOLLOWER_METRIC_LABELS
-        )
-        .unwrap()
+    pub(super) static FOLLOWER_DL_SIZE: LazyLock<[IntGaugeVec; 3]> = LazyLock::new(|| {
+        FOLLOWER_METRIC_NETWORKS.map(|network| {
+            register_int_gauge_vec!(
+                format!("{network}_follower_dl_size"),
+                "Size of the download archive",
+                &FOLLOWER_METRIC_LABELS
+            )
+            .unwrap()
+        })
     });
 
     /// The number of times extraction failed.
-    pub(super) static FOLLOWER_EXTRACT_FAILURES: LazyLock<IntGaugeVec> = LazyLock::new(|| {
-        register_int_gauge_vec!(
-            "follower_extract_failures",
-            "Number of times extraction failed",
-            &FOLLOWER_METRIC_LABELS
-        )
-        .unwrap()
+    pub(super) static FOLLOWER_EXTRACT_FAILURES: LazyLock<[IntGaugeVec; 3]> = LazyLock::new(|| {
+        FOLLOWER_METRIC_NETWORKS.map(|network| {
+            register_int_gauge_vec!(
+                format!("{network}_follower_extract_failures"),
+                "Number of times extraction failed",
+                &FOLLOWER_METRIC_LABELS
+            )
+            .unwrap()
+        })
     });
 
     /// The size of last extracted snapshot.
-    pub(super) static FOLLOWER_EXTRACT_SIZE: LazyLock<IntGaugeVec> = LazyLock::new(|| {
-        register_int_gauge_vec!(
-            "follower_extract_size",
-            "Size of last extracted snapshot",
-            &FOLLOWER_METRIC_LABELS
-        )
-        .unwrap()
+    pub(super) static FOLLOWER_EXTRACT_SIZE: LazyLock<[IntGaugeVec; 3]> = LazyLock::new(|| {
+        FOLLOWER_METRIC_NETWORKS.map(|network| {
+            register_int_gauge_vec!(
+                format!("{network}_follower_extract_size"),
+                "Size of last extracted snapshot",
+                &FOLLOWER_METRIC_LABELS
+            )
+            .unwrap()
+        })
     });
 }
