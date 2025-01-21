d0fcb5c2f67419849cc9a5662242a8518fed2b60
Ryszard Schossler <rysiek9801@gmail.com>
2025-01-21 12:35:11 +0100

    feat: making section hero widget more generic

--- catalyst_voices/apps/voices/lib/pages/discovery/discovery_page.dart
+++ catalyst_voices/apps/voices/lib/pages/discovery/discovery_page.dart
@@ -1,5 +1,10 @@
 import 'package:catalyst_voices/pages/discovery/how_it_works.dart';
+import 'package:catalyst_voices/widgets/buttons/voices_filled_button.dart';
+import 'package:catalyst_voices/widgets/buttons/voices_outlined_button.dart';
+import 'package:catalyst_voices/widgets/heros/section_hero.dart';
+import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
+import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -46,7 +51,7 @@ class _GuestVisitorBody extends StatelessWidget {
           sliver: SliverList(
             delegate: SliverChildListDelegate(
               [
-                const _HeroSection(),
+                const _CampaignHeroSection(),
                 const HowItWorks(),
                 const _CurrentCampaign(),
                 const _CampaignCategories(),
@@ -60,17 +65,79 @@ class _GuestVisitorBody extends StatelessWidget {
   }
 }
 
-class _HeroSection extends StatelessWidget {
-  const _HeroSection({super.key});
+class _CampaignHeroSection extends StatelessWidget {
+  const _CampaignHeroSection();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return HeroSection(
+      asset: VoicesAssets.videos.heroDesktop,
+      assetPackageName: 'catalyst_voices_assets',
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 120,
+          bottom: 64,
+          top: 32,
+        ),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 450,
+          ),
+          child: const _CampaignBrief(),
+        ),
+      ),
+    );
+  }
+}
+
+class _CampaignBrief extends StatelessWidget {
+  const _CampaignBrief();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          context.l10n.heroSectionTitle,
+          style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+        ),
+        const SizedBox(height: 32),
+        Text(
+          context.l10n.projectCatalystDescription,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const SizedBox(height: 32),
+        Row(
+          children: [
+            VoicesFilledButton(
+              onTap: () {
+                // TODO(LynxxLynx): implement redirect to current campaign
+              },
+              child: Text(context.l10n.viewCurrentCampaign),
+            ),
+            const SizedBox(width: 8),
+            Offstage(
+              offstage: true,
+              child: VoicesOutlinedButton(
+                onTap: () {
+                  // TODO(LynxxLynx): implement redirect to my proposals
+                },
+                child: Text(context.l10n.myProposals),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
 
 class _CampaignCategories extends StatelessWidget {
-  const _CampaignCategories({super.key});
+  const _CampaignCategories();
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +149,7 @@ class _CampaignCategories extends StatelessWidget {
 }
 
 class _CurrentCampaign extends StatelessWidget {
-  const _CurrentCampaign({super.key});
+  const _CurrentCampaign();
 
   @override
   Widget build(BuildContext context) {
@@ -94,7 +161,7 @@ class _CurrentCampaign extends StatelessWidget {
 }
 
 class _LatestProposals extends StatelessWidget {
-  const _LatestProposals({super.key});
+  const _LatestProposals();
 
   @override
   Widget build(BuildContext context) {
