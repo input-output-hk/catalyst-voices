30319ddef46c8bcceda863cd503b3d0c291cb95f
Damian Molinski <damian.molinski@iohk.io>
2025-01-22 11:49:27 +0100

    feat: wallet link errors

--- catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/registration/cubits/wallet_link_cubit.dart
+++ catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/registration/cubits/wallet_link_cubit.dart
@@ -101,6 +101,27 @@ final class WalletLinkCubit extends Cubit<WalletLinkStateData>
       emit(newState);
 
       return true;
+    } on WalletApiException catch (error, stackTrace) {
+      _logger.log(
+        error.code != WalletApiErrorCode.refused ? Level.SEVERE : Level.FINER,
+        'select wallet',
+        error,
+        stackTrace,
+      );
+
+      _selectedWallet = null;
+
+      emit(
+        state.copyWith(
+          selectedWallet: const Optional.empty(),
+          walletConnection: const Optional.empty(),
+          walletSummary: const Optional.empty(),
+        ),
+      );
+
+      emitError(LocalizedWalletLinkException(code: error.code));
+
+      return false;
     } catch (error, stackTrace) {
       _logger.severe('selectWallet', error, stackTrace);
 
