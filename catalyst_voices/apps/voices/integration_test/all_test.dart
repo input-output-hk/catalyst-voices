02beec5050cb5c59b7026679552b2d68d6b442a1
januszjanus <janusz.janus@iohk.io>
2025-01-23 10:46:40 +0100

    try 3

--- catalyst_voices/apps/voices/integration_test/all_test.dart
+++ catalyst_voices/apps/voices/integration_test/all_test.dart
@@ -2,7 +2,7 @@ import 'package:catalyst_voices/configs/bootstrap.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'suites/account_test.dart' as account_test;
+import 'suites/app_test.dart' as app_test;
 
 void main() async {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +12,7 @@ void main() async {
     await bootstrap(router: buildAppRouter());
   });
 
-  // app_test.main();
+  app_test.main();
   // onboarding_test.main();
-  account_test.main();
+  // account_test.main();
 }
