40f596849a586f4912f87f0ca90b2e40ab5885a7
Ryszard Schossler <rysiek9801@gmail.com>
2025-01-24 12:59:46 +0100

    feat: campaign categories ui

--- catalyst_voices/packages/internal/catalyst_voices_view_models/lib/src/campaign/campaign_category.dart
+++ catalyst_voices/packages/internal/catalyst_voices_view_models/lib/src/campaign/campaign_category.dart
@@ -12,3 +12,33 @@ final class CampaignCategory extends Equatable {
   @override
   List<Object?> get props => [id, name];
 }
+
+final class CampaignCategoryCardViewModel extends CampaignCategory {
+  final String subname;
+  final String description;
+  final int proposalsCount;
+  final int availableFunds;
+  final String imageUrl;
+
+  const CampaignCategoryCardViewModel({
+    required this.subname,
+    required this.description,
+    required this.proposalsCount,
+    required this.availableFunds,
+    required this.imageUrl,
+    required super.id,
+    required super.name,
+  });
+
+  factory CampaignCategoryCardViewModel.dummy() =>
+      const CampaignCategoryCardViewModel(
+        id: '1',
+        name: 'Cardano Open:',
+        subname: 'Developers',
+        description:
+            '''Supports development of open source technology, centered around improving the Cardano developer experience and creating developer-friendly tooling that streamlines an integrated development environment.''',
+        proposalsCount: 263,
+        availableFunds: 8000000,
+        imageUrl: 'https://picsum.photos/id/10/200/300',
+      );
+}
