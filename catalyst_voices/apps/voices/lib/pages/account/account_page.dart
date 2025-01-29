4d13bc1e25a8d73696df4bed41e19f61475ba558
Damian Molinski <damian.molinski@iohk.io>
2025-01-29 13:28:03 +0100

    chore: spacings

--- catalyst_voices/apps/voices/lib/pages/account/account_page.dart
+++ catalyst_voices/apps/voices/lib/pages/account/account_page.dart
@@ -13,6 +13,7 @@ import 'package:catalyst_voices/pages/account/widgets/account_status_banner.dart
 import 'package:catalyst_voices/routes/routes.dart';
 import 'package:catalyst_voices/widgets/widgets.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -39,31 +40,36 @@ class _AccountPageState extends State<AccountPage> {
                 padding: EdgeInsets.all(24),
                 children: [
                   AccountPageTitle(),
+                  SizedBox(height: 42),
                   Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Expanded(child: AccountHeaderTile()),
+                      SizedBox(width: 28),
                       Expanded(child: AccountActionTile())
                     ],
                   ),
                   SizedBox(height: 40),
                   Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Expanded(
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
-                          children: [
+                          children: <Widget>[
                             AccountDisplayNameTile(),
                             AccountEmailTile(),
-                          ],
+                          ].separatedBy(SizedBox(height: 20)).toList(),
                         ),
                       ),
+                      SizedBox(width: 28),
                       Expanded(
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
-                          children: [
+                          children: <Widget>[
                             AccountRolesTile(),
                             AccountKeychainTile(),
-                          ],
+                          ].separatedBy(SizedBox(height: 20)).toList(),
                         ),
                       ),
                     ],
