50f9de22606024b8bcadcb17c67096b8c5fe8140
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-01-29 13:05:38 +0700

    chore: fmtfix

--- catalyst-gateway/bin/src/db/index/queries/registrations/get_from_stake_hash.rs
+++ catalyst-gateway/bin/src/db/index/queries/registrations/get_from_stake_hash.rs
@@ -48,7 +48,9 @@ impl GetStakeAddrQuery {
             true,
         )
         .await
-        .inspect_err(|error| error!(error=%error, "Failed to prepare get stake addr from stake hash query."))
+        .inspect_err(
+            |error| error!(error=%error, "Failed to prepare get stake addr from stake hash query."),
+        )
         .map_err(|error| anyhow::anyhow!("{error}\n--\n{GET_STAKE_ADDR_FROM_STAKE_HASH}"))
     }
 
