3b468fd4babd2fea29547461a759ada848a745a0
Joshua Gilman <joshuagilman@gmail.com>
2025-01-30 13:29:52 -0800

    chore(cat-gateway): adds deployment

--- catalyst-gateway/event-db/setup-db.sql
+++ catalyst-gateway/event-db/setup-db.sql
@@ -16,11 +16,18 @@
 \echo -> dbDescription .......... = :dbDescription
 \echo -> dbUser ................. = :dbUser
 \echo -> dbUserPw ............... = xxxx
-\echo test test
+\echo -> dbSuperUser ............ = :dbSuperUser
 
 -- Cleanup if we already ran this before.
 DROP DATABASE IF EXISTS :"dbName"; -- noqa: PRS
-DROP USER IF EXISTS :"dbUser"; -- noqa: PRS
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'gateway') THEN
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE EXECUTE ON FUNCTIONS FROM gateway;
+        DROP ROLE gateway;
+    END IF;
+END;
+$$;
 
 -- Create the test user we will use with the local dev database.
 CREATE USER :"dbUser" WITH PASSWORD :'dbUserPw'; -- noqa: PRS
@@ -30,10 +37,11 @@ ALTER DEFAULT PRIVILEGES GRANT EXECUTE ON FUNCTIONS TO public;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO :"dbUser"; -- noqa: PRS
 
-GRANT EXECUTE ON FUNCTION PG_READ_FILE(TEXT, BIGINT, BIGINT, BOOLEAN) TO :"dbUser"; -- noqa: PRS
-
 -- Create the database.
 CREATE DATABASE :"dbName" WITH OWNER :"dbUser"; -- noqa: PRS
+
+-- This is necessary for RDS to work.
+GRANT :"dbUser" TO :"dbSuperUser";
 
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO :"dbUser"; -- noqa: PRS
 
