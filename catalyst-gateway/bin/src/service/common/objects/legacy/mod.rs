15e97b62779d56a42fa84fb84dbb5e43dc9c47b4
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-02-03 15:41:38 +0700

    chore: remove common legacy objects

--- catalyst-gateway/bin/src/service/common/objects/legacy/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Legacy objects
-
-pub(crate) mod block;
-pub(crate) mod delegate_public_key;
-pub(crate) mod fragment_status;
-pub(crate) mod hash;
-pub(crate) mod stake_public_key;
-pub(crate) mod voter_group_id;
-pub(crate) mod voter_info;
