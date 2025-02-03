309f46ef81aad5e7f72ad7ac47e59df336703d57
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-03 19:12:20 +0200

    fix

--- catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
+++ catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
@@ -1,15 +1,11 @@
 import schemathesis
-from schemathesis import hooks, models
 
 
-@schemathesis.hook
-def before_init_operation(context: hooks.HookContext, operation: models.APIOperation):
-    # Get security definitions safely
-    security_definitions = operation.definition.raw.get("security", [])
-
-    # Check if 'NoAuthorization' is in the security definitions
+@schemathesis.check
+def ignored_auth_custom(ctx, response, case):
+    security_definitions = case.operation.definition.raw.get("security", [])
     if any("NoAuthorization" in sec for sec in security_definitions):
-        # Remove ignored_auth check from this specific operation
-        operation.checks = [
-            check for check in operation.checks if check.__name__ != "ignored_auth"
-        ]
+        # If 'NoAuthorization' is in the security definitions skip verification
+        return None
+    else:
+        return schemathesis.specs.openapi.checks.ignored_auth(ctx, response, case)
