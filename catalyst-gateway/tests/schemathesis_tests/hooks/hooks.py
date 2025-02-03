2f1372e7bb352a3009e760f675647757013703dd
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-03 12:47:22 +0200

    add before_init_operation hook

--- catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
+++ catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
@@ -1,65 +1,15 @@
-import copy
 import schemathesis
-import schemathesis.schemas
-from typing import Any, Dict
+from schemathesis import hooks, models
 
 
 @schemathesis.hook
-def before_load_schema(
-    context: schemathesis.hooks.HookContext,
-    raw_schema: Dict[str, Any],
-) -> None:
-    paths = dict(raw_schema["paths"])
-    for endpoint in paths.keys():
-        endpoint_data = dict(raw_schema["paths"][endpoint])
+def before_init_operation(context: hooks.HookContext, operation: models.APIOperation):
+    # Get security definitions safely
+    security_definitions = operation.definition.raw.get("security", [])
 
-        # Get first method data as reference
-        ref_info = next(iter(endpoint_data.values()))
-
-        # Create new method data using reference data, replacing 'responses' field
-        new_info = copy.deepcopy(ref_info)
-        new_info["responses"] = {"405": {"description": "method not allowed"}}
-
-        # Update endpoint if a method is not declared
-        if "get" not in endpoint_data:
-            endpoint_data.update(
-                [
-                    ("get", new_info),
-                ]
-            )
-        if "post" not in endpoint_data:
-            endpoint_data.update(
-                [
-                    ("post", new_info),
-                ]
-            )
-        if "put" not in endpoint_data:
-            endpoint_data.update(
-                [
-                    ("put", new_info),
-                ]
-            )
-        if "patch" not in endpoint_data:
-            endpoint_data.update(
-                [
-                    ("patch", new_info),
-                ]
-            )
-        if "delete" not in endpoint_data:
-            endpoint_data.update(
-                [
-                    ("delete", new_info),
-                ]
-            )
-
-        # Update endpoint
-        raw_schema["paths"][endpoint] = endpoint_data
-
-
-@schemathesis.hooks.register
-def before_call(context, case, **kwargs):
-    if "NoAuthorization" in context.operation.security:
-        # Skip ignored_auth check
-        case._checks = [
-            check for check in case._checks if check.__name__ != "ignored_auth"
+    # Check if 'NoAuthorization' is in the security definitions
+    if any("NoAuthorization" in sec for sec in security_definitions):
+        # Remove ignored_auth check from this specific operation
+        operation.checks = [
+            check for check in operation.checks if check.__name__ != "ignored_auth"
         ]
