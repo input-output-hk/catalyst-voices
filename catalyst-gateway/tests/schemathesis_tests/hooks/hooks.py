54394230bc5108a557ab1a927d4bd77dfa58580f
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-03 12:01:33 +0200

    fix

--- catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
+++ catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
@@ -3,13 +3,14 @@ import schemathesis
 import schemathesis.schemas
 from typing import Any, Dict
 
+
 @schemathesis.hook
 def before_load_schema(
     context: schemathesis.hooks.HookContext,
     raw_schema: Dict[str, Any],
-    ) -> None:
+) -> None:
     paths = dict(raw_schema["paths"])
-    for endpoint in paths.keys(): 
+    for endpoint in paths.keys():
         endpoint_data = dict(raw_schema["paths"][endpoint])
 
         # Get first method data as reference
@@ -21,16 +22,44 @@ def before_load_schema(
 
         # Update endpoint if a method is not declared
         if "get" not in endpoint_data:
-            endpoint_data.update([("get", new_info),])
+            endpoint_data.update(
+                [
+                    ("get", new_info),
+                ]
+            )
         if "post" not in endpoint_data:
-            endpoint_data.update([("post", new_info),])
+            endpoint_data.update(
+                [
+                    ("post", new_info),
+                ]
+            )
         if "put" not in endpoint_data:
-            endpoint_data.update([("put", new_info),])
+            endpoint_data.update(
+                [
+                    ("put", new_info),
+                ]
+            )
         if "patch" not in endpoint_data:
-            endpoint_data.update([("patch", new_info),])
+            endpoint_data.update(
+                [
+                    ("patch", new_info),
+                ]
+            )
         if "delete" not in endpoint_data:
-            endpoint_data.update([("delete", new_info),])
+            endpoint_data.update(
+                [
+                    ("delete", new_info),
+                ]
+            )
 
         # Update endpoint
         raw_schema["paths"][endpoint] = endpoint_data
-        
+
+
+@schemathesis.hooks.register
+def before_call(context, case, **kwargs):
+    if "NoAuthorization" in context.operation.security:
+        # Skip ignored_auth check
+        case._checks = [
+            check for check in case._checks if check.__name__ != "ignored_auth"
+        ]
