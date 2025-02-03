fd15a7c3d5a3f9efd39516c31a198293f7c9daf5
Dominik Toton <dominik.toton@iohk.io>
2025-02-03 12:40:48 +0100

    fix: section or subsection logic unified

--- catalyst_voices/apps/voices/lib/widgets/tiles/document_builder_section_tile.dart
+++ catalyst_voices/apps/voices/lib/widgets/tiles/document_builder_section_tile.dart
@@ -257,7 +257,7 @@ class _PropertyListBuilder extends StatelessWidget {
           onChanged: onChanged,
         ),
         ...list.properties
-            .whereNot((child) => child.schema.isSubsection)
+            .whereNot((child) => child.schema.isSectionOrSubsection)
             .map<Widget>((child) {
           return _PropertyBuilder(
             key: ValueKey(child.nodeId),
@@ -302,7 +302,7 @@ class _PropertyObjectBuilder extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: property.properties
-              .whereNot((child) => child.schema.isSubsection)
+              .whereNot((child) => child.schema.isSectionOrSubsection)
               .map<Widget>((child) {
                 return _PropertyBuilder(
                   key: ValueKey(child.nodeId),
