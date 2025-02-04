7b50d7eeec8fe21c1f3305714a87d7872955aae3
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-02-04 17:07:11 +0700

    revert: mod legacy

--- catalyst-gateway/bin/src/db/event/mod.rs
+++ catalyst-gateway/bin/src/db/event/mod.rs
@@ -19,6 +19,7 @@ use crate::settings::Settings;
 pub(crate) mod common;
 pub(crate) mod config;
 pub(crate) mod error;
+pub(crate) mod legacy;
 pub(crate) mod schema_check;
 pub(crate) mod signed_docs;
 
