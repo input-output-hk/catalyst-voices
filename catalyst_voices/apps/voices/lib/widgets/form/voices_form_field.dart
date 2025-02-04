8391360b2687b4e0e29ccd1d343c3c8ae9db9f12
Dominik Toton <dominik.toton@iohk.io>
2025-02-04 10:57:33 +0100

    docs: clarification

--- catalyst_voices/apps/voices/lib/widgets/form/voices_form_field.dart
+++ catalyst_voices/apps/voices/lib/widgets/form/voices_form_field.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// A [FormField] which keeps a value which can be dynamically updated
+/// by the parent.
+///
+/// A traditional form field only accepts a [FormField.initialValue] which
+/// can be set once, here the value can be updated as many times as needed.
 class VoicesFormField<T> extends FormField<T> {
   final T? value;
   final ValueChanged<T?> onChanged;
