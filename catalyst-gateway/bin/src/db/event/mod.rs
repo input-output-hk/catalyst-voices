8658ff7319295057998830e3210f4480b010b469
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-02-04 16:41:48 +0700

    feat: initial remove

--- catalyst-gateway/bin/src/db/event/mod.rs
+++ catalyst-gateway/bin/src/db/event/mod.rs
@@ -19,7 +19,6 @@ use crate::settings::Settings;
 pub(crate) mod common;
 pub(crate) mod config;
 pub(crate) mod error;
-pub(crate) mod legacy;
 pub(crate) mod schema_check;
 pub(crate) mod signed_docs;
 
