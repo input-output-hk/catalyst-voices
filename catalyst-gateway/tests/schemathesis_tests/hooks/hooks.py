1048cc952b0f0ce901903a8ae0dd6c0a32e41f84
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-04 22:16:32 +0200

    try

--- catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
+++ catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
@@ -38,3 +38,30 @@ def ignored_auth_custom(ctx, response, case):
         return None
     else:
         return schemathesis.specs.openapi.checks.ignored_auth(ctx, response, case)
+
+
+@schemathesis.check
+def negative_data_rejection_custom(ctx, response, case):
+    try:
+        return schemathesis.specs.openapi.checks.negative_data_rejection(
+            ctx, response, case
+        )
+    except Exception as e:
+        print("")
+        print("negative_data_rejection_custom")
+        print("path_parameters", case.path_parameters)
+        print("headers", case.headers)
+        print("cookies", case.cookies)
+        print("query", case.query)
+        print("body", case.body)
+        print(
+            f"headers: {case.meta.headers} query: {case.meta.query} path_parameters: {case.meta.path_parameters} cookies: {case.meta.cookies} body: {case.meta.body}"
+        )
+        for name, value in case.headers.items():
+            try:
+                schemathesis.cli.callbacks._validate_header(name, value, "")
+                print("negative_data_rejection_custom Valid header")
+            except Exception as e:
+                print(f"negative_data_rejection_custom {e}")
+                pass
+        raise e
