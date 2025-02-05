27aee52305b0ed0673b1910ed27c3c4e107a574f
bkioshn <bkioshn@gmail.com>
2025-02-05 14:41:13 +0700

    fix(cat-gateway): new static doc impl
    Signed-off-by: bkioshn <bkioshn@gmail.com>

---
+++ catalyst-gateway/bin/src/service/api/documents/templates/data.rs
@@ -0,0 +1,106 @@
+//! Document templates list.
+
+use std::sync::LazyLock;
+
+use super::SignedDocTemplate;
+
+/// Proposal document type.
+const PROPOSAL_DOC_TYPE: &str = "0ce8ab38-9258-4fbc-a62e-7faa6e58318f";
+/// Category document type.
+const CATEGORY_DOC_TYPE: &str = "65b1e8b0-51f1-46a5-9970-72cdf26884be";
+/// Comment document type.
+const COMMENT_DOC_TYPE: &str = "0b8424d4-ebfd-46e3-9577-1775a69d290c";
+
+/// List of category templates, 12 categories for Fund 14.
+#[rustfmt::skip]
+// FIXME: Content
+const CATEGORY_TEMPLATES: [(&str, &[u8]); 12] = [
+    //              ID and Version          |                    Content
+    ("0194d490-30bf-7473-81c8-a0eaef369619", include_bytes!("./docs/category/f14_category_1_template.schema.json")),
+    ("0194d490-30bf-7043-8c5c-f0e09f8a6d8c", include_bytes!("./docs/category/f14_category_2_template.schema.json")),
+    ("0194d490-30bf-7e75-95c1-a6cf0e8086d9", include_bytes!("./docs/category/f14_category_3_template.schema.json")),
+    ("0194d490-30bf-7703-a1c0-83a916b001e7", include_bytes!("./docs/category/f14_category_4_template.schema.json")),
+    ("0194d490-30bf-79d1-9a0f-84943123ef38", include_bytes!("./docs/category/f14_category_5_template.schema.json")),
+    ("0194d490-30bf-706d-91c6-0d4707f74cdf", include_bytes!("./docs/category/f14_category_6_template.schema.json")),
+    ("0194d490-30bf-759e-b729-304306fbaa5e", include_bytes!("./docs/category/f14_category_7_template.schema.json")),
+    ("0194d490-30bf-7e27-b5fd-de3133b54bf6", include_bytes!("./docs/category/f14_category_8_template.schema.json")),
+    ("0194d490-30bf-7f9e-8a5d-91fb67c078f2", include_bytes!("./docs/category/f14_category_9_template.schema.json")),
+    ("0194d490-30bf-7676-9658-36c0b67e656e", include_bytes!("./docs/category/f14_category_10_template.schema.json")),
+    ("0194d490-30bf-7978-b031-7aa2ccc5e3fd", include_bytes!("./docs/category/f14_category_11_template.schema.json")),
+    ("0194d490-30bf-7d34-bba9-8498094bd627", include_bytes!("./docs/category/f14_category_12_template.schema.json")),
+
+];
+
+/// List of proposal templates, 12 proposals each of which is uniquely associated with one of the predefined categories.
+#[rustfmt::skip]
+const PROPOSAL_TEMPLATES: [(&str, &str); 12] = [
+    //        ID and Version                |      Category ID
+    ("0194d492-1daa-75b5-b4a4-5cf331cd8d1a", CATEGORY_TEMPLATES[0].0),
+    ("0194d492-1daa-7371-8bd3-c15811b2b063", CATEGORY_TEMPLATES[1].0),
+    ("0194d492-1daa-79c7-a222-2c3b581443a8", CATEGORY_TEMPLATES[2].0),
+    ("0194d492-1daa-716f-a04e-f422f08a99dc", CATEGORY_TEMPLATES[3].0),
+    ("0194d492-1daa-78fc-818a-bf20fc3e9b87", CATEGORY_TEMPLATES[4].0),
+    ("0194d492-1daa-7d98-a3aa-c57d99121f78", CATEGORY_TEMPLATES[5].0),
+    ("0194d492-1daa-77be-a1a5-c238fe25fe4f", CATEGORY_TEMPLATES[6].0),
+    ("0194d492-1daa-7254-a512-30a4cdecfb90", CATEGORY_TEMPLATES[7].0),
+    ("0194d492-1daa-7de9-b535-1a0b0474ed4e", CATEGORY_TEMPLATES[8].0),
+    ("0194d492-1daa-7fce-84ee-b872a4661075", CATEGORY_TEMPLATES[9].0),
+    ("0194d492-1daa-7878-9bcc-2c79fef0fc13", CATEGORY_TEMPLATES[10].0),
+    ("0194d492-1daa-722f-94f4-687f2c068a5d", CATEGORY_TEMPLATES[11].0),
+
+];
+
+/// List of comment templates, 12 comments each of which is uniquely associated with one of the predefined categories.
+#[rustfmt::skip]
+const COMMENT_TEMPLATES: [(&str, &str); 12] = [
+    //        ID and Version                |      Category ID
+    ("0194d494-4402-7e0e-b8d6-171f8fea18b0", CATEGORY_TEMPLATES[0].0),
+    ("0194d494-4402-7444-9058-9030815eb029", CATEGORY_TEMPLATES[1].0),
+    ("0194d494-4402-7351-b4f7-24938dc2c12e", CATEGORY_TEMPLATES[2].0),
+    ("0194d494-4402-79ad-93ba-4d7a0b65d563", CATEGORY_TEMPLATES[3].0),
+    ("0194d494-4402-7cee-a5a6-5739839b3b8a", CATEGORY_TEMPLATES[4].0),
+    ("0194d494-4402-7aee-8b24-b5300c976846", CATEGORY_TEMPLATES[5].0),
+    ("0194d494-4402-7d75-be7f-1c4f3471a53c", CATEGORY_TEMPLATES[6].0),
+    ("0194d494-4402-7a2c-8971-1b4c255c826d", CATEGORY_TEMPLATES[7].0),
+    ("0194d494-4402-7074-86ac-3efd097ba9b0", CATEGORY_TEMPLATES[8].0),
+    ("0194d494-4402-7202-8ebb-8c4c47c286d8", CATEGORY_TEMPLATES[9].0),
+    ("0194d494-4402-7fb5-b680-c23fe4beb088", CATEGORY_TEMPLATES[10].0),
+    ("0194d494-4402-7aa5-9dbc-5fe886e60ebc", CATEGORY_TEMPLATES[11].0),
+];
+
+/// List of raw templates, containing information to build signed document.
+pub(crate) static TEMPLATE_DATA: LazyLock<Vec<SignedDocTemplate>> = LazyLock::new(|| {
+    let mut templates = Vec::new();
+    for tem in CATEGORY_TEMPLATES {
+        templates.push(SignedDocTemplate {
+            id: tem.0.to_owned(),
+            ver: tem.0.to_owned(),
+            doc_type: CATEGORY_DOC_TYPE.to_owned(),
+            content: tem.1.to_vec(),
+            category_id: tem.0.to_owned(),
+        });
+    }
+
+    let proposal_template = include_bytes!("./docs/f14_proposal_template.schema.json");
+    for tem in PROPOSAL_TEMPLATES {
+        templates.push(SignedDocTemplate {
+            id: tem.0.to_owned(),
+            ver: tem.0.to_owned(),
+            doc_type: PROPOSAL_DOC_TYPE.to_owned(),
+            content: proposal_template.to_vec(),
+            category_id: tem.1.to_owned(),
+        });
+    }
+
+    let comment_template = include_bytes!("./docs/f14_comment_template.schema.json");
+    for tem in COMMENT_TEMPLATES {
+        templates.push(SignedDocTemplate {
+            id: tem.0.to_owned(),
+            ver: tem.0.to_owned(),
+            doc_type: COMMENT_DOC_TYPE.to_owned(),
+            content: comment_template.to_vec(),
+            category_id: tem.1.to_owned(),
+        });
+    }
+    templates
+});
--- catalyst-gateway/bin/src/service/api/documents/templates/mod.rs
+++ catalyst-gateway/bin/src/service/api/documents/templates/mod.rs
@@ -1,16 +1,69 @@
 //! Catalyst signed document templates.
 
-mod proposal;
+mod data;
 
-use std::sync::LazyLock;
+use std::{collections::HashMap, env, sync::LazyLock};
 
 use catalyst_signed_doc::{Builder, CatalystSignedDocument, Metadata};
+use catalyst_types::id_uri::IdUri;
+use data::TEMPLATE_DATA;
+use ed25519_dalek::SigningKey;
+use hex::FromHex;
 use minicbor::{Encode, Encoder};
-use proposal::PROPOSAL;
+use tracing::error;
+use uuid::Uuid;
 
-/// A list of templates.
-pub(crate) static TEMPLATES: LazyLock<[CatalystSignedDocument; 1]> =
-    LazyLock::new(|| [PROPOSAL.to_signed_doc()]);
+/// Catalyst brand ID.
+const BRAND_ID: &str = "0194cfcd-bddc-7bb3-b5e9-455168bd3ff7";
+
+/// Fund 14 Campaign ID.
+const CAMPAIGN_ID: &str = "0194cfcf-15a2-7e32-b559-386b93d0724e";
+
+/// Content type.
+const CONTENT_TYPE: &str = "application/json";
+
+/// Content encoding.
+const CONTENT_ENCODING: &str = "br";
+
+/// A map of signed document templates to its ID.
+pub(crate) static TEMPLATES: LazyLock<Option<HashMap<Uuid, CatalystSignedDocument>>> =
+    LazyLock::new(|| {
+        // Load the secret key from the environment
+        // Expect hex encoded secret key that start with 0x
+        let value = env::var("SIGNED_DOC_SK");
+
+        // If no secret key is found, cannot generate sign documents
+        if let Ok(v) = value {
+            // Strip the prefix and convert to 32 bytes array
+            let byte_array: [u8; 32] = if let Some(bytes) = v
+                .strip_prefix("0x")
+                .and_then(|s| Vec::from_hex(s).ok())
+                .and_then(|bytes| bytes.as_slice().try_into().ok())
+            {
+                bytes
+            } else {
+                error!(
+                    id = "static_template",
+                    "Failed to convert secret key to bytes"
+                );
+                return None;
+            };
+            let sk = SigningKey::from_bytes(&byte_array);
+
+            let mut map = HashMap::new();
+            for t in TEMPLATE_DATA.iter() {
+                let signed_doc = t.to_signed_doc(&sk);
+                map.insert(signed_doc.doc_id().uuid(), signed_doc);
+            }
+            Some(map)
+        } else {
+            error!(
+                id = "static_template_env",
+                "Secret key not found in environment"
+            );
+            None
+        }
+    });
 
 #[derive(Clone)]
 /// Signed document template.
@@ -23,65 +76,51 @@ pub(crate) struct SignedDocTemplate {
     doc_type: String,
     /// Content bytes.
     content: Vec<u8>,
-    /// Content type.
-    content_type: String,
-    /// Content encoding method.
-    content_encoding: String,
+    /// Category ID.
+    category_id: String,
 }
 
 impl SignedDocTemplate {
     /// Convert the template to a catalyst signed document.
     /// This function should not fail, because template details are hardcoded.
     #[allow(clippy::expect_used)]
-    fn to_signed_doc(&self) -> CatalystSignedDocument {
+    fn to_signed_doc(&self, sk: &SigningKey) -> CatalystSignedDocument {
         let metadata: Metadata = serde_json::from_value(serde_json::json!({
-            "content-type": self.content_type,
-            "content-encoding": self.content_encoding,
             "type": self.doc_type,
             "id": self.id,
             "ver": self.ver,
+            "category_id": {"id": self.category_id},
+            "content-type": CONTENT_TYPE,
+            "content-encoding": CONTENT_ENCODING,
+            "campaign_id": {"id": CAMPAIGN_ID},
+            "brand_id":  {"id": BRAND_ID},
         }))
-        // This should not happen
         .expect("Failed to build Metadata from template");
 
+        let kid = IdUri::new("cardano", None, sk.verifying_key());
         Builder::new()
             .with_metadata(metadata.clone())
-            .with_content(self.content.clone())
+            .with_decoded_content(self.content.clone())
+            .add_signature(sk.to_bytes(), kid)
+            .expect("Failed to add signature for template")
             .build()
-            // This should not happen
             .expect("Failed to build CatalystSignedDocument from template")
     }
 }
 
 /// Get a static document template from ID and version.
-pub(crate) fn get_doc_static_template(
-    document_id: uuid::Uuid, version: Option<uuid::Uuid>,
-) -> Option<Vec<u8>> {
-    for template in TEMPLATES.iter() {
-        // Check if the document ID matches
-        if template.doc_id().uuid() == document_id {
-            // If version is None, or version match, proceed with encoding
-            if version.is_none() || version == Some(template.doc_ver().uuid()) {
-                let mut buffer = Vec::new();
-                let mut encoder = Encoder::new(&mut buffer);
-                // Cannot encode the template, return None
-                // Should not happen
-                template.encode(&mut encoder, &mut ()).ok()?;
-                return Some(buffer);
-            }
-        }
+pub(crate) fn get_doc_static_template(document_id: uuid::Uuid) -> Option<Vec<u8>> {
+    let data = TEMPLATES
+        .as_ref()
+        .and_then(|templates| templates.get(&document_id));
+
+    if let Some(template) = data {
+        let mut buffer = Vec::new();
+        let mut encoder = Encoder::new(&mut buffer);
+        // Cannot encode the template, return None
+        // Should not happen
+        template.encode(&mut encoder, &mut ()).ok()?;
+        return Some(buffer);
     }
     None
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn templates() {
-        for tem in TEMPLATES.iter() {
-            assert!(tem.doc_content().is_json());
-        }
-    }
 }
