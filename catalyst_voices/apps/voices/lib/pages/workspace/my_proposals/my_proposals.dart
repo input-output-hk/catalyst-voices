ba36628d58ee8231f869f5bbf2c801cd6891a60f
minikin <djminikin@gmail.com>
2025-02-06 17:09:43 +0100

    Update my_proposals.dart

--- catalyst_voices/apps/voices/lib/pages/workspace/my_proposals/my_proposals.dart
+++ catalyst_voices/apps/voices/lib/pages/workspace/my_proposals/my_proposals.dart
@@ -1,5 +1,6 @@
 import 'package:catalyst_voices/routes/routes.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
+import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
