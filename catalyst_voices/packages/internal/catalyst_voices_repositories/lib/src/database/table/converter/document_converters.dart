45e3e7a8517689586acacae16981eff9ecd2a8d4
Damian Molinski <damian.molinski@iohk.io>
2025-02-07 12:19:14 +0100

    docs: converters context

--- catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/table/converter/document_converters.dart
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/table/converter/document_converters.dart
@@ -9,14 +9,19 @@ typedef DocumentMetadataJsonBConverter
     = JsonTypeConverter2<SignedDocumentMetadata, Uint8List, Object?>;
 
 abstract final class DocumentConverters {
+  /// Converts [SignedDocumentType] to String for text column.
   static const TypeConverter<SignedDocumentType, String> type =
       _DocumentTypeConverter();
 
+  /// Converts [SignedDocumentContent] into json for bloc column.
+  /// Required for jsonb queries.
   static final DocumentContentJsonBConverter content = TypeConverter.jsonb(
     fromJson: (json) => SignedDocumentContent(json! as Map<String, Object?>),
     toJson: (content) => content.data,
   );
 
+  /// Converts [SignedDocumentMetadata] into json for bloc column.
+  /// Required for jsonb queries.
   static final DocumentMetadataJsonBConverter metadata = TypeConverter.jsonb(
     fromJson: (json) =>
         SignedDocumentMetadataDto.fromJson(json! as Map<String, Object?>)
