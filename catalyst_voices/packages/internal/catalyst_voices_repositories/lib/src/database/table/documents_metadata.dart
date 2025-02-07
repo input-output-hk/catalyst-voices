08097748c38bd050bee829dbbf4e641a99d10047
Damian Molinski <damian.molinski@iohk.io>
2025-02-07 09:35:50 +0100

    fix: document metadata reference

--- catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/table/documents_metadata.dart
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/table/documents_metadata.dart
@@ -1,4 +1,4 @@
-import 'package:catalyst_voices_repositories/src/database/table/documents.dart';
+import 'package:catalyst_voices_repositories/src/database/table/documents.drift.dart';
 import 'package:catalyst_voices_repositories/src/database/table/mixin/ver_table_mixin.dart';
 import 'package:drift/drift.dart';
 
@@ -18,27 +18,20 @@ enum DocumentMetadataFieldKey {
   columns: {#fieldKey, #fieldValue},
 )
 class DocumentsMetadata extends Table with VerTableMixin {
-  @override
-  Int64Column get verHi => int64().references(
-        Documents,
-        #verHi,
-        onDelete: KeyAction.cascade,
-        onUpdate: KeyAction.cascade,
-      )();
-
-  @override
-  Int64Column get verLo => int64().references(
-        Documents,
-        #verLo,
-        onDelete: KeyAction.cascade,
-        onUpdate: KeyAction.cascade,
-      )();
-
   /// e.g. 'category', 'title', 'description'
   TextColumn get fieldKey => textEnum<DocumentMetadataFieldKey>()();
 
   /// The actual value (for category, title, description, etc.)
   TextColumn get fieldValue => text()();
+
+  @override
+  List<String> get customConstraints => [
+        /// Referring with two columns throws a
+        /// "SqliteException(1): foreign key mismatch"
+        /// But when doing it explicitly it no longer complains
+        // ignore: lines_longer_than_80_chars
+        'FOREIGN KEY("ver_hi", "ver_lo") REFERENCES "${$DocumentsTable.$name}"("ver_hi", "ver_lo") ON DELETE CASCADE ON UPDATE CASCADE',
+      ];
 
   @override
   Set<Column> get primaryKey => {
