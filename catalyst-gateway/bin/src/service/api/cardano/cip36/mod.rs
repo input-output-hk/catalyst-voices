818666e0e9a3b59159fac932008e329fb3b282dc
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-07 10:58:21 +0200

    fix

--- catalyst-gateway/bin/src/service/api/cardano/cip36/mod.rs
+++ catalyst-gateway/bin/src/service/api/cardano/cip36/mod.rs
@@ -39,7 +39,7 @@ impl Api {
     //    provided).
     // If none of the above provided, return not found.
     #[oai(
-        path = "/cardano/registration/cip36",
+        path = "/api/cardano/registration/cip36",
         method = "get",
         operation_id = "cardanoRegistrationCip36"
     )]
