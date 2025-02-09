6aed955ddd2f9a8978d03a017834f75d16de3890
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-09 15:18:35 +0200

    fix

--- catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
+++ catalyst-gateway/tests/schemathesis_tests/hooks/hooks.py
@@ -6,13 +6,14 @@ import hypothesis
 import cbor2
 
 
- @schemathesis.serializer("application/cbor")
- class CborSerializer:
+@schemathesis.serializer("application/cbor")
+class CborSerializer:
     def as_requests(self, context, value):
         cbor2.dumps(value)
 
     def as_werkzeug(self, context, value):
         cbor2.dumps(value)
+
 
 @schemathesis.auth()
 class MyAuth:
