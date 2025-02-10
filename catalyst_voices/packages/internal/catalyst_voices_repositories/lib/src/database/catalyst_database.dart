5b5e3a90a688b39ab6882bbbc6daa4bba41dad9a
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2025-02-10 13:48:32 +0100

    feat(cat-voices): setup database (#1789)
    * chore: add drift dependencies
    
    * chore: define first iteration of db and tables
    
    * chore: expose clean interface for working with db + config
    
    * chore: README docs
    
    * feat: drafts dao
    
    * chore: enable db build_runner in melos script
    
    * feat: prepare for schema migrations
    
    * feat: start with foreign_keys enabled and prepare for unit tests
    
    * chore: sync test version
    
    * chore: headers docs
    
    * feat: define tables(Documents,DocumentsMetadata and Drafts) columns
    
    * use jsonb for content and metadata columns
    
    * fix: UuidHiLo 64 bits range
    
    * feat: document type converter
    
    * feat: clear db
    
    * fix: document metadata reference
    
    * test: db dao tests
    
    * docs: converters context
    
    * refactor: more explicit table mixin names
    
    * chore: docs fixes
    
    * chore: readme adjustments
    
    * chore: readme new line
    
    * chore: readme new line
    
    * chore: readme new line
    
    * chore: rollback generate for paths. Exclude drift files from test coverage
    
    * test
    
    * wip
    
    * test
    
    * wip
    
    * wip
    
    * fix: PR review comments apply
    
    * chore: use feat branch for CI
    
    * feat: synchronized clear
    
    * chore: do not generate managers
    
    * refactor: Rename db classes to *Entity
    
    ---------
    
    Co-authored-by: Dominik Toton <166132265+dtscalac@users.noreply.github.com>

---
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/catalyst_database.dart
@@ -0,0 +1,107 @@
+import 'package:catalyst_voices_repositories/src/database/catalyst_database.drift.dart';
+import 'package:catalyst_voices_repositories/src/database/catalyst_database_config.dart';
+import 'package:catalyst_voices_repositories/src/database/dao/documents_dao.dart';
+import 'package:catalyst_voices_repositories/src/database/dao/drafts_dao.dart';
+import 'package:catalyst_voices_repositories/src/database/migration/drift_migration_strategy.dart';
+import 'package:catalyst_voices_repositories/src/database/table/documents.dart';
+import 'package:catalyst_voices_repositories/src/database/table/documents.drift.dart';
+import 'package:catalyst_voices_repositories/src/database/table/documents_metadata.dart';
+import 'package:catalyst_voices_repositories/src/database/table/drafts.dart';
+import 'package:catalyst_voices_repositories/src/database/table/drafts.drift.dart';
+import 'package:drift/drift.dart';
+import 'package:drift_flutter/drift_flutter.dart';
+import 'package:flutter/foundation.dart';
+import 'package:synchronized/synchronized.dart';
+
+/// Database interface which expose only publicly accessible functions and
+/// classes.
+///
+/// Its implementation agnostic.
+abstract interface class CatalystDatabase {
+  /// Returns drift implementation of [CatalystDatabase]. Implementation
+  /// itself is not public.
+  factory CatalystDatabase.drift({
+    required CatalystDriftDatabaseConfig config,
+  }) = DriftCatalystDatabase.withConfig;
+
+  /// Contains all operations related to [DocumentEntity] which is db specific.
+  /// Do not confuse it with other documents.
+  DocumentsDao get documentsDao;
+
+  /// Contains all operations related to [DraftEntity] which is db specific.
+  /// Do not confuse it with other documents / drafts.
+  DraftsDao get draftsDao;
+
+  /// Removes all data from this db.
+  Future<void> clear();
+}
+
+@DriftDatabase(
+  tables: [
+    Documents,
+    DocumentsMetadata,
+    Drafts,
+  ],
+  daos: [
+    DriftDocumentsDao,
+    DriftDraftsDao,
+  ],
+  queries: {},
+  views: [],
+  include: {},
+)
+class DriftCatalystDatabase extends $DriftCatalystDatabase
+    implements CatalystDatabase {
+  final _clearLock = Lock();
+
+  @visibleForTesting
+  DriftCatalystDatabase(super.connection);
+
+  DriftCatalystDatabase.withConfig({
+    required CatalystDriftDatabaseConfig config,
+  }) : this(
+          driftDatabase(
+            name: config.name,
+            web: DriftWebOptions(
+              sqlite3Wasm: config.web.sqlite3Wasm,
+              driftWorker: config.web.driftWorker,
+            ),
+
+            // TODO(damian-molinski): Native not supported yet
+            native: null,
+          ),
+        );
+
+  @override
+  DocumentsDao get documentsDao => driftDocumentsDao;
+
+  @override
+  DraftsDao get draftsDao => driftDraftsDao;
+
+  @override
+  int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration {
+    return DriftMigrationStrategy(
+      database: this,
+      destructiveFallback: destructiveFallback,
+    );
+  }
+
+  @override
+  Future<void> clear() {
+    return _clearLock.synchronized(() async {
+      try {
+        await customStatement('PRAGMA foreign_keys = OFF');
+        await transaction(() async {
+          for (final table in allTables) {
+            await delete(table).go();
+          }
+        });
+      } finally {
+        await customStatement('PRAGMA foreign_keys = ON');
+      }
+    });
+  }
+}
