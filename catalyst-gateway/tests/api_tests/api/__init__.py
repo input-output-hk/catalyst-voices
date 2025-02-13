b0dcdf53309aab6d271edaef1e0842ce0da5fd95
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2025-02-13 16:43:40 +0100

    chore reenable db test

--- catalyst-gateway/tests/api_tests/api/__init__.py
+++ catalyst-gateway/tests/api_tests/api/__init__.py
@@ -1,4 +1,13 @@
-from integration import CAT_GATEWAY_TEST_URL
+import os
+import sys
+
+try:
+   os.environ["CAT_GATEWAY_TEST_URL"]
+except KeyError:
+   print ("Please set the environment variable CAT_GATEWAY_TEST_URL")
+   sys.exit(1)
+
+CAT_GATEWAY_TEST_URL = os.environ["CAT_GATEWAY_TEST_URL"]
 
 def cat_gateway_endpoint_url(endpoint: str):
     return f"{CAT_GATEWAY_TEST_URL}/{endpoint}"
--- catalyst-gateway/tests/api_tests/integration/__init__.py
+++ catalyst-gateway/tests/api_tests/integration/__init__.py
@@ -3,17 +3,10 @@ from loguru import logger
 import os
 
 try:
-   os.environ["CAT_GATEWAY_TEST_URL"]
-except KeyError:
-   print ("Please set the environment variable CAT_GATEWAY_TEST_URL")
-   sys.exit(1)
-
-try:
    os.environ["EVENT_DB_TEST_URL"]
 except KeyError:
    print("Please set the environment variable EVENT_DB_TEST_URL")
    sys.exit(1)
 
 EVENT_DB_TEST_URL = os.environ["EVENT_DB_TEST_URL"]
-CAT_GATEWAY_TEST_URL = os.environ["CAT_GATEWAY_TEST_URL"]
 
