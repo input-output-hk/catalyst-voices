2fd87676d0ea0e1c1d291b82fe08c790de4d6d66
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-02-13 21:15:40 +0700

    chore: fmtfix

--- catalyst-gateway/bin/src/service/common/types/generic/error_list.rs
+++ catalyst-gateway/bin/src/service/common/types/generic/error_list.rs
@@ -1,7 +1,5 @@
 //! Implement newtype of `ErrorList`
 
-use std::sync::LazyLock;
-
 use poem_openapi::{
     registry::{MetaSchema, MetaSchemaRef},
     types::{Example, ParseError, ParseFromJSON, ParseResult, ToJSON, Type},
@@ -10,18 +8,17 @@ use poem_openapi::{
 use super::error_msg::ErrorMessage;
 use crate::service::common::types::array_types::impl_array_types;
 
-/// `OpenAPI` schema for the header in documentation.
-static SCHEMA: LazyLock<MetaSchema> = LazyLock::new(|| {
-    MetaSchema {
+// List of Errors
+impl_array_types!(
+    ErrorList,
+    ErrorMessage,
+    Some(MetaSchema {
         example: ErrorList::example().to_json(),
         max_items: Some(10),
         items: Some(Box::new(ErrorMessage::schema_ref())),
         ..poem_openapi::registry::MetaSchema::ANY
-    }
-});
-
-// List of Errors
-impl_array_types!(ErrorList, ErrorMessage, Some(SCHEMA.clone()));
+    })
+);
 
 impl Example for ErrorList {
     fn example() -> Self {
