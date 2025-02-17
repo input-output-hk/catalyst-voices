707c70940a078fea3e8bf2dc6693c8e4ff014da2
minikin <djminikin@gmail.com>
2025-02-17 18:03:54 +0100

    chore: discard changes in spaces_route.dart

--- catalyst_voices/apps/voices/lib/routes/routing/spaces_route.dart
+++ catalyst_voices/apps/voices/lib/routes/routing/spaces_route.dart
@@ -21,97 +21,6 @@ part 'spaces_route.g.dart';
 
 const _prefix = Routes.currentMilestone;
 
-final class CategoryDetailRoute extends GoRouteData
-    with FadePageTransitionMixin {
-  final String categoryId;
-
-  const CategoryDetailRoute(this.categoryId);
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return CategoryPage(categoryId: categoryId);
-  }
-}
-
-final class DiscoveryRoute extends GoRouteData with FadePageTransitionMixin {
-  const DiscoveryRoute();
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return const DiscoveryPage();
-  }
-}
-
-final class FundedProjectsRoute extends GoRouteData
-    with FadePageTransitionMixin, CompositeRouteGuardMixin {
-  const FundedProjectsRoute();
-
-  @override
-  List<RouteGuard> get routeGuards => const [
-        SessionUnlockedGuard(),
-        UserAccessGuard(),
-      ];
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return const FundedProjectsPage();
-  }
-}
-
-final class ProposalBuilderDraftRoute extends GoRouteData
-    with FadePageTransitionMixin, CompositeRouteGuardMixin {
-  final String? templateId;
-
-  const ProposalBuilderDraftRoute({
-    this.templateId,
-  });
-
-  @override
-  List<RouteGuard> get routeGuards => const [
-        SessionUnlockedGuard(),
-        UserAccessGuard(),
-      ];
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return ProposalBuilderPage(templateId: templateId);
-  }
-}
-
-final class ProposalBuilderRoute extends GoRouteData
-    with FadePageTransitionMixin, CompositeRouteGuardMixin {
-  final String proposalId;
-
-  const ProposalBuilderRoute({
-    required this.proposalId,
-  });
-
-  @override
-  List<RouteGuard> get routeGuards => const [
-        SessionUnlockedGuard(),
-        UserAccessGuard(),
-      ];
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return ProposalBuilderPage(proposalId: proposalId);
-  }
-}
-
-final class ProposalsRoute extends GoRouteData with FadePageTransitionMixin {
-  final String? categoryId;
-
-  const ProposalsRoute({
-    this.categoryId,
-  });
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return ProposalsPage(
-      categoryId: categoryId,
-    );
-  }
-}
-
 @TypedShellRoute<SpacesShellRouteData>(
   routes: <TypedRoute<RouteData>>[
     TypedGoRoute<DiscoveryRoute>(
@@ -177,19 +86,82 @@ final class SpacesShellRouteData extends ShellRouteData {
   }
 }
 
-final class TreasuryRoute extends GoRouteData
+final class DiscoveryRoute extends GoRouteData with FadePageTransitionMixin {
+  const DiscoveryRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const DiscoveryPage();
+  }
+}
+
+final class WorkspaceRoute extends GoRouteData
     with FadePageTransitionMixin, CompositeRouteGuardMixin {
-  const TreasuryRoute();
+  const WorkspaceRoute();
 
   @override
   List<RouteGuard> get routeGuards => const [
         SessionUnlockedGuard(),
-        AdminAccessGuard(),
+        UserAccessGuard(),
       ];
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return const TreasuryPage();
+    return const WorkspacePage();
+  }
+}
+
+final class ProposalsRoute extends GoRouteData with FadePageTransitionMixin {
+  final String? categoryId;
+
+  const ProposalsRoute({
+    this.categoryId,
+  });
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return ProposalsPage(
+      categoryId: categoryId,
+    );
+  }
+}
+
+final class ProposalBuilderDraftRoute extends GoRouteData
+    with FadePageTransitionMixin, CompositeRouteGuardMixin {
+  final String? templateId;
+
+  const ProposalBuilderDraftRoute({
+    this.templateId,
+  });
+
+  @override
+  List<RouteGuard> get routeGuards => const [
+        SessionUnlockedGuard(),
+        UserAccessGuard(),
+      ];
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return ProposalBuilderPage(templateId: templateId);
+  }
+}
+
+final class ProposalBuilderRoute extends GoRouteData
+    with FadePageTransitionMixin, CompositeRouteGuardMixin {
+  final String proposalId;
+
+  const ProposalBuilderRoute({
+    required this.proposalId,
+  });
+
+  @override
+  List<RouteGuard> get routeGuards => const [
+        SessionUnlockedGuard(),
+        UserAccessGuard(),
+      ];
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return ProposalBuilderPage(proposalId: proposalId);
   }
 }
 
@@ -209,9 +181,9 @@ final class VotingRoute extends GoRouteData
   }
 }
 
-final class WorkspaceRoute extends GoRouteData
+final class FundedProjectsRoute extends GoRouteData
     with FadePageTransitionMixin, CompositeRouteGuardMixin {
-  const WorkspaceRoute();
+  const FundedProjectsRoute();
 
   @override
   List<RouteGuard> get routeGuards => const [
@@ -221,6 +193,34 @@ final class WorkspaceRoute extends GoRouteData
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return const WorkspacePage();
+    return const FundedProjectsPage();
+  }
+}
+
+final class TreasuryRoute extends GoRouteData
+    with FadePageTransitionMixin, CompositeRouteGuardMixin {
+  const TreasuryRoute();
+
+  @override
+  List<RouteGuard> get routeGuards => const [
+        SessionUnlockedGuard(),
+        AdminAccessGuard(),
+      ];
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const TreasuryPage();
+  }
+}
+
+final class CategoryDetailRoute extends GoRouteData
+    with FadePageTransitionMixin {
+  final String categoryId;
+
+  const CategoryDetailRoute(this.categoryId);
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return CategoryPage(categoryId: categoryId);
   }
 }
