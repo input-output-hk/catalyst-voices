117cd38e38b5ab8c4685edf4905747b94e902f46
januszjanus <janusz.janus@iohk.io>
2025-02-21 10:02:19 +0100

    cleanup

--- catalyst_voices/apps/voices/integration_test/all_test.dart
+++ catalyst_voices/apps/voices/integration_test/all_test.dart
@@ -4,8 +4,8 @@ import 'package:integration_test/integration_test.dart';
 
 import 'suites/account_test.dart' as account_test;
 import 'suites/app_test.dart' as app_test;
-import 'suites/onboarding_test.dart' as onboarding_test;
 import 'suites/discovery_test.dart' as discovery_test;
+import 'suites/onboarding_test.dart' as onboarding_test;
 
 void main() async {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -15,8 +15,8 @@ void main() async {
     await bootstrap(router: buildAppRouter());
   });
 
-  // app_test.main();
-  // onboarding_test.main();
-  // account_test.main();
+  app_test.main();
+  onboarding_test.main();
+  account_test.main();
   discovery_test.main();
 }
