f7650f32ba472d6988d21dafeda85b161712c6cd
Joaquín Rosales <joaquin.rosales@iohk.io>
2025-02-24 21:48:30 -0600

    fix(cat-gateway): fix panic catcher to disable live service flag

--- catalyst-gateway/bin/src/service/utilities/health.rs
+++ catalyst-gateway/bin/src/service/utilities/health.rs
@@ -2,10 +2,8 @@
 
 use std::{
     sync::atomic::{AtomicBool, AtomicU64, Ordering},
-    time::Duration,
+    time::SystemTime,
 };
-
-use crate::{db::index::session::CassandraSession, settings::Settings};
 
 /// Flag to determine if the service is live.
 ///
@@ -15,27 +13,30 @@ static IS_LIVE: AtomicBool = AtomicBool::new(true);
 /// Timestamp in seconds used to determine if the service is live.
 static LIVE_COUNTER: AtomicU64 = AtomicU64::new(0);
 
-/// Get the `IS_LIVE` flag
+/// Get the `IS_LIVE` flag.
 pub(crate) fn is_live() -> bool {
-    IS_LIVE.load(Ordering::Acquire) && CassandraSession::is_ready()
+    IS_LIVE.load(Ordering::Acquire)
 }
 
-/// Set the `IS_LIVE` flag to `false`
+/// Set the `IS_LIVE` flag to `false`.
 pub(crate) fn set_not_live() {
     IS_LIVE.store(false, Ordering::Release);
 }
 
-/// Get the `LIVE_COUNTER` timestamp value in seconds.
+/// Get the `LIVE_COUNTER` timestamp value, in seconds.
 pub(crate) fn get_live_counter() -> u64 {
     LIVE_COUNTER.load(Ordering::SeqCst)
 }
 
-/// Set the `LIVE_COUNTER` to current timestamp in seconds.
+/// Set the `LIVE_COUNTER` to current timestamp, in seconds.
 pub(crate) fn set_live_counter(now: u64) {
     LIVE_COUNTER.store(now, Ordering::SeqCst);
 }
 
-/// Returns `true` when `LIVE_COUNTER` is under the pre-defined threshold.
-pub(crate) fn is_live_counter_under_threshold() -> bool {
-    Duration::from_secs(get_live_counter()) < Settings::service_live_timeout_interval()
+/// Returns the timestamp of current system time, in seconds.
+pub(crate) fn get_current_timestamp() -> Option<u64> {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .ok()
+        .map(|s| s.as_secs())
 }
