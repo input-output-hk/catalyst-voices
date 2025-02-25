f978d70a017fbab2fa166f8c969d737476ca89de
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-02-25 20:40:11 +0700

    feat: metrics declaration

--- catalyst-gateway/bin/src/metrics/chain_indexer.rs
+++ catalyst-gateway/bin/src/metrics/chain_indexer.rs
@@ -1,1 +1,62 @@
 //! Metrics related to Chain Indexer analytics.
+
+/// All the related Chain Indexer reporting metrics to the Prometheus service are inside
+/// this module.
+pub(crate) mod reporter {
+    use std::sync::LazyLock;
+
+    use prometheus::{register_int_gauge_vec, IntGaugeVec};
+
+    /// Labels for the metrics.
+    const METRIC_LABELS: [&str; 0] = [];
+
+    /// Chain Indexer current tip slot#.
+    pub(crate) static CURRENT_TIP_SLOT: LazyLock<IntGaugeVec> = LazyLock::new(|| {
+        register_int_gauge_vec!(
+            "indexer_current_tip_slot",
+            "Chain Indexer current tip slot#",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+
+    /// Chain Indexer highest complete indexed slot#.
+    pub(crate) static HIGHEST_COMPLETE_INDEXED_SLOT: LazyLock<IntGaugeVec> = LazyLock::new(|| {
+        register_int_gauge_vec!(
+            "indexer_highest_complete_indexed_slot",
+            "Chain Indexer highest complete indexed slot#",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+
+    /// Chain Indexer number of current running indexer tasks.
+    pub(crate) static RUNNING_INDEXER_TASKS_COUNT: LazyLock<IntGaugeVec> = LazyLock::new(|| {
+        register_int_gauge_vec!(
+            "indexer_running_indexer_tasks_count",
+            "Chain Indexer number of current running indexer tasks",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+
+    /// Chain Indexer number of times triggering data purge.
+    pub(crate) static TRIGGERED_DATA_PURGES_COUNT: LazyLock<IntGaugeVec> = LazyLock::new(|| {
+        register_int_gauge_vec!(
+            "indexer_triggered_data_purges_count",
+            "Chain Indexer number of times triggering data purge",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+
+    /// Chain Indexer indicator whether tip is reached or in progress.
+    pub(crate) static REACHED_TIP: LazyLock<IntGaugeVec> = LazyLock::new(|| {
+        register_int_gauge_vec!(
+            "indexer_reached_tip_indicator",
+            "Chain Indexer indicator whether tip is reached or in progress",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+}
