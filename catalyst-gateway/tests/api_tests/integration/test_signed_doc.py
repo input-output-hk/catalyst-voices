c1bd149b366ff526c6522087a7612cd2c974ba53
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-25 12:02:51 +0200

    update tests

--- catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
+++ catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
@@ -22,6 +22,8 @@ proposal_metadata_json = {
     # referenced to the defined category id, comes from the 'templates/data.rs' file
     "category_id": {"id": "0194d490-30bf-7473-81c8-a0eaef369619"},
 }
+with open("./test_data/signed_docs/proposal.json", "r") as proposal_json_file:
+    proposal_json = json.load(proposal_json_file)
 
 comment_doc_id = uuid7str()
 comment_metadata_json = {
@@ -36,6 +38,8 @@ comment_metadata_json = {
     # referenced to the defined comment template id, comes from the 'templates/data.rs' file
     "template": {"id": "0194d494-4402-7e0e-b8d6-171f8fea18b0"},
 }
+with open("./test_data/signed_docs/comment.json", "r") as comment_json_file:
+    comment_json = json.load(comment_json_file)
 
 
 def build_signed_doc(
@@ -65,15 +69,9 @@ def build_signed_doc(
         return signed_doc_hex
 
 
-def test_signed_doc():
+def test_proposal_doc():
     health.is_live()
     health.is_ready()
-
-    with open("./test_data/signed_docs/proposal.json", "r") as proposal_json_file:
-        proposal_json = json.load(proposal_json_file)
-
-    with open("./test_data/signed_docs/comment.json", "r") as comment_json_file:
-        comment_json = json.load(comment_json_file)
 
     # Put a proposal document
     proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
@@ -87,26 +85,8 @@ def test_signed_doc():
         resp.status_code == 204
     ), f"Failed to publish document: {resp.status_code} - {resp.text}"
 
-    # Put a comment document
-    comment_doc = build_signed_doc(comment_metadata_json, comment_json)
-    resp = document.put(data=comment_doc)
-    assert (
-        resp.status_code == 201
-    ), f"Failed to publish document: {resp.status_code} - {resp.text}"
-    # Put a comment document again
-    resp = document.put(data=comment_doc)
-    assert (
-        resp.status_code == 204
-    ), f"Failed to publish document: {resp.status_code} - {resp.text}"
-
     # Get the proposal document
     resp = document.get(document_id=proposal_doc_id)
-    assert (
-        resp.status_code == 200
-    ), f"Failed to get document: {resp.status_code} - {resp.text}"
-
-    # Get the comment document
-    resp = document.get(document_id=comment_doc_id)
     assert (
         resp.status_code == 200
     ), f"Failed to get document: {resp.status_code} - {resp.text}"
@@ -132,6 +112,69 @@ def test_signed_doc():
     assert (
         resp.status_code == 201
     ), f"Failed to publish document: {resp.status_code} - {resp.text}"
+
+    # Put a proposal document corrupted content
+    proposal_metadata_json["ver"] = uuid7str()
+    proposal_doc = build_signed_doc(proposal_metadata_json, {"random": "random"})
+    resp = document.put(data=proposal_doc)
+    assert (
+        resp.status_code == 422
+    ), f"Publish document, expected 422 Unprocessable Content: {resp.status_code} - {resp.text}"
+
+    # Put a proposal document with the not known template field
+    proposal_metadata_json["template"] = {"id": uuid7str()}
+    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    resp = document.put(data=proposal_doc)
+    assert (
+        resp.status_code == 422
+    ), f"Publish document, expected 422 Unprocessable Content: {resp.status_code} - {resp.text}"
+
+    logger.info("Proposal document test successful.")
+
+
+def test_comment_doc():
+    health.is_live()
+    health.is_ready()
+
+    # Put a comment document
+    comment_doc = build_signed_doc(comment_metadata_json, comment_json)
+    resp = document.put(data=comment_doc)
+    assert (
+        resp.status_code == 201
+    ), f"Failed to publish document: {resp.status_code} - {resp.text}"
+    # Put a comment document again
+    resp = document.put(data=comment_doc)
+    assert (
+        resp.status_code == 204
+    ), f"Failed to publish document: {resp.status_code} - {resp.text}"
+
+    # Get the comment document
+    resp = document.get(document_id=comment_doc_id)
+    assert (
+        resp.status_code == 200
+    ), f"Failed to get document: {resp.status_code} - {resp.text}"
+
+    # Post a signed document with filter ID
+    resp = document.post("/index", filter={"id": {"eq": proposal_doc_id}})
+    assert (
+        resp.status_code == 200
+    ), f"Failed to post document: {resp.status_code} - {resp.text}"
+
+    # Put a comment document corrupted content
+    comment_metadata_json["ver"] = uuid7str()
+    comment_doc = build_signed_doc(comment_metadata_json, {"random": "random"})
+    resp = document.put(data=comment_doc)
+    assert (
+        resp.status_code == 422
+    ), f"Publish document, expected 422 Unprocessable Content: {resp.status_code} - {resp.text}"
+
+    # Put a comment document referencing to the not known proposal
+    comment_metadata_json["ref"] = {"id": uuid7str()}
+    comment_doc = build_signed_doc(comment_metadata_json, comment_json)
+    resp = document.put(data=comment_doc)
+    assert (
+        resp.status_code == 422
+    ), f"Publish document, expected 422 Unprocessable Content: {resp.status_code} - {resp.text}"
 
     logger.info("Signed document test successful.")
 
