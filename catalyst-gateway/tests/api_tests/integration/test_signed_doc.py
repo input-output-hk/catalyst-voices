959fdd33e33cafa5d34c24ab4fd27bcc9310dab5
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-25 12:43:52 +0200

    fix

--- catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
+++ catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
@@ -8,39 +8,6 @@ import json
 from typing import Dict, Any
 from uuid_extensions import uuid7str
 
-proposal_doc_id = uuid7str()
-proposal_metadata_json = {
-    "alg": "EdDSA",
-    "id": proposal_doc_id,
-    "ver": proposal_doc_id,
-    # Proposal document type
-    "type": "7808d2ba-d511-40af-84e8-c0d1625fdfdc",
-    "content-type": "application/json",
-    "content-encoding": "br",
-    # referenced to the defined proposal template id, comes from the 'templates/data.rs' file
-    "template": {"id": "0194d492-1daa-75b5-b4a4-5cf331cd8d1a"},
-    # referenced to the defined category id, comes from the 'templates/data.rs' file
-    "category_id": {"id": "0194d490-30bf-7473-81c8-a0eaef369619"},
-}
-with open("./test_data/signed_docs/proposal.json", "r") as proposal_json_file:
-    proposal_json = json.load(proposal_json_file)
-
-comment_doc_id = uuid7str()
-comment_metadata_json = {
-    "alg": "EdDSA",
-    "id": comment_doc_id,
-    "ver": comment_doc_id,
-    # Comment document type
-    "type": "b679ded3-0e7c-41ba-89f8-da62a17898ea",
-    "content-type": "application/json",
-    "content-encoding": "br",
-    "ref": {"id": proposal_doc_id},
-    # referenced to the defined comment template id, comes from the 'templates/data.rs' file
-    "template": {"id": "0194d494-4402-7e0e-b8d6-171f8fea18b0"},
-}
-with open("./test_data/signed_docs/comment.json", "r") as comment_json_file:
-    comment_json = json.load(comment_json_file)
-
 
 def build_signed_doc(
     metadata_json: Dict[str, Any], doc_content_json: Dict[str, Any]
@@ -69,9 +36,51 @@ def build_signed_doc(
         return signed_doc_hex
 
 
-def test_proposal_doc():
+def test_signed_doc():
     health.is_live()
     health.is_ready()
+
+    # prepare the data
+    proposal_doc_id = uuid7str()
+    proposal_metadata_json = {
+        "alg": "EdDSA",
+        "id": proposal_doc_id,
+        "ver": proposal_doc_id,
+        # Proposal document type
+        "type": "7808d2ba-d511-40af-84e8-c0d1625fdfdc",
+        "content-type": "application/json",
+        "content-encoding": "br",
+        # referenced to the defined proposal template id, comes from the 'templates/data.rs' file
+        "template": {"id": "0194d492-1daa-75b5-b4a4-5cf331cd8d1a"},
+        # referenced to the defined category id, comes from the 'templates/data.rs' file
+        "category_id": {"id": "0194d490-30bf-7473-81c8-a0eaef369619"},
+    }
+    with open("./test_data/signed_docs/proposal.json", "r") as proposal_json_file:
+        proposal_json = json.load(proposal_json_file)
+
+    comment_doc_id = uuid7str()
+    comment_metadata_json = {
+        "alg": "EdDSA",
+        "id": comment_doc_id,
+        "ver": comment_doc_id,
+        # Comment document type
+        "type": "b679ded3-0e7c-41ba-89f8-da62a17898ea",
+        "content-type": "application/json",
+        "content-encoding": "br",
+        "ref": {"id": proposal_doc_id},
+        # referenced to the defined comment template id, comes from the 'templates/data.rs' file
+        "template": {"id": "0194d494-4402-7e0e-b8d6-171f8fea18b0"},
+    }
+    with open("./test_data/signed_docs/comment.json", "r") as comment_json_file:
+        comment_json = json.load(comment_json_file)
+
+    proposal_doc_check(proposal_metadata_json, proposal_json)
+    comment_doc_check(comment_metadata_json, comment_json)
+    pagination_out_of_range_check()
+
+
+def proposal_doc_check(proposal_metadata_json, proposal_json):
+    proposal_doc_id = proposal_metadata_json["id"]
 
     # Put a proposal document
     proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
@@ -132,9 +141,8 @@ def test_proposal_doc():
     logger.info("Proposal document test successful.")
 
 
-def test_comment_doc():
-    health.is_live()
-    health.is_ready()
+def comment_doc_check(comment_metadata_json, comment_json):
+    comment_doc_id = comment_metadata_json["id"]
 
     # Put a comment document
     comment_doc = build_signed_doc(comment_metadata_json, comment_json)
@@ -155,7 +163,7 @@ def test_comment_doc():
     ), f"Failed to get document: {resp.status_code} - {resp.text}"
 
     # Post a signed document with filter ID
-    resp = document.post("/index", filter={"id": {"eq": proposal_doc_id}})
+    resp = document.post("/index", filter={"id": {"eq": comment_doc_id}})
     assert (
         resp.status_code == 200
     ), f"Failed to post document: {resp.status_code} - {resp.text}"
@@ -179,10 +187,7 @@ def test_comment_doc():
     logger.info("Signed document test successful.")
 
 
-def test_pagination_out_of_range():
-    health.is_live()
-    health.is_ready()
-
+def pagination_out_of_range_check():
     # Pagination out of range
     resp = document.post(
         "/index?page=92233720368547759",
