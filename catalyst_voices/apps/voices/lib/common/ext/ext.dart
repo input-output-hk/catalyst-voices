83ff893a041f104b99009b59902631a89297d4b7
Dominik Toton <dominik.toton@iohk.io>
2025-02-25 14:07:49 +0100

    fix: imports

--- catalyst_voices/apps/voices/lib/common/ext/ext.dart
+++ catalyst_voices/apps/voices/lib/common/ext/ext.dart
@@ -1,4 +1,3 @@
 export 'brand_ext.dart';
 export 'build_context_ext.dart';
 export 'space_ext.dart';
-export 'string_ext.dart';
--- catalyst_voices/apps/voices/lib/pages/proposal_builder/appbar/proposal_builder_status_action.dart
+++ catalyst_voices/apps/voices/lib/pages/proposal_builder/appbar/proposal_builder_status_action.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:catalyst_voices/common/ext/string_ext.dart';
 import 'package:catalyst_voices/pages/proposal_builder/dialog/proposal_builder_delete_confirmation_dialog.dart';
 import 'package:catalyst_voices/widgets/modals/proposals/publish_proposal_iteration_dialog.dart';
 import 'package:catalyst_voices/widgets/modals/proposals/submit_proposal_for_review_dialog.dart';
