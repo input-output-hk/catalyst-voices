90dd24ddb64f8a5649f5ef6bce489c9cfac82d6c
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-25 10:19:12 +0200

    update test

--- catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
+++ catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
@@ -8,6 +8,35 @@ import json
 from typing import Dict, Any
 from uuid_extensions import uuid7str
 
+proposal_doc_id = uuid7str()
+proposal_metadata_json = {
+    "alg": "EdDSA",
+    "id": proposal_doc_id,
+    "ver": proposal_doc_id,
+    # Proposal document type
+    "type": "7808d2ba-d511-40af-84e8-c0d1625fdfdc",
+    "content-type": "application/json",
+    "content-encoding": "br",
+    # referenced to the defined proposal template id, comes from the 'templates/data.rs' file
+    "template": {"id": "0194d492-1daa-75b5-b4a4-5cf331cd8d1a"},
+    # referenced to the defined category id, comes from the 'templates/data.rs' file
+    "category_id": {"id": "0194d490-30bf-7473-81c8-a0eaef369619"},
+}
+
+comment_doc_id = uuid7str()
+comment_metadata_json = {
+    "alg": "EdDSA",
+    "id": comment_doc_id,
+    "ver": comment_doc_id,
+    # Comment document type
+    "type": "b679ded3-0e7c-41ba-89f8-da62a17898ea",
+    "content-type": "application/json",
+    "content-encoding": "br",
+    "ref": {"id": proposal_doc_id},
+    # referenced to the defined comment template id, comes from the 'templates/data.rs' file
+    "template": {"id": "0194d494-4402-7e0e-b8d6-171f8fea18b0"},
+}
+
 
 def build_signed_doc(
     metadata_json: Dict[str, Any], doc_content_json: Dict[str, Any]
@@ -40,73 +69,82 @@ def test_signed_doc():
     health.is_live()
     health.is_ready()
 
-    doc_id = uuid7str()
-    metadata_json = {
-        "alg": "EdDSA",
-        "id": doc_id,
-        "ver": doc_id,
-        # Proposal document type
-        "type": "7808d2ba-d511-40af-84e8-c0d1625fdfdc",
-        "content-type": "application/json",
-        "content-encoding": "br",
-        # referenced to the defined proposal template id, comes from the 'templates/data.rs' file
-        "template": {"id": "0194d492-1daa-75b5-b4a4-5cf331cd8d1a"},
-        # referenced to the defined category id, comes from the 'templates/data.rs' file
-        "category_id": {"id": "0194d490-30bf-7473-81c8-a0eaef369619"},
-    }
-
     with open("./test_data/signed_docs/proposal.json", "r") as proposal_json_file:
         proposal_json = json.load(proposal_json_file)
 
-    # valid proposal document
-    signed_doc = build_signed_doc(metadata_json, proposal_json)
+    with open("./test_data/signed_docs/comment.json", "r") as comment_json_file:
+        comment_json = json.load(comment_json_file)
 
-    # Put a signed document
-    resp = document.put(data=signed_doc)
+    # Put a proposal document
+    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 201
     ), f"Failed to publish document: {resp.status_code} - {resp.text}"
-    # Put a signed document again
-    resp = document.put(data=signed_doc)
+    # Put a proposal document again
+    resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 204
     ), f"Failed to publish document: {resp.status_code} - {resp.text}"
 
-    # Get the signed document
-    resp = document.get(document_id=doc_id)
+    # Put a comment document
+    comment_doc = build_signed_doc(comment_metadata_json, comment_json)
+    resp = document.put(data=comment_doc)
+    assert (
+        resp.status_code == 201
+    ), f"Failed to publish document: {resp.status_code} - {resp.text}"
+    # Put a comment document again
+    resp = document.put(data=comment_doc)
+    assert (
+        resp.status_code == 204
+    ), f"Failed to publish document: {resp.status_code} - {resp.text}"
+
+    # Get the proposal document
+    resp = document.get(document_id=proposal_doc_id)
+    assert (
+        resp.status_code == 200
+    ), f"Failed to get document: {resp.status_code} - {resp.text}"
+
+    # Get the comment document
+    resp = document.get(document_id=comment_doc_id)
     assert (
         resp.status_code == 200
     ), f"Failed to get document: {resp.status_code} - {resp.text}"
 
     # Post a signed document with filter ID
-    resp = document.post("/index", filter={"id": {"eq": doc_id}})
+    resp = document.post("/index", filter={"id": {"eq": proposal_doc_id}})
     assert (
         resp.status_code == 200
     ), f"Failed to post document: {resp.status_code} - {resp.text}"
 
-    # Put a signed document with same ID different content
+    # Put a proposal document with same ID different content
     proposal_json["setup"]["title"]["title"] = "another title"
-    signed_doc = build_signed_doc(metadata_json, proposal_json)
-    resp = document.put(data=signed_doc)
+    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 422
     ), f"Publish document, expected 422 Unprocessable Content: {resp.status_code} - {resp.text}"
 
     # Put a signed document with same ID, but different version and different content
-    metadata_json["ver"] = uuid7str()
-    signed_doc = build_signed_doc(metadata_json, proposal_json)
-    resp = document.put(data=signed_doc)
+    proposal_metadata_json["ver"] = uuid7str()
+    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 201
     ), f"Failed to publish document: {resp.status_code} - {resp.text}"
 
+    logger.info("Signed document test successful.")
+
+
+def test_pagination_out_of_range():
+    health.is_live()
+    health.is_ready()
+
     # Pagination out of range
     resp = document.post(
         "/index?page=92233720368547759",
-        filter={"id": {"eq": doc_id}},
+        filter={},
     )
     assert (
         resp.status_code == 412
     ), f"Post document, expected 412 Precondition Failed: {resp.status_code} - {resp.text}"
-
-    logger.info("Signed document test successful.")
