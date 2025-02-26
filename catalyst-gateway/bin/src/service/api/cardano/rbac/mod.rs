8659dec5323e9f95104604357b3296b7a4dda4b9
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-26 13:20:24 +0200

    wip

--- catalyst-gateway/bin/src/service/api/cardano/rbac/mod.rs
+++ catalyst-gateway/bin/src/service/api/cardano/rbac/mod.rs
@@ -1,77 +1,52 @@
 //! RBAC endpoints.
 
-use poem_openapi::{param::Path, OpenApi};
+use poem::http::HeaderMap;
+use poem_openapi::{param::Query, payload::Json, OpenApi};
 
 use crate::service::common::{
-    auth::none_or_rbac::NoneOrRBAC,
-    objects::cardano::hash::Hash128,
-    tags::ApiTags,
-    types::cardano::{catalyst_id::CatalystId, cip19_stake_address::Cip19StakeAddress},
+    auth::none_or_rbac::NoneOrRBAC, tags::ApiTags,
+    types::cardano::query::stake_or_voter::StakeOrVoter,
 };
 
-mod chain_root_get;
 mod registrations_get;
-mod role0_chain_root_get;
+mod unprocessable_content;
 
 /// Cardano RBAC API Endpoints
 pub(crate) struct Api;
 
 #[OpenApi(tag = "ApiTags::Cardano")]
 impl Api {
-    // TODO: "Chain root" was replaced by "Catalyst ID", so this endpoint needs to be updated
-    // or removed.
-    #[oai(
-        path = "/draft/rbac/chain_root/:stake_address",
-        method = "get",
-        operation_id = "rbacChainRootGet"
-    )]
-    /// Get RBAC chain root
-    ///
-    /// This endpoint returns the RBAC certificate chain root for a given stake address.
-    async fn rbac_chain_root_get(
-        &self,
-        /// Stake address to get the chain root for.
-        Path(stake_address): Path<Cip19StakeAddress>,
-        /// No Authorization required, but Token permitted.
-        _auth: NoneOrRBAC,
-    ) -> chain_root_get::AllResponses {
-        chain_root_get::endpoint(stake_address).await
-    }
-
     #[oai(
         path = "/draft/rbac/registrations/:catalyst_id",
         method = "get",
         operation_id = "rbacRegistrations"
     )]
-    /// Get registrations by Catalyst short ID.
+    /// Get RBAC registrations
     ///
-    /// This endpoint returns RBAC registrations for the given Catalyst short identifier.
+    /// This endpoint returns RBAC registrations by provided auth Catalyst Id credentials
+    /// or by the `lookup` argument if provided.
     async fn rbac_registrations_get(
         &self,
-        /// A Catalyst short ID to get the registrations for.
-        Path(catalyst_id): Path<CatalystId>,
+        /// Stake address to get the RBACE registration for.
+        Query(lookup): Query<Option<StakeOrVoter>>,
+        /// Headers, used if the query is requesting ALL to determine if the secret API
+        /// Key is also defined.
+        headers: &HeaderMap,
         /// No Authorization required, but Token permitted.
         _auth: NoneOrRBAC,
     ) -> registrations_get::AllResponses {
-        registrations_get::endpoint(catalyst_id.into()).await
-    }
+        // Special validation for the `lookup` parameter.
+        // If the parameter is ALL, BUT we do not have a valid API Key, just report the parameter
+        // is invalid.
+        if let Some(lookup) = lookup.clone() {
+            if lookup.is_all(headers).is_err() {
+                return registrations_get::Responses::UnprocessableContent(Json(
+                    unprocessable_content::RbacUnprocessableContent::new("Invalid Stake Address or Voter key"),
+                ))
+                .into();
+            }
+        }
 
-    // TODO: "Chain root" was replaced by "Catalyst ID", so this endpoint needs to be updated
-    // or removed.
-    #[oai(
-        path = "/draft/rbac/role0_chain_root/:role0_key",
-        method = "get",
-        operation_id = "rbacRole0KeyChainRoot"
-    )]
-    /// Get RBAC chain root for a given role0 key.
-    ///
-    /// This endpoint returns the RBAC certificate chain root for a given role 0 key.
-    async fn rbac_role0_key_chain_root(
-        &self, /// Role0 key to get the chain root for.
-        Path(role0_key): Path<Hash128>,
-        /// No Authorization required, but Token permitted.
-        _auth: NoneOrRBAC,
-    ) -> role0_chain_root_get::AllResponses {
-        role0_chain_root_get::endpoint(role0_key.to_string()).await
+        registrations_get::endpoint(lookup).await
     }
 }
--- catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get.rs
+++ catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get.rs
@@ -1,140 +1,50 @@
 //! Implementation of the GET `/rbac/registrations` endpoint.
 
-use anyhow::anyhow;
-use cardano_blockchain_types::TransactionId;
-use catalyst_types::id_uri::IdUri;
-use futures::StreamExt;
-use poem_openapi::{
-    payload::Json,
-    types::{Example, ToJSON},
-    ApiResponse, Object,
-};
+use poem_openapi::{payload::Json, ApiResponse};
 use tracing::error;
 
+use super::unprocessable_content::RbacUnprocessableContent;
 use crate::{
-    db::index::{
-        queries::rbac::get_rbac_registrations::{Query, QueryParams},
-        session::CassandraSession,
-    },
+    db::index::session::CassandraSession,
     service::common::{
-        objects::cardano::hash::Hash256,
         responses::WithErrorResponses,
-        types::{array_types::impl_array_types, headers::retry_after::RetryAfterOption},
+        types::{
+            cardano::query::stake_or_voter::StakeOrVoter, headers::retry_after::RetryAfterOption,
+        },
     },
 };
-
-/// GET RBAC registrations by chain root response list item.
-#[derive(Object, Debug, Clone)]
-#[oai(example = true)]
-pub(crate) struct RbacRegistration {
-    /// Registration transaction hash.
-    tx_hash: Hash256,
-}
-
-impl Example for RbacRegistration {
-    fn example() -> Self {
-        Self {
-            tx_hash: Example::example(),
-        }
-    }
-}
-
-// List of RBAC Registrations
-impl_array_types!(
-    RegistrationRbacList,
-    RbacRegistration,
-    Some(poem_openapi::registry::MetaSchema {
-        example: Self::example().to_json(),
-        max_items: Some(100_000),
-        items: Some(Box::new(RbacRegistration::schema_ref())),
-        ..poem_openapi::registry::MetaSchema::ANY
-    })
-);
-
-impl Example for RegistrationRbacList {
-    fn example() -> Self {
-        Self(vec![Example::example()])
-    }
-}
-
-/// GET RBAC registrations by chain root response.
-#[derive(Object, Debug, Clone)]
-#[oai(example = true)]
-pub(crate) struct RbacRegistrationsResponse {
-    /// Registrations by RBAC chain root.
-    registrations: RegistrationRbacList,
-}
-
-impl Example for RbacRegistrationsResponse {
-    fn example() -> Self {
-        Self {
-            registrations: Example::example(),
-        }
-    }
-}
 
 /// Endpoint responses.
 #[derive(ApiResponse)]
+#[allow(dead_code)]
 pub(crate) enum Responses {
     /// ## Ok
     ///
     /// Success returns a list of registration transaction ids.
     #[oai(status = 200)]
-    Ok(Json<RbacRegistrationsResponse>),
+    Ok(Json<String>),
+
+    /// No valid registration.
+    #[oai(status = 404)]
+    NotFound,
+
+    /// Response for unprocessable content.
+    #[oai(status = 422)]
+    UnprocessableContent(Json<RbacUnprocessableContent>),
 }
 
 pub(crate) type AllResponses = WithErrorResponses<Responses>;
 
-/// Get chain root endpoint.
-pub(crate) async fn endpoint(catalyst_id: IdUri) -> AllResponses {
-    let Some(session) = CassandraSession::get(true) else {
+/// Get RBAC registration endpoint.
+#[allow(clippy::unused_async)]
+pub(crate) async fn endpoint(_lookup: Option<StakeOrVoter>) -> AllResponses {
+    let Some(_session) = CassandraSession::get(true) else {
         error!("Failed to acquire db session");
         let err = anyhow::anyhow!("Failed to acquire db session");
         return AllResponses::service_unavailable(&err, RetryAfterOption::Default);
     };
 
-    let query_res = Query::execute(&session, QueryParams {
-        catalyst_id: catalyst_id.into(),
-    })
-    .await;
+    
 
-    let mut row_iter = match query_res {
-        Ok(row_iter) => row_iter,
-        Err(err) => {
-            error!(
-                error = ?err,
-                "Failed to execute get registrations by chain root query"
-            );
-            return AllResponses::internal_error(&err);
-        },
-    };
-
-    let mut registrations = Vec::new();
-    while let Some(row_res) = row_iter.next().await {
-        let row = match row_res {
-            Ok(row) => row,
-            Err(err) => {
-                error!(error = ?err, "Failed to parse get registrations by chain root query row");
-                let err = anyhow!(err);
-                return AllResponses::internal_error(&err);
-            },
-        };
-
-        let tx_hash: Vec<_> = TransactionId::from(row.txn_id).into();
-        let tx_hash = match TryFrom::try_from(tx_hash) {
-            Ok(tx_hash) => tx_hash,
-            Err(err) => {
-                error!(error = ?err, "Failed to parse tx hash value from row");
-                return AllResponses::internal_error(&err);
-            },
-        };
-
-        let item = RbacRegistration { tx_hash };
-        registrations.push(item);
-    }
-
-    Responses::Ok(Json(RbacRegistrationsResponse {
-        registrations: registrations.into(),
-    }))
-    .into()
+    Responses::NotFound.into()
 }
