ad1fa33023bdbedf4b296c8d6fb099338db3ba67
Damian Molinski <damian.molinski@iohk.io>
2025-02-27 11:28:06 +0100

    feat: define empty tiles

---
+++ catalyst_voices/apps/voices/lib/pages/proposal/tiles/proposal_metadata_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProposalMetadataTile extends StatelessWidget {
+  const ProposalMetadataTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(child: Text('Metadata'));
+  }
+}
---
+++ catalyst_voices/apps/voices/lib/pages/proposal/tiles/proposal_overview_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProposalOverviewTile extends StatelessWidget {
+  const ProposalOverviewTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(child: Text('Overview'));
+  }
+}
