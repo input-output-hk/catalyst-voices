b85b6295966c0b7b46b42b723a58f84de5286aab
Mr-Leshiy <leshiy12345678@gmail.com>
2025-02-28 12:12:08 +0200

    move build_signed_doc to utils mod

--- catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
+++ catalyst-gateway/tests/api_tests/integration/test_signed_doc.py
@@ -1,40 +1,11 @@
 import pytest
 from loguru import logger
-from utils import health
+from utils import health, signed_doc
 from api.v1 import document
-from tempfile import NamedTemporaryFile
-import subprocess
 import os
 import json
 from typing import Dict, Any, List
 from uuid_extensions import uuid7str
-
-
-def build_signed_doc(
-    metadata_json: Dict[str, Any], doc_content_json: Dict[str, Any]
-) -> str:
-    with NamedTemporaryFile() as metadata_file, NamedTemporaryFile() as doc_content_file, NamedTemporaryFile() as signed_doc_file:
-
-        json_str = json.dumps(metadata_json)
-        metadata_file.write(json_str.encode(encoding="utf-8"))
-        metadata_file.flush()
-
-        json_str = json.dumps(doc_content_json)
-        doc_content_file.write(json_str.encode(encoding="utf-8"))
-        doc_content_file.flush()
-
-        subprocess.run(
-            [
-                "./mk_signed_doc",
-                "build",
-                doc_content_file.name,
-                signed_doc_file.name,
-                metadata_file.name,
-            ],
-        )
-
-        signed_doc_hex = signed_doc_file.read().hex()
-        return signed_doc_hex
 
 
 def test_signed_doc():
@@ -124,7 +95,7 @@ def proposal_doc_check(
     proposal_doc_id = proposal_metadata_json["id"]
 
     # Put a proposal document
-    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    proposal_doc = signed_doc.build_signed_doc(proposal_metadata_json, proposal_json)
     resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 201
@@ -149,7 +120,7 @@ def proposal_doc_check(
 
     # Put a proposal document with same ID different content
     proposal_json["setup"]["title"]["title"] = "another title"
-    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    proposal_doc = signed_doc.build_signed_doc(proposal_metadata_json, proposal_json)
     resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 422
@@ -157,7 +128,7 @@ def proposal_doc_check(
 
     # Put a signed document with same ID, but different version and different content
     proposal_metadata_json["ver"] = uuid7str()
-    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    proposal_doc = signed_doc.build_signed_doc(proposal_metadata_json, proposal_json)
     resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 201
@@ -165,7 +136,7 @@ def proposal_doc_check(
 
     # Put a proposal document with empty content
     proposal_metadata_json["ver"] = uuid7str()
-    proposal_doc = build_signed_doc(proposal_metadata_json, {})
+    proposal_doc = signed_doc.build_signed_doc(proposal_metadata_json, {})
     resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 422
@@ -173,7 +144,7 @@ def proposal_doc_check(
 
     # Put a proposal document with the not known template field
     proposal_metadata_json["template"] = {"id": uuid7str()}
-    proposal_doc = build_signed_doc(proposal_metadata_json, proposal_json)
+    proposal_doc = signed_doc.build_signed_doc(proposal_metadata_json, proposal_json)
     resp = document.put(data=proposal_doc)
     assert (
         resp.status_code == 422
@@ -188,7 +159,7 @@ def comment_doc_check(
     comment_doc_id = comment_metadata_json["id"]
 
     # Put a comment document
-    comment_doc = build_signed_doc(comment_metadata_json, comment_json)
+    comment_doc = signed_doc.build_signed_doc(comment_metadata_json, comment_json)
     resp = document.put(data=comment_doc)
     assert (
         resp.status_code == 201
@@ -213,7 +184,7 @@ def comment_doc_check(
 
     # Put a comment document with empty content
     comment_metadata_json["ver"] = uuid7str()
-    comment_doc = build_signed_doc(comment_metadata_json, {})
+    comment_doc = signed_doc.build_signed_doc(comment_metadata_json, {})
     resp = document.put(data=comment_doc)
     assert (
         resp.status_code == 422
@@ -221,7 +192,7 @@ def comment_doc_check(
 
     # Put a comment document referencing to the not known proposal
     comment_metadata_json["ref"] = {"id": uuid7str()}
-    comment_doc = build_signed_doc(comment_metadata_json, comment_json)
+    comment_doc = signed_doc.build_signed_doc(comment_metadata_json, comment_json)
     resp = document.put(data=comment_doc)
     assert (
         resp.status_code == 422
