909a5188f86eb12f75ff8b250de84e542ab44af9
kukkok3 <93382903+kukkok3@users.noreply.github.com>
2025-03-05 10:54:23 +0100

    fix: adds dependecies to flutter test

--- catalyst_voices/utilities/poc_local_storage/macos/Flutter/GeneratedPluginRegistrant.swift
+++ catalyst_voices/utilities/poc_local_storage/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import flutter_secure_storage_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
