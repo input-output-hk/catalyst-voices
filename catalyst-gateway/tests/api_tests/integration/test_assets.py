0777c1068abb9d09bacb09209a2a74381d12bc08
Mr-Leshiy <leshiy12345678@gmail.com>
2025-03-07 09:49:14 +0200

    wip

--- catalyst-gateway/tests/api_tests/integration/test_assets.py
+++ catalyst-gateway/tests/api_tests/integration/test_assets.py
@@ -3,6 +3,7 @@ from utils.address import stake_public_key_to_address
 from utils.snapshot import snapshot
 from api.v1 import cardano
 
+
 # @pytest.mark.skip('To be refactored when the api is ready')
 def test_persistent_ada_amount_endpoint(snapshot):
     # health.is_live()
@@ -16,13 +17,15 @@ def test_persistent_ada_amount_endpoint(snapshot):
             network_type=snapshot.network,
         )
         resp = cardano.assets(stake_address, snapshot.slot_no, snapshot.network)
-        assert resp.status_code == 200 or resp.status_code == 404
-        if resp.status_code == 200:
-            assets = resp.json()
-            assert (
-                assets["persistent"]["ada_amount"] == expected_amount
-            ), f"Not expected ada amount for stake_address: {stake_address}"
-        # it is possible that snapshot tool collected data for the stake key which does not have any unspent utxo
-        # at this case cat-gateway return 404, that is why we are checking this case additionally
-        if resp.status_code == 400:
-            assert expected_amount == 0
+        if expected_amount == 0 and resp.status_code == 404:
+            # it is possible that snapshot tool collected data for the stake key which does not have any unspent utxo
+            # at this case cat-gateway return 404, that is why we are checking this case additionally
+            return
+
+        assets = resp.json()
+        assert (
+            resp.status_code == 200
+        ), f"Cannot find assets for stake_address: {stake_address}"
+        assert (
+            assets["persistent"]["ada_amount"] == expected_amount
+        ), f"Not expected ada amount for stake_address: {stake_address}"
