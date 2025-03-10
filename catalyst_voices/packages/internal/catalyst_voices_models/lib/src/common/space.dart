d62e36c5c997b984f33ddd534cc75f1ee470bd97
Ryszard Schossler <rysiek9801@gmail.com>
2025-03-10 13:35:17 +0100

    refactor: remove unnecessary isAdmin parameter from Space.spaces method in space.dart

--- catalyst_voices/packages/internal/catalyst_voices_models/lib/src/common/space.dart
+++ catalyst_voices/packages/internal/catalyst_voices_models/lib/src/common/space.dart
@@ -5,10 +5,4 @@ enum Space {
   voting,
   fundedProjects,
   treasury;
-
-  // ignore: avoid_positional_boolean_parameters
-  static List<Space> spaces(bool isAdmin) {
-    if (isAdmin) return Space.values;
-    return Space.values.sublist(0, 4);
-  }
 }
