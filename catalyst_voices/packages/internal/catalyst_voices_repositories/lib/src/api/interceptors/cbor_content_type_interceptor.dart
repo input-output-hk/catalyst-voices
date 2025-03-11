1edd7e2633dd79ff5f2264241bf01ac4a1f69cad
Dominik Toton <dominik.toton@iohk.io>
2025-03-11 10:06:44 +0100

    feat: auth token provider

---
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/api/interceptors/cbor_content_type_interceptor.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:chopper/chopper.dart';
+
+/// An [Interceptor] which overrides the content-type header for [supportedRequests].
+///
+/// The swagger_dart_code_generator package doesn't support
+/// generating requests with application/cbor content-type,
+/// instead it sends these requests as application/json.
+final class CborContentTypeInterceptor implements Interceptor {
+  static const headerKey = 'Content-Type';
+  static const headerValue = 'application/cbor';
+  static const supportedRequests = [
+    (method: 'PUT', path: '/api/v1/document'),
+  ];
+
+  const CborContentTypeInterceptor();
+
+  @override
+  FutureOr<Response<BodyType>> intercept<BodyType>(
+    Chain<BodyType> chain,
+  ) async {
+    var request = chain.request;
+
+    for (final cborRequest in supportedRequests) {
+      if (cborRequest.method.toUpperCase() == request.method.toUpperCase() &&
+          cborRequest.path.toUpperCase() == request.uri.path.toUpperCase()) {
+        request = applyHeader(request, headerKey, headerValue);
+        break;
+      }
+    }
+
+    return chain.proceed(request);
+  }
+}
