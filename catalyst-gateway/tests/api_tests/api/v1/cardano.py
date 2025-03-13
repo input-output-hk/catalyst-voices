e8e899567853a6d22d0083c124e559998bb90728
Mr-Leshiy <leshiy12345678@gmail.com>
2025-03-13 16:41:17 +0200

    fix

--- catalyst-gateway/tests/api_tests/api/v1/cardano.py
+++ catalyst-gateway/tests/api_tests/api/v1/cardano.py
@@ -6,21 +6,19 @@ URL = cat_gateway_endpoint_url("api/v1/cardano")
 
 # cardano assets GET
 def assets(stake_address: str, slot_no: int, network: str):
-    document_url = f"{URL}/assets/{stake_address}?asat=SLOT:{slot_no}&network={network}"
+    url = f"{URL}/assets/{stake_address}?asat=SLOT:{slot_no}&network={network}"
     headers = {
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json",
     }
-    return requests.get(document_url, headers=headers)
+    return requests.get(url, headers=headers)
 
 
 # cardano cip36 registrations GET
-def cip36_registration(lookup: str, slot_no: int, limit: int):
-    document_url = (
-        f"{URL}/registration/cip36?asat=SLOT:{slot_no}&lookup={lookup}&limit={limit}"
-    )
+def cip36_registration(lookup: str, slot_no: int, limit: int, valid: bool):
+    url = f"{URL}/registration/cip36?asat=SLOT:{slot_no}&lookup={lookup}&limit={limit}&invalid={not valid}"
     headers = {
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json",
     }
-    return requests.get(document_url, headers=headers)
+    return requests.get(url, headers=headers)
