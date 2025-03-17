76767294617b2743966447a0ec112645da10466f
Dominik Toton <dominik.toton@iohk.io>
2025-03-17 13:35:55 +0100

    fix: imports

--- catalyst_voices/apps/voices/lib/widgets/modals/proposals/create_new_proposal_dialog.dart
+++ catalyst_voices/apps/voices/lib/widgets/modals/proposals/create_new_proposal_dialog.dart
@@ -7,6 +7,7 @@ import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
