a765c7bfcfecc23e4202c020507bd0dd370bfab6
januszjanus <janusz.janus@iohk.io>
2025-03-20 15:28:31 +0100

    2060 add links checking for proposals share section

--- catalyst_voices/apps/voices/integration_test/utils/mock_url_launcher_platform.dart
+++ catalyst_voices/apps/voices/integration_test/utils/mock_url_launcher_platform.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
@@ -6,22 +5,28 @@ import 'package:url_launcher_platform_interface/url_launcher_platform_interface.
 class MockUrlLauncherPlatform extends Mock
     with MockPlatformInterfaceMixin
     implements UrlLauncherPlatform {
+  late UrlLauncherPlatform originalPlatform;
+  String capturedUrl = '';
+
+  MockUrlLauncherPlatform() {
+    originalPlatform = UrlLauncherPlatform.instance;
+    UrlLauncherPlatform.instance = this;
+  }
+
+  void tearDownMock() {
+    UrlLauncherPlatform.instance = originalPlatform;
+    reset(this);
+    capturedUrl = '';
+  }
+
   @override
   Future<bool> launchUrl(String url, LaunchOptions options) async {
-    print('^^^.Launching url1: $url');
-    return true; // Simulate success
+    capturedUrl = url;
+    return true;
   }
 
   @override
   Future<bool> canLaunch(String url) async {
-    return true; // Simulate can launch
+    return true;
   }
-
-  Future<bool> canLaunchUrl(String url) async {
-    print('^^^.Can launch url');
-    return true; // Simulate can launch
-  }
-
-  // @override
-  // void closeWebView() {} // No implementation needed for this test
 }
