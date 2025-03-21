14bbc6cfed3e3c7d5d32f8e990aab40332764662
januszjanus <janusz.janus@iohk.io>
2025-03-21 12:06:58 +0100

    accidental change fix

--- catalyst_voices/apps/voices/integration_test/all_test.dart
+++ catalyst_voices/apps/voices/integration_test/all_test.dart
@@ -2,6 +2,11 @@ import 'package:catalyst_voices/configs/bootstrap.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'suites/account_test.dart' as account_test;
+import 'suites/app_test.dart' as app_test;
+import 'suites/discovery_test.dart' as discovery_test;
+import 'suites/onboarding_restore_test.dart' as onboarding_restore_test;
+import 'suites/onboarding_test.dart' as onboarding_test;
 import 'suites/proposals_test.dart' as proposals_test;
 
 void main() async {
@@ -11,10 +16,10 @@ void main() async {
   setUpAll(() async {
     await bootstrap(router: buildAppRouter());
   });
-  // app_test.main();
-  // onboarding_restore_test.main();
-  // onboarding_test.main();
-  // account_test.main();
-  // discovery_test.main();
+  app_test.main();
+  onboarding_restore_test.main();
+  onboarding_test.main();
+  account_test.main();
+  discovery_test.main();
   proposals_test.main();
 }
