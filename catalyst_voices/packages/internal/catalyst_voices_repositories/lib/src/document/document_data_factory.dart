d87d4cbe24f97f02c44bb483b5f6639f4aa964fd
Damian Molinski <damian.molinski@iohk.io>
2025-03-22 13:19:20 +0100

    docs: DocumentDataFactory

--- catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/document/document_data_factory.dart
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/document/document_data_factory.dart
@@ -1,9 +1,17 @@
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_repositories/catalyst_voices_repositories.dart';
 
+/// Centralizes mapping [SignedDocument] to a [DocumentData].
 final class DocumentDataFactory {
   DocumentDataFactory._();
 
+  /// Creates correct [DocumentData] from [document].
+  ///
+  /// Throws [SignedDocumentMetadataMalformed] in case of any required fields
+  /// missing.
+  ///
+  /// Throws [UnknownSignedDocumentContentType] in case of not supported
+  /// [document] contentType.
   static DocumentData create(SignedDocument document) {
     final malformedReasons = <String>[];
     final id = document.metadata.id;
