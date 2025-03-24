c7245c2115e61afff22a0481fa5cbb238bee0396
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2025-03-24 11:25:41 +0100

    feat(cat-voices): Proposal builder error handling (#2077)
    * feat: error handling for publish/submit proposal
    
    * fix: error dialog ui adjustments
    
    * chore: extract dialogs
    
    * fix: spelling
    
    * chore: switch to desktop info dialog, cleanup keychain deleted dialog
    
    * feat: go back to workspace after publishing/submitting proposal

---
+++ catalyst_voices/apps/voices/lib/widgets/modals/proposals/submit_proposal_error_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:catalyst_voices/widgets/buttons/voices_filled_button.dart';
+import 'package:catalyst_voices/widgets/modals/voices_dialog.dart';
+import 'package:catalyst_voices/widgets/modals/voices_info_dialog.dart';
+import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
+import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
+import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
+import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
+import 'package:flutter/material.dart';
+
+/// Error dialog when submitting proposal for review fails..
+class SubmitProposalErrorDialog {
+  static Future<void> show({
+    required BuildContext context,
+    required ProposalBuilderSubmitException exception,
+  }) {
+    return VoicesDialog.show(
+      context: context,
+      routeSettings: const RouteSettings(
+        name: '/proposal-builder/submit-error',
+      ),
+      builder: (context) {
+        return VoicesDesktopInfoDialog(
+          icon: VoicesAssets.icons.exclamation.buildIcon(
+            color: Theme.of(context).colors.iconsWarning,
+          ),
+          title: Text(exception.title(context)),
+          message: Text(exception.message(context)),
+          action: VoicesFilledButton(
+            onTap: () => Navigator.of(context).pop(),
+            child: Text(context.l10n.okay),
+          ),
+        );
+      },
+    );
+  }
+}
--- catalyst_voices/packages/internal/catalyst_voices_view_models/lib/src/proposal_builder/exception/proposal_builder_exception.dart
+++ catalyst_voices/packages/internal/catalyst_voices_view_models/lib/src/proposal_builder/exception/proposal_builder_exception.dart
@@ -2,6 +2,32 @@ import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:catalyst_voices_view_models/src/exception/localized_exception.dart';
 import 'package:flutter/material.dart';
 
+final class ProposalBuilderPublishException extends LocalizedException {
+  const ProposalBuilderPublishException();
+
+  @override
+  String message(BuildContext context) {
+    return context.l10n.proposalEditorPublishErrorDialogMessage;
+  }
+
+  String title(BuildContext context) {
+    return context.l10n.proposalEditorPublishErrorDialogTitle;
+  }
+}
+
+final class ProposalBuilderSubmitException extends LocalizedException {
+  const ProposalBuilderSubmitException();
+
+  @override
+  String message(BuildContext context) {
+    return context.l10n.proposalEditorSubmitErrorDialogMessage;
+  }
+
+  String title(BuildContext context) {
+    return context.l10n.proposalEditorSubmitErrorDialogTitle;
+  }
+}
+
 final class ProposalBuilderValidationException extends LocalizedException {
   final List<String> fields;
 
