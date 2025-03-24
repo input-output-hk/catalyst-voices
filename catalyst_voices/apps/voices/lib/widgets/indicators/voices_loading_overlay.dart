201609e82bb4d5e3ae1ebfe2303ed7ad81991822
Dominik Toton <dominik.toton@iohk.io>
2025-03-24 13:58:00 +0100

    feat: minimum loader delay

--- catalyst_voices/apps/voices/lib/widgets/indicators/voices_loading_overlay.dart
+++ catalyst_voices/apps/voices/lib/widgets/indicators/voices_loading_overlay.dart
@@ -1,12 +1,17 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:flutter/material.dart';
 
 /// A full screen overlay that blurs the background
 /// and shows a loading animation at the center.
-class VoicesLoadingOverlay extends StatelessWidget {
+///
+/// The overlay is always shown for a minimum duration even when
+/// [show] is false, this mechanism ensures that animations are smooth.
+class VoicesLoadingOverlay extends StatefulWidget {
   final bool show;
 
   const VoicesLoadingOverlay({
@@ -15,11 +20,22 @@ class VoicesLoadingOverlay extends StatelessWidget {
   });
 
   @override
+  State<VoicesLoadingOverlay> createState() => _VoicesLoadingOverlayState();
+}
+
+class _VoicesLoadingOverlayState extends State<VoicesLoadingOverlay> {
+  static const Duration _minimumShowDuration = Duration(seconds: 1);
+
+  Timer? _timer;
+  late bool _show;
+  DateTime? _showingSince;
+
+  @override
   Widget build(BuildContext context) {
     return Offstage(
-      offstage: !show,
+      offstage: !_show,
       child: TickerMode(
-        enabled: show,
+        enabled: _show,
         child: AbsorbPointer(
           child: Container(
             width: double.infinity,
@@ -46,5 +62,56 @@ class VoicesLoadingOverlay extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void didUpdateWidget(VoicesLoadingOverlay oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.show != oldWidget.show) {
+      if (widget.show) {
+        _showNow();
+      } else {
+        _scheduleHide();
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _show = widget.show;
+  }
+
+  void _hideNow() {
+    setState(() {
+      _timer?.cancel();
+      _show = false;
+      _showingSince = null;
+    });
+  }
+
+  void _scheduleHide() {
+    final showingSince = _showingSince;
+    if (showingSince == null) {
+      _hideNow();
+      return;
+    }
+
+    final hasBeenShownFor = DateTimeExt.now().difference(showingSince);
+    if (hasBeenShownFor >= _minimumShowDuration) {
+      _hideNow();
+    } else {
+      final remainingDuration = _minimumShowDuration - hasBeenShownFor;
+      _timer = Timer(remainingDuration, _hideNow);
+    }
+  }
+
+  void _showNow() {
+    setState(() {
+      _timer?.cancel();
+      _show = true;
+      _showingSince = DateTimeExt.now();
+    });
   }
 }
