6276ec22c0b2c7a07f94ffb852ec10fd959bd46c
Emir Hodzic <emir.hodzich@gmail.com>
2025-03-24 14:03:02 +0100

    test(cat-voices): 2041 implement account tests (#2057)
    * test(cat-voices): additional onboarding tests
    
    * test(cat-voices): add account tests

--- catalyst_voices/apps/voices/integration_test/pageobject/unlock_modal_page.dart
+++ catalyst_voices/apps/voices/integration_test/pageobject/unlock_modal_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol_finders/patrol_finders.dart';
+
+import '../utils/translations_utils.dart';
 
 class UnlockModalPage {
-  static const unlockKeychainDialog = Key('UnlockKeychainDialog');
-  static const unlockKeychainInfoPanel = Key('UnlockKeychainInfoPanel');
-  static const unlockPasswordTextField = Key('UnlockPasswordTextField');
-  static const unlockConfirmPasswordButton = Key('UnlockConfirmPasswordButton');
-  static const unlockRecoverButton = Key('UnlockRecoverButton');
-  static const unlockContinueAsGuestButton = Key('UnlockContinueAsGuestButton');
+  UnlockModalPage(this.$);
+  late PatrolTester $;
+
+  final unlockKeychainDialog = const Key('UnlockKeychainDialog');
+  final unlockKeychainInfoPanel = const Key('UnlockKeychainInfoPanel');
+  final unlockPasswordTextField = const Key('UnlockPasswordTextField');
+  final unlockConfirmPasswordButton = const Key('UnlockConfirmPasswordButton');
+  final unlockRecoverButton = const Key('UnlockRecoverButton');
+  final unlockContinueAsGuestButton = const Key('UnlockContinueAsGuestButton');
+  final passwordTextField = const Key('PasswordTextField');
+
+  Future<void> incorrectPasswordErrorShowsUp() async {
+    expect(
+      find.text((await t()).unlockDialogIncorrectPassword),
+      findsOneWidget,
+    );
+  }
 }
