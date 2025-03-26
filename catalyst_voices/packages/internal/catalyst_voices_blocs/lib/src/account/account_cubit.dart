96c904d66c2e2534eca7c1f44e86117e91df3d6a
Damian Molinski <damian.molinski@iohk.io>
2025-03-26 16:18:30 +0100

    hide account status

--- catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/account/account_cubit.dart
+++ catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/account/account_cubit.dart
@@ -82,7 +82,8 @@ final class AccountCubit extends Cubit<AccountState> {
         .toList();
 
     return AccountState(
-      status: const AccountFinalized(),
+      // Note. acccount status is not supported for f14.
+      status: const None(),
       catalystId: catalystId,
       username: Username.pure(catalystId?.username ?? ''),
       email: Email.pure(from?.email ?? ''),
