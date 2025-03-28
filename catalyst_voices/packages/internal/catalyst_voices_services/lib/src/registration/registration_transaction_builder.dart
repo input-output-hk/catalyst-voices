252f3ac963bde6c1c5dc95bc9327f66141f451e0
Damian Molinski <damian.molinski@iohk.io>
2025-03-28 11:53:32 +0100

    refactor: fix imports

--- catalyst_voices/packages/internal/catalyst_voices_services/lib/src/registration/registration_transaction_builder.dart
+++ catalyst_voices/packages/internal/catalyst_voices_services/lib/src/registration/registration_transaction_builder.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
 import 'package:catalyst_cardano_serialization/catalyst_cardano_serialization.dart'
-    hide Ed25519PublicKey;
+    as cs show Ed25519PublicKey;
 import 'package:catalyst_cardano_serialization/catalyst_cardano_serialization.dart'
-as cs showhide Ed25519PublicKey;
+    hide Ed25519PublicKey;
 import 'package:catalyst_key_derivation/catalyst_key_derivation.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_services/src/crypto/key_derivation_service.dart';
