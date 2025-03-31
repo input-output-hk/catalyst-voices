6fb1848c1f567df888458519c78aa3b6ebcc81ae
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2025-03-31 11:11:21 +0200

    feat(cat-voices): Proposal editor comments (#2113)
    * feat: pass category id
    
    * feat: make category required
    
    * feat: refactor proposal builder to use cache object
    
    * chore: hide dialog
    
    * chore: generate code
    
    * feat: extract comments and add them in proposal builder
    
    * fix: comments editing in proposal builder
    
    * chore: cleanup
    
    * feat: add comments header
    
    * chore: cleanup
    
    * fix: can reply
    
    * chore: sort exports
    
    * chore: sort exports
    
    * chore: cleanup
    
    * feat: allow to view/add coments for the opened proposal version
    
    * fix: loading state

--- catalyst_voices/apps/voices/macos/Flutter/GeneratedPluginRegistrant.swift
+++ catalyst_voices/apps/voices/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import file_selector_macos
 import flutter_secure_storage_macos
 import package_info_plus
@@ -18,6 +19,7 @@ import video_player_avfoundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
