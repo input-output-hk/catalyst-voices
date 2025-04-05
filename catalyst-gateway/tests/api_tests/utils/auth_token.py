9979b940689a52e55ae9333c350a47e66e483268
bkioshn <bkioshn@gmail.com>
2025-04-05 20:40:57 +0700

    test(cat-gateway): generate auth token with already registered account
    Signed-off-by: bkioshn <bkioshn@gmail.com>

---
+++ catalyst-gateway/tests/api_tests/utils/auth_token.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timezone
+import base64
+import pytest
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
+
+@pytest.fixture
+def rbac_auth_token() -> str:
+    # Already registered as role 0
+    # https://preprod.cexplorer.io/tx/764139bb8924f436486fa686ec836c180e98cc8d3e8ff413e3c11222fcf23836
+
+    sk_hex = "e81127f0586aa9c6cf0b6048330f3200e1791187ea87ab780e14337f814e3f51a1f265eac3449c1109ba026d425c4b08f45088a4b47c3892157e740042a18e37"
+    sk_bytes = bytes.fromhex(sk_hex)
+    sk = Ed25519PrivateKey.from_private_bytes(sk_bytes[:32])
+    
+    pk_hex = "6e42f8e589a76ebb13ef279df7841efce978f106bee196f0e3cfd347bb31a2e8"
+    pk_bytes = bytes.fromhex(pk_hex)
+    pk = Ed25519PublicKey.from_public_bytes(pk_bytes)
+    
+
+    return generate_rbac_auth_token("cardano", "preprod", pk, sk)
+
+def generate_rbac_auth_token(network: str, subnet: str | None, role0_pk: Ed25519PublicKey, sk: Ed25519PrivateKey) -> str:
+    prefix = "catid.:"
+    nonce = int(datetime.now(timezone.utc).timestamp())
+    subnet = f"{subnet}." if subnet else ""
+    role0_pk_b64 = base64_url(role0_pk.public_bytes_raw())
+    
+    catid_without_sig = f"{prefix}{nonce}@{subnet}{network}/{role0_pk_b64}."
+    
+    signature = sk.sign(catid_without_sig.encode()) 
+    signature_b64 = base64_url(signature)
+
+    return f"{catid_without_sig}{signature_b64}"
+    
+
+def base64_url(data: bytes) -> str:
+    # URL safety and no padding base 64
+    return base64.urlsafe_b64encode(data).decode().rstrip("=")
