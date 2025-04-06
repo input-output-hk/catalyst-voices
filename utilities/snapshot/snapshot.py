135a27590857809d18e38fcb85a9318f4c9e041d
cong-or <conchubhar.gannon@gmail.com>
2025-04-06 20:44:15 +0100

    feat(cat gateway snapshot tool): barebones

---
+++ utilities/snapshot/snapshot.py
@@ -0,0 +1,202 @@
+import typer
+
+from requests.exceptions import HTTPError
+import requests
+import json
+from hashlib import blake2b
+from bitcoin.segwit_addr import bech32_encode, convertbits
+import threading
+import multiprocessing
+from pprint import pprint
+
+
+# Typer is a library for building CLI applications
+app = typer.Typer()
+
+
+@app.command()
+def snapshot(
+    bearer_token: str = (typer.Option(...)),
+    api_key: str = (typer.Option(...)),
+    host: str = (typer.Option(...)),
+    slot_no: str = (typer.Option(...)),
+    page_limit: str = (typer.Option(...)),
+):
+    """
+    Generate snapshot given slot no.
+    Writes valids to snapshot.json and
+    any unsuccessful attempts to snapshot_errors.json
+    """
+
+    snapshot_data = []
+    snapshot_errors = []
+
+    url = f"{host}/api/v1/cardano/registration/cip36?lookup=ALL&limit={page_limit}&asat=SLOT:{slot_no}"
+    headers = {"Authorization": bearer_token, "X-API-Key": api_key}
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        registrations = json.loads(response.text)["voting_key"]
+
+        pprint("Processing registrations..!")
+
+        # The number of usable CPUs can be obtained with os.process_cpu_count()
+        pprint(f"You have this many usable cpus {multiprocessing.cpu_count()}")
+
+        thread_list = []
+
+        # chunk and process registrations in parallel
+        chunks = split_list(registrations, multiprocessing.cpu_count())
+
+        for chunk in chunks:
+            thread_list.append(
+                threading.Thread(
+                    target=process_chunk,
+                    args=(
+                        chunk,
+                        bearer_token,
+                        api_key,
+                        host,
+                        snapshot_data,
+                        snapshot_errors,
+                    ),
+                )
+            )
+
+        for thread in thread_list:
+            thread.start()
+
+        for thread in thread_list:
+            thread.join()
+
+    except HTTPError as e:
+        pprint(e.response.text)
+
+    # Convert and write JSON object to file
+    with open("snapshot.json", "w") as outfile:
+        json.dump(snapshot_data, outfile, indent=2)
+
+    # Convert and write JSON object to file
+    with open("snapshot_errors.json", "w") as outfile:
+        json.dump(snapshot_errors, outfile, indent=2)
+
+    pprint(f"Number of registrations in snapshot file: {len(snapshot_data)}")
+
+
+def process_chunk(
+    chunk_of_registrations: list,
+    bearer_token: str,
+    api_key: str,
+    host: str,
+    snapshot_data: list,
+    snapshot_errors: list,
+):
+    """
+    Process chunk of registrations.
+    """
+    for chunk in chunk_of_registrations:
+
+        # Voter may have multiple registration
+        for registration in chunk["registrations"]:
+
+            latest_stake_pub_key = registration["stake_pub_key"]
+
+            # latest regsistration slot no to query staked ada
+            slot_num = registration["slot_no"]
+
+            stake_address = stake_public_key_to_address(
+                key=latest_stake_pub_key[2:],
+                is_stake=True,
+                network_type="mainnet",
+            )
+
+            if stake_address == None:
+                snapshot_errors.append(registration)
+                continue
+
+            url = f"{host}/api/v1/cardano/assets/{stake_address}?&asat=SLOT:{slot_num}"
+            headers = {"Authorization": bearer_token, "X-API-Key": api_key}
+
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                # stake address has no staked ada. Assign -1.
+                registration["voting_power"] = -1
+
+            else:
+                json_data = json.loads(response.text)
+                # Append voting power field
+                registration["voting_power"] = json_data["persistent"]["ada_amount"]
+
+            # Add stake address field
+            registration["stake_addr"] = stake_address
+
+            snapshot_data.append(registration)
+
+            if len(snapshot_data) % 500 == 0:
+                pprint(f"Registrations processed:{len(snapshot_data)}")
+
+
+def stake_public_key_to_address(key: str, is_stake: bool, network_type: str):
+    """
+    According to [CIP-19](https://cips.cardano.org/cips/cip19/).
+    """
+
+    key_bytes = bytes.fromhex(key)
+    key_hash = blake2b(key_bytes, digest_size=28)
+    header = stake_header(is_stake=is_stake, network_type=network_type)
+    key_hash_with_header = header.to_bytes(1, "big") + key_hash.digest()
+
+    if network_type == "mainnet":
+        hrp = "stake"
+    elif (
+        network_type == "testnet"
+        or network_type == "preprod"
+        or network_type == "preview"
+    ):
+        hrp = "stake_test"
+    else:
+        raise f"Unknown network type: {network_type}"
+
+    return bech32_encode(hrp, convertbits(key_hash_with_header, 8, 5))
+
+
+def stake_header(is_stake: bool, network_type: str):
+    if is_stake:
+        # stake key hash
+        typeid = int("1110", 2)
+    else:
+        # script hash
+        typeid = int("1111", 2)
+    if network_type == "mainnet":
+        network_id = 1
+    elif (
+        network_type == "testnet"
+        or network_type == "preprod"
+        or network_type == "preview"
+    ):
+        network_id = 0
+    else:
+        raise f"Unknown network type: {network_type}"
+
+    typeid = typeid << 4
+    return typeid | network_id
+
+
+def split_list(l: list, parts: int) -> list:
+    """Takes a list as input, and splits it into "parts" number of sub-lists,
+    which are then inserted as elements in the returned meta-list.
+
+    The function will try to make the sub-lists as equal in length as
+    possible, so running
+    split_list( [1, 2, 3, 4, 5, 6], 4 )
+    will return
+    [ [1, 2], [3, 4], [5], [6] ]
+    """
+    n = min(parts, max(len(l), 1))
+    k, m = divmod(len(l), n)
+    return [l[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n)]
+
+
+if __name__ == "__main__":
+    app()
