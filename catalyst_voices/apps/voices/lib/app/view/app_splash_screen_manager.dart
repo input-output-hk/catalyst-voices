8e77f59873651d1471c4b7cb1a00190453d0a52f
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2025-04-08 16:02:10 +0200

    feat(cat-voices): Initial splash screen (#2174)
    * chore: upgrade gradle
    
    * chore: sync ios pods
    
    * feat: add splash screen configuration
    
    * chore: generate splash screen
    
    * chore: cleanup
    
    * chore: cleanup
    
    * chore: cleanup
    
    * chore: format markdown

---
+++ catalyst_voices/apps/voices/lib/app/view/app_splash_screen_manager.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+/// Hides the splash screen after a frame is drawn when the widget initializes.
+class AppSplashScreenManager extends StatefulWidget {
+  final Widget child;
+
+  const AppSplashScreenManager({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<AppSplashScreenManager> createState() => _AppSplashScreenManagerState();
+
+  /// Hides the splash screen.
+  ///
+  /// https://pub.dev/packages/flutter_native_splash#3-set-up-app-initialization-optional
+  static void hideSplashScreen() {
+    FlutterNativeSplash.remove();
+  }
+
+  /// Flutter by default removes the splash screen when it draws the first
+  /// frame, we'd like to preserve it until we've loaded the content.
+  ///
+  /// https://pub.dev/packages/flutter_native_splash#3-set-up-app-initialization-optional
+  static void preserveSplashScreen(WidgetsBinding widgetsBinding) {
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  }
+}
+
+class _AppSplashScreenManagerState extends State<AppSplashScreenManager>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        AppSplashScreenManager.hideSplashScreen();
+      }
+    });
+  }
+}
