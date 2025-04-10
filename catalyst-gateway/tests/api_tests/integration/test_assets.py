c8359e9b640777356353ecee708e8eb766c6ecab
Alex Pozhylenkov <leshiy12345678@gmail.com>
2025-04-10 19:16:23 +0900

    feat(cat-gateway): `cardano/assets` integration test (#2139)
    * add assets test
    
    * wip
    
    * fix
    
    * wip
    
    * wip

---
+++ catalyst-gateway/tests/api_tests/integration/test_assets.py
@@ -0,0 +1,41 @@
+import pytest
+from loguru import logger
+from utils.address import stake_public_key_to_address
+from utils.snapshot import snapshot
+from utils.auth_token import rbac_auth_token_factory
+from api.v1 import cardano
+
+
+@pytest.mark.skip("To be refactored when the api is ready")
+def test_persistent_ada_amount_endpoint(snapshot, rbac_auth_token_factory):
+    # health.is_live()
+    # health.is_ready()
+    logger.info(f"{snapshot.network}, {snapshot.slot_no}")
+    token = rbac_auth_token_factory()
+
+    total_len = len(snapshot.data)
+    for i, entry in enumerate(snapshot.data):
+        logger.info(f"Checking .... {round(i / total_len * 100, 1)}%")
+        expected_amount = entry["voting_power"]
+        stake_address = stake_public_key_to_address(
+            key=entry["stake_public_key"][2:],
+            is_stake=True,
+            network_type=snapshot.network,
+        )
+        resp = cardano.assets(stake_address, snapshot.slot_no, token)
+        if expected_amount == 0 and resp.status_code == 404:
+            # it is possible that snapshot tool collected data for the stake key which does not have any unspent utxo
+            # at this case cat-gateway return 404, that is why we are checking this case additionally
+            continue
+
+        assert (
+            resp.status_code == 200
+        ), f"Cannot find assets for stake_address: {stake_address}"
+        assets = resp.json()
+        if assets["persistent"]["ada_amount"] != expected_amount:
+            logger.error(
+                f"Not expected ada amount for stake_address: {stake_address}, {entry["stake_public_key"]}"
+            )
+        # assert (
+        #     assets["persistent"]["ada_amount"] == expected_amount
+        # ), f"Not expected ada amount for stake_address: {stake_address}, {entry["stake_public_key"]}"
