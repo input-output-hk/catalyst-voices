9d005848c0afb64947dca2357dd47982e70bb912
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2025-04-16 14:25:08 +0200

    feat(cat-voices): Tests for validating proposal node ids (#2298)
    * feat: add tests
    
    * feat: unify document templates, stop using flutter custom templates
    
    * fix: docs loading

---
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/test/src/proposal/proposal_document_test.dart
@@ -0,0 +1,125 @@
+import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_repositories/src/dto/document/document_data_dto.dart';
+import 'package:catalyst_voices_repositories/src/dto/document/document_dto.dart';
+import 'package:catalyst_voices_repositories/src/dto/document/schema/document_schema_dto.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../fixture/voices_document_templates.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group(ProposalDocument, () {
+    late Map<String, dynamic> schemaJson;
+    late Map<String, dynamic> documentJson;
+
+    setUpAll(() async {
+      schemaJson = await VoicesDocumentsTemplates.proposalF14Schema;
+      documentJson = await VoicesDocumentsTemplates.proposalF14Document;
+    });
+
+    ProposalDocument createProposalDocument() {
+      final documentDto = DocumentDto.fromJsonSchema(
+        DocumentDataContentDto.fromJson(documentJson),
+        DocumentSchemaDto.fromJson(schemaJson).toModel(),
+      );
+      final document = documentDto.toModel();
+
+      return ProposalDocument(
+        metadata: ProposalMetadata(
+          selfRef: DraftRef.generateFirstRef(),
+          templateRef: SignedDocumentRef.generateFirstRef(),
+          categoryId: SignedDocumentRef.generateFirstRef(),
+          authors: const [],
+        ),
+        document: document,
+      );
+    }
+
+    test('allNodeIds can be resolved in the document template', () {
+      final proposalDocument = createProposalDocument();
+      final document = proposalDocument.document;
+
+      for (final nodeId in ProposalDocument.allNodeIds) {
+        final property = document.getProperty(nodeId);
+        expect(
+          property,
+          isNotNull,
+          reason: 'Expected property {$nodeId} did not '
+              'appear in the document template. '
+              'Make sure to include it.',
+        );
+      }
+    });
+
+    test('authorName can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.authorName,
+        isNotEmpty,
+        reason: 'Make sure the ${ProposalDocument.authorNameNodeId} '
+            'property exists.',
+      );
+    });
+
+    test('description can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.description,
+        isNotEmpty,
+        reason: 'Make sure the ${ProposalDocument.descriptionNodeId} '
+            'property exists.',
+      );
+    });
+
+    test('duration can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.duration,
+        isNotNull,
+        reason: 'Make sure the ${ProposalDocument.durationNodeId} '
+            'property exists.',
+      );
+    });
+
+    test('fundsRequested can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.fundsRequested,
+        isNotNull,
+        reason: 'Make sure the ${ProposalDocument.requestedFundsNodeId} '
+            'property exists.',
+      );
+    });
+
+    test('milestoneCount can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.milestoneCount,
+        isNotNull,
+        reason: 'Make sure the ${ProposalDocument.milestoneListNodeId} '
+            'property exists.',
+      );
+    });
+
+    test('tag can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.tag,
+        isNotEmpty,
+        reason: 'Make sure the ${ProposalDocument.tagNodeId} '
+            'property exists.',
+      );
+    });
+
+    test('title can be extracted from document', () {
+      final document = createProposalDocument();
+      expect(
+        document.title,
+        isNotEmpty,
+        reason: 'Make sure the ${ProposalDocument.titleNodeId} '
+            'property exists.',
+      );
+    });
+  });
+}
