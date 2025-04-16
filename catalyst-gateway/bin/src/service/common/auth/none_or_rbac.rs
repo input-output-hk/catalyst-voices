01ef2ca37f063bc66bfc83f0bd695f65b8b2a565
Alex Pozhylenkov <leshiy12345678@gmail.com>
2025-04-16 15:16:13 +0900

    remove unused `NoneOrApiKey`, update processing of `NoneOrRBAC` auth (#2294)
    Co-authored-by: Oleksandr Prokhorenko <djminikin@gmail.com>

--- catalyst-gateway/bin/src/service/common/auth/none_or_rbac.rs
+++ catalyst-gateway/bin/src/service/common/auth/none_or_rbac.rs
@@ -1,11 +1,14 @@
 //! Either has No Authorization, or RBAC Token.
 
 use catalyst_types::id_uri::IdUri;
-use poem_openapi::SecurityScheme;
+use poem::{
+    web::headers::{authorization::Bearer, Authorization, HeaderMapExt},
+    Request, RequestBody,
+};
+use poem_openapi::{registry::Registry, ApiExtractor, ApiExtractorType, ExtractParamOptions};
 
 use super::{none::NoAuthorization, rbac::scheme::CatalystRBACSecurityScheme};
 
-#[derive(SecurityScheme)]
 #[allow(dead_code, clippy::upper_case_acronyms, clippy::large_enum_variant)]
 /// Endpoint allows Authorization with or without RBAC Token.
 pub(crate) enum NoneOrRBAC {
@@ -13,6 +16,36 @@ pub(crate) enum NoneOrRBAC {
     RBAC(CatalystRBACSecurityScheme),
     /// Has No Authorization.
     None(NoAuthorization),
+}
+
+impl<'a> ApiExtractor<'a> for NoneOrRBAC {
+    type ParamRawType = ();
+    type ParamType = ();
+
+    const TYPES: &'static [ApiExtractorType] = &[ApiExtractorType::SecurityScheme];
+
+    fn register(registry: &mut Registry) {
+        CatalystRBACSecurityScheme::register(registry);
+        NoAuthorization::register(registry);
+    }
+
+    fn security_schemes() -> Vec<&'static str> {
+        let mut schemas = Vec::new();
+        schemas.extend(CatalystRBACSecurityScheme::security_schemes());
+        schemas.extend(NoAuthorization::security_schemes());
+        schemas
+    }
+
+    async fn from_request(
+        req: &'a Request, body: &mut RequestBody, param_opts: ExtractParamOptions<Self::ParamType>,
+    ) -> poem::Result<Self> {
+        if req.headers().typed_get::<Authorization<Bearer>>().is_some() {
+            let auth = CatalystRBACSecurityScheme::from_request(req, body, param_opts).await?;
+            Ok(NoneOrRBAC::RBAC(auth))
+        } else {
+            Ok(NoneOrRBAC::None(NoAuthorization))
+        }
+    }
 }
 
 impl From<NoneOrRBAC> for Option<IdUri> {
