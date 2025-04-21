d25d16c643304cfdce6bf14413fd74b0d05ea593
Dominik Toton <166132265+dtscalac@users.noreply.github.com>
2025-04-21 18:48:43 +0200

    fix(cat-voices): Proposal builder UI fixes (#2320)
    * chore: rename files
    
    * fix: make sure guidance for subsection is not shown under parent section
    
    * fix: guidance title
    
    * fix: jump scroll to top when guidance changes

--- catalyst_voices/apps/voices/lib/pages/proposals/widgets/proposals_pagination.dart
+++ catalyst_voices/apps/voices/lib/pages/proposals/widgets/proposals_pagination.dart
@@ -1,7 +1,7 @@
 import 'package:catalyst_voices/pages/proposals/widgets/proposals_pagination_empty_state.dart';
 import 'package:catalyst_voices/pages/proposals/widgets/proposals_pagination_tile.dart';
 import 'package:catalyst_voices/widgets/pagination/builders/paged_wrap_child_builder.dart';
-import 'package:catalyst_voices/widgets/pagination/layouts/paginated_grid_view.dart.dart';
+import 'package:catalyst_voices/widgets/pagination/layouts/paginated_grid_view.dart';
 import 'package:catalyst_voices/widgets/pagination/paging_controller.dart';
 import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
