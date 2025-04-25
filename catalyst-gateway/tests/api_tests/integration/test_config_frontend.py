f4215eb7cb772db5c62a9a8601f1661cf062082a
Mr-Leshiy <leshiy12345678@gmail.com>
2025-04-25 14:02:54 +0900

    add frontend config integration test

---
+++ catalyst-gateway/tests/api_tests/integration/test_config_frontend.py
@@ -0,0 +1,37 @@
+import pytest
+from api.v1 import config_frontend
+
+
+def test_config_frontend():
+    exp_config = {"some": "thing"}
+    resp = config_frontend.put(exp_config)
+    assert (
+        resp.status_code == 204
+    ), f"Failed to publish frontend config: {resp.status_code} - {resp.text}"
+
+    resp = config_frontend.get()
+    assert (
+        resp.status_code == 200
+    ), f"Failed to get published frontend config: {resp.status_code} - {resp.text}"
+    config = resp.json()
+    assert (
+        config == exp_config
+    ), f"Loaded config does not match with the expected: epx {exp_config}, loaded: {config}"
+
+    exp_config2 = {"some": "other thing"}
+    resp = config_frontend.put(exp_config2)
+    assert (
+        resp.status_code == 204
+    ), f"Failed to publish frontend config: {resp.status_code} - {resp.text}"
+
+    resp = config_frontend.get()
+    assert (
+        resp.status_code == 200
+    ), f"Failed to get published frontend config: {resp.status_code} - {resp.text}"
+    config = resp.json()
+    assert (
+        config != exp_config
+    ), f"Loaded config should not match with the previous: previous {exp_config}, loaded: {config}"
+    assert (
+        config == exp_config2
+    ), f"Loaded config does not match with the expected: epx {exp_config2}, loaded: {config}"
