9d76705e96827d613fb3818d89dc49b576f1711e
Dominik Toton <dominik.toton@iohk.io>
2025-04-29 12:06:54 +0200

    fix: imports

--- catalyst_voices/apps/voices/lib/pages/workspace/header/workspace_header.dart
+++ catalyst_voices/apps/voices/lib/pages/workspace/header/workspace_header.dart
@@ -5,14 +5,13 @@ import 'package:catalyst_voices/common/ext/build_context_ext.dart';
 import 'package:catalyst_voices/common/ext/space_ext.dart';
 import 'package:catalyst_voices/routes/routing/routing.dart';
 import 'package:catalyst_voices/widgets/campaign_timeline/campaign_timeline_card.dart';
-import 'package:catalyst_voices/widgets/modals/proposals/proposal_limit_reached_dialog.dart';
+import 'package:catalyst_voices/widgets/modals/proposals/create_new_proposal_dialog.dart';
 import 'package:catalyst_voices/widgets/widgets.dart';
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart'
     show ProposalDocument, Space;
-import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
 
