4e1047c4754929c22dfa9ee97ada98860b6f45cb
Damian Molinski <damian.molinski@iohk.io>
2025-05-02 15:03:01 +0200

    feat: confirm seed phrase words only when input is not empty

---
+++ catalyst_voices/apps/voices/lib/pages/registration/widgets/upload_seed_phrase/upload_seed_phrase_mixin.dart
@@ -0,0 +1,50 @@
+import 'package:catalyst_voices/pages/registration/incorrect_seed_phrase_dialog.dart';
+import 'package:catalyst_voices/pages/registration/widgets/upload_seed_phrase/upload_seed_phrase_confirmation_dialog.dart';
+import 'package:catalyst_voices/pages/registration/widgets/upload_seed_phrase/upload_seed_phrase_dialog.dart';
+import 'package:catalyst_voices_models/catalyst_voices_models.dart'
+    show SeedPhrase, SeedPhraseWord;
+import 'package:flutter/widgets.dart';
+
+mixin UploadSeedPhraseMixin<T extends StatefulWidget> on State<T> {
+  Future<List<SeedPhraseWord>?> importSeedPhraseWords({
+    bool requireConfirmation = true,
+    bool validate = true,
+  }) async {
+    if (requireConfirmation) {
+      final confirmed = await UploadSeedPhraseConfirmationDialog.show(context);
+      if (!confirmed) {
+        return null;
+      }
+    }
+
+    if (!mounted) {
+      return null;
+    }
+
+    final words = await UploadSeedPhraseDialog.show(context);
+
+    if (words == null || !mounted) {
+      return null;
+    }
+
+    if (!validate) {
+      return words;
+    }
+
+    final isValid = SeedPhrase.isValid(words: words);
+
+    if (!isValid) {
+      final showUpload = await IncorrectSeedPhraseDialog.show(context);
+      if (!showUpload || !mounted) {
+        return null;
+      }
+
+      return importSeedPhraseWords(
+        requireConfirmation: false,
+        validate: validate,
+      );
+    }
+
+    return words;
+  }
+}
