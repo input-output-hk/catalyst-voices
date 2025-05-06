ed8d5323e65df033fce02191726c662e1fd86566
Dominik Toton <dominik.toton@iohk.io>
2025-05-06 20:52:04 +0200

    feat: not found page

---
+++ catalyst_voices/apps/voices/lib/pages/not_found/not_found_page.dart
@@ -0,0 +1,123 @@
+import 'dart:math';
+
+import 'package:catalyst_voices/routes/routing/root_route.dart';
+import 'package:catalyst_voices/widgets/widgets.dart';
+import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
+import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
+import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
+import 'package:flutter/material.dart';
+
+/// 404 not found page.
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Stack(
+      children: [
+        Positioned.fill(
+          child: _Background(),
+        ),
+        Positioned.fill(
+          left: 24,
+          right: 24,
+          child: Column(
+            children: [
+              Spacer(flex: 3),
+              _Image(),
+              SizedBox(height: 56),
+              _Title(),
+              SizedBox(height: 12),
+              _Message(),
+              SizedBox(height: 32),
+              _Button(),
+              Spacer(flex: 4),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Background extends StatelessWidget {
+  const _Background();
+
+  @override
+  Widget build(BuildContext context) {
+    return CatalystImage.asset(
+      VoicesAssets.images.bgBubbles.path,
+      fit: BoxFit.fill,
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button();
+
+  @override
+  Widget build(BuildContext context) {
+    return VoicesTextButton(
+      leading: VoicesAssets.icons.arrowNarrowRight.buildIcon(),
+      child: Text(context.l10n.notFoundButton),
+      onTap: () => const RootRoute().go(context),
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final minImageWidth = max(300, screenWidth * 0.2);
+    final maxImageWidth = min(500, screenWidth * 0.9);
+    final preferredImageWidth = screenWidth * 0.6;
+
+    final imageWidth = max(
+      min(preferredImageWidth, maxImageWidth),
+      minImageWidth,
+    );
+
+    return Center(
+      child: SizedBox(
+        width: imageWidth.toDouble(),
+        child: CatalystImage.asset(
+          VoicesAssets.images.notFound404.path,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _Message extends StatelessWidget {
+  const _Message();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Text(
+      context.l10n.notFoundMessage,
+      textAlign: TextAlign.center,
+      style: theme.textTheme.bodyLarge
+          ?.copyWith(color: theme.colors.textOnPrimaryLevel1),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Text(
+      context.l10n.notFoundTitle,
+      textAlign: TextAlign.center,
+      style: theme.textTheme.headlineLarge
+          ?.copyWith(color: theme.colorScheme.primary),
+    );
+  }
+}
