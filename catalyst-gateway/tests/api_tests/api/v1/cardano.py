2bfb3612a45202b6e1a56383fc36a70abd758220
cong-or <conchubhar.gannon@gmail.com>
2025-05-07 12:50:42 +0100

    feat(stake addr lookup w cat id): auth

--- catalyst-gateway/tests/api_tests/api/v1/cardano.py
+++ catalyst-gateway/tests/api_tests/api/v1/cardano.py
@@ -6,7 +6,7 @@ URL = cat_gateway_endpoint_url("api/v1/cardano")
 
 # cardano assets GET
 def assets(stake_address: str, slot_no: int, token: str):
-    url = f"{URL}/assets/{stake_address}?asat=SLOT:{slot_no}"
+    url = f"{URL}/assets?stake_address={stake_address}?asat=SLOT:{slot_no}"
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
