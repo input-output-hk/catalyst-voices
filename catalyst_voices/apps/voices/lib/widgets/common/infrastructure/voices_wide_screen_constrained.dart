171c3c63ef1dbc90d854a88f7a4f0ea888430c51
Ryszard Schossler <rysiek9801@gmail.com>
2025-05-07 10:00:29 +0200

    feat: first approach to ultrawide screens

---
+++ catalyst_voices/apps/voices/lib/widgets/common/infrastructure/voices_wide_screen_constrained.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class VoicesWideScreenConstrained extends StatelessWidget {
+  /// Width of the screen. Defaults to 1440. When child has padding
+  /// of horizontal 120 so actual content is 1200
+  final double maxWidth;
+  final Color? backgroundColor;
+  final Widget child;
+
+  const VoicesWideScreenConstrained({
+    super.key,
+    this.maxWidth = 1440,
+    this.backgroundColor,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: maxWidth),
+        child: child,
+      ),
+    );
+
+    if (backgroundColor != null) {
+      content = Container(
+        width: double.infinity,
+        color: backgroundColor,
+        child: content,
+      );
+    }
+
+    return content;
+  }
+}
+
+extension ConstrainedSliverListExtension on List<Widget> {
+  List<Widget> constrainedDelegate({
+    double maxWidth = 1440,
+    List<Type> excludeTypes = const [],
+  }) {
+    return map((widget) {
+      if (excludeTypes.contains(widget.runtimeType)) {
+        return widget;
+      }
+
+      return VoicesWideScreenConstrained(
+        maxWidth: maxWidth,
+        child: widget,
+      );
+    }).toList();
+  }
+}
+
+extension ConstrainedWidgetExtension on Widget {
+  Widget withBackground(Color color) {
+    return VoicesWideScreenConstrained(
+      backgroundColor: color,
+      child: this,
+    );
+  }
+}
