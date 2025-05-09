5db36180c1ba0a869dfaf7074f8036ff3052a08f
Dominik Toton <dominik.toton@iohk.io>
2025-05-09 09:34:51 +0200

    chore: cleanup

--- catalyst_voices/packages/internal/catalyst_voices_services/lib/src/blockchain/blockchain_service.dart
+++ catalyst_voices/packages/internal/catalyst_voices_services/lib/src/blockchain/blockchain_service.dart
@@ -3,7 +3,6 @@ import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_repositories/catalyst_voices_repositories.dart';
 
 // TODO(dtscalac): move most blockchain/wallet related code to here
-// ignore: one_member_abstracts
 abstract interface class BlockchainService {
   const factory BlockchainService(BlockchainRepository blockchainRepository) =
       BlockchainServiceImpl;
