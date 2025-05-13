7b09d37c0dc3a899f6bde71183f76c6f5d6f5deb
Damian Molinski <damian.molinski@iohk.io>
2025-05-13 11:55:17 +0200

    fix: snack bar action

--- catalyst_voices/apps/voices/lib/pages/proposal/snack_bar/username_updated_snack_bar.dart
+++ catalyst_voices/apps/voices/lib/pages/proposal/snack_bar/username_updated_snack_bar.dart
@@ -1,5 +1,4 @@
 import 'package:catalyst_voices/widgets/snackbar/voices_snackbar.dart';
-import 'package:catalyst_voices/widgets/snackbar/voices_snackbar_action.dart';
 import 'package:catalyst_voices/widgets/snackbar/voices_snackbar_type.dart';
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
@@ -10,25 +9,14 @@ final class UsernameUpdatedSnackBar extends VoicesSnackBar {
     return UsernameUpdatedSnackBar._(
       title: context.l10n.usernameSavedTitle,
       message: context.l10n.usernameSavedMessage,
-      action: VoicesSnackBarPrimaryAction(
-        type: VoicesSnackBarType.success,
-        onPressed: () {
-          if (context.mounted) {
-            VoicesSnackBar.hideCurrent(context);
-          }
-        },
-        child: VoicesAssets.icons.x.buildIcon(),
-      ),
     );
   }
 
   UsernameUpdatedSnackBar._({
     required super.title,
     required super.message,
-    required Widget action,
   }) : super(
           type: VoicesSnackBarType.success,
           icon: VoicesAssets.icons.check.buildIcon(),
-          actions: [action],
         );
 }
