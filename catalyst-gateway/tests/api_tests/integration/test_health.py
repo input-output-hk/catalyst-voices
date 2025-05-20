157860e578922e6863a56e0da36be9d0c5a13f6a
Joaquín Rosales <joaquin.rosales@iohk.io>
2025-05-19 19:52:18 -0600

    fix(cat-gateway): add index db outage test and fix spelling

--- catalyst-gateway/tests/api_tests/integration/test_health.py
+++ catalyst-gateway/tests/api_tests/integration/test_health.py
@@ -6,6 +6,9 @@ from utils import health
 import pytest
 import time
 
+from utils.rbac_chain import rbac_chain_factory, Chain
+from api.v1 import rbac
+
 @pytest.fixture
 def event_db_proxy() -> Generator[Any, Any, Any]:
     proxy = TestProxy("Event DB", 18080, 5432)
@@ -31,7 +34,7 @@ def cat_gateway_service() -> Generator[Any, Any, Any]:
 
 
 @pytest.mark.health_endpoint
-def test_live_endpoint_when_starting_cat_gateway(event_db_proxy, index_db_proxy, cat_gateway_service):
+def test_ready_endpoint_with_event_db_outage(event_db_proxy, index_db_proxy, cat_gateway_service, rbac_chain_factory):
     cat_gateway_service.start()
     time.sleep(2) # wait for cat-gateway API to start
     health.is_ready() #assertion
@@ -40,14 +43,31 @@ def test_live_endpoint_when_starting_cat_gateway(event_db_proxy, index_db_proxy,
     event_db_proxy.suspend()
     # fetch endpoint that uses event db
     resp = document.index_post({})
-
     assert(resp.status_code == 503), f"Expected document index to fail: {resp.status_code} - {resp.text}"
+    # call ready endpoint, expect 503
     health.is_not_ready() #assertion
 
     # re-enable event db
     event_db_proxy.resume()
-    health.is_ready() #Call `ready` endpoint to attempt re-connection that should succeed
+    #Call `ready` endpoint to attempt re-connection that should succeed
+    health.is_ready()
 
     # fetch endpoint that uses event db
     resp = document.index_post({})
     assert(resp.status_code == 200), f"Expected document index to succeed: {resp.status_code} - {resp.text}"
+
+    # Index DB testing
+    auth_token = rbac_chain_factory(Chain.Role0).auth_token()
+
+    # Not registered stake address lookup
+    # Cardano test data CIP0019
+    # <https://github.com/cardano-foundation/CIPs/blob/master/CIP-0019/README.md>
+    # cspell:disable-next-line
+    stake_address = "stake_test17rphkx6acpnf78fuvxn0mkew3l0fd058hzquvz7w36x4gtcljw6kf"
+    resp = rbac.get(lookup=stake_address, token=auth_token)
+    assert(resp.status_code == 404), f"Expected not registered stake address: {resp.status_code} - {resp.text}"
+
+    # disable index db
+    index_db_proxy.suspend()
+    resp = rbac.get(lookup=stake_address, token=auth_token)
+    assert(resp.status_code == 503), f"Expected RBAC lookup to fail: {resp.status_code} - {resp.text}"
