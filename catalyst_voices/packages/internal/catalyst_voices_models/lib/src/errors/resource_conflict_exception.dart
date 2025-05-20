5890dcbf3c74603b7e80b0b164b4029a45a4c31c
Dominik Toton <dominik.toton@iohk.io>
2025-05-20 14:34:26 +0200

    fix: missing props

--- catalyst_voices/packages/internal/catalyst_voices_models/lib/src/errors/resource_conflict_exception.dart
+++ catalyst_voices/packages/internal/catalyst_voices_models/lib/src/errors/resource_conflict_exception.dart
@@ -6,7 +6,7 @@ final class ResourceConflictException extends Equatable implements Exception {
   const ResourceConflictException({this.message});
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [message];
 
   @override
   String toString() {
