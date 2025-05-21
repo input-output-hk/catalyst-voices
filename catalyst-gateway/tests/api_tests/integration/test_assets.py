044c28913c34ea1da94d2187fbfba59c414922f6
Mr-Leshiy <leshiy12345678@gmail.com>
2025-05-21 20:01:26 +0900

    wip

--- catalyst-gateway/tests/api_tests/integration/test_assets.py
+++ catalyst-gateway/tests/api_tests/integration/test_assets.py
@@ -11,7 +11,7 @@ from api.v1 import cardano
 def test_persistent_ada_amount_endpoint():
     # could the file from https://github.com/input-output-hk/catalyst-storage/blob/main/cardano-asset-preprod.json
     ASSETS_DATA_PATH = os.environ["ASSETS_DATA_PATH"]
-    # 5% failure rate
+    # 10% failure rate
     ALLOWED_FAILURE_RATE = 0.1
 
     test_data: dict[str, any] = {}
