2e2a95fd1f79ad8ffdee00b4339d4293450097fe
Ryszard Schossler <51096731+LynxLynxx@users.noreply.github.com>
2025-05-30 13:24:50 +0200

    feat(cat-voices): ui changes to proposal builder to comply with new template (#2640)
    * feat: ui changes to proposal builder to complie with new template
    
    * fix: format
    
    * feat: guidance card text style
    
    * feat: extracting NodeId into static field

--- catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/proposal_builder/proposal_builder_bloc.dart
+++ catalyst_voices/packages/internal/catalyst_voices_blocs/lib/src/proposal_builder/proposal_builder_bloc.dart
@@ -304,10 +304,14 @@ final class ProposalBuilderBloc extends Bloc<ProposalBuilderEvent, ProposalBuild
     }
 
     final guidance = property.schema.guidance;
+
+    final sectionTitle = property.schema.nodeId.isChildOf(ProposalDocument.milestoneListChildNodeId)
+        ? ''
+        : property.schema.title;
     if (guidance != null) {
       yield ProposalGuidanceItem(
         segmentTitle: segment.schema.title,
-        sectionTitle: property.schema.title,
+        sectionTitle: sectionTitle,
         description: guidance,
       );
     }
