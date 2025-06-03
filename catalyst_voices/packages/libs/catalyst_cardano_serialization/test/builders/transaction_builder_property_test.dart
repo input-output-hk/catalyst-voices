c3a37bde6756c13c14f558023bfe487e98fb51b4
Dominik Toton <dominik.toton@iohk.io>
2025-06-03 21:41:37 +0200

    feat: update translation builder property tests

--- catalyst_voices/packages/libs/catalyst_cardano_serialization/test/builders/transaction_builder_property_test.dart
+++ catalyst_voices/packages/libs/catalyst_cardano_serialization/test/builders/transaction_builder_property_test.dart
@@ -1,14 +1,11 @@
 import 'package:catalyst_cardano_serialization/catalyst_cardano_serialization.dart';
 import 'package:test/test.dart';
 
+import '../test_utils/fee_utils.dart';
 import '../test_utils/test_data.dart';
 import '../test_utils/transaction_property_test_utils.dart';
 
 void main() {
-  final config = transactionBuilderConfig(
-    selectionStrategy: const ExactBiggestAssetSelectionStrategy(),
-  );
-
   group(TransactionBuilder, () {
     transactionPropertyTest(
       'Transaction fee calculation should be consistent with the valid '
@@ -18,30 +15,20 @@ void main() {
         selectionStrategy: const ExactBiggestAssetSelectionStrategy(),
       ),
       (data) {
-        final builder = data.builder;
-        final appliedBuilder = builder.applySelection();
-        final txBody = appliedBuilder.buildBody();
-        final calculatedFee = txBody.fee;
+        final builder = data.builder.applySelection();
+        final txBody = builder.buildBody();
 
-        final signedFakeTx = Transaction(
+        final transaction = Transaction(
           body: txBody,
           isValid: true,
           witnessSet: TransactionBuilder.generateFakeWitnessSet(
-            appliedBuilder.inputs,
-            appliedBuilder.requiredSigners,
+            builder.inputs,
+            builder.requiredSigners,
           ),
+          auxiliaryData: builder.auxiliaryData,
         );
 
-        final validFee = config.feeAlgo.minFee(signedFakeTx, {
-          ...builder.inputs,
-          ...?builder.referenceInputs,
-        });
-
-        expect(
-          calculatedFee,
-          equals(validFee),
-          reason: 'Calculated transaction fee should match the valid fee',
-        );
+        verifyTransactionFee(transaction, builder);
       },
     );
 
@@ -53,30 +40,20 @@ void main() {
         selectionStrategy: const ExactBiggestAssetSelectionStrategy(),
       ),
       (data) {
-        final builder = data.builder;
-        final appliedBuilder = builder.withChangeIfNeeded();
-        final txBody = appliedBuilder.buildBody();
-        final calculatedFee = txBody.fee;
+        final builder = data.builder.withChangeIfNeeded();
+        final txBody = builder.buildBody();
 
-        final signedFakeTx = Transaction(
+        final transaction = Transaction(
           body: txBody,
           isValid: true,
           witnessSet: TransactionBuilder.generateFakeWitnessSet(
-            appliedBuilder.inputs,
-            appliedBuilder.requiredSigners,
+            builder.inputs,
+            builder.requiredSigners,
           ),
+          auxiliaryData: builder.auxiliaryData,
         );
 
-        final validFee = config.feeAlgo.minFee(signedFakeTx, {
-          ...builder.inputs,
-          ...?builder.referenceInputs,
-        });
-
-        expect(
-          calculatedFee,
-          equals(validFee),
-          reason: 'Calculated transaction fee should match the valid fee',
-        );
+        verifyTransactionFee(transaction, builder);
       },
     );
   });
