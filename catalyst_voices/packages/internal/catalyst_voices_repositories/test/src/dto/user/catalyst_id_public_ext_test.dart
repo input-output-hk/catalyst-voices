ee2ba9dd3bdc0644c81c8caf4a2a5636f780eb88
Damian Molinski <damian.molinski@iohk.io>
2025-06-09 13:21:53 +0200

    fix: decode account public id username

---
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/test/src/dto/user/catalyst_id_public_ext_test.dart
@@ -0,0 +1,46 @@
+import 'package:catalyst_voices_repositories/generated/api/cat_reviews.models.swagger.dart';
+import 'package:catalyst_voices_repositories/src/dto/user/catalyst_id_public_ext.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('decode', () {
+    test('spaces are handled correctly', () {
+      // Given
+      const sourceUsername = 'First%20Last';
+      const expectedUsername = 'First Last';
+      const id = CatalystIDPublic(username: sourceUsername);
+
+      // When
+      final decodedId = id.decode();
+
+      // Then
+      expect(decodedId.username, expectedUsername);
+    });
+
+    test('diacritical characters are handled correctly', () {
+      // Given
+      const sourceUsername = 'Peverell Heureuxó';
+      const expectedUsername = 'Peverell Heureuxó';
+      const id = CatalystIDPublic(username: sourceUsername);
+
+      // When
+      final decodedId = id.decode();
+
+      // Then
+      expect(decodedId.username, expectedUsername);
+    });
+
+    test('diacritical characters with while spaces are handled correctly', () {
+      // Given
+      const sourceUsername = 'Peverell%20Heureuxó';
+      const expectedUsername = 'Peverell Heureuxó';
+      const id = CatalystIDPublic(username: sourceUsername);
+
+      // When
+      final decodedId = id.decode();
+
+      // Then
+      expect(decodedId.username, expectedUsername);
+    });
+  });
+}
