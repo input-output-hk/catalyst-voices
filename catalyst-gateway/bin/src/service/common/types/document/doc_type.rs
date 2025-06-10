9b5acc96d62ce10810b24932249402d6c88fbeb7
bkioshn <bkioshn@gmail.com>
2025-06-10 18:05:02 +0700

    fix(cat-gateway): use impl array for doctype
    Signed-off-by: bkioshn <bkioshn@gmail.com>

--- catalyst-gateway/bin/src/service/common/types/document/doc_type.rs
+++ catalyst-gateway/bin/src/service/common/types/document/doc_type.rs
@@ -2,19 +2,16 @@
 //!
 //! List of `UUIDv4`.
 
-use std::sync::LazyLock;
-
 use poem_openapi::{
-    registry::{MetaExternalDocument, MetaSchema, MetaSchemaRef},
-    types::{Example, ParseError, ParseFromJSON, ParseResult, ToJSON, Type},
+    registry::{MetaExternalDocument, MetaSchema},
+    types::{Example, ToJSON},
 };
 
 use self::generic::uuidv4;
-use crate::service::common::types::generic::{self, uuidv4::UUIDv4};
-
-/// Document type - list of `UUIDv4`
-#[derive(Debug, Clone)]
-pub(crate) struct DocumentType(Vec<uuidv4::UUIDv4>);
+use crate::service::common::types::{
+    array_types::impl_array_types,
+    generic::{self, uuidv4::UUIDv4},
+};
 
 /// Title.
 const TITLE: &str = "Signed Document Type";
@@ -29,20 +26,25 @@ const URI_DESCRIPTION: &str = "Specification";
 const MAX_LENGTH: usize = usize::MAX;
 /// Minimum length
 const MIN_LENGTH: usize = 1;
-/// Schema
-static SCHEMA: LazyLock<MetaSchema> = LazyLock::new(|| {
-    MetaSchema {
+
+impl_array_types!(
+    /// Document type - list of `UUIDv4`
+    DocumentType,
+    UUIDv4,
+    Some( MetaSchema {
         title: Some(TITLE.into()),
         description: Some(DESCRIPTION),
-        max_length: Some(MAX_LENGTH),
-        min_length: Some(MIN_LENGTH),
+        max_items: Some(MAX_LENGTH),
+        min_items: Some(MIN_LENGTH),
+        items: Some(Box::new(UUIDv4::schema_ref())),
         external_docs: Some(MetaExternalDocument {
             url: URI.to_owned(),
             description: Some(URI_DESCRIPTION.to_owned()),
         }),
+        example: Self::example().to_json(),
         ..MetaSchema::ANY
-    }
-});
+    })
+);
 
 impl DocumentType {
     /// Convert to sql format used for filter '{"uuid", "uuid"}'
@@ -58,55 +60,9 @@ impl DocumentType {
     }
 }
 
-impl Type for DocumentType {
-    type RawElementValueType = Self;
-    type RawValueType = Self;
-
-    const IS_REQUIRED: bool = true;
-
-    fn name() -> std::borrow::Cow<'static, str> {
-        "DocumentType".into()
-    }
-
-    fn schema_ref() -> MetaSchemaRef {
-        let schema_ref =
-            MetaSchemaRef::Inline(Box::new(MetaSchema::new_with_format("array", "string")));
-        schema_ref.merge(SCHEMA.clone())
-    }
-
-    fn as_raw_value(&self) -> Option<&Self::RawValueType> {
-        Some(self)
-    }
-
-    fn raw_element_iter<'a>(
-        &'a self,
-    ) -> Box<dyn Iterator<Item = &'a Self::RawElementValueType> + 'a> {
-        Box::new(self.as_raw_value().into_iter())
-    }
-}
-
-impl ParseFromJSON for DocumentType {
-    fn parse_from_json(value: Option<serde_json::Value>) -> ParseResult<Self> {
-        Vec::parse_from_json(value)
-            .map_err(ParseError::propagate)
-            .map(Self)
-    }
-}
-
 impl Example for DocumentType {
     fn example() -> Self {
         Self(vec![uuidv4::UUIDv4::example(), uuidv4::UUIDv4::example()])
-    }
-}
-
-impl ToJSON for DocumentType {
-    fn to_json(&self) -> Option<serde_json::Value> {
-        Some(serde_json::Value::Array(
-            self.0
-                .iter()
-                .filter_map(poem_openapi::types::ToJSON::to_json)
-                .collect(),
-        ))
     }
 }
 
