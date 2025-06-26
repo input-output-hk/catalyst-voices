33a34f985a3c805b0eeb9cba76f92e3be45a7313
Emir Hodzic <emir.hodzich@gmail.com>
2025-06-26 15:35:13 +0200

    test: Playwright initial setup (#2878)

---
+++ catalyst_voices/apps/voices/e2e_tests/data/walletConfigs.ts
@@ -0,0 +1,138 @@
+import { BrowserExtensionName } from "../models/browserExtensionModel";
+import { WalletConfigModel } from "../models/walletConfigModel";
+import { getBrowserExtension } from "./browserExtensionConfigs";
+
+export const walletConfigs: WalletConfigModel[] = [
+  {
+    id: "1",
+    extension: getBrowserExtension(BrowserExtensionName.Lace),
+    seed: [
+      "stomach",
+      "horn",
+      "rail",
+      "afraid",
+      "flip",
+      "also",
+      "abandon",
+      "speed",
+      "chaos",
+      "daring",
+      "soon",
+      "soft",
+      "okay",
+      "online",
+      "benefit",
+    ],
+    username: "test123",
+    password: "test12345678@",
+    cipBridge: ["cip-95"],
+  },
+  {
+    id: "2",
+    extension: getBrowserExtension(BrowserExtensionName.Typhon),
+    seed: [
+      "stomach",
+      "horn",
+      "rail",
+      "afraid",
+      "flip",
+      "also",
+      "abandon",
+      "speed",
+      "chaos",
+      "daring",
+      "soon",
+      "soft",
+      "okay",
+      "online",
+      "benefit",
+    ],
+    username: "test123",
+    password: "test12345678@",
+    cipBridge: ["cip-30"],
+  },
+  {
+    id: "3",
+    extension: getBrowserExtension(BrowserExtensionName.Eternl),
+    seed: [
+      "stomach",
+      "horn",
+      "rail",
+      "afraid",
+      "flip",
+      "also",
+      "abandon",
+      "speed",
+      "chaos",
+      "daring",
+      "soon",
+      "soft",
+      "okay",
+      "online",
+      "benefit",
+    ],
+    username: "test123",
+    password: "test12345678@!!",
+    cipBridge: ["cip-30", "cip-95"],
+  },
+  {
+    id: "4",
+    extension: getBrowserExtension(BrowserExtensionName.Yoroi),
+    seed: [
+      "stomach",
+      "horn",
+      "rail",
+      "afraid",
+      "flip",
+      "also",
+      "abandon",
+      "speed",
+      "chaos",
+      "daring",
+      "soon",
+      "soft",
+      "okay",
+      "online",
+      "benefit",
+    ],
+    username: "test123",
+    password: "test12345678@!!",
+    cipBridge: ["cip-95"],
+  },
+  {
+    id: "5",
+    extension: getBrowserExtension(BrowserExtensionName.Nufi),
+    seed: [
+      "stomach",
+      "horn",
+      "rail",
+      "afraid",
+      "flip",
+      "also",
+      "abandon",
+      "speed",
+      "chaos",
+      "daring",
+      "soon",
+      "soft",
+      "okay",
+      "online",
+      "benefit",
+    ],
+    username: "test123",
+    password: "test12345678@!!",
+    cipBridge: ["cip-95"],
+  },
+];
+
+export const getWalletConfig = (id: string): WalletConfigModel => {
+  const walletConfig = walletConfigs.find(
+    (walletConfig) => walletConfig.id === id
+  );
+  if (!walletConfig) {
+    throw new Error(`Wallet config with id ${id} not found`);
+  }
+  return walletConfig;
+};
+
+export const getWalletConfigs = (): WalletConfigModel[] => walletConfigs;
