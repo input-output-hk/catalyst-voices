da8e9cf67ae5eec0a890bd393028c24418dd61bb
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-06-30 12:21:40 +0200

    Update RBAC endpoint to use latest RBAC cache

--- catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get/mod.rs
+++ catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get/mod.rs
@@ -1,9 +1,9 @@
 //! Implementation of the GET `/rbac/registrations` endpoint.
 
+use anyhow::Context;
 pub use response::AllResponses;
 
 mod binary_data;
-mod chain_info;
 mod extended_data;
 mod key_data;
 mod key_data_list;
@@ -17,14 +17,13 @@ mod role_data;
 mod role_map;
 mod unprocessable_content;
 
-use cardano_blockchain_types::StakeAddress;
 use poem_openapi::payload::Json;
 
 use crate::{
-    rbac_cache::RBAC_CACHE,
+    rbac::{latest_rbac_chain, latest_rbac_chain_by_address, ChainInfo},
     service::{
         api::cardano::rbac::registrations_get::{
-            chain_info::ChainInfo, registration_chain::RbacRegistrationChain, response::Responses,
+            registration_chain::RbacRegistrationChain, response::Responses,
             unprocessable_content::RbacUnprocessableContent,
         },
         common::{
@@ -35,49 +34,42 @@ use crate::{
 };
 
 /// Get RBAC registration endpoint.
-pub(crate) fn endpoint(
+pub(crate) async fn endpoint(
     lookup: Option<CatIdOrStake>, token: Option<CatalystRBACTokenV1>,
 ) -> AllResponses {
-    let (persistent_chain, volatile_chain) = match lookup {
-        Some(CatIdOrStake::CatId(id)) => {
-            let id = id.into();
-            (RBAC_CACHE.get(&id, true), RBAC_CACHE.get(&id, false))
-        },
-        Some(CatIdOrStake::Address(address)) => {
-            let address: StakeAddress = match address.try_into() {
-                Ok(a) => a,
-                Err(e) => return AllResponses::handle_error(&e),
-            };
-            (
-                RBAC_CACHE.get_by_address(&address, true),
-                RBAC_CACHE.get_by_address(&address, false),
-            )
-        },
-        None => {
-            match token {
-                Some(token) => {
-                    (
-                        RBAC_CACHE.get(token.catalyst_id(), true),
-                        RBAC_CACHE.get(token.catalyst_id(), false),
-                    )
-                },
-                None => {
-                    return Responses::UnprocessableContent(Json(RbacUnprocessableContent::new(
-                        "Either lookup parameter or token must be provided",
-                    )))
-                    .into()
-                },
-            }
-        },
-    };
+    if lookup.is_none() && token.is_none() {
+        return Responses::UnprocessableContent(Json(RbacUnprocessableContent::new(
+            "Either lookup parameter or token must be provided",
+        )))
+        .into();
+    }
 
-    match ChainInfo::new(persistent_chain, volatile_chain) {
-        Some(info) => {
+    match chain_info(lookup, token).await {
+        Err(e) => AllResponses::handle_error(&e),
+        Ok(Some(info)) => {
             match RbacRegistrationChain::new(&info) {
                 Ok(c) => Responses::Ok(Json(Box::new(c))).into(),
                 Err(e) => AllResponses::handle_error(&e),
             }
         },
-        None => Responses::NotFound.into(),
+        Ok(None) => Responses::NotFound.into(),
+    }
+}
+
+/// Returns a `ChainInfo` object using either a Catalyst ID or a stake address.
+async fn chain_info(
+    lookup: Option<CatIdOrStake>, token: Option<CatalystRBACTokenV1>,
+) -> anyhow::Result<Option<ChainInfo>> {
+    match lookup {
+        Some(CatIdOrStake::CatId(id)) => latest_rbac_chain(&id.into()).await,
+        Some(CatIdOrStake::Address(address)) => {
+            let address = address.try_into().context("Invalid stake address")?;
+            latest_rbac_chain_by_address(&address).await
+        },
+        None => {
+            // This error is properly handled above and shouldn't occur here.
+            let token = token.context("Either lookup parameter or token must be provided")?;
+            latest_rbac_chain(token.catalyst_id()).await
+        },
     }
 }
