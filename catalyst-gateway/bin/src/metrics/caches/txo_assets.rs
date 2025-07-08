8c5d3d5cc6de596457c23e4ef40df92352dd713e
Joaquín Rosales <joaquin.rosales@iohk.io>
2025-07-07 20:53:21 -0600

    wip(cat-gateway): add metrics for asset caches

--- catalyst-gateway/bin/src/metrics/caches/txo_assets.rs
+++ catalyst-gateway/bin/src/metrics/caches/txo_assets.rs
@@ -6,7 +6,7 @@ mod reporter {
     //! Prometheus reporter metrics.
     use std::sync::LazyLock;
 
-    use prometheus::{register_int_gauge_vec, IntGaugeVec};
+    use prometheus::{register_counter_vec, register_int_gauge_vec, CounterVec, IntGaugeVec};
 
     /// Labels for the metrics
     const METRIC_LABELS: [&str; 3] = ["api_host_names", "service_id", "network"];
@@ -30,6 +30,26 @@ mod reporter {
         )
         .unwrap()
     });
+
+    /// Number of hits in the TXO Assets cache.
+    pub(crate) static TXO_ASSETS_CACHE_HIT_COUNT: LazyLock<CounterVec> = LazyLock::new(|| {
+        register_counter_vec!(
+            "cache_txo_assets_hits_count",
+            "Returns an approximate number of TXO Assets entries in this cache",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+
+    /// Number of misses in the TXO Assets cache.
+    pub(crate) static TXO_ASSETS_CACHE_MISSES_COUNT: LazyLock<CounterVec> = LazyLock::new(|| {
+        register_counter_vec!(
+            "cache_txo_assets_misses_count",
+            "Returns an approximate number of TXO Assets entries in this cache",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
 }
 
 /// Update Cache metrics
@@ -40,9 +60,33 @@ pub(crate) fn update() {
 
     reporter::TXO_ASSETS_CACHE_SIZE
         .with_label_values(&[&api_host_names, service_id, &network])
-        .set(0i64);
+        .set(i64::try_from(0u64).unwrap_or(-1));
 
     reporter::TXO_ASSETS_CACHE_ENTRIES_COUNT
         .with_label_values(&[&api_host_names, service_id, &network])
-        .set(0i64);
+        .set(i64::try_from(0u64).unwrap_or(-1));
+
+    // todo!("WIP: get cache values after refactoring");
+}
+
+/// Increment the TXO Assets Cache hits count.
+pub(crate) fn txo_assets_hits_inc() {
+    let api_host_names = Settings::api_host_names().join(",");
+    let service_id = Settings::service_id();
+    let network = Settings::cardano_network().to_string();
+
+    reporter::TXO_ASSETS_CACHE_HIT_COUNT
+        .with_label_values(&[&api_host_names, service_id, &network])
+        .inc();
+}
+
+/// Increment the TXO Assets Cache misses count.
+pub(crate) fn txo_assets_miss_inc() {
+    let api_host_names = Settings::api_host_names().join(",");
+    let service_id = Settings::service_id();
+    let network = Settings::cardano_network().to_string();
+
+    reporter::TXO_ASSETS_CACHE_MISSES_COUNT
+        .with_label_values(&[&api_host_names, service_id, &network])
+        .inc();
 }
