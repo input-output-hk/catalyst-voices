d124de149363a1f0921a18f82143fc497695f44e
Joaquín Rosales <joaquin.rosales@iohk.io>
2025-07-07 21:08:46 -0600

    wip(cat-gateway): add metrics for asset caches

--- catalyst-gateway/bin/src/metrics/caches/native_assets.rs
+++ catalyst-gateway/bin/src/metrics/caches/native_assets.rs
@@ -6,7 +6,7 @@ mod reporter {
     //! Prometheus reporter metrics.
     use std::sync::LazyLock;
 
-    use prometheus::{register_int_gauge_vec, IntGaugeVec};
+    use prometheus::{register_counter_vec, register_int_gauge_vec, CounterVec, IntGaugeVec};
 
     /// Labels for the metrics
     const METRIC_LABELS: [&str; 3] = ["api_host_names", "service_id", "network"];
@@ -31,6 +31,27 @@ mod reporter {
             )
             .unwrap()
         });
+
+    /// Number of hits in the Native Assets cache.
+    pub(crate) static NATIVE_ASSETS_CACHE_HIT_COUNT: LazyLock<CounterVec> = LazyLock::new(|| {
+        register_counter_vec!(
+            "cache_txo_assets_hits_count",
+            "Returns an approximate number of Native Assets entries in this cache",
+            &METRIC_LABELS
+        )
+        .unwrap()
+    });
+
+    /// Number of misses in the Native Assets cache.
+    pub(crate) static NATIVE_ASSETS_CACHE_MISSES_COUNT: LazyLock<CounterVec> =
+        LazyLock::new(|| {
+            register_counter_vec!(
+                "cache_txo_assets_misses_count",
+                "Returns an approximate number of Native Assets entries in this cache",
+                &METRIC_LABELS
+            )
+            .unwrap()
+        });
 }
 
 /// Update Cache metrics
@@ -46,4 +67,26 @@ pub(crate) fn update() {
     reporter::NATIVE_ASSETS_CACHE_ENTRIES_COUNT
         .with_label_values(&[&api_host_names, service_id, &network])
         .set(0i64);
+}
+
+/// Increment the Native Assets Cache hits count.
+pub(crate) fn native_assets_hits_inc() {
+    let api_host_names = Settings::api_host_names().join(",");
+    let service_id = Settings::service_id();
+    let network = Settings::cardano_network().to_string();
+
+    reporter::NATIVE_ASSETS_CACHE_HIT_COUNT
+        .with_label_values(&[&api_host_names, service_id, &network])
+        .inc();
+}
+
+/// Increment the Native Assets Cache misses count.
+pub(crate) fn native_assets_miss_inc() {
+    let api_host_names = Settings::api_host_names().join(",");
+    let service_id = Settings::service_id();
+    let network = Settings::cardano_network().to_string();
+
+    reporter::NATIVE_ASSETS_CACHE_MISSES_COUNT
+        .with_label_values(&[&api_host_names, service_id, &network])
+        .inc();
 }
