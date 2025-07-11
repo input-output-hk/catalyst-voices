5c721a3a189834ce500e044cf9db6b6029f4052e
Mr-Leshiy <leshiy12345678@gmail.com>
2025-07-11 12:19:52 +0200

    fix

--- catalyst-gateway/tests/schemathesis_tests/entry.sh
+++ catalyst-gateway/tests/schemathesis_tests/entry.sh
@@ -2,13 +2,10 @@
 
 st run --checks=all ${API_SPEC} \
                     --exclude-path-regex '.*\/draft\/.*' \
-                    # excluding since this is a internal debug endpoint
                     --exclude-path '/api/v1/health/inspection' \
-                    --exclude-checks=ignored_auth \ 
-                    # overridden check with negative_data_rejection_custom
+                    --exclude-checks=ignored_auth \
                     --exclude-checks=negative_data_rejection \
-                    # overridden check with not_a_server_error_custom 
-                    --exclude-checks=not_a_server_error \ 
+                    --exclude-checks=not_a_server_error \
                     --workers=2 \
                     --wait-for-schema=${WAIT_FOR_SCHEMA} \
                     --max-response-time=${MAX_RESPONSE_TIME} \
