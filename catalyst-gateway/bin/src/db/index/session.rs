d1eae0a844cc62d86293eb290e40fe6dce386b33
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-07-14 15:40:49 +0200

    Fix after merge

--- catalyst-gateway/bin/src/db/index/session.rs
+++ catalyst-gateway/bin/src/db/index/session.rs
@@ -174,11 +174,6 @@ impl CassandraSession {
         }
     }
 
-    /// Returns a flag notifying that the current `CassandraSession` is persistent or not
-    pub(crate) fn is_persistent(&self) -> bool {
-        self.persistent
-    }
-
     /// Executes a select query with the given parameters.
     ///
     /// Returns an iterator that iterates over all the result pages that the query
