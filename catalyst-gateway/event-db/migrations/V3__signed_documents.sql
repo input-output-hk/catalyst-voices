e46ea7733f63ec02fee62cbb9be5899b0cf34acb
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-07-17 15:37:52 +0700

    chore: clean new migration table

--- catalyst-gateway/event-db/migrations/V3__signed_documents.sql
+++ catalyst-gateway/event-db/migrations/V3__signed_documents.sql
@@ -2,4 +2,5 @@
 -- * `type` metadata field changed from the single `UUIDv4` value to become an array `[UUIDv4]`.
 
 ALTER TABLE signed_docs
-ALTER COLUMN type TYPE UUID[] USING ARRAY[type];
+  ALTER COLUMN type TYPE UUID[]
+  USING ARRAY[type];
