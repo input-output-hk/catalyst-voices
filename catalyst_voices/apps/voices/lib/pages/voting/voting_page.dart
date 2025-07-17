70c96108676503d6f8a7e74b7caec211cbe44c83
Dominik Toton <dominik.toton@iohk.io>
2025-07-17 12:35:33 +0200

    feat: voting route + internals

--- catalyst_voices/apps/voices/lib/pages/voting/voting_page.dart
+++ catalyst_voices/apps/voices/lib/pages/voting/voting_page.dart
@@ -1,11 +1,31 @@
+import 'package:catalyst_voices/routes/routes.dart';
 import 'package:catalyst_voices/widgets/layouts/header_and_content_layout.dart';
+import 'package:catalyst_voices/widgets/pagination/paging_controller.dart';
+import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
+import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
 
 part 'widgets/voting_content.dart';
 part 'widgets/voting_header.dart';
 
-class VotingPage extends StatelessWidget {
-  const VotingPage({super.key});
+class VotingPage extends StatefulWidget {
+  final SignedDocumentRef? categoryId;
+  final ProposalsFilterType? type;
+
+  const VotingPage({
+    super.key,
+    this.categoryId,
+    this.type,
+  });
+
+  @override
+  State<VotingPage> createState() => _VotingPageState();
+}
+
+class _VotingPageState extends State<VotingPage> with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+  late final PagingController<ProposalBrief> _pagingController;
 
   @override
   Widget build(BuildContext context) {
@@ -13,5 +33,78 @@ class VotingPage extends StatelessWidget {
       header: _VotingHeader(),
       content: _VotingContent(),
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    final proposalsFilterType = _determineFilterType();
+
+    _tabController = TabController(
+      initialIndex: proposalsFilterType.index,
+      length: ProposalsFilterType.values.length,
+      vsync: this,
+    );
+
+    _pagingController = PagingController(
+      initialPage: 0,
+      initialMaxResults: 0,
+    );
+
+    // TODO(dt-iohk): handle cubit initialisation when VotingCubit is there.
+    // context.read<ProposalsCubit>().init(
+    //       onlyMyProposals: widget.type?.isMy ?? false,
+    //       category: widget.categoryId,
+    //       type: proposalsFilterType,
+    //       order: const Alphabetical(),
+    //     );
+
+    _pagingController
+      ..addPageRequestListener(_handleProposalsPageRequest)
+      ..notifyPageRequestListeners(0);
+  }
+
+  ProposalsFilterType _determineFilterType() {
+    final isProposerUnlock = context.read<SessionCubit>().state.isProposerUnlock;
+    final requestedType = widget.type;
+
+    if (!isProposerUnlock && (requestedType?.isMy ?? false)) {
+      _updateRoute(filterType: ProposalsFilterType.total);
+    }
+
+    return requestedType ?? ProposalsFilterType.total;
+  }
+
+  Future<void> _handleProposalsPageRequest(
+    int pageKey,
+    int pageSize,
+    ProposalBrief? lastProposalId,
+  ) async {
+    // TODO(dt-iohk): handle cubit callback when VotingCubit is there
+    // final request = PageRequest(page: pageKey, size: pageSize);
+    // await context.read<ProposalsCubit>().getProposals(request);
+  }
+
+  void _updateRoute({
+    Optional<String>? categoryId,
+    ProposalsFilterType? filterType,
+  }) {
+    Router.neglect(context, () {
+      final effectiveCategoryId = categoryId.dataOr(widget.categoryId?.id);
+      final effectiveType = filterType?.name ?? widget.type?.name;
+
+      VotingRoute(
+        categoryId: effectiveCategoryId,
+        type: effectiveType,
+      ).replace(context);
+    });
   }
 }
