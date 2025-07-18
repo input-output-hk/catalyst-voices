cee3ddb11a6df549b4c606c0213632b163cdbc11
Dominik Toton <dominik.toton@iohk.io>
2025-07-18 15:02:26 +0200

    feat: ui adjustments

--- catalyst_voices/apps/voices/lib/widgets/specialized/voting/account_voting_power_card.dart
+++ catalyst_voices/apps/voices/lib/widgets/specialized/voting/account_voting_power_card.dart
@@ -8,6 +8,11 @@ import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
 
+/// To align columns in two different rows we let each row's column
+/// hardcode the width so that they're horizontally aligned
+/// when displayed one above the other.
+const _votingPowerWidth = 192.0;
+
 class AccountVotingPowerCard extends StatelessWidget {
   final VotingPowerViewModel votingPower;
 
@@ -19,29 +24,32 @@ class AccountVotingPowerCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final labelStyle = theme.textTheme.labelMedium!
+        .copyWith(color: theme.colors.textOnPrimaryLevel1.withValues(alpha: 0.7));
+
     return Container(
       padding: const EdgeInsets.fromLTRB(16, 16, 16, 20),
       decoration: BoxDecoration(
         color: theme.colors.elevationsOnSurfaceNeutralLv1White,
         borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: theme.colorScheme.outline),
       ),
       child: Column(
-        spacing: 48,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           VoicesAssets.icons.lightningBolt.buildIcon(
             color: theme.colors.textOnPrimaryLevel1,
             size: 24,
           ),
-          Row(
-            spacing: 48,
-            children: [
-              _VotingPower(votingPower: votingPower.power),
-              _StatusAndUpdated(
-                status: votingPower.status,
-                updatedAt: votingPower.updatedAt,
-              ),
-            ],
+          const SizedBox(height: 44),
+          _VotingPowerLabelRow(
+            votingPower: votingPower,
+            labelStyle: labelStyle,
+          ),
+          const SizedBox(height: 16),
+          _VotingPowerValueRow(
+            votingPower: votingPower,
+            labelStyle: labelStyle,
           ),
         ],
       ),
@@ -51,18 +59,36 @@ class AccountVotingPowerCard extends StatelessWidget {
 
 class _Status extends StatelessWidget {
   final VotingPowerStatus status;
-  final TextStyle style;
+  final TextStyle labelStyle;
 
   const _Status({
     required this.status,
-    required this.style,
+    required this.labelStyle,
   });
 
   @override
   Widget build(BuildContext context) {
-    return VoicesChip(
-      content: Text(_getStatusText(context), style: style),
-      borderRadius: BorderRadius.circular(4),
+    final theme = Theme.of(context);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 6,
+      children: [
+        Text(
+          context.l10n.status,
+          style: labelStyle,
+        ),
+        VoicesChip(
+          padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+          borderRadius: BorderRadius.circular(4),
+          backgroundColor: Theme.of(context).colors.onSurfacePrimary016,
+          content: Text(
+            _getStatusText(context),
+            style: theme.textTheme.labelSmall!.copyWith(color: theme.colors.textOnPrimaryLevel1),
+          ),
+        ),
+      ],
     );
   }
 
@@ -74,94 +100,104 @@ class _Status extends StatelessWidget {
   }
 }
 
-class _StatusAndUpdated extends StatelessWidget {
-  final VotingPowerStatus status;
+class _UpdatedAt extends StatelessWidget {
   final DateTime updatedAt;
+  final TextStyle labelStyle;
 
-  const _StatusAndUpdated({
-    required this.status,
+  const _UpdatedAt({
     required this.updatedAt,
+    required this.labelStyle,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final labelStyle = theme.textTheme.labelMedium!
-        .copyWith(color: theme.colors.textOnPrimaryLevel1.withAlpha(178));
-    final valueStyle = labelStyle.copyWith(color: theme.colors.textOnPrimaryLevel1);
 
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 4,
       children: [
-        Text(
-          context.l10n.status,
-          style: labelStyle,
-        ),
-        const SizedBox(height: 4),
-        _Status(
-          status: status,
-          style: valueStyle,
-        ),
-        const SizedBox(height: 16),
         Text(
           context.l10n.updated,
           style: labelStyle,
         ),
-        const SizedBox(height: 4),
-        _UpdatedAt(
-          updatedAt: updatedAt,
-          style: valueStyle,
+        TimezoneDateTimeText(
+          updatedAt,
+          formatter: (context, dateTime) => DateFormatter.formatFullDate24Format(dateTime),
+          style: theme.textTheme.labelMedium!.copyWith(color: theme.colors.textOnPrimaryLevel1),
         ),
       ],
     );
   }
 }
 
-class _UpdatedAt extends StatelessWidget {
-  final DateTime updatedAt;
-  final TextStyle style;
+class _VotingPowerLabelRow extends StatelessWidget {
+  final VotingPowerViewModel votingPower;
+  final TextStyle labelStyle;
 
-  const _UpdatedAt({
-    required this.updatedAt,
-    required this.style,
+  const _VotingPowerLabelRow({
+    required this.votingPower,
+    required this.labelStyle,
   });
-
-  @override
-  Widget build(BuildContext context) {
-    return TimezoneDateTimeText(
-      updatedAt,
-      formatter: (context, dateTime) => DateFormatter.formatFullDate24Format(dateTime),
-      style: style,
-    );
-  }
-}
-
-class _VotingPower extends StatelessWidget {
-  final String votingPower;
-
-  const _VotingPower({required this.votingPower});
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Column(
+    return Row(
       mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 32,
       children: [
-        Text(
-          context.l10n.votingPower,
-          style: theme.textTheme.titleMedium?.copyWith(
-            fontSize: 18,
+        SizedBox(
+          width: _votingPowerWidth,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 2),
+            child: Text(
+              context.l10n.votingPower,
+              style: theme.textTheme.titleMedium?.copyWith(
+                fontSize: 18,
+                color: theme.colors.textOnPrimaryLevel1,
+              ),
+            ),
           ),
         ),
-        Text(
-          votingPower,
-          style: theme.textTheme.displayMedium?.copyWith(
-            fontSize: 18,
+        _Status(
+          status: votingPower.status,
+          labelStyle: labelStyle,
+        ),
+      ],
+    );
+  }
+}
+
+class _VotingPowerValueRow extends StatelessWidget {
+  final VotingPowerViewModel votingPower;
+  final TextStyle labelStyle;
+
+  const _VotingPowerValueRow({
+    required this.votingPower,
+    required this.labelStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: _votingPowerWidth,
+          child: Text(
+            votingPower.power,
+            style: theme.textTheme.displayMedium?.copyWith(
+              color: theme.colors.textOnPrimaryLevel1,
+            ),
           ),
+        ),
+        _UpdatedAt(
+          updatedAt: votingPower.updatedAt,
+          labelStyle: labelStyle,
         ),
       ],
     );
