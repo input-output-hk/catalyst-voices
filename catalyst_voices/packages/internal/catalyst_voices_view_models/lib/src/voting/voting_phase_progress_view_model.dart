79c41116559d0e4a6e1631101ee22f81931b03bc
Dominik Toton <166132265+dt-iohk@users.noreply.github.com>
2025-07-21 13:22:05 +0200

    feat(cat-voices): VotingPhaseProgressCard (#3077)
    * feat: voting phase card
    
    * chore: cleanup
    
    * chore: cleanup
    
    * chore: cleanup
    
    * fix: revert
    
    * chore: test
    
    * refactor: tests

---
+++ catalyst_voices/packages/internal/catalyst_voices_view_models/lib/src/voting/voting_phase_progress_view_model.dart
@@ -0,0 +1,88 @@
+import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
+import 'package:equatable/equatable.dart';
+
+final class VotingPhaseProgressDetailsViewModel extends Equatable {
+  final CampaignPhaseStatus status;
+  final DateTime start;
+  final DateTime end;
+  final Duration phaseEndsIn;
+  final double progressValue;
+
+  const VotingPhaseProgressDetailsViewModel({
+    required this.status,
+    required this.start,
+    required this.end,
+    required this.phaseEndsIn,
+    required this.progressValue,
+  });
+
+  @override
+  List<Object?> get props => [status, start, end, phaseEndsIn, progressValue];
+}
+
+final class VotingPhaseProgressViewModel extends Equatable {
+  final DateRange votingDateRange;
+
+  /// The starting date of a phase that precedes the voting phase.
+  ///
+  /// Usually this is needed when voting hasn't yet started
+  /// to show a progress bar when the voting starts.
+  final DateTime previousPhaseStartDate;
+
+  const VotingPhaseProgressViewModel({
+    required this.votingDateRange,
+    required this.previousPhaseStartDate,
+  });
+
+  @override
+  List<Object?> get props => [votingDateRange, previousPhaseStartDate];
+
+  VotingPhaseProgressDetailsViewModel progress(DateTime now) {
+    DateTime start;
+    DateTime end;
+
+    final status = CampaignPhaseStatus.fromRange(votingDateRange, now);
+    switch (status) {
+      case CampaignPhaseStatus.upcoming:
+        start = previousPhaseStartDate;
+        end = votingDateRange.from ?? now;
+
+      case CampaignPhaseStatus.active:
+      case CampaignPhaseStatus.post:
+        start = votingDateRange.from ?? now;
+        end = votingDateRange.to ?? now;
+    }
+
+    if (now.isBefore(start) || now.isAtSameMomentAs(start)) {
+      return VotingPhaseProgressDetailsViewModel(
+        status: status,
+        start: start,
+        end: end,
+        phaseEndsIn: Duration.zero,
+        progressValue: 0,
+      );
+    } else if (now.isAfter(end) || now.isAtSameMomentAs(end)) {
+      return VotingPhaseProgressDetailsViewModel(
+        status: status,
+        start: start,
+        end: end,
+        phaseEndsIn: Duration.zero,
+        progressValue: 1,
+      );
+    } else {
+      final phaseDuration = end.difference(start);
+      final phaseCurrentTs = now.difference(start);
+      final phaseEndsIn = end.difference(now);
+      final phaseValue = phaseCurrentTs.inMicroseconds / phaseDuration.inMicroseconds;
+
+      return VotingPhaseProgressDetailsViewModel(
+        status: status,
+        start: start,
+        end: end,
+        phaseEndsIn: phaseEndsIn,
+        progressValue: phaseValue,
+      );
+    }
+  }
+}
