3c6c124c43eb456b764f108a5580c95f1b62d81b
Apisit Ritreungroj <apisit_lon@hotmail.com>
2025-07-23 22:30:46 +0700

    fix: revert doc type

--- catalyst-gateway/bin/src/service/api/documents/templates/mod.rs
+++ catalyst-gateway/bin/src/service/api/documents/templates/mod.rs
@@ -83,7 +83,7 @@ pub(crate) struct SignedDocData {
     /// Version.
     ver: &'static str,
     /// Document type.
-    doc_type: Vec<Uuid>,
+    doc_type: Uuid,
     /// Content bytes.
     content: &'static [u8],
     /// Category ID.
