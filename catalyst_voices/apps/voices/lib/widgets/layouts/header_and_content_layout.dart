866f0b5d2e0da9e7ce10e353a7589d087ab637a9
Ryszard Schossler <ryszard.schossler@iohk.io>
2025-07-25 16:24:35 +0200

    feat: content for pre voting page

--- catalyst_voices/apps/voices/lib/widgets/layouts/header_and_content_layout.dart
+++ catalyst_voices/apps/voices/lib/widgets/layouts/header_and_content_layout.dart
@@ -3,30 +3,36 @@ import 'package:flutter/material.dart';
 class HeaderAndContentLayout extends StatelessWidget {
   final Widget header;
   final Widget content;
+  final Widget? background;
 
   const HeaderAndContentLayout({
     super.key,
     required this.header,
     required this.content,
+    this.background,
   });
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverPadding(
-          padding:
-              const EdgeInsets.symmetric(horizontal: 32).add(const EdgeInsets.only(bottom: 32)),
-          sliver: SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const SizedBox(height: 16),
-                header,
-                const SizedBox(height: 40),
-                content,
-              ],
+    return Stack(
+      children: [
+        if (background != null) background!,
+        CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 32).add(const EdgeInsets.only(bottom: 32)),
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    header,
+                    const SizedBox(height: 40),
+                    content,
+                  ],
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ],
     );
