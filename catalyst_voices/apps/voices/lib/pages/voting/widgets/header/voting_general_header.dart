c116c21a5af1107410299c53f0b16233f44bcf0b
Dominik Toton <dominik.toton@iohk.io>
2025-07-31 13:27:42 +0200

    fix: padding

--- catalyst_voices/apps/voices/lib/pages/voting/widgets/header/voting_general_header.dart
+++ catalyst_voices/apps/voices/lib/pages/voting/widgets/header/voting_general_header.dart
@@ -31,7 +31,7 @@ class VotingGeneralHeader extends StatelessWidget {
               child: _CatalystFund(),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 6, right: 32),
+              padding: EdgeInsets.only(top: 7, right: 32),
               child: VotingCategoryPickerSelector(),
             ),
           ],
