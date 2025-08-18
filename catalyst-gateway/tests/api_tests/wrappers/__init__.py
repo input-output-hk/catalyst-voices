dbceb769576552f6193f63948881ab1098dc048c
januszjanus <janusz.janus@iohk.io>
2025-08-18 15:06:15 +0200

    change socat command settings for haproxy

--- catalyst-gateway/tests/api_tests/wrappers/__init__.py
+++ catalyst-gateway/tests/api_tests/wrappers/__init__.py
@@ -1,5 +1,6 @@
 # Proxy wrapper used for integration testing
 import subprocess
+import os
 from loguru import logger
 
 class TestProxy:
@@ -8,13 +9,21 @@ class TestProxy:
         self.backend = backend
         self.server = server
 
+    def _get_host_env(self):
+        """Return the HAProxy host to use depending on environment."""
+        #Check if running inside Docker
+        if os.path.exists("/.dockerenv"):
+            # inside container: assume container name is reachable via Docker network
+            return "haproxy"
+        else:
+            # running on host: localhost
+            return "localhost"
+
     def _exec_haproxy_cmd(self, haproxy_cmd):
-        """Run a command inside the HAProxy container."""
-        cmd = [
-            "docker", "exec", self.container_name,
-            "sh", "-c", f"echo \"{haproxy_cmd}\" | socat /var/run/haproxy/haproxy.sock stdio"
-        ]
-        subprocess.run(cmd, check=True)
+        """Run a HAProxy command"""
+        cmd = f"echo \"{haproxy_cmd}\" | socat tcp:{self._get_host_env()}:9999 stdio"
+        result = subprocess.run(cmd, capture_output=True, text=True, shell=True, check=True)
+        return result.stdout.strip()
 
     def enable(self):
         self._exec_haproxy_cmd(f"enable server {self.backend}/{self.server}")
@@ -27,16 +36,12 @@ class TestProxy:
         logger.info(f"Disabled {self.backend}/{self.server} on {self.container_name}")
 
     def is_running(self):
-        """Check if server is operational (OpState=2, AdminState=0)."""
-        check_cmd = (
-            f"echo \"show servers state\" | socat /var/run/haproxy/haproxy.sock stdio | "
+        """Check if server is operational (state = 2)."""
+        result = self._exec_haproxy_cmd("show servers state")
+        extract_state = (
+            f"echo \"{result}\" | "
             f"awk -v be=\"{self.backend}\" -v srv=\"{self.server}\" "
             f"'$2==be {{print $6}}'"
         )
-        cmd = [
-            "docker", "exec", self.container_name,
-            "sh", "-c", check_cmd
-        ]
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        op_state = result.stdout.strip()
-        return op_state == "2"
+        result_extract_state = subprocess.run(extract_state, capture_output=True, text=True, shell=True, check=True)
+        return result_extract_state.stdout.strip() == "2"
