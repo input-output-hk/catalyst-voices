5b0daf2f4fa54c1000c6af1edcd3a958e65625be
januszjanus <janusz.janus@iohk.io>
2025-08-19 07:32:31 +0200

    cleanup

--- catalyst-gateway/tests/api_tests/wrappers/__init__.py
+++ catalyst-gateway/tests/api_tests/wrappers/__init__.py
@@ -3,41 +3,44 @@ import subprocess
 import os
 from loguru import logger
 
+
+def _get_host_env():
+    """Return the HAProxy host to use depending on environment."""
+    #Check if running inside Docker
+    if os.path.exists("/.dockerenv"):
+        # inside container: assume container name is reachable via Docker network
+        return "haproxy"
+    else:
+        # running on host: localhost
+        return "localhost"
+
+
+def _exec_haproxy_cmd(haproxy_cmd):
+    """Run a HAProxy command"""
+    cmd = f"echo \"{haproxy_cmd}\" | socat tcp:{_get_host_env()}:9999 stdio"
+    result = subprocess.run(cmd, capture_output=True, text=True, shell=True, check=True, timeout=5)
+    return result.stdout.strip()
+
+
 class TestProxy:
-    def __init__(self, container_name, backend, server):
+    def __init__(self, container_name:str, backend:str, server:str):
         self.container_name = container_name
         self.backend = backend
         self.server = server
 
-    def _get_host_env(self):
-        """Return the HAProxy host to use depending on environment."""
-        #Check if running inside Docker
-        if os.path.exists("/.dockerenv"):
-            # inside container: assume container name is reachable via Docker network
-            return "haproxy"
-        else:
-            # running on host: localhost
-            return "localhost"
-
-    def _exec_haproxy_cmd(self, haproxy_cmd):
-        """Run a HAProxy command"""
-        cmd = f"echo \"{haproxy_cmd}\" | socat tcp:{self._get_host_env()}:9999 stdio"
-        result = subprocess.run(cmd, capture_output=True, text=True, shell=True, check=True, timeout=5)
-        return result.stdout.strip()
-
     def enable(self):
-        self._exec_haproxy_cmd(f"enable server {self.backend}/{self.server}")
+        _exec_haproxy_cmd(f"enable server {self.backend}/{self.server}")
         assert self.is_running()
         logger.info(f"Enabled {self.backend}/{self.server} on {self.container_name}")
 
     def disable(self):
-        self._exec_haproxy_cmd(f"disable server {self.backend}/{self.server}")
+        _exec_haproxy_cmd(f"disable server {self.backend}/{self.server}")
         assert not self.is_running()
         logger.info(f"Disabled {self.backend}/{self.server} on {self.container_name}")
 
     def is_running(self):
         """Check if server is operational (state = 2)."""
-        result = self._exec_haproxy_cmd("show servers state")
+        result = _exec_haproxy_cmd("show servers state")
         extract_state = (
             f"echo \"{result}\" | "
             f"awk -v be=\"{self.backend}\" -v srv=\"{self.server}\" "
--- catalyst-gateway/tests/haproxy_entry.sh
+++ catalyst-gateway/tests/haproxy_entry.sh
@@ -2,4 +2,3 @@
 mkdir -p /var/run/haproxy
 chown -R haproxy:haproxy /var/run/haproxy
 exec su-exec haproxy haproxy -f /usr/local/etc/haproxy/haproxy.cfg
-ń
