6f5fa8b926f3b8b79b3e9f8d16f8e4bed3dd9d87
januszjanus <janusz.janus@iohk.io>
2025-08-21 13:40:42 +0200

    add live test with panic

--- catalyst-gateway/tests/api_tests/integration/test_health.py
+++ catalyst-gateway/tests/api_tests/integration/test_health.py
@@ -1,6 +1,11 @@
 import pytest
 from utils import health
+from scripts import panic_requests
 
 @pytest.mark.health_endpoint
-def test_live_endpoint():
+@pytest.mark.asyncio
+async def test_live_endpoint_panic_under_threshold():
     health.is_ready() #assertion
+    health.is_live() #assertion
+    await panic_requests.hit_panic_endpoint(100)
+    health.is_live() #assertion
