9d42dd9f84b0810aa575022e279dc40420837261
Damian Molinski <damian.molinski@iohk.io>
2025-08-21 15:57:54 +0200

    chore: remove attachScreenshot and debug from sentry remote config

--- catalyst_voices/packages/internal/catalyst_voices_services/lib/src/config/config_service.dart
+++ catalyst_voices/packages/internal/catalyst_voices_services/lib/src/config/config_service.dart
@@ -1,6 +1,5 @@
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_repositories/catalyst_voices_repositories.dart';
-import 'package:flutter/foundation.dart';
 
 // ignore: one_member_abstracts
 abstract interface class ConfigService {
@@ -21,13 +20,7 @@ final class ConfigServiceImpl implements ConfigService {
   @override
   Future<AppConfig> getAppConfig({
     required AppEnvironmentType env,
-  }) async {
-    final config = await _repository.getConfig(env: env);
-    final sentry = config.sentry.copyWith(
-      debug: kDebugMode,
-      attachScreenshot: env == AppEnvironmentType.dev,
-    );
-
-    return config.copyWith(sentry: sentry);
+  }) {
+    return _repository.getConfig(env: env);
   }
 }
