fd4ad485805fd009054ea536d99c3f80b4b1ddee
Dominik Toton <dominik.toton@iohk.io>
2025-08-25 13:20:04 +0200

    chore: cleanup

--- catalyst_voices/apps/voices/lib/pages/registration/wallet_link/stage/select_wallet/widget/wallet_list_tile.dart
+++ catalyst_voices/apps/voices/lib/pages/registration/wallet_link/stage/select_wallet/widget/wallet_list_tile.dart
@@ -7,13 +7,11 @@ import 'package:flutter/material.dart';
 class WalletListTile extends StatefulWidget {
   final WalletMetadata wallet;
   final AsyncValueSetter<WalletMetadata> onSelectWallet;
-  final String? semanticsIdentifier;
 
   const WalletListTile({
     super.key,
     required this.wallet,
     required this.onSelectWallet,
-    this.semanticsIdentifier,
   });
 
   @override
