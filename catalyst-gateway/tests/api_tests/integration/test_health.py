372a2ce1ad185a9701be3af2513039fb9a2d174a
januszjanus <janusz.janus@iohk.io>
2025-08-25 18:13:31 +0200

    PR comments 2

--- catalyst-gateway/tests/api_tests/integration/test_health.py
+++ catalyst-gateway/tests/api_tests/integration/test_health.py
@@ -3,7 +3,7 @@ import pytest
 import os
 
 from utils import health
-from scripts import panic_requests
+from utils import panic
 
 LIVE_THRESHOLD = int(os.getenv("SERVICE_LIVE_COUNTER_THRESHOLD"))
 LIVE_TIMEOUT = int(os.getenv("SERVICE_LIVE_TIMEOUT_INTERVAL"))
@@ -13,11 +13,11 @@ LIVE_TIMEOUT = int(os.getenv("SERVICE_LIVE_TIMEOUT_INTERVAL"))
 async def test_live_endpoint_panic_under_threshold_with_reset():
     health.is_ready() #assertion
     health.is_live() #assertion
-    await panic_requests.hit_panic_endpoint(LIVE_THRESHOLD)
+    await panic.hit_panic_endpoint(LIVE_THRESHOLD)
     health.is_live() #assertion
     #wait for panic counter reset
     time.sleep(LIVE_TIMEOUT + 1)
-    await panic_requests.hit_panic_endpoint(LIVE_THRESHOLD)
+    await panic.hit_panic_endpoint(LIVE_THRESHOLD)
     health.is_live() #assertion
 
 @pytest.mark.health_endpoint
@@ -25,8 +25,8 @@ async def test_live_endpoint_panic_under_threshold_with_reset():
 async def test_live_endpoint_panic_threshold_exceeded():
     health.is_ready() #assertion
     health.is_live() #assertion
-    await panic_requests.hit_panic_endpoint(LIVE_THRESHOLD)
+    await panic.hit_panic_endpoint(LIVE_THRESHOLD)
     time.sleep(LIVE_TIMEOUT/2)
-    await panic_requests.hit_panic_endpoint(1)
+    await panic.hit_panic_endpoint(1)
     health.is_ready() #assertion
     health.is_not_live() #assertion
