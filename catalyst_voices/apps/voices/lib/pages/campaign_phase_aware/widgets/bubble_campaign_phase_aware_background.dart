b24c76bf62dbc918e3479408bd240a1e4823e1be
Ryszard Schossler <ryszard.schossler@iohk.io>
2025-08-26 11:49:01 +0200

    feat: update background in loading in app splash for mobile

--- catalyst_voices/apps/voices/lib/pages/campaign_phase_aware/widgets/bubble_campaign_phase_aware_background.dart
+++ catalyst_voices/apps/voices/lib/pages/campaign_phase_aware/widgets/bubble_campaign_phase_aware_background.dart
@@ -1,4 +1,5 @@
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
+import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:flutter/material.dart';
 
 class BubbleCampaignPhaseAwareBackground extends StatelessWidget {
@@ -6,9 +7,42 @@ class BubbleCampaignPhaseAwareBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (CatalystPlatform.isMobile) {
+      return const _MobileBubbleCampaignPhaseAwareBackground();
+    }
     return CatalystImage.asset(
       VoicesAssets.images.bgBubbles.path,
       fit: BoxFit.fill,
+    );
+  }
+}
+
+class _MobileBubbleCampaignPhaseAwareBackground extends StatelessWidget {
+  const _MobileBubbleCampaignPhaseAwareBackground();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          left: 0,
+          top: 0,
+          bottom: 0,
+          child: CatalystImage.asset(
+            VoicesAssets.images.mobileLeftLayer.path,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Positioned(
+          right: 0,
+          top: 0,
+          bottom: 0,
+          child: CatalystImage.asset(
+            VoicesAssets.images.mobileRightLayer.path,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ],
     );
   }
 }
