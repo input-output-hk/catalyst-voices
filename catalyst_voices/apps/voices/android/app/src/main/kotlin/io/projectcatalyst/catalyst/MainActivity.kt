c92f6fe66592b4639a2d744c21492a5e74654cb9
Ryszard Schossler <ryszard.schossler@iohk.io>
2025-08-26 11:21:31 +0200

    feat: adding icons

---
+++ catalyst_voices/apps/voices/android/app/src/main/kotlin/io/projectcatalyst/catalyst/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.projectcatalyst.catalyst
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- catalyst_voices/apps/voices/android/build.gradle.kts
+++ catalyst_voices/apps/voices/android/build.gradle.kts
@@ -5,18 +5,13 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.get()
-
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.buildDir = file("../build")
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }
