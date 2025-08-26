04234177be38d10a838f047a53ece6b0e2848ede
Damian Molinski <damian.molinski@iohk.io>
2025-08-26 10:40:17 +0200

    reporting_mediator

---
+++ catalyst_voices/packages/internal/catalyst_voices_services/lib/src/reporting/reporting_service_mediator.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:catalyst_voices_models/catalyst_voices_models.dart';
+import 'package:catalyst_voices_services/catalyst_voices_services.dart';
+import 'package:catalyst_voices_services/src/reporting/reporting_service.dart';
+
+abstract interface class ReportingServiceMediator {
+  FutureOr<void> dispose();
+
+  FutureOr<void> init();
+}
+
+final class ReportingServiceMediatorImpl implements ReportingServiceMediator {
+  final ReportingService _reportingService;
+  final UserService _userService;
+
+  StreamSubscription<Account?>? _activeAccountSub;
+
+  ReportingServiceMediatorImpl(
+    this._reportingService,
+    this._userService,
+  );
+
+  @override
+  FutureOr<void> dispose() async {
+    await _activeAccountSub?.cancel();
+    _activeAccountSub = null;
+  }
+
+  @override
+  FutureOr<void> init() {
+    assert(_activeAccountSub == null, 'Reporting service already initialized');
+
+    _activeAccountSub = _userService.watchUser
+        .map((event) => event.activeAccount)
+        .distinct()
+        .listen(_onActiveAccountChanged);
+  }
+
+  void _onActiveAccountChanged(Account? account) {
+    unawaited(_reportingService.reportingAs(account));
+  }
+}
