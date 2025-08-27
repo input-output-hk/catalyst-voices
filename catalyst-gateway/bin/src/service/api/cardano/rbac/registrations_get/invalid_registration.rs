90d09bb7e93afd6b74b4ca5212d451402307f959
Stanislav Tkach <stanislav.tkach@gmail.com>
2025-08-27 22:40:07 +0200

    Add time to invalid registrations

--- catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get/invalid_registration.rs
+++ catalyst-gateway/bin/src/service/api/cardano/rbac/registrations_get/invalid_registration.rs
@@ -8,14 +8,17 @@ use crate::{
     db::index::queries::rbac::get_rbac_invalid_registrations::Query,
     service::common::types::{
         cardano::{slot_no::SlotNo, transaction_id::TxnId, txn_index::TxnIndex},
-        generic::{error_msg::ErrorMessage, uuidv4::UUIDv4},
+        generic::{date_time::DateTime, error_msg::ErrorMessage, uuidv4::UUIDv4},
     },
+    settings::Settings,
 };
 
 /// An invalid RBAC registration.
 #[derive(Object, Debug, Clone)]
 #[oai(example)]
 pub struct InvalidRegistration {
+    /// A time of the registration transaction.
+    time: DateTime,
     /// A transaction identifier (hash).
     txn_id: TxnId,
     /// A block slot number.
@@ -33,6 +36,7 @@ pub struct InvalidRegistration {
 impl Example for InvalidRegistration {
     fn example() -> Self {
         Self {
+            time: DateTime::example(),
             txn_id: TxnId::example(),
             slot: SlotNo::example(),
             txn_index: TxnIndex::example(),
@@ -45,7 +49,12 @@ impl Example for InvalidRegistration {
 
 impl From<Query> for InvalidRegistration {
     fn from(q: Query) -> Self {
+        let time = Settings::cardano_network()
+            .slot_to_time(q.slot_no.into())
+            .into();
+
         Self {
+            time,
             txn_id: TransactionId::from(q.txn_id).into(),
             slot: Slot::from(q.slot_no).into(),
             txn_index: q.txn_index.into(),
