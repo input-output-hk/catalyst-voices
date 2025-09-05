cd263ae125dd00c5aa9a55771ae4e2b8b56e1c33
Ryszard Schossler <ryszard.schossler@iohk.io>
2025-09-05 10:58:20 +0200

    chore: remove files

--- catalyst_voices/apps/voices/lib/permissions/ios_permission_strategy.dart
+++
@@ -1,7 +0,0 @@
-import 'package:catalyst_voices/permissions/permission_strategy.dart';
-
-// Concrete Strategy - iOS
-final class IOSPermissionStrategy extends BasePermissionStrategy {
-  const IOSPermissionStrategy();
-  // iOS uses permissions as-is, no mapping needed
-}
