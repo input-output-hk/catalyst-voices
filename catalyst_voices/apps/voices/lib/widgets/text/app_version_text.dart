609126039e6ce86bb4b4fb61bd5e42a374c194bc
Ryszard Schossler <ryszard.schossler@iohk.io>
2025-09-19 09:46:44 +0200

    using devtools to get appinfo

--- catalyst_voices/apps/voices/lib/widgets/text/app_version_text.dart
+++ catalyst_voices/apps/voices/lib/widgets/text/app_version_text.dart
@@ -1,44 +1,30 @@
-import 'dart:async';
-
 import 'package:catalyst_voices/common/ext/build_context_ext.dart';
+import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
+import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
-class AppVersionText extends StatefulWidget {
+class AppVersionText extends StatelessWidget {
   final Color color;
 
   const AppVersionText({super.key, required this.color});
 
   @override
-  State<AppVersionText> createState() => _AppVersionTextState();
-}
-
-class _AppVersionTextState extends State<AppVersionText> {
-  PackageInfo? _packageInfo;
-
-  @override
   Widget build(BuildContext context) {
-    if (_packageInfo case final packageInfo?) {
-      return Text(
-        context.l10n.appVersion(packageInfo.version, packageInfo.buildNumber),
-        style: context.textTheme.bodyMedium?.copyWith(color: widget.color),
-        textAlign: TextAlign.center,
-      );
-    }
-    return const Offstage();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    unawaited(_initPackageInfo());
-  }
-
-  Future<void> _initPackageInfo() async {
-    final info = await PackageInfo.fromPlatform();
-    setState(() {
-      _packageInfo = info;
-    });
+    return BlocSelector<DevToolsBloc, DevToolsState, AppInfo?>(
+      selector: (state) {
+        return state.appInfo;
+      },
+      builder: (context, appInfo) {
+        if (appInfo == null) {
+          return const Offstage();
+        }
+        return Text(
+          context.l10n.appVersion(appInfo.version, appInfo.buildNumber),
+          style: context.textTheme.bodyMedium?.copyWith(color: color),
+          textAlign: TextAlign.center,
+        );
+      },
+    );
   }
 }
