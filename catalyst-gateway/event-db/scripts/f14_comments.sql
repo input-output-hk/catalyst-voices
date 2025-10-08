41f53c1e014c33ccedc0ea5255db3661cec33e5c
Mr-Leshiy <leshiy12345678@gmail.com>
2025-10-08 23:52:42 +0700

    make f14_comments.sql transactional

--- catalyst-gateway/event-db/scripts/f14_comments.sql
+++ catalyst-gateway/event-db/scripts/f14_comments.sql
@@ -1,3 +1,4 @@
+BEGIN;
 -- Updating the Fund 14 Comments documents, by adding a 'parameters' field into the metadata column
 UPDATE signed_docs AS t1
 SET
@@ -11,3 +12,4 @@ WHERE
   AND t1.type = 'b679ded3-0e7c-41ba-89f8-da62a17898ea' -- modifying only comments
   AND (t1.metadata -> 'ref' ->> 'id')::UUID = t2.id
   AND (t1.metadata -> 'ref' ->> 'ver')::UUID = t2.ver;
+COMMIT;
