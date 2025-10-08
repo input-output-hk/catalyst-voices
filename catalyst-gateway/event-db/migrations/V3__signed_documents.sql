0d246ce96983742f60294a4fe4c383d8dcc69f28
Mr-Leshiy <leshiy12345678@gmail.com>
2025-10-08 18:26:43 +0700

    fix linter

--- catalyst-gateway/event-db/migrations/V3__signed_documents.sql
+++ catalyst-gateway/event-db/migrations/V3__signed_documents.sql
@@ -1,13 +1,12 @@
-
 UPDATE signed_docs AS t1
 SET
-  metadata = t1.metadata || jsonb_build_object(
+  metadata = t1.metadata || JSONB_BUILD_OBJECT(
     'parameters', t2.metadata -> 'parameters' -- set referenced proposal document 'parameters' metadata field
   )
 FROM
   signed_docs AS t2
 WHERE
-  t1.metadata IS NOT NULL  
+  t1.metadata IS NOT NULL
   AND t1.type = 'b679ded3-0e7c-41ba-89f8-da62a17898ea' -- modifying only comments
-  AND (t1.metadata -> 'ref' ->> 'id')::uuid = t2.id
-  AND (t1.metadata -> 'ref' ->> 'ver')::uuid = t2.ver;
+  AND (t1.metadata -> 'ref' ->> 'id')::UUID = t2.id
+  AND (t1.metadata -> 'ref' ->> 'ver')::UUID = t2.ver;
