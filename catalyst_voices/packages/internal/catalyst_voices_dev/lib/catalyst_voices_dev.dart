99d4958f6441b3e3197e024df1c4ab4c5cf04ead
Ryszard Schossler <ryszard.schossler@iohk.io>
2025-10-20 11:07:01 +0200

    chore: resturcture dev package

--- catalyst_voices/packages/internal/catalyst_voices_dev/lib/catalyst_voices_dev.dart
+++ catalyst_voices/packages/internal/catalyst_voices_dev/lib/catalyst_voices_dev.dart
@@ -1,6 +1,3 @@
 library catalyst_voices_dev;
 
-// ignore: depend_on_referenced_packages
-export 'package:test/test.dart';
-
 export 'src/catalyst_voices_dev.dart';
--- catalyst_voices/packages/internal/catalyst_voices_dev/lib/src/catalyst_voices_dev.dart
+++ catalyst_voices/packages/internal/catalyst_voices_dev/lib/src/catalyst_voices_dev.dart
@@ -1,8 +1,9 @@
 library catalyst_voices_dev;
 
-export 'crypto/bip32_ed25519_fakes.dart';
-export 'crypto/catalyst_crypto_fakes.dart';
-export 'repositories/repository_mocks.dart';
-export 'services/compression_fakes.dart';
-export 'services/key_derivation_fakes.dart';
-export 'services/keychain_fakes.dart';
+export 'catalyst_compression/catalyst_compressor_fakes.dart';
+export 'catalyst_key_derivation/bip32_ed25519_fakes.dart';
+export 'catalyst_key_derivation/key_derivation_service_fakes.dart';
+export 'catalyst_voices_models/crypto/catalyst_crypto_fakes.dart';
+export 'catalyst_voices_repositories/api/api_fakes.dart';
+export 'catalyst_voices_repositories/repository_mocks.dart';
+export 'catalyst_voices_repositories/user/keychain_fakes.dart';
