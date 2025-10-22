72af612ceb696a3decdaf4bd188c02dd273c753d
Apisit Ritruengroj <apisit_lon@hotmail.com>
2025-10-22 19:58:27 +0700

    chore: remove validation.rs

--- catalyst-gateway/bin/src/db/index/block/rbac509/mod.rs
+++ catalyst-gateway/bin/src/db/index/block/rbac509/mod.rs
@@ -14,7 +14,8 @@ use std::{
 use anyhow::{Context, Result};
 use cardano_chain_follower::{hashes::TransactionId, MultiEraBlock, Slot, TxnIndex};
 use rbac_registration::{
-    cardano::cip509::Cip509, registration::cardano::validation::RbacValidationError,
+    cardano::cip509::Cip509,
+    registration::cardano::validation::{start_new_chain, update_chain, RbacValidationError},
 };
 use scylla::client::session::Session;
 use tokio::sync::watch;
@@ -22,11 +23,14 @@ use tracing::{debug, error};
 
 use crate::{
     db::index::{
-        queries::{FallibleQueryTasks, PreparedQuery, SizedBatch},
+        queries::{
+            rbac::get_catalyst_id_from_stake_address::cache_stake_address, FallibleQueryTasks,
+            PreparedQuery, SizedBatch,
+        },
         session::CassandraSession,
     },
     metrics::caches::rbac::{inc_index_sync, inc_invalid_rbac_reg_count},
-    rbac::{validate_rbac_registration, RbacBlockIndexingContext},
+    rbac::{cache_persistent_rbac_chain, RbacBlockIndexingContext},
     settings::cassandra_db::EnvVars,
 };
 
@@ -122,14 +126,90 @@ impl Rbac509InsertQuery {
         wait_for_previous_blocks(pending_blocks, our_end, block.slot()).await?;
 
         let previous_transaction = cip509.previous_transaction();
+
         // `Box::pin` is used here because of the future size (`clippy::large_futures` lint).
-        match Box::pin(validate_rbac_registration(
-            cip509,
-            block.is_immutable(),
-            context,
-        ))
-        .await
-        {
+        let result = if let Some(previous_txn) = cip509.previous_transaction() {
+            let result = Box::pin(update_chain(
+                cip509,
+                previous_txn,
+                block.is_immutable(),
+                context,
+            ))
+            .await;
+
+            // Everything is fine: update the context.
+            if let Ok((new_chain, reg)) = &result {
+                let catalyst_id = reg
+                    .catalyst_id()
+                    .context("Cip509 error: cannot read Catalyst ID")?;
+                let txn_id = reg.txn_hash();
+                let stake_addresses = reg.stake_addresses();
+                let public_keys = reg.public_keys();
+                let origin = reg.origin();
+
+                context.insert_transaction(txn_id, catalyst_id.clone());
+                context.insert_addresses(stake_addresses.clone(), catalyst_id);
+                context.insert_public_keys(public_keys.clone(), catalyst_id);
+                context.insert_registration(
+                    catalyst_id.clone(),
+                    txn_id,
+                    origin.point().slot_or_default(),
+                    origin.txn_index(),
+                    Some(previous_txn),
+                    // Only a new chain can remove stake addresses from an existing one.
+                    HashSet::new(),
+                );
+
+                if block.is_immutable() {
+                    cache_persistent_rbac_chain(catalyst_id.clone(), new_chain.clone());
+                }
+            }
+
+            result
+        } else {
+            let result = Box::pin(start_new_chain(cip509, block.is_immutable(), context)).await;
+
+            // Everything is fine: update the context.
+            if let Ok((new_chain, reg)) = &result {
+                let catalyst_id = reg
+                    .catalyst_id()
+                    .context("Cip509 error: cannot read Catalyst ID")?;
+                let public_keys = reg.public_keys();
+                let new_addresses = new_chain.stake_addresses();
+
+                context.insert_transaction(new_chain.current_tx_id_hash(), catalyst_id.clone());
+                // This will also update the addresses that are already present in the context if
+                // they were reassigned to the new chain.
+                context.insert_addresses(new_addresses.clone(), catalyst_id);
+                context.insert_public_keys(public_keys.clone(), catalyst_id);
+                context.insert_registration(
+                    catalyst_id.clone(),
+                    new_chain.current_tx_id_hash(),
+                    new_chain.current_point().slot_or_default(),
+                    new_chain.current_txn_index(),
+                    // No previous transaction for the root registration.
+                    None,
+                    // This chain has just been created, so no addresses have been removed from it.
+                    HashSet::new(),
+                );
+
+                // This cache must be updated because these addresses previously belonged to other
+                // chains.
+                for (catalyst_id, addresses) in reg.modified_chains() {
+                    for address in addresses {
+                        cache_stake_address(
+                            block.is_immutable(),
+                            address.clone(),
+                            catalyst_id.clone(),
+                        );
+                    }
+                }
+            }
+
+            result
+        };
+
+        match result {
             // Write updates to the database. There can be multiple updates in one registration
             // because a new chain can take ownership of stake addresses of the existing chains and
             // in that case we want to record changes to all those chains as well as the new one.
--- catalyst-gateway/bin/src/rbac/mod.rs
+++ catalyst-gateway/bin/src/rbac/mod.rs
@@ -4,9 +4,8 @@ mod chain_info;
 mod chains_cache;
 mod get_chain;
 mod indexing_context;
-mod validation;
 
 pub use chain_info::ChainInfo;
+pub use chains_cache::cache_persistent_rbac_chain;
 pub use get_chain::latest_rbac_chain;
 pub use indexing_context::RbacBlockIndexingContext;
-pub use validation::validate_rbac_registration;
