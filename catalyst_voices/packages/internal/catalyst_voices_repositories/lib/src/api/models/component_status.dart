7597f95d6aead6aaec8a961be37683a4f0ab96fb
Bartek Stoliński <bartek.stolinski@iohk.io>
2025-10-22 14:58:12 +0200

    Disable cSpell for ComponentStatus enum clean up project.dic

--- catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/api/models/component_status.dart
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/api/models/component_status.dart
@@ -2,10 +2,12 @@ import 'package:json_annotation/json_annotation.dart';
 
 @JsonEnum(valueField: 'value')
 enum ComponentStatus {
+  /* cSpell:disable */
   operational('OPERATIONAL'),
   partialOutage('PARTIALOUTAGE'),
   minorOutage('MINOROUTAGE'),
   majorOutage('MAJOROUTAGE');
+  /* cSpell:enable */
 
   final String value;
 
