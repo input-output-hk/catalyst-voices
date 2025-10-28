932e3120918e841fd0647db1ecb2a057735805ca
Mr-Leshiy <leshiy12345678@gmail.com>
2025-10-28 16:32:15 +0700

    fix spelling lint

--- catalyst-gateway/bin/src/service/api/documents/templates/mod.rs
+++ catalyst-gateway/bin/src/service/api/documents/templates/mod.rs
@@ -178,7 +178,7 @@ mod tests {
         w: &mut impl std::io::Write,
         mut docs_iter: std::iter::Peekable<I>,
     ) -> anyhow::Result<()> {
-        const INSERT_STMT: &str = "INSERT INTO signed_docs (\n  id,\n  ver,\n  type,\n  authors,\n  metadata,\n  payload,\n  raw\n)\nVALUES";
+        const INSERT_STMT: &str = "INSERT INTO signed_docs (\n  id,\n  ver,\n  type,\n  authors,\n  metadata,\n  payload,\n  raw\n)\nVALUES\n-- cspell: disable";
         writeln!(w, "{INSERT_STMT}").unwrap();
 
         while let Some(doc) = docs_iter.next() {
@@ -201,7 +201,7 @@ mod tests {
 
             write!(
                 w,
-                "(\n  '{id}',\n  '{ver}',\n  '{type}',\n  [{authors}],\n  '{metadata}',\n  '{payload}',\n  DECODE('{raw}')\n)",
+                "(\n  '{id}',\n  '{ver}',\n  '{type}',\n  [{authors}], -- noqa: LT05\n  '{metadata}', -- noqa: LT05\n  '{payload}', -- noqa: LT05\n  DECODE('{raw}', 'hex') -- noqa: LT05\n)",
                 id = doc.doc_id()?,
                 ver = doc.doc_ver()?,
                 r#type = doc.doc_type()?,
@@ -219,7 +219,7 @@ mod tests {
                 writeln!(w, ",")?;
             }
         }
-        write!(w, ";")?;
+        write!(w, ";\n-- cspell: enable")?;
         Ok(())
     }
 
@@ -228,7 +228,6 @@ mod tests {
         let mut rand = OsRng;
         let sk_bytes: SigningKey = SigningKey::generate(&mut rand);
         let sk_hex = format!("0x{}", hex::encode(sk_bytes.to_bytes()).as_str());
-        println!("sk: {sk_hex}");
         unsafe {
             env::set_var("SIGNED_DOC_SK", sk_hex);
         }
