8c4b7c9f76e88880018756625dcda7bfa1f97ac5
Dominik Toton <dominik.toton@iohk.io>
2025-11-13 10:54:29 +0100

    feat: wrap initial and full cardano wallet api in JS

---
+++ catalyst_voices/packages/libs/catalyst_cardano/catalyst_cardano_web/assets/js/cardano_wallet.js
@@ -0,0 +1,269 @@
+/**
+ * The initial API of the CIP-30 wallet interface: https://cips.cardano.org/cip/CIP-30#initial-api.
+ *
+ * All the methods, properties or fields are pass-through only to the wallet delegate
+ * as well the return types are pass through from the wallet delegate to the caller.
+ *
+ * All this class does is wrap each method, pass the parameters to the delegate,
+ * catch errors and map them to a format understandable by the dart layer.
+ */
+export class CardanoWalletInitialApi {
+  /**
+   * Creates a new instance of the class.
+   *
+   * @param {any} delegate - The CIP-30 initial api delegate.
+   */
+  constructor(delegate) {
+    if (!delegate) throw new Error("Missing delegate in CardanoWalletInitialApi!");
+    this.delegate = delegate;
+  }
+
+  /**
+   * [CIP-30] enable({ extensions: Extension[] } = {}): Promise<API>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#cardanowalletnameenable-extensions-extension----promiseapi}
+   */
+  async enable(...args) {
+    try {
+        const fullApi = await this.delegate.enable(...args);
+        return new CardanoWalletFullApi(fullApi);
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] isEnabled(): Promise<bool>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#cardanowalletnameisenabled-promisebool}
+   */
+  async isEnabled() {
+    try {
+      return await this.delegate.isEnabled();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] apiVersion: String
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#cardanowalletnameapiversion-string}
+   */
+  get apiVersion() {
+    try {
+      return this.delegate.apiVersion;
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] supportedExtensions: Extension[]
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#cardanowalletnamesupportedextensions-extension}
+   */
+  get supportedExtensions() {
+    try {
+      return this.delegate.supportedExtensions;
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] name: String
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#cardanowalletnamename-string}
+   */
+  get name() {
+    try {
+      return this.delegate.name;
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] icon: String
+   *
+   * @see {@link hhttps://cips.cardano.org/cip/CIP-30#cardanowalletnameicon-string}
+   */
+  get icon() {
+    try {
+      return this.delegate.icon;
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+}
+
+/**
+ * The full API of the CIP-30 wallet interface: https://cips.cardano.org/cip/CIP-30#full-api.
+ *
+ * All the methods, properties or fields are pass-through only to the wallet delegate
+ * as well the return types are pass through from the wallet delegate to the caller.
+ *
+ * All this class does is wrap each method, pass the parameters to the delegate,
+ * catch errors and map them to a format understandable by the dart layer.
+ */
+export class CardanoWalletFullApi {
+  /**
+   * Creates a new instance of the class.
+   *
+   * @param {any} delegate - The CIP-30 full api delegate.
+   */
+  constructor(delegate) {
+    if (!delegate) throw new Error("Missing delegate in CardanoWalletFullApi!");
+    this.delegate = delegate;
+  }
+
+  /**
+   * [CIP-30] getExtensions(): Promise<Extension[]>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetextensions-promiseextension}
+   */
+  async getExtensions() {
+    try {
+      return await this.delegate.getExtensions();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getNetworkId(): Promise<number>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetnetworkid-promisenumber}
+   */
+  async getNetworkId() {
+    try {
+      return await this.delegate.getNetworkId();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getUtxos(amount: cbor<value> = undefined, paginate: Paginate = undefined): Promise<TransactionUnspentOutput[] | null>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetutxosamount-cborvalue--undefined-paginate-paginate--undefined-promisetransactionunspentoutput--null}
+   */
+  async getUtxos(...args) {
+    try {
+      return await this.delegate.getUtxos(...args);
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getBalance(): Promise<cbor<value>>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetbalance-promisecborvalue}
+   */
+  async getBalance() {
+    try {
+      return await this.delegate.getBalance();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getUsedAddresses(paginate: Paginate = undefined): Promise<Address[]>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetusedaddressespaginate-paginate--undefined-promiseaddress}
+   */
+  async getUsedAddresses(...args) {
+    try {
+      return await this.delegate.getUsedAddresses(...args);
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getUnusedAddresses(): Promise<Address[]>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetunusedaddresses-promiseaddress}
+   */
+  async getUnusedAddresses() {
+    try {
+      return await this.delegate.getUnusedAddresses();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getChangeAddress(): Promise<Address>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetchangeaddress-promiseaddress}
+   */
+  async getChangeAddress() {
+    try {
+      return await this.delegate.getChangeAddress();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] getRewardAddresses(): Promise<Address[]>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apigetrewardaddresses-promiseaddress}
+   */
+  async getRewardAddresses() {
+    try {
+      return await this.delegate.getRewardAddresses();
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] signTx(tx: cbor<transaction>, partialSign: bool = false): Promise<cbor<transaction_witness_set>>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apisigntxtx-cbortransaction-partialsign-bool--false-promisecbortransaction_witness_set}
+   */
+  async signTx(...args) {
+    try {
+      return await this.delegate.signTx(...args);
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] signData(addr: Address, payload: Bytes): Promise<DataSignature>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apisigndataaddr-address-payload-bytes-promisedatasignature}
+   */
+  async signData(...args) {
+    try {
+      return await this.delegate.signData(...args);
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+
+  /**
+   * [CIP-30] submitTx(tx: cbor<transaction>): Promise<hash32>
+   *
+   * @see {@link https://cips.cardano.org/cip/CIP-30#apisubmittxtx-cbortransaction-promisehash32}
+   */
+  async submitTx(...args) {
+    try {
+      return await this.delegate.submitTx(...args);
+    } catch (err) {
+      throw _mapError(err);
+    }
+  }
+}
+
+function _mapError(err) {
+  // TODO(dt-iohk): extract error details from err, map it to a format understandable
+  // by dart side and then deserialize it in dart and convert to dart model.
+  return err;
+}
--- catalyst_voices/packages/libs/catalyst_cardano/catalyst_cardano_web/assets/js/catalyst_cardano.js
+++ catalyst_voices/packages/libs/catalyst_cardano/catalyst_cardano_web/assets/js/catalyst_cardano.js
@@ -1,29 +1,34 @@
+import { CardanoWalletInitialApi } from "./cardano_wallet.js";
+
 // Returns available wallet extensions exposed under
 // cardano.{walletName} according to CIP-30 standard.
 function _getWallets() {
-    const cardano = window.cardano;
-    if (cardano) {
-        const keys = Object.keys(window.cardano);
-        const possibleWallets = keys.map((k) => cardano[k]);
-        return possibleWallets.filter((w) => typeof w === "object" && "enable" in w && "apiVersion" in w);
-    }
+  const cardano = window.cardano;
+  if (cardano) {
+    const keys = Object.keys(window.cardano);
+    const possibleWallets = keys.map((k) => cardano[k]);
+    const validWallets = possibleWallets.filter(
+      (w) => typeof w === "object" && "enable" in w && "apiVersion" in w
+    );
+    return validWallets.map((w) => new CardanoWalletInitialApi(w));
+  }
 
-    return [];
+  return [];
 }
 
 // Returns an instance of `undefined` to dart layer as `undefined`
 // cannot be constructed directly in dart layer. Dart nulls are translated
 // to JS nulls and JS distinguishes between undefined and null.
 function _makeUndefined() {
-    return undefined;
+  return undefined;
 }
 
 // A namespace containing the JS functions that
 // can be executed from dart side
 const catalyst_cardano = {
-    getWallets: _getWallets,
-    makeUndefined: _makeUndefined,
-}
+  getWallets: _getWallets,
+  makeUndefined: _makeUndefined,
+};
 
 // Expose cardano multiplatform as globally accessible
 // so that we can call it via catalyst_cardano.function_name() from
