f5e0a593e3e3e0e7a4cefbf2ea00d7e01dc3b03d
Damian Molinski <damian.molinski@iohk.io>
2025-11-21 12:15:02 +0100

    docs: documents sources interfaces documentation

--- catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/document/source/signed_document_data_local_source.dart
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/document/source/signed_document_data_local_source.dart
@@ -1,8 +1,15 @@
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:catalyst_voices_repositories/catalyst_voices_repositories.dart';
 
+/// Interface for accessing immutable, cryptographically signed documents.
+///
+/// Signed documents are final and cannot be modified, only superseded by
+/// newer versions.
 /// See [DatabaseDocumentsDataSource].
 abstract interface class SignedDocumentDataSource implements DocumentDataLocalSource {
+  /// Retrieves a single signed document matching the filters.
+  ///
+  /// * [authorId]: Filters documents authored by a specific [CatalystId].
   @override
   Future<DocumentData?> getWhere({
     DocumentType? type,
@@ -11,6 +18,9 @@ abstract interface class SignedDocumentDataSource implements DocumentDataLocalSo
     CatalystId? authorId,
   });
 
+  /// Watches for changes to a list of signed documents.
+  ///
+  /// * [authorId]: Filters documents authored by a specific [CatalystId].
   @override
   Stream<List<DocumentData>> watchAll({
     DocumentType? type,
