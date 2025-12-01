467950d59c68979cbdc5d7f4fa1a3b7b7f1f77a0
Dominik Toton <dominik.toton@iohk.io>
2025-12-01 20:34:14 +0100

    chore: cleanup

--- catalyst_voices/apps/voices/lib/widgets/rich_text/voices_rich_text_shortcuts.dart
+++ catalyst_voices/apps/voices/lib/widgets/rich_text/voices_rich_text_shortcuts.dart
@@ -10,30 +10,33 @@ final SpaceShortcutEvent formatDividerToDividerStyle = SpaceShortcutEvent(
   handler: (node, controller) => _handleFormatBlockStyleBySpaceEvent(
     controller: controller,
     character: _dividerStyle3,
+    embeddable: _dividerEmbed,
   ),
 );
 
 bool _handleFormatBlockStyleBySpaceEvent({
   required QuillController controller,
   required String character,
+  required Embeddable embeddable,
 }) {
   assert(
     character.trim().isNotEmpty && character != '\n',
     'Expected character that cannot be empty, a whitespace or a new line. Got $character',
   );
 
-  _updateSelectionForKeyPhrase(character, controller);
+  _updateSelectionForKeyPhrase(character, controller, embeddable);
   return true;
 }
 
 void _updateSelectionForKeyPhrase(
   String phrase,
   QuillController controller,
+  Embeddable embeddable,
 ) {
   controller.replaceText(
     controller.selection.baseOffset - phrase.length,
     phrase.length,
-    _dividerEmbed,
+    embeddable,
     TextSelection.collapsed(offset: controller.selection.baseOffset - phrase.length + 1),
   );
 
