f4fa322386c4013772fa717ee25acd1f61e75b68
Dominik Toton <dominik.toton@iohk.io>
2025-12-19 14:40:29 +0100

    chore: extract common widgets

--- catalyst_voices/apps/voices/lib/pages/proposal/widget/proposal_collaborator_banner.dart
+++ catalyst_voices/apps/voices/lib/pages/proposal/widget/proposal_collaborator_banner.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
 
+import 'package:catalyst_voices/pages/proposal/widget/proposal_collaborator_banner_widgets.dart';
 import 'package:catalyst_voices/pages/proposal/widget/proposal_collaborator_dialog.dart';
-import 'package:catalyst_voices/widgets/common/affix_decorator.dart';
 import 'package:catalyst_voices/widgets/widgets.dart';
 import 'package:catalyst_voices_assets/catalyst_voices_assets.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
-import 'package:catalyst_voices_models/catalyst_voices_models.dart';
-import 'package:catalyst_voices_shared/catalyst_voices_shared.dart';
 import 'package:catalyst_voices_view_models/catalyst_voices_view_models.dart';
 import 'package:flutter/material.dart';
 
@@ -34,42 +32,12 @@ class ProposalCollaboratorBanner extends StatelessWidget {
   }
 }
 
-class _AcceptButton extends StatelessWidget {
-  final String message;
-  final VoidCallback onTap;
-
-  const _AcceptButton({
-    required this.message,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final style = FilledButton.styleFrom(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-    );
-
-    return ResponsiveChildBuilder(
-      xs: (context) => VoicesIconButton.filled(
-        style: style,
-        onTap: onTap,
-        child: VoicesAssets.icons.check.buildIcon(),
-      ),
-      sm: (context) => VoicesFilledButton(
-        style: style,
-        onTap: onTap,
-        child: Text(message),
-      ),
-    );
-  }
-}
-
 class _AcceptedFinalProposalBanner extends StatelessWidget {
   const _AcceptedFinalProposalBanner();
 
   @override
   Widget build(BuildContext context) {
-    return _DismissibleBanner(
+    return ProposalCollaboratorDismissibleBanner(
       icon: VoicesAssets.icons.check.buildIcon(color: Theme.of(context).colors.iconsSuccess),
       message: context.l10n.proposalViewAcceptedFinalProposalBannerMessage,
       button: const _ChangeToDenyFinalProposalButton(),
@@ -82,7 +50,7 @@ class _AcceptedInvitationBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _DismissibleBanner(
+    return ProposalCollaboratorDismissibleBanner(
       icon: VoicesAssets.icons.check.buildIcon(color: Theme.of(context).colors.iconsSuccess),
       message: context.l10n.proposalViewAcceptedInvitationBannerMessage,
       button: const _ChangeToDenyInvitationButton(),
@@ -95,7 +63,7 @@ class _AcceptFinalProposalButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _AcceptButton(
+    return ProposalCollaboratorBannerAcceptButton(
       message: context.l10n.approve,
       onTap: () => _showDialog(context),
     );
@@ -115,7 +83,7 @@ class _AcceptInvitationButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _AcceptButton(
+    return ProposalCollaboratorBannerAcceptButton(
       message: context.l10n.proposalViewPendingInvitationBannerAcceptButton,
       onTap: () => _showDialog(context),
     );
@@ -206,107 +174,12 @@ class _ChangeToDenyInvitationButton extends StatelessWidget {
   }
 }
 
-class _DismissibleBanner extends StatelessWidget {
-  final Widget icon;
-  final String message;
-  final Widget button;
-
-  const _DismissibleBanner({
-    required this.icon,
-    required this.message,
-    required this.button,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colors.elevationsOnSurfaceNeutralLv0,
-        border: Border(bottom: BorderSide(color: Theme.of(context).colors.outlineBorder)),
-      ),
-      child: AffixDecorator(
-        // Reserve space for the 'x' button to have symmetry.
-        prefix: const SizedBox.square(dimension: 28),
-        suffix: VoicesIconButton(
-          child: VoicesAssets.icons.x.buildIcon(
-            size: 28,
-            color: Theme.of(context).colors.iconsPrimary,
-          ),
-          onTap: () => context.read<ProposalCubit>().dismissCollaboratorBanner(),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconTheme(
-              data: IconTheme.of(context).copyWith(size: 28),
-              child: icon,
-            ),
-            const SizedBox(width: 8),
-            Flexible(
-              child: MarkdownText(
-                MarkdownData(message),
-                pStyle: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ),
-            const SizedBox(width: 16),
-            button,
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _PendingBanner extends StatelessWidget {
-  final String message;
-  final Widget acceptButton;
-  final Widget rejectButton;
-
-  const _PendingBanner({
-    required this.message,
-    required this.acceptButton,
-    required this.rejectButton,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colors.elevationsOnSurfaceNeutralLv0,
-        border: Border(bottom: BorderSide(color: Theme.of(context).colors.outlineBorder)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          VoicesAssets.icons.userGroup.buildIcon(
-            size: 28,
-            color: Theme.of(context).colors.iconsPrimary,
-          ),
-          const SizedBox(width: 20),
-          Flexible(
-            child: Text(
-              message,
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-          ),
-          const SizedBox(width: 20),
-          acceptButton,
-          const SizedBox(width: 8),
-          rejectButton,
-        ],
-      ),
-    );
-  }
-}
-
 class _PendingFinalProposalBanner extends StatelessWidget {
   const _PendingFinalProposalBanner();
 
   @override
   Widget build(BuildContext context) {
-    return _PendingBanner(
+    return ProposalCollaboratorPendingBanner(
       message: context.l10n.proposalViewPendingFinalProposalBannerMessage,
       acceptButton: const _AcceptFinalProposalButton(),
       rejectButton: const _RejectFinalProposalButton(),
@@ -319,40 +192,10 @@ class _PendingInvitationBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _PendingBanner(
+    return ProposalCollaboratorPendingBanner(
       message: context.l10n.proposalViewPendingInvitationBannerMessage,
       acceptButton: const _AcceptInvitationButton(),
       rejectButton: const _RejectInvitationButton(),
-    );
-  }
-}
-
-class _RejectButton extends StatelessWidget {
-  final String message;
-  final VoidCallback onTap;
-
-  const _RejectButton({
-    required this.message,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final style = OutlinedButton.styleFrom(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-    );
-
-    return ResponsiveChildBuilder(
-      xs: (context) => VoicesIconButton.outlined(
-        style: style,
-        onTap: onTap,
-        child: VoicesAssets.icons.x.buildIcon(),
-      ),
-      sm: (context) => VoicesOutlinedButton(
-        style: style,
-        onTap: onTap,
-        child: Text(message),
-      ),
     );
   }
 }
@@ -362,7 +205,7 @@ class _RejectedFinalProposalBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _DismissibleBanner(
+    return ProposalCollaboratorDismissibleBanner(
       icon: VoicesAssets.icons.xCircle.buildIcon(color: Theme.of(context).colors.iconsError),
       message: context.l10n.proposalViewRejectedFinalProposalBannerMessage,
       button: const _ChangeToAllowFinalProposalButton(),
@@ -375,7 +218,7 @@ class _RejectedInvitationBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _DismissibleBanner(
+    return ProposalCollaboratorDismissibleBanner(
       icon: VoicesAssets.icons.xCircle.buildIcon(color: Theme.of(context).colors.iconsError),
       message: context.l10n.proposalViewRejectedInvitationBannerMessage,
       button: const _ChangeToAllowInvitationButton(),
@@ -388,7 +231,7 @@ class _RejectFinalProposalButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _RejectButton(
+    return ProposalCollaboratorBannerRejectButton(
       message: context.l10n.reject,
       onTap: () => _showDialog(context),
     );
@@ -408,7 +251,7 @@ class _RejectInvitationButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _RejectButton(
+    return ProposalCollaboratorBannerRejectButton(
       message: context.l10n.proposalViewPendingInvitationBannerRejectButton,
       onTap: () => _showDialog(context),
     );
