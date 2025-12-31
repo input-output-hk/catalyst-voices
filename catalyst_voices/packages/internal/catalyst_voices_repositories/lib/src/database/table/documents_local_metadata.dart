8e2313899b27f7ae60e01619cb553caa0cad24d9
Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
2025-12-31 09:11:57 +0100

    feat(cat-voices): db tables indexes and simpler effectiveProposals CTE (#3924)
    * DB tables indexes and simpler effectiveProposals CTE
    
    * docs: update times

--- catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/table/documents_local_metadata.dart
+++ catalyst_voices/packages/internal/catalyst_voices_repositories/lib/src/database/table/documents_local_metadata.dart
@@ -1,6 +1,7 @@
 import 'package:drift/drift.dart';
 
 @DataClassName('DocumentLocalMetadataEntity')
+@TableIndex(name: 'idx_local_metadata_favorite', columns: {#isFavorite})
 class DocumentsLocalMetadata extends Table {
   TextColumn get id => text()();
 
