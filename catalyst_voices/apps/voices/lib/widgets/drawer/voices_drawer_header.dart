aef4a8fbbdb3df3cde409c2681928ba66ed23cae
Bartek Stoliński <53336850+bstolinski@users.noreply.github.com>
2025-12-31 12:06:22 +0100

    feat(cat-voices): My actions page and sub-routes navigation (#3891)
    * add `My Actions` drawer
    
    * Add proposal approval and co-proposer consent pages
    
    * Use ShellRoute for actions drawer
    
    * refactor
    
    * improve end drawer route handling
    
    * preserve query parameters on route stack rebuild
    
    * refactor
    
    * refactor
    
    * chore: add ActionsShellPage.close()
    
    ---------
    
    Co-authored-by: Damian Moliński <47773413+damian-molinski@users.noreply.github.com>
    Co-authored-by: Damian Molinski <damian.molinski@iohk.io>

---
+++ catalyst_voices/apps/voices/lib/widgets/drawer/voices_drawer_header.dart
@@ -0,0 +1,38 @@
+import 'package:catalyst_voices/common/ext/build_context_ext.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class VoicesDrawerHeader extends StatelessWidget {
+  final VoidCallback? onCloseTap;
+  final bool showBackButton;
+  final String text;
+
+  const VoicesDrawerHeader({
+    super.key,
+    required this.text,
+    this.showBackButton = false,
+    this.onCloseTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (showBackButton)
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: context.pop,
+          ),
+        Expanded(
+          child: Text(
+            text,
+            style: context.textTheme.titleLarge,
+          ),
+        ),
+        CloseButton(
+          onPressed: onCloseTap != null ? onCloseTap!.call : Navigator.maybeOf(context)?.pop,
+        ),
+      ],
+    );
+  }
+}
