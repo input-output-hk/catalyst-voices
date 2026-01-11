6a2f62938cf9a82644e0eea9a929a6a82fa82949
Dominik Toton <dominik.toton@iohk.io>
2026-01-11 18:06:29 +0100

    feat: translations + page skeleton

--- catalyst_voices/apps/voices/lib/pages/actions/proposal_approval/proposal_approval_page.dart
+++ catalyst_voices/apps/voices/lib/pages/actions/proposal_approval/proposal_approval_page.dart
@@ -1,6 +1,8 @@
 import 'package:catalyst_voices/common/ext/build_context_ext.dart';
 import 'package:catalyst_voices/pages/actions/actions_shell_page.dart';
 import 'package:catalyst_voices/widgets/drawer/voices_drawer_header.dart';
+import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
+import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:flutter/material.dart';
 
 class ProposalApprovalPage extends StatelessWidget {
@@ -8,15 +10,14 @@ class ProposalApprovalPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        VoicesDrawerHeader(
-          text: 'Final Proposal Approval',
-          onCloseTap: () => ActionsShellPage.close(context),
-          showBackButton: true,
-        ),
-        const SizedBox(height: 30),
-        const _Content(),
+        _Header(),
+        SizedBox(height: 16),
+        _Subheader(),
+        SizedBox(height: 30),
+        _Content(),
       ],
     );
   }
@@ -34,6 +35,40 @@ class _Content extends StatelessWidget {
           style: context.textTheme.bodyLarge?.copyWith(
             color: context.colors.textOnPrimaryLevel1,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24, top: 22),
+      child: VoicesDrawerHeader(
+        text: context.l10n.proposalApprovalHeader,
+        onCloseTap: () => ActionsShellPage.close(context),
+        showBackButton: true,
+      ),
+    );
+  }
+}
+
+class _Subheader extends StatelessWidget {
+  const _Subheader();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Text(
+        context.l10n.proposalApprovalSubheader,
+        style: theme.textTheme.bodyMedium?.copyWith(
+          color: theme.colors.textOnPrimaryLevel1,
         ),
       ),
     );
