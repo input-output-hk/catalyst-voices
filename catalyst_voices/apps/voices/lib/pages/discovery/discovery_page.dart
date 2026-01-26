8ba840dbb04d016ec0ddcbc40e11915f1a57a2a5
Ryszard Schossler <rysiek9801@gmail.com>
2026-01-26 19:37:09 +0100

    chore: revert discovery

--- catalyst_voices/apps/voices/lib/pages/discovery/discovery_page.dart
+++ catalyst_voices/apps/voices/lib/pages/discovery/discovery_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:catalyst_voices/common/error_handler.dart';
-import 'package:catalyst_voices/common/ext/build_context_ext.dart';
 import 'package:catalyst_voices/pages/campaign_phase_aware/proposal_submission_phase_aware.dart';
 import 'package:catalyst_voices/pages/discovery/sections/campaign_details/campaign_details.dart';
 import 'package:catalyst_voices/pages/discovery/sections/campaign_hero.dart';
@@ -9,7 +8,6 @@ import 'package:catalyst_voices/pages/discovery/sections/how_it_works.dart';
 import 'package:catalyst_voices/pages/discovery/sections/most_recent_proposals/most_recent_proposals.dart';
 import 'package:catalyst_voices/pages/discovery/sections/stay_involved/stay_involved.dart';
 import 'package:catalyst_voices/widgets/common/infrastructure/voices_wide_screen_constrained.dart';
-import 'package:catalyst_voices/widgets/countdown/animated_voices_countdown.dart';
 import 'package:catalyst_voices_blocs/catalyst_voices_blocs.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
@@ -21,15 +19,8 @@ class DiscoveryPage extends StatefulWidget {
   State<DiscoveryPage> createState() => _DiscoveryPageState();
 }
 
-class _Body extends StatefulWidget {
+class _Body extends StatelessWidget {
   const _Body();
-
-  @override
-  State<_Body> createState() => _BodyState();
-}
-
-class _BodyState extends State<_Body> {
-  final _date = DateTime.now().add(const Duration(days: 5));
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +29,6 @@ class _BodyState extends State<_Body> {
         SliverList(
           delegate: SliverChildListDelegate(
             [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-                decoration: BoxDecoration(color: context.colors.primaryContainer),
-                child: AnimatedVoicesCountdown(
-                  dateTime: _date,
-                ),
-              ),
               const CampaignHeroSection(),
               const HowItWorks(),
               const CampaignDetails(),
