0d7f6ec9be43772ced591cb734be25883217c62e
Ryszard Schossler <rysiek9801@gmail.com>
2026-01-27 12:18:06 +0100

    fix: test

--- catalyst_voices/apps/voices/test/widgets/countdown/voices_countdown_test.dart
+++ catalyst_voices/apps/voices/test/widgets/countdown/voices_countdown_test.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:catalyst_voices/widgets/cards/countdown_value_card.dart';
 import 'package:catalyst_voices/widgets/countdown/voices_countdown.dart';
+import 'package:catalyst_voices_brands/catalyst_voices_brands.dart';
 import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -42,39 +43,42 @@ void main() {
                   required ValueListenable<int> hours,
                   required ValueListenable<int> minutes,
                   required ValueListenable<int> seconds,
-                }) => Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ValueListenableBuilder<int>(
-                      valueListenable: days,
-                      builder: (context, value, _) => CountDownValueCard(
-                        value: value,
-                        unit: context.l10n.days(value),
+                }) => AnimatedVoicesCountdownTheme(
+                  data: AnimatedVoicesCountdownThemeData.defaultTheme(context),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ValueListenableBuilder<int>(
+                        valueListenable: days,
+                        builder: (context, value, _) => CountDownValueCard(
+                          value: value,
+                          unit: context.l10n.days(value),
+                        ),
                       ),
-                    ),
-                    ValueListenableBuilder<int>(
-                      valueListenable: hours,
-                      builder: (context, value, _) => CountDownValueCard(
-                        value: value,
-                        unit: context.l10n.hours(value),
+                      ValueListenableBuilder<int>(
+                        valueListenable: hours,
+                        builder: (context, value, _) => CountDownValueCard(
+                          value: value,
+                          unit: context.l10n.hours(value),
+                        ),
                       ),
-                    ),
-                    ValueListenableBuilder<int>(
-                      valueListenable: minutes,
-                      builder: (context, value, _) => CountDownValueCard(
-                        value: value,
-                        unit: context.l10n.minutes(value),
+                      ValueListenableBuilder<int>(
+                        valueListenable: minutes,
+                        builder: (context, value, _) => CountDownValueCard(
+                          value: value,
+                          unit: context.l10n.minutes(value),
+                        ),
                       ),
-                    ),
-                    ValueListenableBuilder<int>(
-                      valueListenable: seconds,
-                      builder: (context, value, _) => CountDownValueCard(
-                        value: value,
-                        unit: context.l10n.seconds(value),
+                      ValueListenableBuilder<int>(
+                        valueListenable: seconds,
+                        builder: (context, value, _) => CountDownValueCard(
+                          value: value,
+                          unit: context.l10n.seconds(value),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
           ),
         ),
