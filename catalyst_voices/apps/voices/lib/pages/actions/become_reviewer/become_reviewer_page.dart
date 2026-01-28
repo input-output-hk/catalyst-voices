48ad283dfda8cdbc335cd5b8a61a692599cf2b94
Ryszard Schossler <rysiek9801@gmail.com>
2026-01-28 13:24:59 +0100

    feat: become reviewer page header and hint

--- catalyst_voices/apps/voices/lib/pages/actions/become_reviewer/become_reviewer_page.dart
+++ catalyst_voices/apps/voices/lib/pages/actions/become_reviewer/become_reviewer_page.dart
@@ -1,3 +1,8 @@
+import 'package:catalyst_voices/pages/actions/actions_shell_page.dart';
+import 'package:catalyst_voices/pages/actions/become_reviewer/widgets/heads_up_become_reviewer_hint_card.dart';
+import 'package:catalyst_voices/pages/actions/widgets/actions_header_text.dart';
+import 'package:catalyst_voices/widgets/drawer/voices_drawer_header.dart';
+import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
 import 'package:flutter/material.dart';
 
 class BecomeReviewerPage extends StatelessWidget {
@@ -5,6 +10,28 @@ class BecomeReviewerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [Text('Coming soon...')]);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24, top: 22, bottom: 16),
+          child: VoicesDrawerHeader(
+            title: Text(context.l10n.becomeReviewer),
+            onCloseTap: () => ActionsShellPage.close(context),
+            showBackButton: true,
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                ActionsHeaderText(text: context.l10n.becomeReviewerActionHeaderText),
+              ],
+            ),
+          ),
+        ),
+        const HeadsUpBecomeReviewerHintCard(),
+      ],
+    );
   }
 }
---
+++ catalyst_voices/apps/voices/lib/pages/actions/become_reviewer/widgets/heads_up_become_reviewer_hint_card.dart
@@ -0,0 +1,52 @@
+import 'package:catalyst_voices/common/ext/build_context_ext.dart';
+import 'package:catalyst_voices_localization/catalyst_voices_localization.dart';
+import 'package:flutter/material.dart';
+
+class HeadsUpBecomeReviewerHintCard extends StatelessWidget {
+  const HeadsUpBecomeReviewerHintCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints.tightFor(height: 118),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      decoration: BoxDecoration(
+        color: context.colors.elevationsOnSurfaceNeutralLv1White,
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.3),
+            blurRadius: 20,
+            offset: const Offset(0, -4),
+          ),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: context.colors.elevationsOnSurfaceNeutralLv1Grey,
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Column(
+          spacing: 2,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              context.l10n.verifyYourReviewerRegistration,
+              style: context.textTheme.titleSmall?.copyWith(
+                color: context.colors.textOnPrimaryLevel1,
+              ),
+            ),
+            Text(
+              context.l10n.verifyYourReviewerRegistrationDescription,
+              style: context.textTheme.bodySmall?.copyWith(
+                color: context.colors.textOnPrimaryLevel1,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
